e56fff3ec666057e64b055da36089219a5f70939
Rita Vought <ritavought@gmail.com>
2020-06-18 17:48:50 -0400

    / ‘Ismail/rita1stassignment.m’

--- Ismail/rita1stassignment.m
+++ Ismail/rita1stassignment.m
@@ -29,7 +29,7 @@ ismail(j).shuttle_acc = shuttle_acc;
 %% 04_12_2019
 j=2;
 %load /Volumes/Cgate/Data2019/4_12_2019/brown2019_04_12_merged_wEric_ID123.mat
-load /Users/winds/OneDrive/Desktop/Merged_Data/downsampled_data.mat
+load /Users/winds/OneDrive/Desktop/Merged_Data/brown2019_04_12_merged_wEric_ID123.mat
 
 ismail(j).date=04122019;
 ismail(j).filename='4_12_2019/brown2019_04_12_merged_wEric_ID123.mat';
@@ -56,7 +56,8 @@ ismail(j).shuttle_acc = shuttle_acc;
 
 %% 9_25_2019_part1
 j=3;
-load /Volumes/Cgate/Data2019/9_25_2019/Brown2018_09_25_merged_part1_ID2513.mat
+%load /Volumes/Cgate/Data2019/9_25_2019/Brown2018_09_25_merged_part1_ID2513.mat
+load /Users/winds/OneDrive/Desktop/Merged_Data/Brown2018_09_25_merged_part1_ID2513.mat
 
 ismail(j).date=09252019;
 ismail(j).filename='9_25_2019/Brown2018_09_25_merged_part1_ID2513.mat';
@@ -83,7 +84,8 @@ ismail(j).shuttle_acc = shuttle_acc;
 
 %% 9_25_2019_part2
 j=4;
-load /Volumes/Cgate/Data2019/9_25_2019/Brown2018_09_25_merged_part2_ID1476.mat
+%load /Volumes/Cgate/Data2019/9_25_2019/Brown2018_09_25_merged_part2_ID1476.mat
+load /Users/winds/OneDrive/Desktop/Merged_Data/Brown2018_09_25_merged_part2_ID1476.mat
 
 ismail(j).date=09252019;
 ismail(j).filename='9_25_2019/Brown2018_09_25_merged_part2_ID1476.mat';
