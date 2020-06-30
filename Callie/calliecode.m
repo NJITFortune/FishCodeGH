05f8d9e72a199d5c73e8d8bd31c032a33d3e2e39
Eric Fortune <eric.fortune@njit.edu>
2020-06-29 21:55:22 -0400

    / ‘Callie/calliecode.m’

--- Callie/calliecode.m
+++ Callie/calliecode.m
@@ -94,14 +94,14 @@ for kk = 500:10:1600
         for j=1:29
             plot(foo(kk).dat(j,1), foo(kk).dat(j,2), '.', 'MarkerSize', 16, 'Color', cmp(j,:)); 
         end
-        plot(0,0, 'k.', 'MarkerSize', 16);
+        plot(0,0, 'k.', 'MarkerSize', 32);
         axis([-200, 200, -200, 200]);
 
         subplot(122); hold on;
         for j=1:29
             plot(foo(kk).centroidrotate(j,1), foo(kk).centroidrotate(j,2), '.', 'MarkerSize', 16, 'Color', cmp(j,:)); 
         end
-        plot(out.xT(kk), out.yT(kk), 'k.', 'MarkerSize', 16);
+        plot(out.xT(kk), out.yT(kk), 'k.', 'MarkerSize', 32);
         axis([out.xT(kk)-200, out.xT(kk)+200, out.yT(kk)-200, out.yT(kk)+200]);
         
    drawnow;
