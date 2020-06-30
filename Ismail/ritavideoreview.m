0f8588d40d32e94403a8e77237cfbacbd113cb11
Eric Fortune <eric.fortune@njit.edu>
2020-06-30 08:46:59 -0400

    + ‘Ismail/ritavideoreview.m’

---
+++ Ismail/ritavideoreview.m
@@ -0,0 +1,32 @@
+function out = ritavideoreview(filena, dlc)
+% filena is something like '/Volumes/Cgate/Data2019/4_14_2019/individual_trials/trial1509_130hz/Fin_1509_130Hz.mov'
+% or 'd:\Users\Eric\Documents\Data\4_14_2019\individual_trials\trial1509_130hz\Fin_1509_130Hz.mov'
+% dlc is from loading the appropriate corrected position file - load that before running this script!
+
+v = VideoReader(filena); % Read the filename
+
+% Cycle through the video, plotting every jumptim seconds
+
+jumptim = 0.5;
+
+for j = jumptim:jumptim:(v.Duration-0.1)
+
+    v.CurrentTime = j;
+    a = readFrame(v);
+    
+    figure(1); clf;
+    imshow(a);
+    hold on;
+
+    idx = v.CurrentTime*v.FrameRate;
+    
+    plot(dlc.fishx(idx), dlc.fishy(idx), 'm.', 'MarkerSize', 48);
+        plot(dlc.fishx(idx), dlc.fishy(idx), 'w.', 'MarkerSize', 8);
+    plot(dlc.shutx(idx), dlc.shuty(idx), 'g.', 'MarkerSize', 48);
+        plot(dlc.shutx(idx), dlc.shuty(idx), 'w.', 'MarkerSize', 8);
+        
+        drawnow;
+
+end
+
+out = 0;
