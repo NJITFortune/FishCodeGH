44b243053a455b6aa7eabc529dcb7e5b9a17b5d7
Eric Fortune <eric.fortune@njit.edu>
2020-07-08 14:06:28 -0400

    / ‘Ismail/iu_hist.m’

--- Ismail/iu_hist.m
+++ Ismail/iu_hist.m
@@ -53,8 +53,8 @@ subplot(313); title('Acceleration'); hold on;
     % Determine edge boundaries
     
     meanFeature = mean(sig);
-    histBound = abs((meanFeature >= 0) * (meanFeature + std_coeff*std(sig)) + (meanFeature < 0) * (meanFeature - std_coeff*std(sig)));
-    cvrg = 100 * sum(sig > -histBound & sig < histBound) / length(sig);
+    histBound = abs(meanFeature + std_coeff*std(sig));
+    
     edgs = linspace(-histBound, histBound, numOfBins+1);
     
     foo.stimulusHist      = histcounts(sig, edgs);
