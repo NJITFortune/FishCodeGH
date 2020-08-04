f135352527509c3bb9805c649b7f108fbab1a442
Eric Fortune <eric.fortune@gmail.com>
2020-08-04 11:57:39 -0400

    / ‘Brasil/Daphne2019/runDfDist.m’

--- Brasil/Daphne2019/runDfDist.m
+++ Brasil/Daphne2019/runDfDist.m
@@ -14,8 +14,8 @@ realCorrs = []; shuffCorrs = []; shiftCorrs = [];
 idxPs = []; idxKs = []; timStarts = []; meandF = [];
 Fs = 4.8828; % This is the sample rate that emerged from the grid analysis.
 
-CorrWindow = 200; % Time in seconds for correlation analysis
-StepSz = 100; % Time in seconds for the step
+CorrWindow = 600; % Time in seconds for correlation analysis
+StepSz = 300; % Time in seconds for the step
 
 %for kk = 3 % Placeholder for our data
 for kk = 1:length(data)
