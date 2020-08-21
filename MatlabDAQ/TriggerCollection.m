b84ef5949b29e843fa43198fe8aa0bd9661fbc66
Eric Fortune <eric.fortune@njit.edu>
2020-08-21 14:17:21 -0400

    / ‘MatlabDAQ/TriggerCollection.m’

--- MatlabDAQ/TriggerCollection.m
+++ MatlabDAQ/TriggerCollection.m
@@ -8,13 +8,13 @@ s.addAnalogInputChannel('Dev2', 1, 'voltage'); % EOD data
 s.addAnalogInputChannel('Dev2', 2, 'voltage'); % EOD data
 s.addAnalogInputChannel('Dev2', 3, 'voltage'); % Temp data
     s.Rate = 20000;
-    s.DurationInSeconds = 120;
+    s.DurationInSeconds = 1;
     s.NotifyWhenDataAvailableExceeds = s.Rate * s.DurationInSeconds;
 
 % Add and configure Trigger    
 addTriggerConnection(s,'External','Dev2/PFI0','StartTrigger');
     s.Connections.TriggerCondition = 'FallingEdge';
-    s.ExternalTriggerTimeout = 20;
+    s.ExternalTriggerTimeout = 5;
     s.TriggersPerRun = 3;
     
 % Add the listener which can handle the data 
