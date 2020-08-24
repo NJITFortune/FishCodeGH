a7c252b164dca0adf2a35af7c395b79920300a9b
Eric Fortune <eric.fortune@njit.edu>
2020-08-24 12:46:25 -0400

    / ‘MatlabDAQ/TriggerCollection.m’

--- MatlabDAQ/TriggerCollection.m
+++ MatlabDAQ/TriggerCollection.m
@@ -31,7 +31,7 @@ a = 0;
 
     while a < 20
         
-    zz = s.startForeground();
+    [tmpData, tmpTime, tmpTriggerTimess] = s.startForeground();
     pause(10);
     a = a+1;
     fprintf('Entry %i. \n', a);
