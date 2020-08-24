38e5fb043d1341822a0adfaa5b878925e810d1f4
Eric Fortune <eric.fortune@njit.edu>
2020-08-24 12:15:13 -0400

    / ‘MatlabDAQ/LightTimerControl.m’

--- MatlabDAQ/LightTimerControl.m
+++ MatlabDAQ/LightTimerControl.m
@@ -22,8 +22,8 @@ s = daq.createSession('ni');
     D_LightOFFirON.TimerFcn = 's.outputSingleScan([0, 5])';
 
 % Tell the timer objects when to do it.     
-     A_LightONirOFF.startat(2020,8,24,12,15,0); % When you want lights on
-     B_LightOFFirON.startat(2020,8,24,12,15,15); % when you want lights off
-     C_LightONirOFF.startat(2020,8,24,12,15,30); % When you want lights on
-     D_LightOFFirON.startat(2020,8,24,12,15,45); % when you want lights off
+     A_LightONirOFF.startat(2020,8,24,12,16,0); % When you want lights on
+     B_LightOFFirON.startat(2020,8,24,12,16,15); % when you want lights off
+     C_LightONirOFF.startat(2020,8,24,12,16,30); % When you want lights on
+     D_LightOFFirON.startat(2020,8,24,12,16,45); % when you want lights off
 
