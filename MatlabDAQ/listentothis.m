7e9880e531da6ac100b7cfea19287de087e24f0f
Eric Fortune <eric.fortune@njit.edu>
2020-09-01 16:30:02 -0400

    / ‘MatlabDAQ/listentothis.m’

--- MatlabDAQ/listentothis.m
+++ MatlabDAQ/listentothis.m
@@ -4,7 +4,7 @@
     data = evt.Data;
     tim = evt.TimeStamps;
 
-    FileName = sprintf('ZGallmanElectro_%s.mat', datestr(now, 'mm-dd-yyyy_HH-MM-SS'));
+    FileName = sprintf('BrownTest-%s.mat', datestr(now, 'mm-dd-yyyy_HH-MM-SS'));
     save(FileName, 'data', 'tim');
 
     % plot(tim, data);
