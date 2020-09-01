4d3388d3fb196256128e2ad543494a3d925f6948
Eric Fortune <eric.fortune@njit.edu>
2020-09-01 14:32:11 -0400

    / ‘General/AbidArduinoTimer/AbidArduinoTimer.ino’

--- General/AbidArduinoTimer/AbidArduinoTimer.ino
+++ General/AbidArduinoTimer/AbidArduinoTimer.ino
@@ -18,34 +18,46 @@ int interval = hours*60*60 + minutes*60 + seconds;
 
 
 void setup() {
+
+  // We are using pins 12 and 13
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
   Serial.begin(9600);
+
+  // Start with 12 on and 13 off (state is 0)
   digitalWrite(12, HIGH);
   digitalWrite(13, LOW);
   state = 0;
+
+  // Get the starting time for the current state, init_time
   rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
   DateTime now = rtc.now();
   init_time = now.unixtime();
 }
 
 void loop() {
+
+  // Get the current time, nowtime
     DateTime now = rtc.now();
     nowtime = now.unixtime();
-    
+
+  // If enough time has passed, switch  from state 0 to state 1    
     if (nowtime - init_time > interval and state == 0) {
       state = 1;
       digitalWrite(12, HIGH);
       digitalWrite(13, LOW);
 //      Serial.println(state);
+      // RESET start time to current time
       init_time = nowtime;
     }
 
+  // If enough time has passed, switch  from state 1 to state 0    
     if (nowtime - init_time > interval and state == 1) {
       state = 0;
       digitalWrite(12, LOW);
       digitalWrite(13, HIGH);
 //      Serial.println(state);
+      // RESET start time to current time
       init_time = nowtime;
     }
     Serial.println(state);
