163fa7c81d8c303f0741866fb178c4975faac049
Eric Fortune <eric.fortune@njit.edu>
2020-09-09 13:15:33 -0400

    / ‘General/AbidArduinoTimer/AbidArduinoTimer.ino’

--- General/AbidArduinoTimer/AbidArduinoTimer.ino
+++ General/AbidArduinoTimer/AbidArduinoTimer.ino
@@ -69,6 +69,6 @@ void loop() {
       // RESET start time to current time
       init_time = nowtime;
     }
-    Serial.println(now.second(), DEC);
+    Serial.println(init_time);
     Serial.println(state);
     }
