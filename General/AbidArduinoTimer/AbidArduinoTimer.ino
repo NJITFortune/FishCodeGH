cc1813ea426c6f4a74da172692eb35ff9776c69c
Eric Fortune <eric.fortune@njit.edu>
2020-09-10 18:13:13 -0400

    / ‘General/AbidArduinoTimer/AbidArduinoTimer.ino’

--- General/AbidArduinoTimer/AbidArduinoTimer.ino
+++ General/AbidArduinoTimer/AbidArduinoTimer.ino
@@ -35,7 +35,7 @@ if (! rtc.begin()) {
 
   // Get the starting time for the current state, init_time
   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  rtc.adjust(DateTime(2020, 1, 1, 1, 1, 0));
+  rtc.adjust(DateTime(2030, 1, 1, 1, 1, 0));
   DateTime now = rtc.now();
   init_time = now.unixtime();
   init_time = init_time + 1000000;
