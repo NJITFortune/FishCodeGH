58129115197e61304d794d72027abf073297ec64
Eric Fortune <eric.fortune@njit.edu>
2020-09-14 15:59:06 -0400

    / ‘MatlabDAQ/TriggerCollection.m’

--- MatlabDAQ/TriggerCollection.m
+++ MatlabDAQ/TriggerCollection.m
@@ -3,24 +3,25 @@
 s = daq.createSession('ni');
 
 % Add and configure Analogue Channels
-s.addAnalogInputChannel('Dev2', 0, 'voltage'); % EOD data
-s.addAnalogInputChannel('Dev2', 1, 'voltage'); % EOD data
-s.addAnalogInputChannel('Dev2', 2, 'voltage'); % EOD data
-s.addAnalogInputChannel('Dev2', 3, 'voltage'); % Temp data
-s.addAnalogInputChannel('Dev2', 4, 'voltage'); % Light data
+    s.addAnalogInputChannel('Dev2', 0, 'voltage'); % EOD data
+    s.addAnalogInputChannel('Dev2', 1, 'voltage'); % EOD data
+    s.addAnalogInputChannel('Dev2', 2, 'voltage'); % EOD data
+    s.addAnalogInputChannel('Dev2', 3, 'voltage'); % Temp data
+    s.addAnalogInputChannel('Dev2', 4, 'voltage'); % Light data
 
     s.Rate = 20000;
     s.DurationInSeconds = 1;
     s.NotifyWhenDataAvailableExceeds = s.Rate * s.DurationInSeconds;
 
 % Add and configure Trigger    
-addTriggerConnection(s,'External','Dev2/PFI0','StartTrigger');
+    addTriggerConnection(s,'External','Dev2/PFI0','StartTrigger');
+    
     s.Connections.TriggerCondition = 'FallingEdge';
     s.ExternalTriggerTimeout = 144000;
     s.TriggersPerRun = 1;
     
 % Add the listener which can handle the data 
-lh = s.addlistener('DataAvailable', @listentothis);
+    lh = s.addlistener('DataAvailable', @listentothis);
 
 
 %% Start collection
