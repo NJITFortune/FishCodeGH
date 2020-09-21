47a404dc16d669733c93e2d39bf75ea371fec141
Eric Fortune <eric.fortune@njit.edu>
2020-09-21 12:48:32 -0400

    / ‘MatlabDAQ/gallmAnalysis.m’

--- MatlabDAQ/gallmAnalysis.m
+++ MatlabDAQ/gallmAnalysis.m
@@ -52,7 +52,7 @@ end
     minute = str2num(iFiles(k).name(numstart+3:numstart+4));
     second = str2num(iFiles(k).name(numstart+6:numstart+7));
 
-    if k > 1 && ((hour*60*60) + (minute*60) + second) < out(k-1).tim
+    if k > 1 && ((hour*60*60) + (minute*60) + second) < out(k-1).tim24
         daycount = daycount + 1;
     end
         % There are 86400 seconds in a day.
