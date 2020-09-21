c2f55fbaeca055e46ad8c5a759c209ad219eb696
Eric Fortune <eric.fortune@gmail.com>
2020-09-21 15:02:48 -0400

    / ‘General/stochasticresonanceNOISE.m’

--- General/stochasticresonanceNOISE.m
+++ General/stochasticresonanceNOISE.m
@@ -3,22 +3,28 @@ function [im, sr] = stochasticresonanceNOISE(im, thresh, rango)
 x = length(im(:,1));
 y = length(im(1,:));
 
-im(im > thresh) = 255;
-im(im < thresh) = 0;
+sr = zeros(x, y, 'uint8');
+
+% im(im > thresh) = 255;
+% im(im < thresh) = 0;
 
 figure(2); clf; imshow(im);
 
-    sr = zeros(x, y, 'uint8') + thresh;
+    lvl =  + thresh;
+    lvl = lvl + randi([-rango, rango], x, y);
 
-%threshbox = thresh * ones(x, y);
-rnd = randi([0 80], x, y, 'uint8');
-
-for j = 1:length(rnd(:,1))
-    for k = 1:length(rnd(1,:))
-        if in(j,k) > rnd(j,k)+thresh  
-            sr(j,k) = 255;
-        end
-    end
-end
+sr(im > lvl) =  255;
+    
+%     
+% %threshbox = thresh * ones(x, y);
+% rnd = randi([0 80], x, y, 'uint8');
+% 
+% for j = 1:length(rnd(:,1))
+%     for k = 1:length(rnd(1,:))
+%         if in(j,k) > rnd(j,k)+thresh  
+%             sr(j,k) = 255;
+%         end
+%     end
+% end
 
 figure(3); clf; imshow(sr);
