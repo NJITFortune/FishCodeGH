4d5c08f0780b5d5efdb5d9b4539f9e5295d28186
Eric Fortune <eric.fortune@njit.edu>
2020-10-05 15:56:20 -0400

    / ‘General/AbidArduinoTimer/AbidArduinoTimer.ino’

--- General/AbidArduinoTimer/AbidArduinoTimer.ino
+++ General/AbidArduinoTimer/AbidArduinoTimer.ino
@@ -11,9 +11,9 @@ long nowtime;
 //int interval = 6*60*60;
 
 // Set the interval you want
-long hours = 8; 
+long hours = 10; 
 long minutes = 0;
-long seconds = 0;
+long seconds =  0;
 long interval = hours*60*60 + minutes*60 + seconds;
 
 
