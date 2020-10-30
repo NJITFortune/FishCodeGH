eb877e7db13a13e0a75bf6bfc34887cfbafb06fd
Eric Fortune <eric.fortune@njit.edu>
2020-10-30 16:46:52 -0400

    + ‘MatlabDAQ/listentothisA.m’

---
+++ MatlabDAQ/listentothisA.m
@@ -0,0 +1,14 @@
+ function listentothis(~,evt)
+% obj is the DataAcquisition object passed in. evt is not used.
+
+    data = evt.Data;
+    tim = evt.TimeStamps;
+
+    FileName = sprintf('Eigen24LDA-%s.mat', datestr(now, 'mm-dd-yyyy_HH-MM-SS'));
+    save(FileName, 'data', 'tim');
+
+    % plot(tim, data);
+
+    
+ end
+ 
