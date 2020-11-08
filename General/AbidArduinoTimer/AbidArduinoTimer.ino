f5bca4f0991561eaf7af18c73d3d9e409a8b238c
Eric Fortune <eric.fortune@njit.edu>
2020-11-08 14:55:01 -0500

    / ‘General/AbidArduinoTimer/AbidArduinoTimer.ino’

--- General/AbidArduinoTimer/AbidArduinoTimer.ino
+++ General/AbidArduinoTimer/AbidArduinoTimer.ino
@@ -17,13 +17,6 @@ long minutes = 0;
 long seconds =  0;
 long interval = hours*60*60 + minutes*60 + seconds;
 
-/*Export serial monitor output to csv/txt file*/
-//From Arduino to Processing to Txt or cvs etc.
-//import
-//import processing.serial.*;
-//declare
-//PrintWriter output;
-Serial udSerial;
 
 
 void setup() {
@@ -32,10 +25,6 @@ void setup() {
   pinMode(12, OUTPUT); // Typically IR
   pinMode(13, OUTPUT); // Typically light
   
-  /*Export serial monitor output to csv/txt file*/
-  udSerial = new Serial(this, Serial.list()[0], 9600);
-  output = createWriter ("test.csv");
-
   Serial.begin(9600);
 
   #ifndef ESP8266
@@ -44,21 +33,6 @@ void setup() {
   if (! rtc.begin()) {
   Serial.println("No RTC");
   while(1);
-
-        void draw() {
-        if (udSerial.available() > 0) {
-          String SenVal = udSerial.readString();
-          if (SenVal != null) {
-            output.println(SenVal);
-          }
-         }
-        }
-    
-      void keyPressed(){
-        output.flush();
-        output.close();
-        exit();
-      }
   }
  
   // Get the starting time for the current state, init_time
@@ -67,20 +41,20 @@ void setup() {
   DateTime now = rtc.now();
   init_time = now.unixtime();
 
-if (initstat == 0)
-{// Start with 12 on and 13 off (state is 0 - NIGHT)
+  if (initstat == 0)
+  {// Start with 12 on and 13 off (state is 0 - NIGHT)
   digitalWrite(12, HIGH); //IR ON 
   digitalWrite(13, LOW);  //LIGHT OFF
-  state = 0;}
+  state = 0;
+  }
 
 
-if (initstat == 1)
-{// Initialize with 12 off and 13 on (state is 1 - DAY)
+  if (initstat == 1)
+  {// Initialize with 12 off and 13 on (state is 1 - DAY)
   digitalWrite(12, LOW);
   digitalWrite(13, HIGH);
-  state = 1;}
-
-
+  state = 1;
+  }
   
 }
 
@@ -89,7 +63,7 @@ void loop() {
   // Get the current time, nowtime
     DateTime now = rtc.now();
     nowtime = now.unixtime();
-// Quick initialization
+
 
 
   // If enough time has passed, switch  from state 0 to state 1    
