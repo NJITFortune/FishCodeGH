b574dab417479864551c2569d521702bb7cae635
Eric Fortune <eric.fortune@njit.edu>
2020-11-19 12:59:07 -0500

    / ‘MatlabDAQ/gallmAnalysis.m’

--- MatlabDAQ/gallmAnalysis.m
+++ MatlabDAQ/gallmAnalysis.m
@@ -154,8 +154,8 @@ figure(2); clf;
 % Smoothed trend line (20 minute duration window with 10 minute overlap)
 for ttk = 1:143   % Every ten minutes
     tt = find([out.tim24] > ((ttk-1)*10*60) & [out.tim24] < (((ttk-1)*10*60) + (20*60)) );
-    medianCh1sumAmp(ttk) = median([out(tt).Ch1obwAmp]); %huh? %is this just a quick way to replace one with the other?
-    medianCh2sumAmp(ttk) = median([out(tt).Ch2obwAmp]);
+    medianCh1sumAmp(ttk) = median([out(tt).Ch1sumAmp]); %huh? %is this just a quick way to replace one with the other?
+    medianCh2sumAmp(ttk) = median([out(tt).Ch2sumAmp]);
     medianCh1zAmp(ttk) = median([out(tt).Ch1zAmp]);
     medianCh2zAmp(ttk) = median([out(tt).Ch2zAmp]);
     mediantims(ttk) = (((ttk-1)*10*60) + (10*60));
