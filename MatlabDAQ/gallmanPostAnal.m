de490bfd7eb0c570241f28e1f05cff99b5ce0d3f
Eric Fortune <eric.fortune@njit.edu>
2020-11-19 12:53:20 -0500

    / ‘MatlabDAQ/gallmanPostAnal.m’

--- MatlabDAQ/gallmanPostAnal.m
+++ MatlabDAQ/gallmanPostAnal.m
@@ -24,7 +24,7 @@ for ttk = 1:floor(totaltim/(timstep*60))   % Every timwin minutes
     foo.medianCh1zAmp(ttk) = median([out(tt).Ch1zAmp]);
     foo.medianCh2zAmp(ttk) = median([out(tt).Ch2zAmp]);
     
-    foo.mediantims(ttk) = (((ttk-1)*timstep*60) + (timstep*60));
+    foo.mediantims(ttk) = ((((ttk-1)*timstep*60) + (timstep*60))) / 2; % Middle of start and end times
 
 end
 
@@ -38,8 +38,10 @@ subplot(211); hold on;
     plot(foo.mediantims/(60*60), foo.medianCh2zAmp, 'm-', 'LineWidth', 6)
 
 subplot(212); hold on;
-    plot(foo.mediantims, foo.medianCh1sumAmp, 'b-')
-    plot(foo.mediantims, foo.medianCh2sumAmp, 'r-')
+    plot([out.timcont]/(60*60), [out.Ch1sumAmp], '.');
+    plot([out.timcont]/(60*60), [out.Ch2sumAmp], '.');
+    plot(foo.mediantims/(60*60), foo.medianCh1sumAmp, 'b-')
+    plot(foo.mediantims/(60*60), foo.medianCh2sumAmp, 'r-')
 
 end
 
