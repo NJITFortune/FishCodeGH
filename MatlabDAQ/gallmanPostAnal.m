60424ed4d93ffafacd230d5f74f49331e951ccf3
Eric Fortune <eric.fortune@njit.edu>
2020-11-19 13:28:47 -0500

    / ‘MatlabDAQ/gallmanPostAnal.m’

--- MatlabDAQ/gallmanPostAnal.m
+++ MatlabDAQ/gallmanPostAnal.m
@@ -13,7 +13,7 @@ endtim = out(end).timcont;
 totaltim = endtim - startim;
 
 
-% Trend line 
+%% Get the Trend lines 
 for ttk = 1:floor(totaltim/(timstep*60))   % Every timwin minutes
     
     tt = find([out.timcont] > startim+((ttk-1)*timstep*60) & [out.timcont] < startim+(((ttk-1)*timstep*60) + (timwin*60)) );
@@ -34,32 +34,46 @@ for ttk = 1:floor(totaltim/(timstep*60))   % Every timwin minutes
 
 end
 
+%% Take data above trend line
+
+for j = 1:length(out)
+   
+    
+    
+    
+    
+end
+
+
+
+
+%% Plot
 figure(1); clf;
 
 ax(1) = subplot(411); hold on;
     plot([out.timcont]/(60*60), [out.Ch1zAmp], 'b.');
     plot([out.timcont]/(60*60), [out.Ch2zAmp], 'r.');
 
-    plot(foo.mediantims/(60*60), foo.medianCh1zAmp, 'c*', 'LineWidth', 3)
-    plot(foo.mediantims/(60*60), foo.medianCh2zAmp, 'm*', 'LineWidth', 3)
+    plot(foo.mediantims/(60*60), foo.medianCh1zAmp, 'c-', 'LineWidth', 3)
+    plot(foo.mediantims/(60*60), foo.medianCh2zAmp, 'm-', 'LineWidth', 3)
 
 ax(2) = subplot(412); hold on;
     plot([out.timcont]/(60*60), [out.Ch1sumAmp], 'b.');
     plot([out.timcont]/(60*60), [out.Ch2sumAmp], 'r.');
-    plot(foo.mediantims/(60*60), foo.medianCh1sumAmp, 'c.', 'LineWidth', 3)
-    plot(foo.mediantims/(60*60), foo.medianCh2sumAmp, 'm.', 'LineWidth', 3)
+    plot(foo.mediantims/(60*60), foo.medianCh1sumAmp, 'c-', 'LineWidth', 3)
+    plot(foo.mediantims/(60*60), foo.medianCh2sumAmp, 'm-', 'LineWidth', 3)
 
 ax(3) = subplot(413); hold on;
     plot([out.timcont]/(60*60), [out.Ch1obwAmp], 'b.');
     plot([out.timcont]/(60*60), [out.Ch2obwAmp], 'r.');
-    plot(foo.mediantims/(60*60), foo.medianCh1obwAmp, 'c*', 'LineWidth', 3)
-    plot(foo.mediantims/(60*60), foo.medianCh2obwAmp, 'm*', 'LineWidth', 3)
+    plot(foo.mediantims/(60*60), foo.medianCh1obwAmp, 'c-', 'LineWidth', 3)
+    plot(foo.mediantims/(60*60), foo.medianCh2obwAmp, 'm-', 'LineWidth', 3)
     
 ax(4) = subplot(414); hold on;
     plot([out.timcont]/(60*60), [out.Ch1sAmp], 'b.');
     plot([out.timcont]/(60*60), [out.Ch2sAmp], 'r.');
-    plot(foo.mediantims/(60*60), foo.medianCh1sAmp, 'c*', 'LineWidth', 3)
-    plot(foo.mediantims/(60*60), foo.medianCh2sAmp, 'm*', 'LineWidth', 3)
+    plot(foo.mediantims/(60*60), foo.medianCh1sAmp, 'c-', 'LineWidth', 3)
+    plot(foo.mediantims/(60*60), foo.medianCh2sAmp, 'm-', 'LineWidth', 3)
     ylim([0 400]);
 
 
