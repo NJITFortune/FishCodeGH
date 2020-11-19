afdc773327312c7f04a572143e77b2661dad3c0b
Eric Fortune <eric.fortune@njit.edu>
2020-11-19 12:38:07 -0500

    / ‘MatlabDAQ/gallmanPostAnal.m’

--- MatlabDAQ/gallmanPostAnal.m
+++ MatlabDAQ/gallmanPostAnal.m
@@ -4,6 +4,15 @@ function [foo] = gallmanPostAnal(out)
 
 
 
+% Smoothed trend line (20 minute duration window with 10 minute overlap)
+for ttk = 1:143   % Every ten minutes
+    tt = find([out.tim24] > ((ttk-1)*10*60) & [out.tim24] < (((ttk-1)*10*60) + (20*60)) );
+    medianCh1sumAmp(ttk) = median([out(tt).Ch1obwAmp]); %huh? %is this just a quick way to replace one with the other?
+    medianCh2sumAmp(ttk) = median([out(tt).Ch2obwAmp]);
+    medianCh1zAmp(ttk) = median([out(tt).Ch1zAmp]);
+    medianCh2zAmp(ttk) = median([out(tt).Ch2zAmp]);
+    mediantims(ttk) = (((ttk-1)*10*60) + (10*60));
+end
 
 
 
