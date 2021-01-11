a46cb43c454c847a92bffcbebdbfb96b3ec566a2
Eric Fortune <eric.fortune@gmail.com>
2021-01-11 14:10:20 -0500

    / ‘Ismail/iu_hist.m’

--- Ismail/iu_hist.m
+++ Ismail/iu_hist.m
@@ -10,30 +10,60 @@ function out = iu_hist(struct)
 
 % Get the signal values at spike times
 
-    for j=1:length(struct.spikes.
-    out.fishPOS = OccHist(struct.fish_pos, struct.spikes.fish_pos);
+    for j=length(struct.spikes.times):-1:1
+        
+        PreTim = 1; % Position integration time before spike
+        tt = find(struct.time > struct.spikes.times(j)-PreTim & struct.time < struct.spikes.times(j));
+        fishSpikePos(j) = mean(struct.fish_pos(tt));
+        errSpikePos(j) = mean(struct.error_pos(tt));
+        
+        PreTim = PreTim / 2;
+        tt = find(struct.time > struct.spikes.times(j)-PreTim & struct.time < struct.spikes.times(j));
+        fishSpikeVel(j) = mean(struct.fish_vel(tt));
+        errSpikeVel(j) = mean(struct.error_vel(tt));
+        
+        PreTim = PreTim / 2;
+        tt = find(struct.time > struct.spikes.times(j)-PreTim & struct.time < struct.spikes.times(j));
+        fishSpikeAcc(j) = mean(struct.fish_acc(tt));
+        errSpikeAcc(j) = mean(struct.error_acc(tt));
+        
+        PreTim = PreTim / 2;
+        tt = find(struct.time > struct.spikes.times(j)-PreTim & struct.time < struct.spikes.times(j));
+        fishSpikeJrk(j) = mean(struct.fish_jerk(tt));    
+        errSpikeJrk(j) = mean(struct.error_jerk(tt));        
+    end
+    
+    % out.fishPOS = OccHist(struct.fish_pos, struct.spikes.fish_pos);
+    out.fishPOS = OccHist(struct.fish_pos, fishSpikePos);
     out.fishPOSrand = OccHist(struct.fish_pos, struct.spikes_rand.fish_pos);
     
-    out.fishVEL = OccHist(struct.fish_vel, struct.spikes.fish_vel);
+%    out.fishVEL = OccHist(struct.fish_vel, struct.spikes.fish_vel);
+    out.fishVEL = OccHist(struct.fish_vel, fishSpikeVel);
     out.fishVELrand = OccHist(struct.fish_vel, struct.spikes_rand.fish_vel);
     
-    out.fishACC = OccHist(struct.fish_acc, struct.spikes.fish_acc);
+%    out.fishACC = OccHist(struct.fish_acc, struct.spikes.fish_acc);
+    out.fishACC = OccHist(struct.fish_acc, fishSpikeAcc);
     out.fishACCrand = OccHist(struct.fish_acc, struct.spikes_rand.fish_acc);
 
-    out.fishJRK = OccHist(struct.fish_jerk, struct.spikes.fish_jerk);
+%    out.fishJRK = OccHist(struct.fish_jerk, struct.spikes.fish_jerk);
+    out.fishJRK = OccHist(struct.fish_jerk, fishSpikeJrk);
     out.fishJRKrand = OccHist(struct.fish_jerk, struct.spikes_rand.fish_jerk);
 
     
-    out.errorPOS = OccHist(struct.error_pos, struct.spikes.error_pos);
+%    out.errorPOS = OccHist(struct.error_pos, struct.spikes.error_pos);
+    out.errorPOS = OccHist(struct.error_pos, errSpikePos);
     out.errorPOSrand = OccHist(struct.error_pos, struct.spikes_rand.error_pos);
     
-    out.errorVEL = OccHist(struct.error_vel, struct.spikes.error_vel);
+%    out.errorVEL = OccHist(struct.error_vel, struct.spikes.error_vel);
+    out.errorVEL = OccHist(struct.error_vel, errSpikeVel);
     out.errorVELrand = OccHist(struct.error_vel, struct.spikes_rand.error_vel);
     
-    out.errorACC = OccHist(struct.error_acc, struct.spikes.error_acc);
+%    out.errorACC = OccHist(struct.error_acc, struct.spikes.error_acc);
+    out.errorACC = OccHist(struct.error_acc, errSpikeAcc);
     out.errorACCrand = OccHist(struct.error_acc, struct.spikes_rand.error_acc);
 
-    out.errorJRK = OccHist(struct.error_jerk, struct.spikes.error_jerk);
+%    out.errorJRK = OccHist(struct.error_jerk, struct.spikes.error_jerk);
+    out.errorJRK = OccHist(struct.error_jerk, errSpikeJrk);
     out.errorJRKrand = OccHist(struct.error_jerk, struct.spikes_rand.error_jerk);
     
 % figure(27); clf;
