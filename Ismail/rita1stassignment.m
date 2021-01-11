fc035d028171be165bb4a952fc5518176f91e01f
Eric Fortune <eric.fortune@gmail.com>
2021-01-11 09:19:01 -0500

    / ‘Ismail/rita1stassignment.m’

--- Ismail/rita1stassignment.m
+++ Ismail/rita1stassignment.m
@@ -56,6 +56,36 @@ ismail(j).shuttle_pos = shuttle_pos;
 ismail(j).shuttle_vel = shuttle_vel;
 ismail(j).shuttle_acc = shuttle_acc; 
 
+%% 04_10_2019
+j=3;
+%load /Volumes/Cgate/Data2019/4_12_2019/brown2019_04_12_merged_wEric_ID123.mat
+%load /Users/winds/OneDrive/Desktop/Merged_Data/brown2019_04_12_merged_wEric_ID123.mat
+load /Users/eric/Documents/Data2019/IsmailData/3_Brown2019-04-10/brown_123.mat
+
+ismail(j).date=04122019;
+ismail(j).filename='4_12_2019/brown2019_04_12_merged_wEric_ID123.mat';
+
+ismail(j).Fs = fs;
+ismail(j).time = time;         
+
+ismail(j).spikes = spikes;
+ismail(j).spikes_rand = spikes_rand;
+
+ismail(j).fish_pos = fish_pos;
+ismail(j).fish_acc = fish_acc;
+ismail(j).fish_vel = fish_vel;
+ismail(j).fish_jerk = fish_jerk;
+
+ismail(j).error_pos = error_pos;    
+ismail(j).error_vel = error_vel;
+ismail(j).error_acc = error_acc;
+ismail(j).error_jerk = error_jerk;
+
+ismail(j).shuttle_pos = shuttle_pos;
+ismail(j).shuttle_vel = shuttle_vel;
+ismail(j).shuttle_acc = shuttle_acc; 
+
+
 %% 9_25_2019_part1
 j=3;
 %load /Volumes/Cgate/Data2019/9_25_2019/Brown2018_09_25_merged_part1_ID2513.mat
