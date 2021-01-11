81b84c2f0fc7fbecf8f8806d718ebee2926783ef
Eric Fortune <eric.fortune@gmail.com>
2021-01-11 13:54:36 -0500

    / ‘Ismail/iu_tmpPlotHists.m’

--- Ismail/iu_tmpPlotHists.m
+++ Ismail/iu_tmpPlotHists.m
@@ -1,17 +1,17 @@
 figure(1); clf;
-    subplot(211); plot(out.errorVEL.Xs, out.errorVEL.OccCorrHist); 
+    subplot(211); plot(out.errorVEL.Xs, out.errorVEL.OccCorrHist, 'o-'); 
     subplot(212); hold on;
-        plot(out.errorVEL.Xs, out.errorVEL.responseHist); 
-        plot(out.errorVEL.Xs, out.errorVEL.stimulusHist);
+        yyaxis left; plot(out.errorVEL.Xs, out.errorVEL.responseHist, 'o-'); 
+        yyaxis right; plot(out.errorVEL.Xs, out.errorVEL.stimulusHist, 'o-');
         
 figure(2); clf;
-    subplot(211); plot(out.errorACC.edges(1:end-1), out.errorACC.OccCorrHist); 
+    subplot(211); plot(out.errorACC.Xs, out.errorACC.OccCorrHist, 'o-'); 
     subplot(212); hold on; 
-        plot(out.errorACC.edges(1:end-1), out.errorACC.responseHist); 
-        plot(out.errorACC.edges(1:end-1), out.errorACC.stimulusHist);
+        yyaxis left; plot(out.errorACC.Xs, out.errorACC.responseHist, 'o-'); 
+        yyaxis right; plot(out.errorACC.Xs, out.errorACC.stimulusHist, 'o-');
 
 figure(3); clf;
-    subplot(211); plot(out.errorJRK.edges(1:end-1), out.errorJRK.OccCorrHist); 
+    subplot(211); plot(out.errorJRK.Xs, out.errorJRK.OccCorrHist, 'o-'); 
     subplot(212); hold on; 
-        plot(out.errorJRK.edges(1:end-1), out.errorJRK.responseHist); 
-        plot(out.errorJRK.edges(1:end-1), out.errorJRK.stimulusHist);
+        yyaxis left; plot(out.errorJRK.Xs, out.errorJRK.responseHist, 'o-'); 
+        yyaxis right; plot(out.errorJRK.Xs, out.errorJRK.stimulusHist, 'o-');
