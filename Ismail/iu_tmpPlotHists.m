2db601d9c3eccd6a56a5b9d22f37b7e9d48b93fe
Eric Fortune <eric.fortune@gmail.com>
2021-01-11 13:39:19 -0500

    / ‘Ismail/iu_tmpPlotHists.m’

--- Ismail/iu_tmpPlotHists.m
+++ Ismail/iu_tmpPlotHists.m
@@ -7,7 +7,7 @@ figure(1); clf;
 figure(2); clf;
     subplot(211); plot(out.errorACC.edges(1:end-1), out.errorACC.OccCorrHist); 
     subplot(212); hold on; 
-        histogram(out.errorACC.responseHist,out.errorACC.edges); 
+        plot(out.errorACC.edges(1:end-1), out.errorACC.responseHist); 
         plot(out.errorACC.edges(1:end-1), out.errorACC.stimulusHist);
 
 figure(3); clf;
