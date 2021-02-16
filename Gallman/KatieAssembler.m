de4a768bfde7e0cac5525a2d15492fda402c5f36
Eric Fortune <eric.fortune@njit.edu>
2021-02-16 15:04:54 -0500

    + ‘Gallman/KatieAssembler.m’

---
+++ Gallman/KatieAssembler.m
@@ -0,0 +1,69 @@
+function out  = KatieAssembler(userfilespec)
+% This function reads the original data collection files
+% It filters the data and saves it into a single structure
+% Performs these analyses: OBW, zAMP
+
+
+%% SET UP 
+% Get the list of files to be analyzed  
+        iFiles = dir(userfilespec);
+
+               
+% Set up filters
+        % High pass filter cutoff frequency
+        highp = 200;
+        % Low pass filter cutoff frequency
+        lowp = 2000;
+        
+        [b,a] = butter(5, highp/(Fs/2), 'high'); % Filter to eliminate 60Hz contamination
+        [f,e] = butter(5, lowp/(Fs/2), 'low'); % Filter to eliminate high frequency contamination
+        
+            
+    % How much of the sample that we will use (each sample is 1 second)  
+    % This is important because the fish
+        SampleWindw = 0.250; % 250 ms window
+        
+    %Fish limit frequencies for OBW calculation (unlikely to be changed)
+        topFreqOBW = 800;
+        botFreqOBW = 300;
+        
+%% CYCLE THROUGH EVERY FILE IN DIRECTORY
+        
+    for k = length(iFiles):-1:1
+        
+       % LOAD THE DATA FILE
+        load(iFiles(k).name, 'data', 'tim');
+        Fs = 1 / (tim(2)-tim(1)); % Extract the sample rate
+        
+       % Filter data  
+          
+            filtsigCh1 = filtfilt(b,a, data(:,1)); % High pass filter
+            filtsigCh1 = filtfilt(f,e,filtsigCh1); % Low pass filter   
+            
+            filtsigCh2 = filtfilt(b,a, data(:,2)); % High pass filter
+            filtsigCh2 = filtfilt(f,e,filtsigCh2); % Low pass filter   
+
+        % PICK YOUR WINDOW - THIS IS A CRITICAL STEP THAT MAY NEED REVISION
+
+            [~, idx] = max(abs(filtsig)); % FIND THE MAXIMUM
+            
+            maxtim(j) = tim(idx);
+            %place the peak amplitude in the middle of the new sample time window
+            startim(j) = max([0, maxtim(j)-(SampleWindw/2)]); 
+                %if the peak is near the end of the sample, need to just take the last windw 
+                if startim(j)+(SampleWindw/2) > tim(end)
+                   startim(j) = tim(end) - SampleWindw;
+                end
+            %create subsample time index by defining sample duration
+            %sampidx = find(tim > startim(j) & tim < startim(j)+windw); 
+            
+            % Take data from the time window defined by startim for each
+            % channel
+            tmpdat = data(tim > startim(j) & tim < startim(j)+SampleWindw, dataChans(j));            
+            
+            
+            [~,~,~,out(k).obwAmp(j)] = obw(data(sampidx,dataChans(1)), Fs, [botFreqOBW topFreqOBW]);
+        
+        out(k).Ch1obwAmp = kg_obw(
+    end
+    
