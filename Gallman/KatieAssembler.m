105333b6500a6741dd1c251179892247089d6f4a
Eric Fortune <eric.fortune@njit.edu>
2021-02-17 16:18:53 -0500

    / ‘Gallman/KatieAssembler.m’

--- Gallman/KatieAssembler.m
+++ Gallman/KatieAssembler.m
@@ -7,29 +7,26 @@ function out  = KatieAssembler(userfilespec)
 %% SET UP 
 % Get the list of files to be analyzed  
         iFiles = dir(userfilespec);
-
                
 % Set up filters
         % High pass filter cutoff frequency
-        highp = 200;
+            highp = 200;
+            [b,a] = butter(5, highp/(Fs/2), 'high'); % Filter to eliminate 60Hz contamination
         % Low pass filter cutoff frequency
-        lowp = 2000;
-        
-        [b,a] = butter(5, highp/(Fs/2), 'high'); % Filter to eliminate 60Hz contamination
-        [f,e] = butter(5, lowp/(Fs/2), 'low'); % Filter to eliminate high frequency contamination
-        
-            
-    % How much of the sample that we will use (each sample is 1 second)  
-    % This is important because the fish
+            lowp = 2000;    
+            [f,e] = butter(5, lowp/(Fs/2), 'low'); % Filter to eliminate high frequency contamination
+                    
+% How much of the sample that we will use (each sample is 1 second)  
+% This is important because the fish moves
         SampleWindw = 0.250; % 250 ms window
         
-    %Fish limit frequencies for OBW calculation (unlikely to be changed)
+% Fish limit frequencies for OBW calculation (unlikely to be changed)
         topFreqOBW = 800;
         botFreqOBW = 300;
         
 %% CYCLE THROUGH EVERY FILE IN DIRECTORY
         
-    for k = length(iFiles):-1:1
+for k = length(iFiles):-1:1
         
        % LOAD THE DATA FILE
         load(iFiles(k).name, 'data', 'tim');
@@ -37,15 +34,17 @@ function out  = KatieAssembler(userfilespec)
         
        % Filter data  
           
-            filtsigCh1 = filtfilt(b,a, data(:,1)); % High pass filter
-            filtsigCh1 = filtfilt(f,e,filtsigCh1); % Low pass filter   
+            data(:,1) = filtfilt(b,a, data(:,1)); % High pass filter
+            data(:,1) = filtfilt(f,e, data(:,1)); % Low pass filter   
             
-            filtsigCh2 = filtfilt(b,a, data(:,2)); % High pass filter
-            filtsigCh2 = filtfilt(f,e,filtsigCh2); % Low pass filter   
+            data(:,2) = filtfilt(b,a, data(:,2)); % High pass filter
+            data(:,2) = filtfilt(f,e, data(:,2)); % Low pass filter   
 
         % PICK YOUR WINDOW - THIS IS A CRITICAL STEP THAT MAY NEED REVISION
 
-            [~, idx] = max(abs(filtsig)); % FIND THE MAXIMUM
+        for j = 1:2 % For the two channels
+        
+            [~, idx] = max(abs(data(:,j))); % FIND THE MAXIMUM
             
             maxtim(j) = tim(idx);
             %place the peak amplitude in the middle of the new sample time window
@@ -64,6 +63,8 @@ function out  = KatieAssembler(userfilespec)
             
             [~,~,~,out(k).obwAmp(j)] = obw(data(sampidx,dataChans(1)), Fs, [botFreqOBW topFreqOBW]);
         
-        out(k).Ch1obwAmp = kg_obw(
-    end
+        out(k).Ch1obwAmp = kg_obw
+        
+        end
+end
     
