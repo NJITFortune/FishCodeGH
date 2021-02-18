7eb514d6b71fcaec12c503b7aba6fa9cd6193c58
Eric Fortune <eric.fortune@njit.edu>
2021-02-18 14:38:47 -0500

    / ‘Gallman/KatieLabeler.m’

--- Gallman/KatieLabeler.m
+++ Gallman/KatieLabeler.m
@@ -18,9 +18,9 @@ function out  = KatieLabeler(in)
 % LIGHT CYCLE ON/OFF STARTS        
     numbercycles = floor(in(1).sampl(end).timcont/(out.ld*60*60)); %number of cycles in data
     timz = 1:1:numbercycles;
-    out.luz(timz) = timerstart + (out.ld*(timz-1)); %without for-loop
+    out.luz(timz) = startim + (out.ld*(timz-1)); %without for-loop
 
-    meaninitialbright = mean([in(1).sampl([in(1).sampl.timcont] < luz(2)).light]);
+    meaninitialbright = mean([in(1).sampl([in(1).sampl.timcont] < out.luz(2)).light]);
     
     if meaninitialbright < 2.5 % Lights were off
         out.luz(1:2:end) = -out.luz(1:2:end);
