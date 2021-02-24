6de36684378f8ffb8bc09e60c6ddca05516e253a
Eric Fortune <eric.fortune@njit.edu>
2021-02-24 17:26:45 -0500

    / ‘Gallman/k_cspliner.m’

--- Gallman/k_cspliner.m
+++ Gallman/k_cspliner.m
@@ -1,9 +1,9 @@
-function [xx, yy] = k_cspliner(nhours, x, y)
+function [xx, yy] = k_cspliner(x, y)
 
 %%GENERATE CUBIC SPLINE FUNCTION FOR DATA
 %f(x) = csaps(x,y,p); p = 0.9
 p = 0.3; %smoothing factor
-ReFs = 160;  %resample once every minute
+ReFs = 60;  %resample once every minute
 
 spliney = csaps(x, y, p);
 
