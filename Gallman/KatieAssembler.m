fa0d1d451ea5f5f3da197c4e4951e06b00a44581
Eric Fortune <eric.fortune@njit.edu>
2021-02-24 16:30:47 -0500

    / ‘Gallman/KatieAssembler.m’

--- Gallman/KatieAssembler.m
+++ Gallman/KatieAssembler.m
@@ -127,8 +127,7 @@ switch answer
         out(2).s = out(2).s(tt);
         
     case 'No'
-        disp([answer ' coming right up.'])
-        dessert = 2;
+        return
     
 end
  
@@ -136,21 +135,7 @@ end
  
  
  
-    fixme = input('Do you want to trim the data?  '); 
-        
-    if fixme == 'yes' % User didn't click a frame    
-        out(eidx).Ch1 = 0;
-        out(eidx).Ch2 = 0;        
-    end    
-    if ~isempty(framNo) % The fish is in the correct position in these frames
-        fprintf('Our frame started at %i.\n', round(minVartim(framNo)));
-%         out(eidx).Ch1 = tmpsigA(tim > minVartim(framNo) & minVartim(framNo)+winwidth);
-%         out(eidx).Ch2 = tmpsigB(tim > minVartim(framNo) & minVartim(framNo)+winwidth);            
-        out(eidx).Ch1 = [eidx, framNo];
-        out(eidx).Ch2 = [eidx, framNo];
-        tmpEODdat(eidx).Ch1 = tmpsigA(tim > minVartim(framNo) & minVartim(framNo)+winwidth);
-        tmpEODdat(eidx).Ch2 = tmpsigB(tim > minVartim(framNo) & minVartim(framNo)+winwidth);
-    end
+    
         
         
         
