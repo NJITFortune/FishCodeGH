9075aca88da94babde7bf82d70dbee1ebb88982f
Eric Fortune <eric.fortune@njit.edu>
2021-02-24 17:19:47 -0500

    / ‘Gallman/k_resamplespline.m’

--- Gallman/k_resamplespline.m
+++ Gallman/k_resamplespline.m
@@ -4,8 +4,8 @@ function k_resamplespline(in)
 
 % Prepare the data
 
-    %tim = [in.e(1).s.timcont]/(60*60);
-    tim = [in.e(1).s.timcont];
+    tim = [in.e(1).s.timcont]/(60*60);
+    %tim = [in.e(1).s.timcont];
 
     obwdata = [in.e(1).s.obwAmp]; 
     obwdata = obwdata(in.idx(1).obwidx);
