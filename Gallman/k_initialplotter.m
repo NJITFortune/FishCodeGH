ee96000db4cdf46f0cb0580de8faf156a3d367ac
Eric Fortune <eric.fortune@njit.edu>
2021-02-24 15:14:28 -0500

    / ‘Gallman/k_initialplotter.m’

--- Gallman/k_initialplotter.m
+++ Gallman/k_initialplotter.m
@@ -120,7 +120,7 @@ figure(4); clf;
         if out.info.luz(j) > 0  % Light side
 
                 figure(3); subplot(211);     % Light to dark plot  OBW     
-                if ~isempty(find([out.e(2).s(tto{2}).timcont] > lighttimes(j) & out.e(2).s(tto{2}).timcont <= lighttimes(j+2), 1))            
+                if ~isempty(find([out.e(2).s(tto{2}).timcont] > lighttimes(j) & [out.e(2).s(tto{2}).timcont] <= lighttimes(j+2), 1))            
                     ott = find(out.e(2).s(tto{2}).timcont > lighttimes(j) & out.e(2).s(tto{2}).timcont <= lighttimes(j+2));
                     plot(out.e(2).s(tto{2}(ott)).timcont - lighttimes(j), out.e(2).s(tto{2}(ott)).obwAmp, 5, 'o-', 'MarkerSize', 2); 
                 end
