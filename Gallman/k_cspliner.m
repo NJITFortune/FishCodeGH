0cc6f197cc3621a5e59da81485b48d7325c96201
Eric Fortune <eric.fortune@njit.edu>
2021-02-26 11:11:58 -0500

    / ‘Gallman/k_cspliner.m’

--- Gallman/k_cspliner.m
+++ Gallman/k_cspliner.m
@@ -1,9 +1,12 @@
-function [xx, yy] = k_cspliner(x, y, p)
+function o = k_cspliner(in, ReFs, p)
 % GENERATE CUBIC SPLINE FUNCTION FOR DATA
 % Usage: [xx, yy] = k_cspliner(x, y, p)
 % f(x) = csaps(x,y,p); p = 0.9
 
 %% Preparations
+
+%ReFs = 60;  %resample once every minute
+
 
 if nargin < 3
     p = 0.9; %smoothing factor
@@ -11,7 +14,7 @@ end
 
 % Prepare the data
 
-    tto{1} = 1:length([out.e(1).s.timcont]); % tto is indices for obwAmp
+    tto{1} = 1:length([in.e(1).s.timcont]); % tto is indices for obwAmp
     tto{2} = tto{1};
 
     ttz{1} = tto{1}; % ttz is indices for zAmp
@@ -22,41 +25,67 @@ end
     
 % If we have removed outliers via KatieRemover, get the indices...    
     if isfield(out, 'idx')
-        tto{1} = out.idx(1).obwidx; tto{2} = out.idx(2).obwidx; % tto is indices for obwAmp
-        ttz{1} = out.idx(1).zidx; ttz{2} = out.idx(2).zidx; % ttz is indices for zAmp
-        ttsf{1} = out.idx(1).sumfftidx; ttsf{2} = out.idx(2).sumfftidx; % ttsf is indices for sumfftAmp
+        tto{1} = in.idx(1).obwidx; tto{2} = in.idx(2).obwidx; % tto is indices for obwAmp
+        ttz{1} = in.idx(1).zidx; ttz{2} = in.idx(2).zidx; % ttz is indices for zAmp
+        ttsf{1} = in.idx(1).sumfftidx; ttsf{2} = in.idx(2).sumfftidx; % ttsf is indices for sumfftAmp
     end
 
     tim = [in.e(1).s.timcont]/(60*60);
 
-    obwdata = [in.e(1).s.obwAmp]; 
-    obwdata = obwdata(in.idx(1).obwidx);
+    %hard coded because fuck thinking
+    obwdata1 = in.e(1).s(tto{1}).obwAmp; 
+    obwtim1 = tim(tto{1});
+            spliney = csaps(obwtim1, obwdata1, p);
+            o.obw(1).x = obwtim1(1):1/ReFs:obwtim1(end);
+            o.obw(1).y = fnval(o.obw(1).x, spliney);
+            
+    obwdata2 = in.e(2).s(tto{2}).obwAmp; 
+    obwtim2 = tim(tto{2});
+            spliney = csaps(obwtim2, obwdata2, p);
+            o.obw(2).x = obwtim2(1):1/ReFs:obwtim2(end);
+            o.obw(2).y = fnval(o.obw(2).x, spliney);
         
-    obwtim = tim(in.idx(1).obwidx);
-    
-%     zdata = [in.e(1).sampl.zAmp]; zdata = zdata(in.idx(1).zidx);
-%         ztim = tim(in.idx(1).zidx);
-%     sfftdata = [in.e(1).sampl.sumfftAmp]; sfftdata = sfftdata(in.idx(1).sumfftidx);
-%         sffttim = tim(in.idx(1).sumfftidx);
+    zdata1 = in.e(1).s(ttz{1}).zAmp; 
+    ztim1 = tim(ttz{1});
+            spliney = csaps(ztim1, zdata1, p);
+            o.z(1).x = ztim1(1):1/ReFs:ztim1(end);
+            o.z(1).y = fnval(o.z(1).x, spliney);
+            
+    zdata2 = in.e(2).s(ttz{2}).zAmp; 
+    ztim2 = tim(ttz{2});
+            spliney = csaps(ztim2, zdata2, p);
+            o.obw(2).x = ztim2(1):1/ReFs:ztim2(end);
+            o.z(2).y = fnval(o.z(2).x, spliney);
+
+            
+    sfftdata1 = in.e(1).s(ttsf{1}).sfftAmp; 
+    sffttim1 = tim(ttsf{1});
+            spliney = csaps(sffttim1, sfftdata1, p);
+            o.sfft(1).x = sffttim1(1):1/ReFs:sffttim1(end);
+            o.sfft(1).y = fnval(o.sfft(1).x, spliney);
+            
+    sfftdata1 = in.e(2).s(ttsf{2}).sfftAmp; 
+    sffttim1 = tim(ttsf{2});
+            spliney = csaps(sffttim1, sfftdata1, p);
+            o.sfft(2).x = sffttim1(2):1/ReFs:sffttim1(end);
+            o.sfft(2).y = fnval(o.sfft(2).x, spliney);
 
 
 
-ReFs = 60;  %resample once every minute
+%% Resample - original for reference
 
-%% Resample
-
-spliney = csaps(x, y, p);
-
-% fx = fnval(x, spliney);
-
-%%RESAMPLE DATA ALONG SPLINE FUNCTION
-%%Generate uniform (regular) time values
-
-xx = x(1):1/ReFs:x(end);
-%xx = linspace(x(1), x(end), ((x(end)-x(1))*ReFs));
-
-%%Resample at new time values along cubic spline
-yy = fnval(xx, spliney);
+% spliney = csaps(x, y, p);
+% 
+% % fx = fnval(x, spliney);
+% 
+% %%RESAMPLE DATA ALONG SPLINE FUNCTION
+% %%Generate uniform (regular) time values
+% 
+% xx = x(1):1/ReFs:x(end);
+% %xx = linspace(x(1), x(end), ((x(end)-x(1))*ReFs));
+% 
+% %%Resample at new time values along cubic spline
+% yy = fnval(xx, spliney);
 
 
 end
