8965143c0fb867f6801f27d6cea2f707321c1302
Eric Fortune <eric.fortune@njit.edu>
2021-02-26 13:57:51 -0500

    / ‘Gallman/KatieTrimmer.m’

--- Gallman/KatieTrimmer.m
+++ Gallman/KatieTrimmer.m
@@ -5,11 +5,22 @@ function out  = KatieTrimmer(in)
  
 %plot the data over time to check for problems 
 figure (1); hold on; title('sumfftAmp');
+
+  ax(1) = subplot(311); hold on; title('obwAmp');   
     yyaxis right; plot([in(2).s.timcont]/(60*60), [in(2).s.obwAmp], '.');
     yyaxis left; plot([in(1).s.timcont]/(60*60), [in(1).s.obwAmp], '.');
    % plot([out.timcont]/(60*60), [out.Ch3sumAmp], '.');
  
- 
+  ax(2) = subplot(312); hold on; title('frequency (black) and temperature (red)');   
+        yyaxis right; plot([in(2).s.timcont]/(60*60), [in(2).s.fftFreq], '.k', 'Markersize', 8);
+        yyaxis right; plot([in(1).s.timcont]/(60*60), [in(1).s.fftFreq], '.k', 'Markersize', 8);
+        yyaxis left; plot([in(2).s.timcont]/(60*60), [in(2).s.temp], '.r', 'Markersize', 8);
+        yyaxis left; plot([in(1).s.timcont]/(60*60), [in(1).s.temp], '.r', 'Markersize', 8);
+    
+  ax(3) = subplot(313); hold on; title('light transitions');
+    plot([in(2).s.timcont]/(60*60), [in(1).s.light], '.', 'Markersize', 8);
+    ylim([-1, 6]);
+    xlabel('Continuous');
    
    [x, ~] = ginput(2);
     tt = find([out(1).s.timcont] > x(1) & [out(1).s.timcont] < x(2));
