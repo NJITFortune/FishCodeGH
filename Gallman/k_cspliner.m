5994e8591315f2222b91edd1f935d9da09070c57
Eric Fortune <eric.fortune@njit.edu>
2021-02-26 10:53:46 -0500

    / ‘Gallman/k_cspliner.m’

--- Gallman/k_cspliner.m
+++ Gallman/k_cspliner.m
@@ -1,10 +1,28 @@
 function [xx, yy] = k_cspliner(x, y, p)
-
-%%GENERATE CUBIC SPLINE FUNCTION FOR DATA
-%f(x) = csaps(x,y,p); p = 0.9
+% GENERATE CUBIC SPLINE FUNCTION FOR DATA
+% 
+% f(x) = csaps(x,y,p); p = 0.9
 if nargin < 3
     p = 0.9; %smoothing factor
 end
+
+% Prepare the data
+
+    tim = [in.e(1).s.timcont]/(60*60);
+    %tim = [in.e(1).s.timcont];
+
+    obwdata = [in.e(1).s.obwAmp]; 
+    obwdata = obwdata(in.idx(1).obwidx);
+        
+    obwtim = tim(in.idx(1).obwidx);
+    
+%     zdata = [in.e(1).sampl.zAmp]; zdata = zdata(in.idx(1).zidx);
+%         ztim = tim(in.idx(1).zidx);
+%     sfftdata = [in.e(1).sampl.sumfftAmp]; sfftdata = sfftdata(in.idx(1).sumfftidx);
+%         sffttim = tim(in.idx(1).sumfftidx);
+
+
+
 ReFs = 60;  %resample once every minute
 
 spliney = csaps(x, y, p);
