f59c2e8c1e23189a4cb2cf9903e9610f50baebc2
Eric Fortune <eric.fortune@njit.edu>
2021-02-25 19:31:13 -0500

    / ‘Ismail/iu_3DhistB.m’

--- Ismail/iu_3DhistB.m
+++ Ismail/iu_3DhistB.m
@@ -25,17 +25,18 @@ if ~isempty(idx) % just make sure that the user isn't an idiot
 
     for j = 1:length(idx) % cycle to concatonate all of the correct entries
 
-            if sizetmp(1)/sizetmp(2) < 1; pos = [pos in(ent).s(idx(j)).pos]; end % Concatonate position
-            if sizetmp(1)/sizetmp(2) > 1; pos = [pos in(ent).s(idx(j)).pos']; end % Concatonate position
+        sizetmp = size(in(ent).s(idx(j)).pos);
+            if sizetmp(1)/sizetmp(2) < 1; pos = [pos in(ent).s(idx(j)).pos(rf(idx(j)).Pidx)]; end % Concatonate position
+            if sizetmp(1)/sizetmp(2) > 1; pos = [pos in(ent).s(idx(j)).pos(rf(idx(j)).Pidx)']; end % Concatonate position
             
         currtim = 1/Fs:1/Fs:length(in(ent).s(idx(j)).pos)/Fs; % A time base for the currently added position
 
         sizetmp = size(in(ent).s(idx(j)).st);
-            if sizetmp(1)/sizetmp(2) < 1; spikes = [spikes (in(ent).s(idx(j)).st + tim(end))]; end % Concatonate position
-            if sizetmp(1)/sizetmp(2) > 1; spikes = [spikes (in(ent).s(idx(j)).st + tim(end))']; end % Concatonate position
+            if sizetmp(1)/sizetmp(2) < 1; spikes = [spikes (in(ent).s(idx(j)).st(idx(j).Nidx) + tim(end))]; end % Concatonate position
+            if sizetmp(1)/sizetmp(2) > 1; spikes = [spikes (in(ent).s(idx(j)).st(idx(j).Nidx) + tim(end))']; end % Concatonate position
          % Concatonate spike times, adding the time from the end of previous
         
-        tim = [tim (currtim(rf(idx(j)).Pidx + tim(end))]; % Update the time base 
+        tim = [tim (currtim(rf(idx(j)).Pidx) + tim(end))]; % Update the time base 
         
     end
         
