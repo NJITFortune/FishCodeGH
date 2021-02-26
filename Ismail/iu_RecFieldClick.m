250ed1a184371380495be9c376165e2699cf28a6
Eric Fortune <eric.fortune@njit.edu>
2021-02-25 19:18:01 -0500

    / ‘Ismail/iu_RecFieldClick.m’

--- Ismail/iu_RecFieldClick.m
+++ Ismail/iu_RecFieldClick.m
@@ -111,6 +111,7 @@ if a == 1
         
         % SPIKE INDICES
         faketim = 1:1:length(in(ent).s(idx(j)).pos);
+        figure(27); hold on; plot(faketim, in(ent).s(idx(j)).pos);
         spikerpos = interp1(faketim, in(ent).s(idx(j)).pos, in(ent).s(idx(j)).st);
         outy.Nidx{idx(j)} = find(spikerpos > xs(1) & spikerpos < xs(2));
         
