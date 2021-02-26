27557748c04d12e662a58245f7df6661ac396ea7
Eric Fortune <eric.fortune@njit.edu>
2021-02-26 10:56:52 -0500

    / ‘Gallman/k_cspliner.m’

--- Gallman/k_cspliner.m
+++ Gallman/k_cspliner.m
@@ -11,8 +11,23 @@ end
 
 % Prepare the data
 
+    tto{1} = 1:length([out.e(1).s.timcont]); % tto is indices for obwAmp
+    tto{2} = tto{1};
+
+    ttz{1} = tto{1}; % ttz is indices for zAmp
+    ttz{2} = tto{1};
+
+    ttsf{1} = tto{1}; % ttsf is indices for sumfftAmp
+    ttsf{2} = tto{1};
+    
+% If we have removed outliers via KatieRemover, get the indices...    
+    if isfield(out, 'idx')
+        tto{1} = out.idx(1).obwidx; tto{2} = out.idx(2).obwidx; % tto is indices for obwAmp
+        ttz{1} = out.idx(1).zidx; ttz{2} = out.idx(2).zidx; % ttz is indices for zAmp
+        ttsf{1} = out.idx(1).sumfftidx; ttsf{2} = out.idx(2).sumfftidx; % ttsf is indices for sumfftAmp
+    end
+
     tim = [in.e(1).s.timcont]/(60*60);
-    %tim = [in.e(1).s.timcont];
 
     obwdata = [in.e(1).s.obwAmp]; 
     obwdata = obwdata(in.idx(1).obwidx);
