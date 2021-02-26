3737e3c2fb3f64ce90062241a09a96680d151265
Eric Fortune <eric.fortune@njit.edu>
2021-02-25 19:15:17 -0500

    / ‘Ismail/iu_RecFieldClick.m’

--- Ismail/iu_RecFieldClick.m
+++ Ismail/iu_RecFieldClick.m
@@ -106,8 +106,13 @@ if a == 1
     outy.Yrang = sort(xs);
     
     for j = 1:length(idx) % cycle through each 
+        % STIMULUS INDICES 
         outy.Pidx{idx(j)} = find(in(ent).s(idx(j)).pos > xs(1) & in(ent).s(idx(j)).pos < xs(2));
-        outy.Nidx{idx(j)} = find(spikePOS > xs(1) & spikePOS < xs(2));
+        
+        % SPIKE INDICES
+        faketim = 1:length(in(ent).s(idx(j)).pos);
+        spikerpos = interp1(faketim, in(ent).s(idx(j)).pos, spin(ent).s(idx(j)).st);
+        outy.Nidx{idx(j)} = find(spikerpos > xs(1) & spikerpos < xs(2));
     end
     
 end
