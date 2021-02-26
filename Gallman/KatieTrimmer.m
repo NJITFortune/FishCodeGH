582d5c9e92b547726de71b3d702915612bde034c
Eric Fortune <eric.fortune@njit.edu>
2021-02-26 16:08:10 -0500

    / ‘Gallman/KatieTrimmer.m’

--- Gallman/KatieTrimmer.m
+++ Gallman/KatieTrimmer.m
@@ -30,9 +30,9 @@ figure (1); hold on; title('sumfftAmp');
     %Make sure these are the ones we want before we over write the data  
     A = input('Wtf do you want to do?');
     if A == 1 
-        tt = find([in(1).s.timcont] > x(1) & [in(1).s.timcont] < x(2));
+        tt = find([in(1).s.timcont]/(60*60) > x(1) & [in(1).s.timcont]/(60*60) < x(2));
             out(1).s = in(1).s(tt);
-        tt = find([in(2).s.timcont] > x(1) & [in(2).s.timcont] < x(2));
+        tt = find([in(2).s.timcont]/(60*60) > x(1) & [in(2).s.timcont]/(60*60) < x(2));
             out(2).s = in(2).s(tt);
     else 
         return
