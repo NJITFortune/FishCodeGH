5e443b1ff002142c6fd628a92446d3d790572adc
Eric Fortune <eric.fortune@njit.edu>
2021-03-03 14:58:56 -0500

    / ‘Gallman/k_cspliner.m’

--- Gallman/k_cspliner.m
+++ Gallman/k_cspliner.m
@@ -126,7 +126,7 @@ figure(4); clf; hold on;
     L = length(o.obw(1).y); 
     NFFT = 2^nextpow2(L)/2;
     FreqRange = 0.005:0.0001:0.2; % From 6 days to 5 hours 
-    [pxx,f] = pwelch(o.obw(1).y,NFFT,FreqRange,floor(ReFs*0.99),ReFs);    
+    [pxx,f] = pwelch(o.obw(1).y,NFFT,floor(ReFs*0.99),FreqRange,ReFs);    
 
     plot(f,pxx);
     
