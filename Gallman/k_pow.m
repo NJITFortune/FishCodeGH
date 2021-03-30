bdc5c5f869c251beddf72c89bf6afe000337d518
Eric Fortune <eric.fortune@njit.edu>
2021-03-30 13:34:39 -0400

    / ‘Gallman/k_pow.m’

--- Gallman/k_pow.m
+++ Gallman/k_pow.m
@@ -126,7 +126,7 @@ aqua = [0.4784 0.9020 0.7882];
 L = 2*200;
 W = 2*700; %changed from 2*420
 
-figure(1); hold on; 
+figure(1); clf; hold on; 
 %set(figure(1),'Units','normalized','Position',[0 0 .5 .5]); 
 set(gcf, 'Position', [0 0 W L]);
 
