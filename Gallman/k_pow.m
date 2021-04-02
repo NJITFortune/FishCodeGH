de1fb836f4303b2f9d73ce0ec5961d257c3cf16c
Eric Fortune <eric.fortune@njit.edu>
2021-04-02 15:43:29 -0400

    / ‘Gallman/k_pow.m’

--- Gallman/k_pow.m
+++ Gallman/k_pow.m
@@ -139,7 +139,6 @@ W = 2*700; %changed from 2*420
 
 figure(1); hold on; 
 %set(figure(1),'Units','normalized','Position',[0 0 .5 .5]); 
-set(gcf, 'Position', [0 0 W L]);
 
 
     %get ylim variables
@@ -162,20 +161,22 @@ set(gcf, 'Position', [0 0 W L]);
 
     %plot data on log scale
     %fftmachine
-    figure(1); semilogy(f.fftfreq(f.fftfreq < 0.2), f.fftdata(f.fftfreq < 0.2), '-o', 'Color', aqua, 'LineWidth', 2); 
+    figure(1); plot(f.fftfreq(f.fftfreq < 0.2), f.fftdata(f.fftfreq < 0.2), '-o', 'Color', aqua, 'LineWidth', 2); 
     %pwelch
-    figure(1); semilogy(pf,pxx, '-o','Color', rosey, 'LineWidth', 2, 'MarkerSize', 3); ylim([minY, maxY + 0.01]);
+    figure(1); plot(pf,pxx, '-o','Color', rosey, 'LineWidth', 2, 'MarkerSize', 3); ylim([minY, maxY + 0.01]);
     
-%     figure(1);    
-%         for j=1:length(hrs)
-% 
-%             semilogy([1/hrs(j), 1/hrs(j)], [0.000001, maxY], 'k-', 'LineWidth', 1);
-%             label = num2str(hrs(j)/2);
-%             str = label + ":" + label + " LD";
-%             text(1/hrs(j), maxY*0.9, str, 'FontSize', 12);
-% 
-%         end
+    figure(1);    
+        for j=1:length(hrs)
+
+            semilogy([1/hrs(j), 1/hrs(j)], [0.000001, maxY], 'k-', 'LineWidth', 1);
+            label = num2str(hrs(j)/2);
+            str = label + ":" + label + " LD";
+            text(1/hrs(j), maxY*0.9, str, 'FontSize', 12);
+
+        end
     
+        set(gcf, 'Position', [0 0 W L], 'yscale', 'log');
+
     
 %% save peak values
 %[o.Xfftpower, o.Yfftpower] = ginput(1);
