e0431620f5d1d6a86ddd85102df309c7dc01592a
Eric Fortune <eric.fortune@njit.edu>
2021-04-10 14:00:43 -0400

    / ‘Gallman/k_groupplotter.m’

--- Gallman/k_groupplotter.m
+++ Gallman/k_groupplotter.m
@@ -27,13 +27,13 @@ y = [out.e(1).s(ttsf{1}).sumfftAmp]';
 X = [ones(length(x),1) x];
 b = X\y;
 yCalc2 = X*b;
-b1 = x/y;
-yCalc1 = b1*x;
+%b1 = x/y;
+%yCalc1 = b1*x;
 scatter(x,y)
 hold on
 
 plot(x,yCalc2,'--')
-plot(x,yCalc1)
+%plot(x,yCalc1)
 
 
 %% amplitude vs temperature
