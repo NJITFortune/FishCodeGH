34663c5931390b9b0eb1ffc98266ee963b2da976
Eric Fortune <eric.fortune@njit.edu>
2021-04-10 13:33:24 -0400

    / ‘Gallman/k_groupplotter.m’

--- Gallman/k_groupplotter.m
+++ Gallman/k_groupplotter.m
@@ -36,7 +36,7 @@ ax(2) = subplot(512); hold on; title('zAmp');
 
 ax(3) = subplot(513); hold on; title('obwAmp');
     yyaxis right; plot([out.e(2).s(tto{2}).temp], [out.e(2).s(tto{2}).obwAmp], '.');
-    yyaxis left; plot([out.e(1).s(tto{1}).temp]/(60*60*24), [out.e(1).s(tto{1}).obwAmp], '.');
+    yyaxis left; plot([out.e(1).s(tto{1}).temp], [out.e(1).s(tto{1}).obwAmp], '.');
 
 ax(4) = subplot(514); hold on; title('frequency (black) and temperature (red)');   
         yyaxis right; plot([out.e(2).s.temp], [out.e(2).s.fftFreq], '.k', 'Markersize', 8);
