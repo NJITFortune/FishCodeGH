05646d5e85a8170eec1e87f04e0bad7fedd00798
Eric Fortune <eric.fortune@njit.edu>
2021-04-10 14:21:25 -0400

    / ‘Gallman/KatieRegress.m’

--- Gallman/KatieRegress.m
+++ Gallman/KatieRegress.m
@@ -1,4 +1,4 @@
-function [Rsq yCalc] = KatieRegress(x, y)
+function [Rsq, yCalc] = KatieRegress(x, y)
 % plot the data for fun
 % Usage: k_initialplotter(kg(#));
 
@@ -10,7 +10,7 @@ X = [ones(length(x),1) x];
 b = X\y;
 yCalc = X*b;
 
-
+Rsq = 1 - sum((y - yCalc).^2)/sum((y - mean(y)).^2);
 %b1 = x/y;
 %yCalc1 = b1*x;
 end
