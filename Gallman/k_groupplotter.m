9e0d0ed5919bc7003dfc0d0393b8ae09293a8340
Eric Fortune <eric.fortune@njit.edu>
2021-04-10 13:52:15 -0400

    / ‘Gallman/k_groupplotter.m’

--- Gallman/k_groupplotter.m
+++ Gallman/k_groupplotter.m
@@ -24,8 +24,10 @@ close all;
 %% Linear Regression
 x = [out.e(1).s(ttsf{1}).temp];
 y = [out.e(1).s(ttsf{1}).sumfftAmp];
-b1 = x/y;
-yCalc1 = b1*x;
+X = [ones(length(x),1) x];
+b = X\y;
+% b1 = x/y;
+% yCalc1 = b1*x;
 scatter(x,y)
 hold on
 plot(x,yCalc1)
