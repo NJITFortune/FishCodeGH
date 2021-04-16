bafcde5f872c56295454803df848a7f7f0c8cde0
Eric Fortune <eric.fortune@njit.edu>
2021-04-16 15:10:45 -0400

    / ‘Gallman/k_linearplotter.m’

--- Gallman/k_linearplotter.m
+++ Gallman/k_linearplotter.m
@@ -155,7 +155,7 @@ linkaxes(ax, 'x');
     tempfft1 = [out.e(1).s(ttsf{1}).temp];
     lightfft1 = [out.e(1).s(ttsf{1}).light];
     
-    gscatter(tempfft1, sumfft1, lightfft1, 'bgr', 'x.o')
+    gscatter(tempfft1, sumfft1, lightfft1, 'bgr', 'x.o');
 %     line(w,feval(fit,w,'70'),'Color','b','LineWidth',2)
 %     line(w,feval(fit,w,'76'),'Color','g','LineWidth',2)
 %     line(w,feval(fit,w,'82'),'Color','r','LineWidth',2)
