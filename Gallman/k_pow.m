350b84acc8beda9bd75d853f6b8ac5c5b9c3b65c
Eric Fortune <eric.fortune@njit.edu>
2021-04-21 15:44:08 -0400

    / ‘Gallman/k_pow.m’

--- Gallman/k_pow.m
+++ Gallman/k_pow.m
@@ -34,7 +34,7 @@ end
     tim = [in.e(1).s.timcont]/(60*60);
    
     if isfield(in, 'info.poweridx')
-        tt = find(tim > in.info.poweridx(1) & tim < in.info.poweridx(2));
+        tt = find(tim > in.info.poweridx(1) & tim <= in.info.poweridx(2));
     else
         tt = 1:length(tim);
     end
