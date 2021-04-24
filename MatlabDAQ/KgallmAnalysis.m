4b05f827aa075d599c4e88d742cd5a13962cc350
Eric Fortune <eric.fortune@njit.edu>
2021-04-24 12:41:29 -0400

    / ‘MatlabDAQ/KgallmAnalysis.m’

--- MatlabDAQ/KgallmAnalysis.m
+++ MatlabDAQ/KgallmAnalysis.m
@@ -49,7 +49,7 @@ while k <= length(iFiles)
 eval(['load ' iFiles(k).name]);
 
 
-if ~isempty(data(:,3))
+if exist(data(:,3))
     dataChans = [1 2 3];
     tempchan = 4; % Either 4 or 3
     lightchan = 5; % Either 5 or 4
