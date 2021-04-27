3f77ca42ded6f09c9d49a169e4a25da5fceb2b4a
Eric Fortune <eric.fortune@njit.edu>
2021-04-27 15:04:46 -0400

    / ‘Gallman/k_pow.m’

--- Gallman/k_pow.m
+++ Gallman/k_pow.m
@@ -81,7 +81,7 @@ end
             
             
 %% Plot raw data range
-figure(27); clf; hold on;
+figure(27); clf; 
     set(gcf, 'Position', [200 100 2*560 2*420]);
 
 ax(1) = subplot(511); hold on; title('sumfftAmp');
