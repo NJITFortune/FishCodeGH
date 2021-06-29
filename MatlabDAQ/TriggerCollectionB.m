ec071c621578c67bd2a768335a10a8df581031f2
EricFortune <eric.fortune@njit.edu>
2021-06-29 16:01:12 -0400

    / ‘MatlabDAQ/TriggerCollectionB.m’

--- MatlabDAQ/TriggerCollectionB.m
+++ MatlabDAQ/TriggerCollectionB.m
@@ -36,7 +36,7 @@ while numSamples < 100000
     fprintf('We are at %i steps and waiting for next trigger.\n', numSamples);
         s.startForeground();
         a = datestr(now, 'mm-dd-yyyy_HH-MM-SS');
-    fprintf('We are triggered and waiting %s\n', a);
+    fprintf('We are triggered and paused %s\n', a);
         pause(60) % After detection, pause for this long
         
         s.stop;
