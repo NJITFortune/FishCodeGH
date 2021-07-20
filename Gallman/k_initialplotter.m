328c73dbef1ce38df4f01770b6bc7fe5bab1098f
Eric Fortune <eric.fortune@njit.edu>
2021-07-20 12:32:51 -0400

    / ‘Gallman/k_initialplotter.m’

--- Gallman/k_initialplotter.m
+++ Gallman/k_initialplotter.m
@@ -34,17 +34,17 @@ ax(2) = subplot(512); hold on; title('zAmp - green = social');
     yyaxis right; plot([out.e(2).s(ttz{2}).timcont]/(60*60), [out.e(2).s(ttz{2}).zAmp], '.');
     yyaxis left; plot([out.e(1).s(ttz{1}).timcont]/(60*60), [out.e(1).s(ttz{1}).zAmp], '.');
 
-xa(1) = subplot(513); hold on; title('obwAmp');
-    yyaxis right; plot([out.e(2).s(tto{2}).timcont]/(60*60*24), [out.e(2).s(tto{2}).obwAmp], '.');
-    yyaxis left; plot([out.e(1).s(tto{1}).timcont]/(60*60*24), [out.e(1).s(tto{1}).obwAmp], '.');
+ax(3) = subplot(513); hold on; title('obwAmp');
+    yyaxis right; plot([out.e(2).s(tto{2}).timcont]/(60*60), [out.e(2).s(tto{2}).obwAmp], '.');
+    yyaxis left; plot([out.e(1).s(tto{1}).timcont]/(60*60), [out.e(1).s(tto{1}).obwAmp], '.');
 
-ax(3) = subplot(514); hold on; title('frequency (black) and temperature (red)');   
+ax(4) = subplot(514); hold on; title('frequency (black) and temperature (red)');   
         yyaxis right; plot([out.e(2).s.timcont]/(60*60), [out.e(2).s.fftFreq], '.k', 'Markersize', 8);
         yyaxis right; plot([out.e(1).s.timcont]/(60*60), [out.e(1).s.fftFreq], '.k', 'Markersize', 8);
         yyaxis left; plot([out.e(2).s.timcont]/(60*60), [out.e(2).s.temp], '-r', 'Markersize', 8);
         yyaxis left; plot([out.e(1).s.timcont]/(60*60), [out.e(1).s.temp], '-r', 'Markersize', 8);
     
-ax(4) = subplot(515); hold on; title('light transitions');
+ax(5) = subplot(515); hold on; title('light transitions');
     plot([out.e(2).s.timcont]/(60*60), [out.e(1).s.light], '.', 'Markersize', 8);
     ylim([-1, 6]);
     xlabel('Continuous');
@@ -69,7 +69,7 @@ ax(4) = subplot(515); hold on; title('light transitions');
     end    
 
 linkaxes(ax, 'x'); 
-xa.XLim = ax(1).XLim/24;
+%xa.XLim = ax(1).XLim/24;
 
                     drawnow;
 
