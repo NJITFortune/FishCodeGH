5064a7d3b6741079d194c40871489a75c67d7f0f
Eric Fortune <eric.fortune@njit.edu>
2021-07-20 14:16:51 -0400

    / ‘Gallman/KseparationAnxiety.m’

--- Gallman/KseparationAnxiety.m
+++ Gallman/KseparationAnxiety.m
@@ -29,7 +29,16 @@ ff = waitbar(0, 'Cycling through files.');
    % extract the fish frequencies
      t1 = fftmachine(tube1, Fs);
      t2 = fftmachine(tube2, Fs);
+     
+   % click between the two frequency peaks
+    figure(1); clf; 
  
+    semilogy(t1.fftfreq, t1.fftdata);
+    hold on;
+    semilogy(t2.fftfreq, t2.fftdata);
+    xlim([200 600]);
+
+[sepfreq, ~] = ginput(1);
 
 for k = 2:length(iFiles)
        
@@ -46,75 +55,66 @@ for k = 2:length(iFiles)
          
        % extract the fish frequencies
          t1 = fftmachine(tube1, Fs);
-         t2 = fftmachine(tube2, Fs);
+         t2 = fftmachine(tube2, Fs); 
  
-         
- figure(1); clf; 
- 
-    semilogy(t1.fftfreq, t1.fftdata);
-    hold on;
-    semilogy(t2.fftfreq, t2.fftdata);
-    xlim([200 600]);
+       %Assign frequencies to tubles 
+        % Tube 1
+        lfreqs = find(t1.fftfreq < sepfreq & t1.fftfreq > 200);
+            [pwrA1l, idx] = max(t1.fftdata(lfreqs));
+            pwrF1l = t1.fftfreq(lfreqs(idx));
 
-[sepfreq, ~] = ginput(1);
+        hfreqs = find(t1.fftfreq > sepfreq & t1.fftfreq < 700);
+            [pwrA1h, idx] = max(t1.fftdata(hfreqs));
+            pwrF1h = t1.fftfreq(hfreqs(idx));
 
-% Tube 1
-lfreqs = find(t1.fftfreq < sepfreq & t1.fftfreq > 200);
-    [pwrA1l, idx] = max(t1.fftdata(lfreqs));
-    pwrF1l = t1.fftfreq(lfreqs(idx));
-    
-hfreqs = find(t1.fftfreq > sepfreq & t1.fftfreq < 700);
-    [pwrA1h, idx] = max(t1.fftdata(hfreqs));
-    pwrF1h = t1.fftfreq(hfreqs(idx));
-    
-if pwrA1h > pwrA1l
-    pwr1A = pwrA1h; pwr1F = pwrF1h;
-    ratio1 = pwrA1h / pwrA1l;
-else
-    pwr1A = pwrA1l; pwr1F = pwrF1l;
-    ratio1 = pwrA1l / pwrA1h;
-end
-    
-% Tube 2
-lfreqs = find(t2.fftfreq < sepfreq & t2.fftfreq > 200);
-    [pwrA2l, idx] = max(t2.fftdata(lfreqs));
-    pwrF2l = t2.fftfreq(lfreqs(idx));
-    
-hfreqs = find(t2.fftfreq > sepfreq & t2.fftfreq < 700);
-    [pwrA2h, idx] = max(t2.fftdata(hfreqs));
-    pwrF2h = t2.fftfreq(hfreqs(idx));
-     
-    
-if pwrA2h > pwrA2l
-    pwr2A = pwrA2h; pwr2F = pwrF2h;
-    ratio2 = pwrA2h / pwrA2l;
-else
-    pwr2A = pwrA2l; pwr2F = pwrF2l;
-    ratio2 = pwrA2l / pwrA2h;
-end
-    
-if pwr2F == pwr1F
-    fprintf('Sucky ducky.\n');
-    if ratio1 > ratio2
-        if pwrA2h > pwrA2l
-            pwr2A = pwrA2l; pwr2F = pwrF2l;
-        else
-            pwr2A = pwrA2h; pwr2F = pwrF2h;
-            fprintf('Power1 %2.2f > Power2 %2.2f \n', pwrA1l, pwrA2l);
-            fprintf('Power1 %2.2f < Power2 %2.2f \n', pwrA1h, pwrA2h);
-        end
-
-        
-    end
-    if ratio2 > ratio1
         if pwrA1h > pwrA1l
-            pwr1A = pwrA1l; pwr1F = pwrF1l;
-        else
             pwr1A = pwrA1h; pwr1F = pwrF1h;
+            ratio1 = pwrA1h / pwrA1l;
+        else
+            pwr1A = pwrA1l; pwr1F = pwrF1l;
+            ratio1 = pwrA1l / pwrA1h;
         end
-    end
 
-end
+        % Tube 2
+        lfreqs = find(t2.fftfreq < sepfreq & t2.fftfreq > 200);
+            [pwrA2l, idx] = max(t2.fftdata(lfreqs));
+            pwrF2l = t2.fftfreq(lfreqs(idx));
+
+        hfreqs = find(t2.fftfreq > sepfreq & t2.fftfreq < 700);
+            [pwrA2h, idx] = max(t2.fftdata(hfreqs));
+            pwrF2h = t2.fftfreq(hfreqs(idx));
+
+
+        if pwrA2h > pwrA2l
+            pwr2A = pwrA2h; pwr2F = pwrF2h;
+            ratio2 = pwrA2h / pwrA2l;
+        else
+            pwr2A = pwrA2l; pwr2F = pwrF2l;
+            ratio2 = pwrA2l / pwrA2h;
+        end
+
+        if pwr2F == pwr1F
+            fprintf('Sucky ducky.\n');
+            if ratio1 > ratio2
+                if pwrA2h > pwrA2l
+                    pwr2A = pwrA2l; pwr2F = pwrF2l;
+                else
+                    pwr2A = pwrA2h; pwr2F = pwrF2h;
+                    fprintf('Power1 %2.2f > Power2 %2.2f \n', pwrA1l, pwrA2l);
+                    fprintf('Power1 %2.2f < Power2 %2.2f \n', pwrA1h, pwrA2h);
+                end
+
+
+            end
+            if ratio2 > ratio1
+                if pwrA1h > pwrA1l
+                    pwr1A = pwrA1l; pwr1F = pwrF1l;
+                else
+                    pwr1A = pwrA1h; pwr1F = pwrF1h;
+                end
+            end
+
+        end
 
 out = 
 
