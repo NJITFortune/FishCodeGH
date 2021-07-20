83670b51d41f31f460cfb9c08f57ed42b96ad905
Eric Fortune <eric.fortune@njit.edu>
2021-07-20 14:48:48 -0400

    / ‘Gallman/KseparationAnxiety.m’

--- Gallman/KseparationAnxiety.m
+++ Gallman/KseparationAnxiety.m
@@ -45,11 +45,11 @@ daycount = 0;
     
 
 
-ff = waitbar(0, 'Cycling through files.');
+% ff = waitbar(0, 'Cycling through files.');
 
-for k = 2:length(iFiles)
+parfor k = 2:length(iFiles)
        
-     waitbar(k/length(iFiles), ff, 'Assembling', 'modal');
+     %waitbar(k/length(iFiles), ff, 'Assembling', 'modal');
 
     
        % LOAD THE DATA FILE
