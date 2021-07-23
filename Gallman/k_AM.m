18cb3233916c07a69ec6fca1acf8aa15e193d627
Eric Fortune <eric.fortune@njit.edu>
2021-07-23 16:25:08 -0400

    / ‘Gallman/k_AM.m’

--- Gallman/k_AM.m
+++ Gallman/k_AM.m
@@ -1,8 +1,8 @@
-function out = k_AM(datums)
+function out = k_AM(datums, timmy)
 
 %find peaks
 [peaky, idx] = findpeaks(datums); 
 %resample peak idxs to get evenly spaced time indx for fft
-qq = interp1(tim(idx), peaky, tim);
+qq = interp1(timmy(idx), peaky, timmy);
 
 out = fftmachine(fillmissing(qq-nanmean(qq), 'spline'),40000);
