00fa9cde36f10523d943caf8abc69be54753a88c
Eric Fortune <eric.fortune@njit.edu>
2021-07-23 12:47:27 -0400

    / ‘Gallman/KseparationAnxiety.m’

--- Gallman/KseparationAnxiety.m
+++ Gallman/KseparationAnxiety.m
@@ -4,36 +4,18 @@ Fs = 40000;
 % userfilespec = 'Eigen*';
 % Get the list of files to be analyzed  
         iFiles = dir(userfilespec);
-    % numstart = 23;
-    % daycount = 0;
 
-
-% Set up filters
-        % High pass filter cutoff frequency
-            highp = 300;
-            [b,a] = butter(5, highp/(Fs/2), 'high'); % Filter to eliminate 60Hz contamination
-        % Low pass filter cutoff frequency
-             lowp = 600;    
-             [f,e] = butter(5, lowp/(Fs/2), 'low'); % Filter to eliminate high frequency contamination
-
+% Set up filter
         [h,g] = butter(5, [highp/(Fs/2) lowp/(Fs/2)]);
              
-
-
 % CLICK THE FIRST FILE
     load(iFiles(1).name, 'data');
 
    % filter the data
 
-%      tube1 = filtfilt(b,a,data(:,1));
-%      tube2 = filtfilt(b,a,data(:,2));
-%      tube1 = filtfilt(f,e,tube1);
-%      tube2 = filtfilt(f,e,tube2);
-
     tube1 = filtfilt(h,g,data(:,1));
     tube2 = filtfilt(h,g,data(:,2));
          
-
    % extract the fish frequencies
      t1 = fftmachine(tube1, Fs);
      t2 = fftmachine(tube2, Fs);
@@ -47,8 +29,24 @@ Fs = 40000;
     xlim([200 600]);
 
     [sepfreq, ~] = ginput(2);
+    idx1 = find(t1.fftfreq >= sepfreq(1), 1);
+    idx2 = find(t1.fftfreq >= sepfreq(2), 1);
+    
+    tube1f = [t1.fftfreq(idx1) t1.fftfreq(idx2)]; 
+    tube1a = [t1.fftdata(idx1) t1.fftdata(idx2)]; 
+    if tube1a(2) > tube1a(1)
+        tube1f = [tube1f(2) tube1f(1)];
+        tube1a = [tube1a(2) tube1a(1)];
+    end
+ 
+    tube2f = [t2.fftfreq(idx1) t2.fftfreq(idx2)]; 
+    tube2a = [t2.fftdata(idx1) t2.fftdata(idx2)]; 
+    if tube2a(2) > tube2a(1)
+        tube2f = [tube2f(2) tube2f(1)];
+        tube2a = [tube2a(2) tube2a(1)];
+    end
 
-    [tube1f, tube1a, tube2f, tube2a] = getfreqs(t1, t2, sort(sepfreq));
+    % May need to add fixer for ampl crossover problem
     
 
 ff = waitbar(0, 'Cycling through files.');
@@ -60,26 +58,12 @@ for k = 2:length(iFiles)
     
        % LOAD THE DATA FILE
         load(iFiles(k).name, 'data');
-        
-       %filter the data
-        
-%          tube1 = filtfilt(b,a,data(:,1));
-%          tube2 = filtfilt(b,a,data(:,2));
-%          tube1 = filtfilt(f,e,tube1);
-%          tube2 = filtfilt(f,e,tube2);
-    tube1 = filtfilt(h,g,data(:,1));
-    tube2 = filtfilt(h,g,data(:,2));
-         
-       % extract the fish frequencies
-         t1 = fftmachine(tube1, Fs);
-         t2 = fftmachine(tube2, Fs);
-%          
+           
          ttmp(2,:) = sort(tube2f(k-1,:));
          ttmp(1,:) = sort(tube1f(k-1,:));
-
          sepfreq = mean(ttmp);           
            
-         [tube1f(k,:), tube1a(k,:), tube2f(k,:), tube2a(k,:)] = getfreqs(t1, t2, sepfreq);
+         [tube1f(k,:), tube1a(k,:), tube2f(k,:), tube2a(k,:)] = getfreqs(data(:,1), data(:,2), sepfreq);
          
          % sepfreq = ((abs(tube1f(k,1) - tube2f(k,1)))/2) + min([tube1f(k,1), tube2f(k,1)]);
          
@@ -111,16 +95,25 @@ end
     function [t1f,t1a,t2f,t2a] = getfreqs(t1, t2, previousfreaky)       
         %Assign frequencies to tubles 
 
-        wid = 50; % +/- this number of Hz for peak
+        wid = 40; % +/- this number of Hz for filter
+
+        [b,a] = butter(3, [(previousfreaky(1)-wid)/(Fs/2) (previousfreaky(1)+wid)/(Fs/2)]);
+        [d,c] = butter(3, [(previousfreaky(2)-wid)/(Fs/2) (previousfreaky(2)+wid)/(Fs/2)]);
         
-        % Get low frequency in both channels
         
         
        % Tube 1
+       
+        t1fl = filtfilt(b,a,t1);       
+        t1 = fftmachine(t1fl, 40000);
+        
         lfreqs = find(t1.fftfreq > previousfreaky(1)-wid & t1.fftfreq < previousfreaky(1)+wid);
             [pwrA1l, idx] = max(t1.fftdata(lfreqs));
             pwrF1l = t1.fftfreq(lfreqs(idx));
 
+        t1fh = filtfilt(d,c,t1);
+        t1 = fftmachine(t1fh, 40000);
+        
         hfreqs = find(t1.fftfreq > previousfreaky(2)-wid & t1.fftfreq < previousfreaky(2)+wid);
             [pwrA1h, idx] = max(t1.fftdata(hfreqs));
             pwrF1h = t1.fftfreq(hfreqs(idx));
@@ -135,10 +128,16 @@ end
         end
 
         % Tube 2
+        t2fl = filtfilt(b,a,t2);       
+        t2 = fftmachine(t2fl, 40000);
+        
         lfreqs = find(t2.fftfreq < previousfreaky(1)-wid & t2.fftfreq < previousfreaky(1)+wid);
             [pwrA2l, idx] = max(t2.fftdata(lfreqs));
             pwrF2l = t2.fftfreq(lfreqs(idx));
 
+        t2hl = filtfilt(d,c,t2);       
+        t2 = fftmachine(t2hl, 40000);
+        
         hfreqs = find(t2.fftfreq > previousfreaky(2)-wid & t2.fftfreq < previousfreaky(2)+wid);
             [pwrA2h, idx] = max(t2.fftdata(hfreqs));
             pwrF2h = t2.fftfreq(hfreqs(idx));
