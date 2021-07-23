bd20d9521df95724b3093b9b8b73600ab1786064
Eric Fortune <eric.fortune@njit.edu>
2021-07-23 13:04:31 -0400

    / ‘Gallman/KseparationAnxiety.m’

--- Gallman/KseparationAnxiety.m
+++ Gallman/KseparationAnxiety.m
@@ -100,8 +100,8 @@ end
         wid = 40; % +/- this number of Hz for filter
         Fs = 40000;
         
-        [b,a] = butter(3, [(previousfreaky(1)-wid)/(Fs/2) (previousfreaky(1)+wid)/(Fs/2)]);
-        [d,c] = butter(3, [(previousfreaky(2)-wid)/(Fs/2) (previousfreaky(2)+wid)/(Fs/2)]);
+        [b,a] = butter(3, [(previousfreaky(1)-wid)/(Fs/2) (previousfreaky(1)+wid)/(Fs/2)], 'band');
+        [d,c] = butter(3, [(previousfreaky(2)-wid)/(Fs/2) (previousfreaky(2)+wid)/(Fs/2)], 'bandpass');
         
         
         
