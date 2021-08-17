8e8b4a941101d60466927e2e453f283a9b33f21e
Eric Fortune <eric.fortune@njit.edu>
2021-08-17 16:21:44 -0400

    / ‘Gallman/k_spliney.m’

--- Gallman/k_spliney.m
+++ Gallman/k_spliney.m
@@ -1,5 +1,5 @@
-function out = k_spliney(x, y, p)
+function [xx, yy] = k_spliney(x, y, p)
 
             spliney = csaps(x, y, p);
-            out.xx = x(1):1/ReFs:x(end);
-            out.yy = fnval(xx, spliney);
+            xx = x(1):1/ReFs:x(end);
+            yy = fnval(xx, spliney);
