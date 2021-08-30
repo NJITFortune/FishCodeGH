2ff69e3381f8a292dac3a862adb660f7a542b34f
Eric Fortune <eric.fortune@njit.edu>
2021-08-30 14:08:53 -0400

    / ‘Gallman/KseparationNoxious.m’

--- Gallman/KseparationNoxious.m
+++ Gallman/KseparationNoxious.m
@@ -175,98 +175,98 @@ figure(25); clf; %by tube - color is always the same fish
   
 linkaxes(axs, 'x');       
 
-%% extract amplitude data for each tube
-
-%amplitude by frequency
-    %loamp is lowfreq fish, name not releated to amp
-
-%by tube
-%electrode 1
-    e1hi = [out([out.e1hiamp] > [out.e1loamp]).e1hiamp];
-    e1lo = [out([out.e1loamp] > [out.e1hiamp]).e1loamp];
-    
-    length(e1hi)
-    length(e1lo)
-    
-    if length(e1hi) > length(e1lo)
-        e1tubeamp = e1hi;
-    else
-        e1tubeamp = e1lo;
-    end
-
-%%
-    
-%amplitude by frequency
-    %loamp is lowfreq fish, name not releated to amp
-
-%by tube
-%electrode 2
-    e2hi = [out([out.e2hiamp] > [out.e2loamp]).e2hiamp];
-    e2lo = [out([out.e2loamp] > [out.e2hiamp]).e2loamp];
-    
-    length(e2hi)
-    length(e2lo)
-    
-    if length(e2hi) > length(e2lo)
-        e2tubeamp = e2hi;
-    else
-        e2tubeamp = e2lo;
-    end    
-    
-%%    
-        
-%High freq fish
-    hifish1 = [out.e1hiamp] > [out.e2hiamp];
-    hifish1 = [out(hifish1).e1hiamp];
-    hifish2 = [out.e2hiamp] > [out.e1hiamp];
-    hifish2 = [out(hifish2).e2hiamp];
-    
-    length(hifish1)
-    length(hifish2)
-    
-    if length(hifish1) > length(hifish2)
-        e1fishamp = hifish1;
-    else
-        e2fishamp = hifish2;
-    end
-    
-%Low freq fish
-    lofish1 = [out.e1loamp] > [out.e2loamp];
-    lofish1 = [out(lofish1).e1loamp];
-    lofish2 = [out.e2loamp] > [out.e1loamp];
-    lofish2 = [out(lofish2).e2loamp];
-   
-    length(lofish1)
-    length(lofish2)
-    
-    if length(lofish1) > length(lofish2)
-        e1fishamp = lofish1;
-    else
-        e2fishamp = lofish2;
-    end
- 
-    
-%%    
-    
-    
-    
-    
-   e1tim = [out(e1amp).timcont];
-   e2tim = [out(e2amp).timcont];
-    
-%Make sure its not wonky
-figure(7); clf; title('High freq fish')
-    subplot(411); hold on; %fish one - higher freq
-        plot([out.timcont],[out.e1hiamp], 'b.'); plot([out.timcont],[out.e2hiamp], 'r.');
-        %plot([out.timcont], movmean([out.e1hiamp], 5), 'b-');
-    subplot(412); hold on;
-        plot(e1tim, e1amp); 
-    subplot(413); hold on; %fish two - lower freq
-        plot([out.timcont],[out.e1loamp], 'b.'); plot([out.timcont],[out.e2loamp], 'r.');  
-    subplot(414); hold on;
-        plot(e2tim, e2amp); 
-        
-    
-        
-
-        
+% %% extract amplitude data for each tube
+% 
+% %amplitude by frequency
+%     %loamp is lowfreq fish, name not releated to amp
+% 
+% %by tube
+% %electrode 1
+%     e1hi = [out([out.e1hiamp] > [out.e1loamp]).e1hiamp];
+%     e1lo = [out([out.e1loamp] > [out.e1hiamp]).e1loamp];
+%     
+%     length(e1hi)
+%     length(e1lo)
+%     
+%     if length(e1hi) > length(e1lo)
+%         e1tubeamp = e1hi;
+%     else
+%         e1tubeamp = e1lo;
+%     end
+% 
+% %%
+%     
+% %amplitude by frequency
+%     %loamp is lowfreq fish, name not releated to amp
+% 
+% %by tube
+% %electrode 2
+%     e2hi = [out([out.e2hiamp] > [out.e2loamp]).e2hiamp];
+%     e2lo = [out([out.e2loamp] > [out.e2hiamp]).e2loamp];
+%     
+%     length(e2hi)
+%     length(e2lo)
+%     
+%     if length(e2hi) > length(e2lo)
+%         e2tubeamp = e2hi;
+%     else
+%         e2tubeamp = e2lo;
+%     end    
+%     
+% %%    
+%         
+% %High freq fish
+%     hifish1 = [out.e1hiamp] > [out.e2hiamp];
+%     hifish1 = [out(hifish1).e1hiamp];
+%     hifish2 = [out.e2hiamp] > [out.e1hiamp];
+%     hifish2 = [out(hifish2).e2hiamp];
+%     
+%     length(hifish1)
+%     length(hifish2)
+%     
+%     if length(hifish1) > length(hifish2)
+%         e1fishamp = hifish1;
+%     else
+%         e2fishamp = hifish2;
+%     end
+%     
+% %Low freq fish
+%     lofish1 = [out.e1loamp] > [out.e2loamp];
+%     lofish1 = [out(lofish1).e1loamp];
+%     lofish2 = [out.e2loamp] > [out.e1loamp];
+%     lofish2 = [out(lofish2).e2loamp];
+%    
+%     length(lofish1)
+%     length(lofish2)
+%     
+%     if length(lofish1) > length(lofish2)
+%         e1fishamp = lofish1;
+%     else
+%         e2fishamp = lofish2;
+%     end
+%  
+%     
+% %%    
+%     
+%     
+%     
+%     
+%    e1tim = [out(e1amp).timcont];
+%    e2tim = [out(e2amp).timcont];
+%     
+% %Make sure its not wonky
+% figure(7); clf; title('High freq fish')
+%     subplot(411); hold on; %fish one - higher freq
+%         plot([out.timcont],[out.e1hiamp], 'b.'); plot([out.timcont],[out.e2hiamp], 'r.');
+%         %plot([out.timcont], movmean([out.e1hiamp], 5), 'b-');
+%     subplot(412); hold on;
+%         plot(e1tim, e1amp); 
+%     subplot(413); hold on; %fish two - lower freq
+%         plot([out.timcont],[out.e1loamp], 'b.'); plot([out.timcont],[out.e2loamp], 'r.');  
+%     subplot(414); hold on;
+%         plot(e2tim, e2amp); 
+%         
+%     
+%         
+% 
+%         
