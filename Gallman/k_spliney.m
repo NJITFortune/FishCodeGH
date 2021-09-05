826c4e5ea7f7d8c413710fe6ffe9dce8effea3d2
Kathryn Gallman <keg26@njit.edu>
2021-09-05 18:55:27 -0400

    / ‘Gallman/k_spliney.m’

--- Gallman/k_spliney.m
+++ Gallman/k_spliney.m
@@ -1,6 +1,11 @@
-function [xx, yy] = k_spliney(x, y, p)
+function [xx, yy] = k_spliney(x, y)
 
-ReFs = 60;  %resample once every minute
+%fit
+p = 0.7;
+%resample rate
+ReFs = 10;
+
+%ReFs = 60;  %resample once every minute
 
             spliney = csaps(x, y, p);
             xx = x(1):1/ReFs:x(end);
