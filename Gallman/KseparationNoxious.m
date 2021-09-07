267da8ef9f198f12273e57e939ac065128b759e3
Eric Fortune <eric.fortune@njit.edu>
2021-09-07 15:03:53 -0400

    / ‘Gallman/KseparationNoxious.m’

--- Gallman/KseparationNoxious.m
+++ Gallman/KseparationNoxious.m
@@ -302,29 +302,29 @@ linkaxes(axs, 'x');
 %% Plot fish against light/temp
 
 
-% figure(1); clf; 
-% 
-%     
-%     ax(1) = subplot(411); hold on; 
-%         yyaxis right; plot([out.Hitimcont], [out.intubeHi], '.');
-%         yyaxis left; plot([out.Lotimcont], [out.intubeLo], '.');
-% 
-%         legend('High frequency fish', 'Low frequency fish');
-%         
-%     ax(2) = subplot(412); hold on;
-%         yyaxis right; plot([out(Hisortidx).timcont], [out(Hisortidx).hifreq], '.'); 
-%         yyaxis left; plot([out(Losortidx).timcont], [out(Losortidx).lofreq], '.');
-%         
-%     
-%     ax(3) = subplot(413); hold on; 
-%             plot([out(2:end).timcont], [out.temp], '.');
-%     
-%     ax(4) = subplot(414); hold on;
-%         plot([out(2:end).timcont], [out.light]);
-%         ylim([-1, 6]);
-%         
-% linkaxes(ax, 'x');
-% 
+figure(1); clf; 
+
+    
+    ax(1) = subplot(411); hold on; 
+        yyaxis right; plot([out.Hitimcont], [out.intubeHi], '.');
+        yyaxis left; plot([out.Lotimcont], [out.intubeLo], '.');
+
+        legend('High frequency fish', 'Low frequency fish');
+        
+    ax(2) = subplot(412); hold on;
+        yyaxis right; plot([out(Hisortidx).timcont], [out(Hisortidx).hifreq], '.'); 
+        yyaxis left; plot([out(Losortidx).timcont], [out(Losortidx).lofreq], '.');
+        
+    
+    ax(3) = subplot(413); hold on; 
+            plot([out(2:end).timcont], [out.temp], '.');
+    
+    ax(4) = subplot(414); hold on;
+        plot([out(2:end).timcont], [out.light]);
+        ylim([-1, 6]);
+        
+linkaxes(ax, 'x');
+
 
 
 
