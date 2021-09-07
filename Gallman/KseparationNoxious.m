d841b3eecf1d247380aab09278bf49d1bb61c93c
Eric Fortune <eric.fortune@njit.edu>
2021-09-07 11:05:18 -0400

    / ‘Gallman/KseparationNoxious.m’

--- Gallman/KseparationNoxious.m
+++ Gallman/KseparationNoxious.m
@@ -294,20 +294,21 @@ hlshared
 figure(1); clf; 
 
     
-    ax(1) = subplot(311); hold on; 
+    ax(1) = subplot(411); hold on; 
         yyaxis right; plot(Hitimcont, intubeHi, '.');
         yyaxis left; plot(Lotimcont, intubeLo, '.');
 
         legend('High frequency fish', 'Low frequency fish');
         
-    ax(2) = subplot(312); hold on;
+    ax(2) = subplot(412); hold on;
         yyaxis right; plot([out(Hisortidx).timcont], [out(Hisortidx).hifreq], '.'); 
         yyaxis left; plot([out(Losortidx).timcont], [out(Losortidx).lofreq], '.');
-        yyaxis left; plot([out.timcont], [out.temp], '.');
+        
     
-        legend('High frequency fish', 'Low frequency fish', 'Temperature');
+    ax(3) = subplot(413); hold on; 
+            plot([out.timcont], [out.temp], '.');
     
-    ax(3) = subplot(313); hold on;
+    ax(4) = subplot(414); hold on;
         plot([out.timcont], [out.light]);
         
         
