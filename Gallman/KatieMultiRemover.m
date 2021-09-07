632ed127d78fc401f8699c8ba8b0d8022732aab2
Eric Fortune <eric.fortune@njit.edu>
2021-09-07 11:19:22 -0400

    + ‘Gallman/KatieMultiRemover.m’

---
+++ Gallman/KatieMultiRemover.m
@@ -0,0 +1,43 @@
+function out = KatieMultiRemover(in)
+
+% OBW        
+figure(1); clf;
+
+    histogram([in.intubeHi], 100); hold on;
+    
+    %Lower lim
+    %fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+    [cutofffreqL, ~]  = ginput(1);
+    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    %Upper lim
+    %fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+    [cutofffreqH, ~]  = ginput(1);
+    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    out(k).Hiobwidx = find([in.intubeHi] > cutofffreqL & [in.intubeHi] < cutofffreqH);
+    pause(1);
+    
+    
+figure(1); clf;
+
+    histogram([in.intubeLo], 100); hold on;
+    
+    %Lower lim
+    %fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+    [cutofffreqL, ~]  = ginput(1);
+    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    %Upper lim
+    %fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+    [cutofffreqH, ~]  = ginput(1);
+    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    out(k).Loobwidx = find([in.intubeLo] > cutofffreqL & [in.intubeLo] < cutofffreqH);
+    pause(1);
+    
+end
