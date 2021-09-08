454ddfa02e6b46d9294a996f9ff501795ac3b853
Eric Fortune <eric.fortune@njit.edu>
2021-09-08 14:58:09 -0400

    / ‘Gallman/kday.m’

--- Gallman/kday.m
+++ Gallman/kday.m
@@ -57,7 +57,7 @@ for j = 1:length(lighttimeslesslong)-1
         
         %is there data between j and j+1?    
         if ~isempty(find([in.e(1).s(tto{1}).timcont]/(60*60) >= lighttimeslesslong(j) & [in.e(1).s(tto{1}).timcont]/(60*60) < (lighttimeslesslong(j+1)),1))  
-            ott = find([in.e(1).s(tto{1}().timcont]/(60*60) >= lighttimeslesslong(j) & [in.e(1).s(tto{1}).timcont]/(60*60) < lighttimeslesslong(j+1)); 
+            ott = find([in.e(1).s(tto{1}).timcont]/(60*60) >= lighttimeslesslong(j) & [in.e(1).s(tto{1}).timcont]/(60*60) < lighttimeslesslong(j+1)); 
             lighttim = [in.e(1).s(tto{1}(ott)).timcont]/(60*60);
             length(lighttim);
             
