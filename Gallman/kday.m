49c0a41111c18f5e30f2ad90b1834e7ea7eb71c7
Eric Fortune <eric.fortune@njit.edu>
2021-09-10 17:50:50 -0400

    / ‘Gallman/kday.m’

--- Gallman/kday.m
+++ Gallman/kday.m
@@ -429,7 +429,7 @@ figure(70); clf; title('12 hour - channel 1');
    
     for j = 2:2:length(twelvelight)-1
 
-              if length((obwxx >= twelvelight(j-1) & obwxx < twelvelight(j+1))) == length(ot1)
+              if length((obwxx >= twelvelight(j-1) & obwxx < twelvelight(j+1))) >= length(ot1)
                    otx = find(obwxx >= twelvelight(j-1) & obwxx < twelvelight(j+1)); 
                    
                    plot(obwxx(otx) - obwxx(otx(1)), dtobwyy1(otx));
