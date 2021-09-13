7ed48ca2ddd72a0ef123ff4a8d9a2537b928a568
Eric Fortune <eric.fortune@njit.edu>
2021-09-13 14:54:59 -0400

    / ‘Gallman/KatieDessembler.m’

--- Gallman/KatieDessembler.m
+++ Gallman/KatieDessembler.m
@@ -15,8 +15,11 @@ for j = 1:2 % Perform analyses on the two channels
              %divide into trial of 48 hours
              trialwindowidx = [in(j).s.timcont]/(60*60) >= numotrials(jj) & [in(j).s.timcont]/(60*60) < numotrials(jj+1);
 
+             %amplitude data
              trial(jj).in(j).s.obwAmp = [in(j).s(trialwindowidx).obwAmp];
+             trial(jj).in(j).s.zAmp = [in(j).s(trialwindowidx).zAmp];
              
+             %variables
              trial(jj).in(j).s.timcont = [in(j).s(trialwindowidx).timcont];
              trial(jj).in(j).s.light = [in(j).s(trialwindowidx).light];
              trial(jj).in(j).s.temp = [in(j).s(trialwindowidx).temp];
