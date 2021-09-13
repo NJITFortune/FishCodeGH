e3312a13e57f7747c14f1a74136f1e3ecd94b998
Eric Fortune <eric.fortune@njit.edu>
2021-09-13 17:35:45 -0400

    / ‘Gallman/KatieDessembler.m’

--- Gallman/KatieDessembler.m
+++ Gallman/KatieDessembler.m
@@ -8,23 +8,25 @@ function trial  = KatieDessembler(in)
 for j = 1:2 % Perform analyses on the two channels     
     
     numoperiods = (ceil([in(j).s(end).timcont]/(60*60)) / 48) + 1;
-    
+    timz = 1:1:numoperiods;
+    %generate new 12 hour light vector
+    twoday(timz) = [in(j).s(1).timcont]/(60*60) + (48*(timz-1)); 
     
     for jj = 1:length(numoperiods)-1
 
              %divide into trial of 48 hours
-             trialwindowidx = [in(j).s.timcont]/(60*60) >= numoperiods(jj) & [in(j).s.timcont]/(60*60) < numoperiods(jj+1);
+             trialwindowidx = [in(j).s.timcont]/(60*60) >= twoday(jj) & [in(j).s.timcont]/(60*60) < twoday(jj+1);
 
              %amplitude data
-             trial(jj).e(j).s.obwAmp = [in(j).s(trialwindowidx).obwAmp];
-             trial(jj).e(j).s.zAmp = [in(j).s(trialwindowidx).zAmp];
-             trial(jj).e(j).s.sumfftAmp = [in(j).s(trialwindowidx).sumfftAmp];
-             trial(jj).e(j).s.fftFreq = [in(j).s(trialwindowidx).fftFreq];
+             out(jj).e(j).s.obwAmp = [in(j).s(trialwindowidx).obwAmp];
+             out(jj).e(j).s.zAmp = [in(j).s(trialwindowidx).zAmp];
+             out(jj).e(j).s.sumfftAmp = [in(j).s(trialwindowidx).sumfftAmp];
+             out(jj).e(j).s.fftFreq = [in(j).s(trialwindowidx).fftFreq];
              
              %variables
-             trial(jj).e(j).s.timcont = [in(j).s(trialwindowidx).timcont];
-             trial(jj).e(j).s.light = [in(j).s(trialwindowidx).light];
-             trial(jj).e(j).s.temp = [in(j).s(trialwindowidx).temp];
+             out(jj).e(j).s.timcont = [in(j).s(trialwindowidx).timcont];
+             out(jj).e(j).s.light = [in(j).s(trialwindowidx).light];
+             out(jj).e(j).s.temp = [in(j).s(trialwindowidx).temp];
              
              
     end
