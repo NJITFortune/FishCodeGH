221b721fc5cec080d17cf1bc32f70a6fae09dc1d
Eric Fortune <eric.fortune@njit.edu>
2021-09-15 11:32:21 -0400

    / ‘Gallman/KatieDessembler.m’

--- Gallman/KatieDessembler.m
+++ Gallman/KatieDessembler.m
@@ -16,10 +16,10 @@ for j = 1:2
              trialwindowidx = [in(j).s.timcont]/(60*60) >= twoday(jj) & [in(j).s.timcont]/(60*60) < twoday(jj+1);
 
              %amplitude data
-             out(jj).e(j).s.obwAmp = [in(j).s(trialwindowidx).obwAmp];
-             out(jj).e(j).s.zAmp = [in(j).s(trialwindowidx).zAmp];
-             out(jj).e(j).s.sumfftAmp = [in(j).s(trialwindowidx).sumfftAmp];
-             out(jj).e(j).s.fftFreq = [in(j).s(trialwindowidx).fftFreq];
+             out(jj).e(j).obwAmp = [in(j).s(trialwindowidx).obwAmp];
+             out(jj).e(j).zAmp = [in(j).s(trialwindowidx).zAmp];
+             out(jj).e(j).sumfftAmp = [in(j).s(trialwindowidx).sumfftAmp];
+             out(jj).e(j).fftFreq = [in(j).s(trialwindowidx).fftFreq];
              
              %variables
              out(jj).e(j).s(:).timcont = [in(j).s(trialwindowidx).timcont];
