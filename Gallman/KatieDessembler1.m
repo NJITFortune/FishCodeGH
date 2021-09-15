1925e0864e2d9e9d90aa0f305b6eda01ad37b39b
Eric Fortune <eric.fortune@njit.edu>
2021-09-15 16:56:56 -0400

    + ‘Gallman/KatieDessembler1.m’

---
+++ Gallman/KatieDessembler1.m
@@ -0,0 +1,41 @@
+function out = KatieDessembler1(in, orgidx)     
+% Usage: out = KatieDessembler(in(orgidx), orgidx)
+% Example: out = KatieDessembler(in(orgidx), orgidx)
+% And out is something...
+
+%define sample range
+    perd = 48; % default length is 48 hours
+    perd = perd - rem(perd, in.info.ld);       
+    
+    perdsex = perd * 60 * 60; % perd in seconds, for convenience since timcont is in seconds
+
+    % How many samples available?
+    lengthofsampleHOURS = (in.e(1).s(end).timcont/(60*60)) - (in.e(1).s(1).timcont/(60*60));    
+    % How many integer periods
+    numoperiods = floor(lengthofsampleHOURS / perd); % of periods
+    
+for jj = 1:numoperiods
+    
+            % indices for our sample window of perd hours
+            timidx = find([in.e(1).s.timcont] > in.e(1).s(1).timcont + ((jj-1)*perdsex) & ...
+                [in.e(1).s.timcont] < in.e(1).s(1).timcont + (jj*perdsex));
+            
+            for j = 1:2
+            
+             % Data   
+             out(jj).e(j).obwAmp = [in.e(j).s(timidx).obwAmp];
+             out(jj).e(j).zAmp = [in.e(j).s(timidx).zAmp];
+             out(jj).e(j).sumfftAmp = [in.e(j).s(timidx).sumfftAmp];
+             out(jj).e(j).fftFreq = [in.e(j).s(timidx).fftFreq];
+             
+             % Time and treatment 
+             out(jj).e(j).timcont = [in.e(j).s(timidx).timcont] - in.e(j).s(timidx(1)).timcont + 1;
+             out(jj).e(j).light = [in.e(j).s(timidx).light];
+             out(jj).e(j).temp = [in.e(j).s(timidx).temp];
+             
+             out(jj).ld = in.info.ld; 
+             out(jj).kg = orgidx; % idx for kg
+             
+            end
+
+        end     
