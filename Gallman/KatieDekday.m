93c4924a2f993924454c0a32123498148a63af19
Eric Fortune <eric.fortune@njit.edu>
2021-09-16 16:28:48 -0400

    / ‘Gallman/KatieDekday.m’

--- Gallman/KatieDekday.m
+++ Gallman/KatieDekday.m
@@ -339,7 +339,7 @@ end
         
           %create time idex for stardard deviation plot fill
           mout(jj).tt = out(jj).timcont(dayidx) - out(jj).timcont(dayidx(1));
-          length(mout.tt)      
+          length(mout(jj).tt)      
     end
     
     
