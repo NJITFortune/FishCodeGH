f48cb151fc8e9c20a58f8e9ecab50040d8ee7a27
Eric Fortune <eric.fortune@njit.edu>
2021-09-16 17:22:11 -0400

    / ‘Gallman/KatieDekday.m’

--- Gallman/KatieDekday.m
+++ Gallman/KatieDekday.m
@@ -333,6 +333,7 @@ end
                 mout(jj).sumfftavg(kk/2, :) = sumfftyy(dayidx);
                 mout(jj).zavg(kk/2, :) = zyy(dayidx);
                 mout(jj).obwavg(kk/2, :) = obwyy(dayidx);
+                mout(jj).tt(kk/2, :) = out(jj).timcont(dayidx);
                 
                 
           end
