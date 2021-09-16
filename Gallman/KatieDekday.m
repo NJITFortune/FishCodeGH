66846fb2e39a980ea27b73011190e9a2df0329e6
Eric Fortune <eric.fortune@njit.edu>
2021-09-16 18:19:25 -0400

    / ‘Gallman/KatieDekday.m’

--- Gallman/KatieDekday.m
+++ Gallman/KatieDekday.m
@@ -311,6 +311,8 @@ end
     
 %trial Dark/Light averages
  
+    lightidx = find(lighttimes <= out.xx(end));
+    lighttimes = lighttimes(lightidx);
     %add one to lighttimes to prevent data loss from logical indexing
     figure; plot(diff(lighttimes));
 
