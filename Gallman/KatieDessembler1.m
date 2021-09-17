fe2a22d30a8b85119784e062e101798187f7c2b5
Eric Fortune <eric.fortune@njit.edu>
2021-09-17 15:48:03 -0400

    / ‘Gallman/KatieDessembler1.m’

--- Gallman/KatieDessembler1.m
+++ Gallman/KatieDessembler1.m
@@ -185,33 +185,33 @@ end
     meanofmeans = mean(mtrial);
     plot(plot(trial(j).tim, meanofmeans, 'k', 'LineWidth', 3);
 
-
-figure(29); clf; 
-
-    
-for j=1:length(trial) 
-    
-    subplot(211); hold on; title('channel 1');
-        mtrial(j,:) = zeros(1,length(trial(j).tim));
-        for k=1:length(trial(j).day)
-            mtrial(j,:) = mtrial(j,:) + trial(j).day(k).e(1).SobwAmp;
-            mtrial(j,:) = mtrial(j,:) / max(abs(mtrial(j,:)-mean(mtrial(j,:))));
-        end
-        
-        trialmean = mean(mtrial);
-        trialstd = std(mtrial);
-        tt = trial(1).tim;
-        tt = [tt tt(end:-1:1)];
-        
-        plot(trial(j).tim, mtrial(j,:) / length(trial(j).day));
-        
-    subplot(212); hold on; title('channel 1');
-        mtrial(j,:) = zeros(1,length(trial(j).tim));
-        for k=1:length(trial(j).day)
-            mtrial(j,:) = mtrial(j,:) + trial(j).day(k).e(2).SobwAmp;
-        end
-        plot(trial(j).tim, mtrial(j,:) / length(trial(j).day));
-    
-end
-
-
+% 
+% figure(29); clf; 
+% 
+%     
+% for j=1:length(trial) 
+%     
+%     subplot(211); hold on; title('channel 1');
+%         mtrial(j,:) = zeros(1,length(trial(j).tim));
+%         for k=1:length(trial(j).day)
+%             mtrial(j,:) = mtrial(j,:) + trial(j).day(k).e(1).SobwAmp;
+%             mtrial(j,:) = mtrial(j,:) / max(abs(mtrial(j,:)-mean(mtrial(j,:))));
+%         end
+%         
+%         trialmean = mean(mtrial);
+%         trialstd = std(mtrial);
+%         tt = trial(1).tim;
+%         tt = [tt tt(end:-1:1)];
+%         
+%         plot(trial(j).tim, mtrial(j,:) / length(trial(j).day));
+%         
+%     subplot(212); hold on; title('channel 1');
+%         mtrial(j,:) = zeros(1,length(trial(j).tim));
+%         for k=1:length(trial(j).day)
+%             mtrial(j,:) = mtrial(j,:) + trial(j).day(k).e(2).SobwAmp;
+%         end
+%         plot(trial(j).tim, mtrial(j,:) / length(trial(j).day));
+%     
+% end
+% 
+% 
