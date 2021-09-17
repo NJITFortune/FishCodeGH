15e370b4dd1220858f052fdbc33a9c910f01ed80
Eric Fortune <eric.fortune@njit.edu>
2021-09-17 16:44:27 -0400

    / ‘Gallman/KatieDessembler1.m’

--- Gallman/KatieDessembler1.m
+++ Gallman/KatieDessembler1.m
@@ -91,7 +91,7 @@ for jj = 1:numoperiods
             % Get the rest of the indices for the trial  
             timidx = timidx:timidx + (perd*ReFs)-1;
             
-            if obwyy(j,end) >= timidx(end)
+            if length(obwyy(j,:)) >= timidx(end)
              % Data   
              out(jj).e(j).SobwAmp = obwyy(j,timidx);
              out(jj).e(j).SzAmp = zyy(j,timidx);
