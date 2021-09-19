d4a2965256fc55b9a9ad066efb76453704735495
Kathryn Gallman <keg26@njit.edu>
2021-09-19 17:15:15 -0400

    / ‘Gallman/KatieDessembler1.m’

--- Gallman/KatieDessembler1.m
+++ Gallman/KatieDessembler1.m
@@ -7,10 +7,10 @@ function [out, trial] = KatieDessembler1(in, orgidx)
 ReFs = 10;  % Sample rate for splines
 
 for j = 1:2
-[xx(j, :), obwyy(j, :), zyy(j, :), sumfftyy(j, :), luztimes] = k_spliner(in,j, ReFs);
+[xx(j, :), obwyy(j, :), zyy(j, :), sumfftyy(j, :), luztimes (j,:)] = k_spliner(in,j, ReFs);
 end
 
-lighttimes = abs(luztimes);
+lighttimes = abs(luztimes(1,:);
 % Make a time base that starts and ends on lighttimes 
     %necessary to compare spline with raw data
 
