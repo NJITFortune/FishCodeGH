2a57f501d309102de175dfeb1cd58fa6e46a312b
Kathryn Gallman <keg26@njit.edu>
2021-09-19 12:54:40 -0400

    / ‘Gallman/KatieDekday.m’

--- Gallman/KatieDekday.m
+++ Gallman/KatieDekday.m
@@ -65,6 +65,8 @@ end
 
 % take all cells with values and make a new vector
 lighttimes = lighttrim(lighttrim > 0);
+%add back the light time we subtracted 
+lighttimes(end +1) = lighttimes(end) + ld;
 
 
 %% cspline entire data set
