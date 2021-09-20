e83ab1f8f91ece6939f662267325a49696ebb820
Eric Fortune <eric.fortune@njit.edu>
2021-09-20 14:51:54 -0400

    / ‘Gallman/k_spliner.m’

--- Gallman/k_spliner.m
+++ Gallman/k_spliner.m
@@ -1,5 +1,5 @@
 
-function [xx, zyy, sumfftyy, lighttimes] =  k_spliner(in, channel, ReFs)
+function [xx,obwyy, zyy, sumfftyy, lighttimes] =  k_spliner(in, channel, ReFs)
 %% Usage
 %out = [new ReFs time, resampled obw, resampled zAmp, resampled sumfft, lightchange in hours] 
 %in = (kg(#), channel, 10
@@ -78,16 +78,16 @@ if channel == 1
       
       %estimate new yvalues for every x value
       
-            %obw
-%             spliney = csaps([in.e(1).s(tto{1}).timcont]/(60*60), [in.e(1).s(tto{1}).obwAmp], p);
-%             %resample new x values based on light/dark
-%             obwyy = fnval(xx, spliney);
-%             %detrend ydata
-%             dtobwyy = detrend(obwyy,6,'SamplePoints', xx);
-%             %raw data variables
-%                 obwtimOG = [in.e(1).s(tto{1}).timcont]/(60*60);
-%                 obwAmpOG = [in.e(1).s(tto{1}).obwAmp];
-%       
+            obw
+            spliney = csaps([in.e(1).s(tto{1}).timcont]/(60*60), [in.e(1).s(tto{1}).obwAmp], p);
+            %resample new x values based on light/dark
+            obwyy = fnval(xx, spliney);
+            %detrend ydata
+            dtobwyy = detrend(obwyy,6,'SamplePoints', xx);
+            %raw data variables
+                obwtimOG = [in.e(1).s(tto{1}).timcont]/(60*60);
+                obwAmpOG = [in.e(1).s(tto{1}).obwAmp];
+      
             %zAmp
             spliney = csaps([in.e(1).s(ttz{1}).timcont]/(60*60), [in.e(1).s(ttz{1}).zAmp], p);
             %resample new x values based on light/dark
