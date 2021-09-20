44175d224ef6fbe459a4797dcedd22998f693f98
Eric Fortune <eric.fortune@njit.edu>
2021-09-20 16:21:43 -0400

    / ‘Gallman/k_initialplotter.m’

--- Gallman/k_initialplotter.m
+++ Gallman/k_initialplotter.m
@@ -64,18 +64,18 @@ ax(5) = subplot(515); hold on; title('light transitions');
     
 % Add light transitions times to check luz
   
-    if  ~isempty(out.info.luz)
-        
-        %luz by transition type
-            %separate by transition type
-            lighton = out.info.luz(out.info.luz > 0);
-            darkon = out.info.luz(out.info.luz < 0);
-            
-            %plot
-            ax(5) = subplot(515); hold on;
-            plot([lighton' lighton']', [0 6], 'y-', 'LineWidth', 2, 'MarkerSize', 10);
-            plot([abs(darkon)' abs(darkon)']', [0 6], 'k-', 'LineWidth', 2, 'MarkerSize', 10);
-    end    
+%     if  ~isempty(out.info.luz)
+%         
+%         %luz by transition type
+%             %separate by transition type
+%             lighton = out.info.luz(out.info.luz > 0);
+%             darkon = out.info.luz(out.info.luz < 0);
+%             
+%             %plot
+%             ax(5) = subplot(515); hold on;
+%             plot([lighton' lighton']', [0 6], 'y-', 'LineWidth', 2, 'MarkerSize', 10);
+%             plot([abs(darkon)' abs(darkon)']', [0 6], 'k-', 'LineWidth', 2, 'MarkerSize', 10);
+%     end    
 
 linkaxes(ax, 'x'); 
 
