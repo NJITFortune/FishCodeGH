e5c88778d6e54b6884d430accb6370464b2ecb85
Eric Fortune <eric.fortune@njit.edu>
2021-09-20 15:24:38 -0400

    + ‘Gallman/k_welches.m’

---
+++ Gallman/k_welches.m
@@ -0,0 +1,42 @@
+function [freq, pwr, hourperiod] = k_welches(in)
+%in = output from KatieTrialDessembler 
+
+hourperiod = in.ld;
+
+%% Run fft (pwelch)
+
+
+%Analysis zAMp
+% %fftmachine
+% f = fftmachine(o.z(1).y - mean(o.z(1).y), ReFs, 3); 
+%pwelch
+L = length(in.zyy); 
+NFFT = 2^nextpow2(L)/2;
+%NFFT = 8192;
+FreqRange = 0.002:0.0001:0.2;
+
+
+
+    %generate fft
+    [pxx,pf] = pwelch(in.zyy, NFFT, floor(NFFT*0.99), FreqRange, ReFs);  
+    %populate values 
+    zwelch = [pxx', pf'];
+    colNames = {'pxx','pfreq'};
+    pw(1).zAmp = array2table(zwelch,'VariableNames',colNames);
+    
+    %find peak at given frequency
+    range = 0.002;
+    xfreq(1) = 1/(2*hourperiod);
+    hourpeak(1) = mean(pw(1).zAmp.pxx(pw(1).zAmp.pfreq > (1/(2*hourperiod) - range/2) & pw(1).zAmp.pfreq < ((1/(2*hourperiod) + range/2))));
+        freq = xfreq(1);
+        pwr = hourpeak(1);
+        
+        
+   
+    end
+
+
+
+
+
+
