ec881be0d77f5aed73b256161e89636ea0ff82f0
Eric Fortune <eric.fortune@njit.edu>
2021-09-20 10:39:59 -0400

    / ‘Gallman/KatieTrialDessembler.m’

--- Gallman/KatieTrialDessembler.m
+++ Gallman/KatieTrialDessembler.m
@@ -8,11 +8,11 @@ ReFs = 10;  % Sample rate for splines
 ld = in.info.ld; % Whatever - ld is shorter than in.info.ld
 
 
-[xx, obwyy, zyy, sumfftyy, luztimes] = k_spliner(in,channel, ReFs);
+[xx, obwyy, zyy, sumfftyy, lighttimes] = k_spliner(in,channel, ReFs);
 
-lighttimes = abs(luztimes);
-%add back the light time we subtracted 
-lighttimes(end +1) = lighttimes(end) + ld;
+% lighttimes = abs(luztimes);
+% %add back the light time we subtracted 
+% lighttimes(end +1) = lighttimes(end) + ld;
 
 % Make a time base that starts and ends on lighttimes 
     %necessary to compare spline with raw data
