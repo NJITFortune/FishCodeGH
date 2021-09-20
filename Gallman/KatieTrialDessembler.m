a3afbd9c49ae22114dadc8e0c4420e0568a55425
Eric Fortune <eric.fortune@njit.edu>
2021-09-20 10:30:41 -0400

    / ‘Gallman/KatieTrialDessembler.m’

--- Gallman/KatieTrialDessembler.m
+++ Gallman/KatieTrialDessembler.m
@@ -98,7 +98,7 @@ figure(48); clf; hold on; title('spline vs raw data');
 
     for k = 1:length(out) 
       
-        plot(out(k).entiretimcont/3600 - out(k).entiretimcont(1)/3600 , out(k).obwAmp, '.'); 
+        %plot(out(k).entiretimcont/3600 - out(k).entiretimcont(1)/3600 , out(k).obwAmp, '.'); 
         plot(out(k).Sentiretimcont, out(k).SobwAmp, 'k-', 'LineWidth', 3); 
         
         maxlen = max([maxlen out(k).entiretimcont/3600 - out(k).entiretimcont(1)/3600]);        
