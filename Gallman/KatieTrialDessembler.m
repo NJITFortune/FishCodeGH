93f08f87940a7dcd1ea876f9c3dee1c8f06025d4
Eric Fortune <eric.fortune@njit.edu>
2021-09-21 16:00:12 -0400

    / ‘Gallman/KatieTrialDessembler.m’

--- Gallman/KatieTrialDessembler.m
+++ Gallman/KatieTrialDessembler.m
@@ -1,7 +1,7 @@
 %function out = KatieTrialDessembler(in, channel, orgidx)  
 clearvars -except kg
 % 
-in = kg(45);
+in = kg(46);
 channel = 2;
 % % Out is raw data, trial is spline data
 %% Take spline estimate of raw data
