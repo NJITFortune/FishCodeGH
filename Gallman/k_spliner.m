ffecbe367124da7f8c5bc19a06211a599797c789
Eric Fortune <eric.fortune@njit.edu>
2021-09-21 12:51:58 -0400

    / ‘Gallman/k_spliner.m’

--- Gallman/k_spliner.m
+++ Gallman/k_spliner.m
@@ -50,7 +50,7 @@ for j = 1:length(lighttimeslesslong)-1
         if ~isempty(find([in.e(1).s(tto{1}).timcont]/(60*60) >= lighttimeslesslong(j) & [in.e(1).s(tto{1}).timcont]/(60*60) < (lighttimeslesslong(j+1)),1))  
             ott = [in.e(1).s(tto{1}).timcont]/(60*60) >= lighttimeslesslong(j) & [in.e(1).s(tto{1}).timcont]/(60*60) < lighttimeslesslong(j+1); 
             lighttim = [in.e(1).s(tto{1}(ott)).timcont]/(60*60);
-            length(lighttim);
+            
             
             %ensures that we start on the first full lighttime
            % if all(lighttim(1) >= lighttimeslesslong(j) & lighttim(1) < lighttimeslesslong(j) + ld/2)  
