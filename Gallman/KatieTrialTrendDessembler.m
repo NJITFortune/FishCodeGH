7cb3c649a70be3bdb780b5f7cc9573e7e539341a
Eric Fortune <eric.fortune@njit.edu>
2021-09-21 15:09:39 -0400

    / ‘Gallman/KatieTrialTrendDessembler.m’

--- Gallman/KatieTrialTrendDessembler.m
+++ Gallman/KatieTrialTrendDessembler.m
@@ -1,8 +1,8 @@
-%function out = KatieTrialTrendDessembler(in, channel, orgidx)  
+function out = KatieTrialTrendDessembler(in, channel, orgidx)  
 % clearvars -except kg
 % 
- in = kg(3);
- channel = 1;
+%  in = kg(56);
+%  channel = 1;
 % % Out is raw data, trial is spline data
 %% Take spline estimate of raw data
 
@@ -82,7 +82,7 @@ for jj = 1:numotrials
              out(jj).temp = [in.e(j).s(timidx).temp];
              
              out(jj).ld = in.info.ld; 
-             %out(jj).kg = orgidx; % idx for kg
+             out(jj).kg = orgidx; % idx for kg
              
            
 
@@ -117,48 +117,48 @@ end
 %% Plot to check
 %raw data
 
-figure(48); clf; title('spline vs raw data');hold on; 
-
- 
-   % maxlen = 0;
-
-    for k = 1:length(out) 
-      
-        plot(out(k).entiretimcont/3600 , out(k).sumfftAmp, '.'); 
-        plot(out(k).Sentiretimcont, out(k).SsumfftAmp, 'k-', 'LineWidth', 3); 
-        
-       % maxlen = max([maxlen out(k).entiretimcont/3600]);        
-    end
-
-     %xlim([0 maxlen]);
-     
-figure(49); clf;  
-
-
- 
-   maxlen = 0;
-
-    for k = 1:length(out) 
-       
-      subplot(211); hold on; title('spline vs raw data');
-         plot(out(k).timcont/3600, out(k).sumfftAmp, '.'); 
-        
-      subplot(212); hold on; title('spline vs trial data');
-        plot(out(k).Stimcont, out(k).SsumfftAmp, '.', 'MarkerSize', 3); 
-        
-        maxlen = max([maxlen out(k).timcont(end)/3600]);        
-    end
-
-     xlim([0 maxlen]);
-   
-
-
-% figure(49); clf; 
+% figure(48); clf; title('spline vs raw data');hold on; 
 % 
-%      for k = 1:length(out) 
-%          
-%         subplot(211); hold on; title('spline vs light');
+%  
+%    % maxlen = 0;
+% 
+%     for k = 1:length(out) 
+%       
+%         plot(out(k).entiretimcont/3600 , out(k).sumfftAmp, '.'); 
+%         plot(out(k).Sentiretimcont, out(k).SsumfftAmp, 'k-', 'LineWidth', 3); 
 %         
-%          subplot(212); hold on; title('spline vs trial light');
-%     
-
+%        % maxlen = max([maxlen out(k).entiretimcont/3600]);        
+%     end
+% 
+%      %xlim([0 maxlen]);
+%      
+% figure(49); clf;  
+% 
+% 
+%  
+%    maxlen = 0;
+% 
+%     for k = 1:length(out) 
+%        
+%       subplot(211); hold on; title('spline vs raw data');
+%          plot(out(k).timcont/3600, out(k).sumfftAmp, '.'); 
+%         
+%       subplot(212); hold on; title('spline vs trial data');
+%         plot(out(k).Stimcont, out(k).SsumfftAmp, '.', 'MarkerSize', 3); 
+%         
+%         maxlen = max([maxlen out(k).timcont(end)/3600]);        
+%     end
+% 
+%      xlim([0 maxlen]);
+%    
+% 
+% 
+% % figure(49); clf; 
+% % 
+% %      for k = 1:length(out) 
+% %          
+% %         subplot(211); hold on; title('spline vs light');
+% %         
+% %          subplot(212); hold on; title('spline vs trial light');
+% %     
+% 
