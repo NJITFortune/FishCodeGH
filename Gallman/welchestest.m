953f181d032c0c7d869f21f5da009930702ad787
Eric Fortune <eric.fortune@njit.edu>
2021-09-22 13:50:08 -0400

    + ‘Gallman/welchestest.m’

---
+++ Gallman/welchestest.m
@@ -0,0 +1,68 @@
+%function [freq, pwr] = k_welches(in, hourperiod, ReFs)
+%in = output from KatieTrialDessembler 
+
+start = kg(1);
+
+hourperiod = 12;
+
+in = KatieTrialTrendDessembler(start, 1, 48);
+
+in = in(1);
+
+hourfreq = in.ld;
+
+%% Run fft (pwelch)
+
+
+%Analysis zAMp
+% %fftmachine
+% f = fftmachine(o.z(1).y - mean(o.z(1).y), ReFs, 3); 
+%pwelch
+L = length(in.SsumfftAmp); 
+NFFT = 2^nextpow2(L)/2;
+%NFFT = 8192;
+FreqRange = 0.002:0.0001:0.2;
+
+
+
+    %generate fft
+    [pxx,pf] = pwelch(in.SsumfftAmp, NFFT, floor(NFFT*0.99), FreqRange, ReFs);  
+    %calculate peak freq
+    [pkAmp1, pkIDX1] = max(pxx);
+    [btAmp1, btIDX1] = min(pxx);
+    pkfrq1 = pf(pkIDX1);
+    
+    
+    %populate values 
+    zwelch = [pxx', pf'];
+    colNames = {'pxx','pfreq'};
+    pw(1).SsumfftAmp = array2table(zwelch,'VariableNames',colNames);
+    
+    %find peak at given frequency
+    range = 0.002; % 
+    xfreq(1) = 1/(2*hourfreq);
+    hourpeak(1) = mean(pw(1).SsumfftAmp.pxx(pw(1).SsumfftAmp.pfreq > (1/(2*hourperiod) - range/2) & pw(1).SsumfftAmp.pfreq < ((1/(2*hourperiod) + range/2))));
+        freq = xfreq(1);
+        pwr = hourpeak(1);
+        
+        
+  %% plot to check range
+  
+  figure(34); clf; hold on;
+  
+  plot(pxx, pf, '-', 'MarkerSize', 3);
+  
+  plot(1/(2*hourperiod), pwr, 'b*', 'MarkerSize', 5);
+  %max power
+  plot(pkfrq1, pkAmp1, 'r*', 'MarkerSize', 5);
+  %ld
+  plot([1/(hourfreq*2) 1/(hourfreq*2)], [btAmp1, pkAmp1], 'k-', 'LineWidth', 0.25);
+  %freq range for hour period
+  plot([(1/(2*hourperiod) - range/2), (1/(2*hourperiod) + range/2), [btAmp1, pkAmp1], 'b-', 'LineWidth', 0.25);
+  
+
+
+
+
+
+
