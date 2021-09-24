6d25f3e1c1f5ef7e7efd0f068025fb0a17186e40
Eric Fortune <eric.fortune@njit.edu>
2021-09-24 12:22:50 -0400

    / ‘Gallman/welchestest.m’

--- Gallman/welchestest.m
+++ Gallman/welchestest.m
@@ -87,7 +87,7 @@ FreqRange = 0.002:0.002:0.2;
 %     
 %somewhere in the middle
 fs = 10; %in Hz - cycles per sec
-nfft = 2*length(in(jj).SsumfftAmp);
+nfft = 6*length(in(jj).SsumfftAmp);
 npts = length(in(jj).SsumfftAmp); %480;
 data = in.SsumfftAmp;
 datalessmean = data - mean(data);
