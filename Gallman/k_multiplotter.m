42d5e1a64f2c0b200b48a3a41694c8f6ff0bbe64
Eric Fortune <eric.fortune@njit.edu>
2021-09-27 11:18:30 -0400

    / ‘Gallman/k_multiplotter.m’

--- Gallman/k_multiplotter.m
+++ Gallman/k_multiplotter.m
@@ -14,10 +14,15 @@ function k_multiplotter(out)
 figure(1); clf; 
 
     
-    ax(1) = subplot(411); hold on; 
-        plot([out(hitube{1}).timcont], [out(hitube{1}).e1hiamp]
-
-        legend('High frequency fish', 'Low frequency fish');
+    ax(1) = subplot(411); hold on; title('High frequency fish');
+        plot([out(hitube{1}).timcont], [out(hitube{1}).e1hiamp], '.');
+        plot([out(hitube{2}).timcont], [out(hitube{2}).e1hiamp], '.');
+        
+    
+    ax(2) = subplot(412); hold on; title('Low frequency fish');
+        plot([out(lotube{1}).timcont], [out(lotube{1}).e1loamp], '.');
+        plot([out(lotube{2}).timcont], [out(lotube{2}).e1loamp], '.');
+    
         
     ax(2) = subplot(412); hold on;
         yyaxis right; plot([out(Hisortidx).timcont], [out(Hisortidx).hifreq], '.'); 
