465b17989f7732a45acd4e0b84f42bc580e37009
Eric Fortune <eric.fortune@njit.edu>
2021-09-27 11:15:34 -0400

    / ‘Gallman/k_multiplotter.m’

--- Gallman/k_multiplotter.m
+++ Gallman/k_multiplotter.m
@@ -1,45 +1,21 @@
 function k_multiplotter(out)
 %% preparations
 %indicies for fish/tube data
-
-    out.idx(1).Hiobw = intube1hi;
-    out.idx(2).Hiobw = intube2hi;
-    out.idx(1).Loobw = intube1lo;
-    out.idx(2).Loobw = intube2lo;
+ 
+    hitube{1} = out.idx(1).Hiobw;
+    hitube{2} = out.idx(2).Hiobw;
     
-    hitube{1} = out.idx(1).Hiobw
+    lotube{1} = out.idx(1).Loobw;
+    lotube{2} = out.idx(2).Loobw;
     
             
-
-
-
-
-
-
-
-% All the data (set because we may want to plot before running KatieRemover and/or KatieLabeler)
-    tto{1} = 1:length([out.e(2).s.timcont]); % tto is indices for obwAmp
-    tto{2} = tto{1};
-
-    ttz{1} = tto{1}; % ttz is indices for zAmp
-    ttz{2} = tto{1};
-
-    ttsf{1} = tto{1}; % ttsf is indices for sumfftAmp
-    ttsf{2} = tto{1};
-    
-% If we have removed outliers via KatieRemover, get the indices...    
-    if ~isempty(out.idx)
-        tto{1} = out.idx(1).obwidx; tto{2} = out.idx(2).obwidx; % tto is indices for obwAmp
-        ttz{1} = out.idx(1).zidx; ttz{2} = out.idx(2).zidx; % ttz is indices for zAmp
-        ttsf{1} = out.idx(1).sumfftidx; ttsf{2} = out.idx(2).sumfftidx; % ttsf is indices for sumfftAmp
-    end
+%% plots
 
 figure(1); clf; 
 
     
     ax(1) = subplot(411); hold on; 
-        yyaxis right; plot([out(], [out.intubeHi], '.');
-        yyaxis left; plot([out.Lotimcont], [out.intubeLo], '.');
+        plot([out(hitube{1}).timcont], 
 
         legend('High frequency fish', 'Low frequency fish');
         
