f717d6adb6e3e074bd759b947d12b9f88f7a57ed
Eric Fortune <eric.fortune@njit.edu>
2021-09-28 10:21:43 -0400

    / ‘Gallman/KatieDayDessembler.m’

--- Gallman/KatieDayDessembler.m
+++ Gallman/KatieDayDessembler.m
@@ -91,10 +91,10 @@ for k = 1:howmanydaysinsample
     %spline
  figure(27); clf; hold on; title('Day average spline');
     
-
+length(day)
 
     for k = 1:length(day)
-        mday(k,:) = zeros(1, length(day.tim));
+        mday(k,:) = zeros(1, length(day(k).tim));
         plot(day(k).tim, day(k).SobwAmp);
         mday(k,:) = mday(k,:) + day(k).SobwAmp;
     end
