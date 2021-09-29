570778507bcc35545535eb5270c5389910d5ded8
Eric Fortune <eric.fortune@njit.edu>
2021-09-29 11:55:16 -0400

    / ‘Gallman/pied.m’

--- Gallman/pied.m
+++ Gallman/pied.m
@@ -7,12 +7,10 @@ figure(314); clf; hold on;
 
     for j = 1:10
 
-       ld(j,:) = pk(j).ld;
-%         crosshour = (2*pi) / (2 * pk(j).ld);
-%         plot(pk(j).timforpi .* crosshour, pk(j).meanoftrialmeans .* crosshour);
-%         plot([pi pi], ylim, 'k-');
-%         legend(num2str(ld));
+          ld(j,:) = pk(j).ld;
+        crosshour = (2*pi) / (2 * pk(j).ld);
+        plot(pk(j).timforpi .* crosshour, pk(j).meanoftrialmeans .* crosshour);
+        plot([pi pi], ylim, 'k-');
+        legend(num2str(ld(j,:)'));
 
     end
-    
-    ld'
