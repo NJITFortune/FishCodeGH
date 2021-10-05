e6e9967d551527269e4e9f3708834c66e5e75847
Eric Fortune <eric.fortune@njit.edu>
2021-10-05 13:45:27 -0400

    / ‘Gallman/k_multiplotter.m’

--- Gallman/k_multiplotter.m
+++ Gallman/k_multiplotter.m
@@ -21,13 +21,13 @@ figure(5); clf;
 
     
     ax(1) = subplot(511); hold on; title('High frequency fish');
-        plot([out(hitube{1}).timcont], [out(hitube{1}).e1hiamp], '.', 'Color', teal);
-        plot([out(hitube{2}).timcont], [out(hitube{2}).e2hiamp], '.', 'Color', blue);
+        plot([out(hitube{1}).timcont]/3600, [out(hitube{1}).e1hiamp], '.', 'Color', teal);
+        plot([out(hitube{2}).timcont]/3600, [out(hitube{2}).e2hiamp], '.', 'Color', blue);
         
     
     ax(2) = subplot(512); hold on; title('Low frequency fish');
-        plot([out(lotube{1}).timcont], [out(lotube{1}).e1loamp], '.','Color', orange);
-        plot([out(lotube{2}).timcont], [out(lotube{2}).e2loamp], '.', 'Color', yellow);
+        plot([out(lotube{1}).timcont]/3600, [out(lotube{1}).e1loamp], '.','Color', orange);
+        plot([out(lotube{2}).timcont]/3600, [out(lotube{2}).e2loamp], '.', 'Color', yellow);
     
         
     ax(3) = subplot(513); hold on; title('Frequency');
@@ -36,10 +36,10 @@ figure(5); clf;
         
     
     ax(4) = subplot(514); hold on; title('Temperature');
-            plot([out.timcont], [out.temp], 'r.');
+            plot([out.timcont]/3600, [out.temp], 'r.');
     
     ax(5) = subplot(515); hold on;
-        plot([out.timcont], [out.light]);
+        plot([out.timcont]/3600, [out.light]);
         ylim([-1, 6]);
         
 linkaxes(ax, 'x');
