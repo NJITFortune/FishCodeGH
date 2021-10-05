a3519c0dee50170770bf9839c2ba7f9a68c64c22
Eric Fortune <eric.fortune@njit.edu>
2021-10-05 16:17:22 -0400

    / ‘Gallman/KseparationNoxious.m’

--- Gallman/KseparationNoxious.m
+++ Gallman/KseparationNoxious.m
@@ -108,7 +108,7 @@ for j=2:length(iFiles)
     minute = str2double(iFiles(j).name(numstart+3:numstart+4));
     second = str2double(iFiles(j).name(numstart+6:numstart+7));
                 
-        if j > 2 && ((hour*60*60) + (minute*60) + second) < out(j-1).tim24
+        if j > 2 && ((hour*60*60) + (minute*60) + second) < out.tim24(j-1)
                daycount = daycount + 1;
         end
     
