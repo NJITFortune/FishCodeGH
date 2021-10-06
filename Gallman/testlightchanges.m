ee3b062565a37eb3babbc52eac3b75862ca9c22e
Eric Fortune <eric.fortune@njit.edu>
2021-10-06 14:40:21 -0400

    / ‘Gallman/testlightchanges.m’

--- Gallman/testlightchanges.m
+++ Gallman/testlightchanges.m
@@ -11,28 +11,28 @@ figure(58); clf; hold on;
 plot([in.timcont]/3600, [in.temp]);
             
             
-% %find idicies where the light changes (threshold of 2.5)            
-% ipt = findchangepts([in.temp], 'MinThreshold', 2.5);
-% 
-% %create luz vector for light change times
-%     %lights on is +
-%     %lights off is -
-% for j = 1:length(ipt)
-%     
-%     changepts(j) = [in(ipt(j)).timcont]/3600;
-%     if [out(ipt(j)).light] < 2.5
-%         changepts(j) = -changepts(j);
-%     end
-%     
-% end
-%     
-% if changepts > 0
-% plot([abs(changepts)' abs(changepts)'], [-1, 6], 'y-');
-% else
-% plot([abs(changepts)' abs(changepts)'], [-1, 6], 'k-');
-% end
-% 
-% out.luz = changepts;
+%find idicies where the light changes (threshold of 2.5)            
+ipt = findchangepts([in.temp], 'MinThreshold', 2.5);
+
+%create luz vector for light change times
+    %lights on is +
+    %lights off is -
+for j = 1:length(ipt)
+    
+    changepts(j) = [in(ipt(j)).timcont]/3600;
+    if [out(ipt(j)).light] < 2.5
+        changepts(j) = -changepts(j);
+    end
+    
+end
+    
+if changepts > 0
+plot([abs(changepts)' abs(changepts)'], [-1, 6], 'y-');
+else
+plot([abs(changepts)' abs(changepts)'], [-1, 6], 'k-');
+end
+
+out.luz = changepts;
 
 
 
