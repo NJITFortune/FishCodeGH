fd1cb3452e067a22fd494354fd9c8ba02060dad2
Eric Fortune <eric.fortune@njit.edu>
2021-10-06 14:41:11 -0400

    / ‘Gallman/testlightchanges.m’

--- Gallman/testlightchanges.m
+++ Gallman/testlightchanges.m
@@ -12,7 +12,7 @@ plot([in.timcont]/3600, [in.temp]);
             
             
 %find idicies where the light changes (threshold of 2.5)            
-ipt = findchangepts([in.temp], 'MinThreshold', 2.5);
+ipt = findchangepts([in.temp], 'MinThreshold', 2.55);
 
 %create luz vector for light change times
     %lights on is +
@@ -20,19 +20,19 @@ ipt = findchangepts([in.temp], 'MinThreshold', 2.5);
 for j = 1:length(ipt)
     
     changepts(j) = [in(ipt(j)).timcont]/3600;
-    if [out(ipt(j)).light] < 2.5
+    if [out(ipt(j)).light] < 2.55
         changepts(j) = -changepts(j);
     end
     
 end
     
 if changepts > 0
-plot([abs(changepts)' abs(changepts)'], [-1, 6], 'y-');
+plot([abs(changepts)' abs(changepts)'], ylim, 'y-');
 else
-plot([abs(changepts)' abs(changepts)'], [-1, 6], 'k-');
+plot([abs(changepts)' abs(changepts)'], ylim, 'k-');
 end
 
-out.luz = changepts;
+%out.luz = changepts;
 
 
 
