e9a575dfde6b822360308fbdb2c1df9e5d6e5228
Eric Fortune <eric.fortune@njit.edu>
2021-10-06 14:29:47 -0400

    / ‘Gallman/testlightchanges.m’

--- Gallman/testlightchanges.m
+++ Gallman/testlightchanges.m
@@ -10,10 +10,16 @@ ipt = findchangepts([out.light], 'MinThreshold', 2.5);
 
 for j = 1:length(ipt)
     
+  
+    
     changepts(j) = [out(ipt(j)).timcont]/3600;
+    
+    if [out(ipt(j)).light] < 2.5
+        changepts(j) = -changepts(j);
+    
 end
     
 
 
-plot([changepts' changepts'], [-1, 6], 'k-');
+plot([abs(changepts)' abs(changepts)'], [-1, 6], 'k-');
 
