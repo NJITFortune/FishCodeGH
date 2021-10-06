283bae5de72d3eeb677932ad193c622d4cbd926d
Eric Fortune <eric.fortune@njit.edu>
2021-10-06 14:13:19 -0400

    / ‘Gallman/testlightchanges.m’

--- Gallman/testlightchanges.m
+++ Gallman/testlightchanges.m
@@ -6,6 +6,6 @@ plot([out.timcont]/3600, [out.light]);
             ylim([-1, 6]);
             
             
-findchangepts([out.light], 'MaxNumChanges', );
+findchangepts([out.light], 'MaxNumChanges', 20);
 
 %plot([out.timcont]/3600, lightchange);
