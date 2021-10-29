599c5b611d7649e972e403ac93d9fdbfd5eed7d6
Kathryn Gallman <keg26@njit.edu>
2021-10-29 17:33:55 -0400

    / ‘Gallman/tempday.m’

--- Gallman/tempday.m
+++ Gallman/tempday.m
@@ -33,7 +33,7 @@ figure(34); clf; hold on;
     plot(timcont, (freq/200)-mean(freq/200), 'c-', 'LineWidth', 2);
     plot(timcont, tempraw-mean(tempraw), 'r-', 'LineWidth', 1);
     plot([temptims temptims], ylim, 'k-', 'LineWidth', 2);
-    xlim([20,120]);
+   % xlim([20,120]);
 
 %% divide into tempdays
 
