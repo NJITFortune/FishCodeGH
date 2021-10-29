0c21b9eda3a58f943d789a1266172f4b9304a01b
Kathryn Gallman <keg26@njit.edu>
2021-10-29 17:35:42 -0400

    / ‘Gallman/k_tempspliner.m’

--- Gallman/k_tempspliner.m
+++ Gallman/k_tempspliner.m
@@ -47,7 +47,7 @@ lighttimeslong = abs(in.info.luz);
 for j = 1:length(lighttimeslesslong)-1
         
         %is there data between j and j+1?    
-       % if ~isempty(find([in.e(1).s(tto{1}).timcont]/(60*60) >= lighttimeslesslong(j) & [in.e(1).s(tto{1}).timcont]/(60*60) < (lighttimeslesslong(j+1)),1))  
+        if ~isempty(find([in.e(1).s(tto{1}).timcont]/(60*60) >= lighttimeslesslong(j) & [in.e(1).s(tto{1}).timcont]/(60*60) < (lighttimeslesslong(j+1)),1))  
             ott = [in.e(1).s(tto{1}).timcont]/(60*60) >= lighttimeslesslong(j) & [in.e(1).s(tto{1}).timcont]/(60*60) < lighttimeslesslong(j+1); 
             lighttim = [in.e(1).s(tto{1}(ott)).timcont]/(60*60);
             
@@ -58,7 +58,7 @@ for j = 1:length(lighttimeslesslong)-1
               % luztimes(j) = in.info.luz(j);
            % end
          
-       % end 
+        end 
 end
 
 
