1be18f43d00691686c137b1f12f4ae7ec8d3dde0
Kathryn Gallman <keg26@njit.edu>
2021-11-01 23:23:40 -0400

    / ‘Gallman/KatieLightDayTrialDessembler.m’

--- Gallman/KatieLightDayTrialDessembler.m
+++ Gallman/KatieLightDayTrialDessembler.m
@@ -2,7 +2,7 @@ function [trial, day] = KatieLightDayTrialDessembler(in, channel,  ReFs)
 %% usage
 %[trial, day] = KatieDayTrialDessembler(kg(#), channel, triallength, ReFs)
 
-clearvars -except kg lightday1
+clearvars -except kg lightday1 dark1
 
 % in = kg(8);
 % channel = 1;
