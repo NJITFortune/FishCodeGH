b855aa7b0e6040d46591bf6272b923bcbf2cd7a8
Eric Fortune <eric.fortune@njit.edu>
2021-11-09 14:20:35 -0500

    / ‘Gallman/k_multiplotter.m’

--- Gallman/k_multiplotter.m
+++ Gallman/k_multiplotter.m
@@ -3,8 +3,8 @@ function k_multiplotter(out)
 
 %outlier removal indicies
 % All the data (set because we may want to plot before running KatieRemover and/or KatieLabeler)
-    tthi = 1:length([out.HiTim]); % tthi is indices for HiAmp
-    ttlo = 1:length([out.LoTim]); % ttlo is indices for LoAmp
+    tthi = 1:length([out.s.HiTim]); % tthi is indices for HiAmp
+    ttlo = 1:length([out.s.LoTim]); % ttlo is indices for LoAmp
 
     
 % If we have removed outliers via KatieRemover, get the indices... 
@@ -28,22 +28,22 @@ orange = [0.8500 0.3250 0.0980];
 figure(66); clf; title('By fish'); hold on;
 
     axs(1) = subplot(511); hold on; title('High frequency fish');
-        plot([out(tthi).HiTim], [out(tthi).HiAmp], '.', 'Color', teal);
+        plot([out.s(tthi).HiTim], [out.s(tthi).HiAmp], '.', 'Color', teal);
         
 
     axs(2) = subplot(512); hold on; title('Low frequency fish');
-        plot([out(ttlo).LoTim], [out(ttlo).LoAmp], '.','Color', orange);
+        plot([out.s(ttlo).LoTim], [out.s(ttlo).LoAmp], '.','Color', orange);
    
 
     axs(3) = subplot(513); hold on; title('Frequency and Temperature');
-        plot([out(tthi).HiTim], [out(tthi).HIfreq], '.','Color', teal); 
-        plot([out(ttlo).LoTim], [out(ttlo).LOfreq], '.','Color', orange); 
+        plot([out.s(tthi).HiTim], [out.s(tthi).HIfreq], '.','Color', teal); 
+        plot([out.s(ttlo).LoTim], [out.s(ttlo).LOfreq], '.','Color', orange); 
         
     axs(4) = subplot(514); hold on; title('Temperature');
-            plot([out.timcont]/3600, [out.temp], 'r.');
+            plot([out.s.timcont]/3600, [out.s.temp], 'r.');
 
     axs(5) = subplot(515); hold on;
-        plot([out.timcont]/3600, [out.light]);
+        plot([out.s.timcont]/3600, [out.s.light]);
         ylim([-1, 6]);
 
         
