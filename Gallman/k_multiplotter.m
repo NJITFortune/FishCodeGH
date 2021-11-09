4413ae82b99c65ef515f95cdc912e391f502b451
Eric Fortune <eric.fortune@njit.edu>
2021-11-09 13:41:34 -0500

    / ‘Gallman/k_multiplotter.m’

--- Gallman/k_multiplotter.m
+++ Gallman/k_multiplotter.m
@@ -1,6 +1,7 @@
 function k_multiplotter(out)
 %% Preparations
 
+%outlier removal indicies
 % All the data (set because we may want to plot before running KatieRemover and/or KatieLabeler)
     tthi = 1:length([out.HiTim]); % tthi is indices for HiAmp
     ttlo = 1:length([out.LoTim]); % ttlo is indices for LoAmp
@@ -8,11 +9,9 @@ function k_multiplotter(out)
     
 % If we have removed outliers via KatieRemover, get the indices...    
     if ~isempty(out.idx)
-        tthi{1} = out.idx.Hiidx; % tthi is indices for HiAmp
-        ttlo{1} = out.idx(1).zidx; ttz{2} = out.idx(2).zidx; % ttz is indices for zAmp
-        ttsf{1} = out.idx(1).sumfftidx; ttsf{2} = out.idx(2).sumfftidx; % ttsf is indices for sumfftAmp
+        tthi = out.idx.Hiidx; % tthi is indices for HiAmp
+        ttlo = out.idx.Loidx; % ttlo is indices for LoAmp
     end
-    %%
     
 %colors
 teal = [0.2 0.8 0.8];
@@ -21,68 +20,23 @@ blue = [0 0.4470 0.7410];
 orange = [0.8500 0.3250 0.0980];
 yellow = [0.9290 0.6940 0.1250];
 
-% %outlier removal indicies
-% 
-% % All the data (set because we may want to plot before running KatieRemover and/or KatieLabeler)
-%     tto{1} = 1:length([out.e(2).s.timcont]); % tto is indices for obwAmp
-%     tto{2} = tto{1};
-% 
-%     ttz{1} = tto{1}; % ttz is indices for zAmp
-%     ttz{2} = tto{1};
-% 
-%     ttsf{1} = tto{1}; % ttsf is indices for sumfftAmp
-%     ttsf{2} = tto{1};
-%     
-% % If we have removed outliers via KatieRemover, get the indices...    
-%     if ~isempty(out.idx)
-%         tto{1} = out.idx(1).obwidx; tto{2} = out.idx(2).obwidx; % tto is indices for obwAmp
-%         ttz{1} = out.idx(1).zidx; ttz{2} = out.idx(2).zidx; % ttz is indices for zAmp
-%         ttsf{1} = out.idx(1).sumfftidx; ttsf{2} = out.idx(2).sumfftidx; % ttsf is indices for sumfftAmp
-%     end
-            
+
 %% plots
 
-figure(5); clf; title('Can we put both tubes together?')
-
-    
-    ax(1) = subplot(511); hold on; title('High frequency fish');
-        plot([out(hitube{1}).timcont]/3600, [out(hitube{1}).e1hiamp], '.', 'Color', teal);
-        plot([out(hitube{2}).timcont]/3600, [out(hitube{2}).e2hiamp], '.', 'Color', blue);
-        
-    
-    ax(2) = subplot(512); hold on; title('Low frequency fish');
-        plot([out(lotube{1}).timcont]/3600, [out(lotube{1}).e1loamp], '.','Color', orange);
-        plot([out(lotube{2}).timcont]/3600, [out(lotube{2}).e2loamp], '.', 'Color', yellow);
-    
-        
-    ax(3) = subplot(513); hold on; title('Frequency');
-        plot([out.Hitimobw], [out.HIfreq], '.','Color', teal); 
-        plot([out.Lotimobw], [out.LOfreq], '.','Color', orange); 
-        
-    
-    ax(4) = subplot(514); hold on; title('Temperature');
-            plot([out.timcont]/3600, [out.temp], 'r.');
-    
-    ax(5) = subplot(515); hold on;
-        plot([out.timcont]/3600, [out.light]);
-        ylim([-1, 6]);
-        
-linkaxes(ax, 'x');
-
-figure(6); clf; title('By fish')
+figure(66); clf; title('By fish'); hold on;
 
         axs(1) = subplot(511); hold on; title('High frequency fish');
-            plot([out.HiTim], [out.HiAmp], '.', 'Color', teal);
+            plot([out(tthi).HiTim], [out(tthi).HiAmp], '.', 'Color', teal);
             
 
 
         axs(2) = subplot(512); hold on; title('Low frequency fish');
-            plot([out.LoTim], [out.LoAmp], '.','Color', orange);
+            plot([out(ttlo).LoTim], [out(ttlo).LoAmp], '.','Color', orange);
        
 
         axs(3) = subplot(513); hold on; title('Frequency');
-            plot([out.HiTim], [out.HIfreq], '.','Color', teal); 
-            plot([out.LoTim], [out.LOfreq], '.','Color', orange); 
+            plot([out(tthi).HiTim], [out(tthi).HIfreq], '.','Color', teal); 
+            plot([out(ttlo).LoTim], [out(ttlo).LOfreq], '.','Color', orange); 
 
 
         axs(4) = subplot(514); hold on; title('Temperature');
