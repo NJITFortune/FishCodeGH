25fb1ce14ef1406bcbd4a76d14672da3d3fa22f3
Eric Fortune <eric.fortune@njit.edu>
2021-11-12 16:35:20 -0500

    / ‘Gallman/KatieSeparationAnxiety.m’

--- Gallman/KatieSeparationAnxiety.m
+++ Gallman/KatieSeparationAnxiety.m
@@ -4,7 +4,7 @@ function out = KatieSeparationAnxiety(userfilespec)
 %see kgme multi
 %% Prep
     Fs = 40000; %sample rate
-    freqs = [300 550]; %freq range of typical eigen EOD
+    freqs = [300 600]; %freq range of typical eigen EOD
     %userfilespec = 'Eigen*'; %file names
     numstart = 23; %1st position in file name of time stamp
     
