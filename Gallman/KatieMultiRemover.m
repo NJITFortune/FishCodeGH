91499b72893ffdbf2ec789ba7390e9b7f4d4aa41
Eric Fortune <eric.fortune@njit.edu>
2021-11-12 16:13:15 -0500

    / ‘Gallman/KatieMultiRemover.m’

--- Gallman/KatieMultiRemover.m
+++ Gallman/KatieMultiRemover.m
@@ -18,7 +18,7 @@ figure(1); clf;
     drawnow; 
     
     for j=1:length(in.his)
-         if [in(j).his.HiAmp] > cutofffreqL & [in(j).his.HiAmp] < cutofffreqH
+         if in(j).his.HiAmp > cutofffreqL & in(j).his.HiAmp < cutofffreqH
              out(j).Hiidx = j;
          end
     end
@@ -42,7 +42,7 @@ figure(1); clf;
     plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
     drawnow; 
     for j=1:length(in.los)
-         if [in(j).los.LoAmp] > cutofffreqL & [in(j).los.LoAmp] < cutofffreqH
+         if in(j).los.LoAmp > cutofffreqL & in(j).los.LoAmp < cutofffreqH
              out(j).Loidx = j;
          end
     end
