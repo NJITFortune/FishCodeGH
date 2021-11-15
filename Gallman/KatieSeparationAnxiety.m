6d1bc65a532445adb6f1574f609a7adf7216a4b4
Eric Fortune <eric.fortune@njit.edu>
2021-11-15 16:22:52 -0500

    / ‘Gallman/KatieSeparationAnxiety.m’

--- Gallman/KatieSeparationAnxiety.m
+++ Gallman/KatieSeparationAnxiety.m
@@ -4,7 +4,7 @@ function out = KatieSeparationAnxiety(userfilespec)
 %see kgme multi
 %% Prep
     Fs = 40000; %sample rate
-    freqs = [400 750]; %freq range of typical eigen EOD
+    freqs = [400 650]; %freq range of typical eigen EOD
     %userfilespec = 'Eigen*'; %file names
     numstart = 23; %1st position in file name of time stamp
     
@@ -149,10 +149,10 @@ for j=2:length(iFiles)
 
 % Set current frequencies
 
-% currhifreq = tmphifreq2;         
-% currlofreq = tmplofreq2;  
-       currhifreq = mean([tmphifreq1 tmphifreq2]);        
-       currlofreq = mean([tmplofreq1 tmplofreq2]);        
+currhifreq = tmphifreq2;         
+currlofreq = tmplofreq2;  
+%        currhifreq = mean([tmphifreq1 tmphifreq2]);        
+%        currlofreq = mean([tmplofreq1 tmplofreq2]);        
 
         
         
