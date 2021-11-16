e245182c6d51db7585544230a0e93dac75a5baa9
Eric Fortune <eric.fortune@njit.edu>
2021-11-16 15:19:37 -0500

    / ‘Gallman/KatieMultifftDayTrialDessembler.m’

--- Gallman/KatieMultifftDayTrialDessembler.m
+++ Gallman/KatieMultifftDayTrialDessembler.m
@@ -15,6 +15,9 @@ clearvars -except kg kg2
 
 in = kg2(1);
 ReFs = 10;
+light = 3;
+fishfreq = 1;
+
 
 %% prep
 
@@ -38,50 +41,62 @@ ld = in.info.ld; % Whatever - ld is shorter than in.info.ld
     %starts with dark = 3
     %starts with light = 4
 
-[hixx, loxx, HiAmp, HiTim, LoAmp, LoTim, Hiyy, Hiuntyy, Loyy, Lountyy, Hilighttimes, Lolighttimes] =  k_multifftsubspliner(in, ReFs, 3);
+figure(87); clf; hold on;
+
+    if fishfreq < 2 %we are looking at data from the high frequency fish
+
+        %get data
+        [hixx, ~, HiAmp, HiTim, ~, ~, Hiyy, Hiuntyy,~, ~, Hilighttimes, ~] =  k_multifftsubspliner(in, ReFs, light);
 
 
-%Make a time base that starts and ends on lighttimes 
-    %necessary to define length of raw data
+        %Make a time base that starts and ends on lighttimes 
+        %necessary to define length of raw data
+        Hitimcont = HiTim(HiTim >= Hilighttimes(1) & HiTim <= Hilighttimes(end));
+        HiAmp = HiAmp(HiTim >= Hilighttimes(1) & HiTim <= Hilighttimes(end));
+
+        %plot spline fit without detrending to view accuracy
+        plot(Hitimcont, HiAmp, '.', 'MarkerSize', 3);
+        plot(hixx, Hiuntyy, '-', 'LineWidth', 2);
+        for j = 1:length(Hilighttimes)
+            plot([Hilighttimes(j), Hilighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
+        end
+
+    
+    else %we are looking at data from the low frequency fish
+    
+        [~, loxx, ~, ~, LoAmp, LoTim, ~, ~, Loyy, Lountyy, ~, Lolighttimes] =  k_multifftsubspliner(in, ReFs, light);
+    
+        Lotimcont = LoTim(LoTim >= Lolighttimes(1) & LoTim <= Lolighttimes(end));
+        LoAmp = LoAmp(LoTim >= Lolighttimes(1) & LoTim <= Lolighttimes(end));
+
+        plot(Lotimcont, LoAmp, '.', 'MarkerSize', 3);
+        plot(loxx, Lountyy,'-', 'LineWidth', 2);
+        clear j;
+        for j = 1:length(Lolighttimes)
+            plot([Lolighttimes(j), Lolighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
+        end
+
+    end
 
    
-    Hitimcont = HiTim(HiTim >= Hilighttimes(1) & HiTim <= Hilighttimes(end));
-    HiAmp = HiAmp(HiTim >= Hilighttimes(1) & HiTim <= Hilighttimes(end));
-    Lotimcont = LoTim(LoTim >= Lolighttimes(1) & LoTim <= Lolighttimes(end));
-    LoAmp = LoAmp(LoTim >= Lolighttimes(1) & LoTim <= Lolighttimes(end));
-
-
-%spline fit test plot
-figure(87); clf; hold on;
-  subplot(211); title('High frequency fish'); hold on; 
-    plot(Hitimcont, HiAmp, '.', 'MarkerSize', 3);
-    plot(hixx, Hiuntyy, '-', 'LineWidth', 2);
-    for j = 1:length(Hilighttimes)
-        plot([Hilighttimes(j), Hilighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
-    end
-
-  subplot(212); title('Low frequency fish'); hold on;
-    plot(Lotimcont, LoAmp, '.', 'MarkerSize', 3);
-    plot(loxx, Lountyy,'-', 'LineWidth', 2);
-    clear j;
-    for j = 1:length(Lolighttimes)
-        plot([Lolighttimes(j), Lolighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
-    end
+    
+  
+    
  
 %% Define trial period
 
     % How many trials available?
-    HilengthofsampleHOURS = Hilighttimes(end) - Hilighttimes(1); 
-    LolengthofsampleHOURS = Lolighttimes(end) - Lolighttimes(1); 
-
-    % How many integer trials in dataset
-    Hinumotrials = floor(HilengthofsampleHOURS / triallength); % of trials
-    Lonumotrials = floor(LolengthofsampleHOURS / triallength); % of trials
+   
+  s
 
 %% Divide data into trials
 
+if fishfreq < 2 %high frequency fish
 
-    %high frequency fish
+ %define trial period   
+ HilengthofsampleHOURS = Hilighttimes(end) - Hilighttimes(1); % How many trials available?
+ Hinumotrials = floor(HilengthofsampleHOURS / triallength); %# of trials
+    
     for jj = 1:Hinumotrials
         %raw data
                 % indices for our sample window of perd hours
@@ -116,7 +131,14 @@ figure(87); clf; hold on;
     
     end   
 
+else 
     %low frequency fish
+
+    %define trial period 
+    LolengthofsampleHOURS = Lolighttimes(end) - Lolighttimes(1); % How many trials available
+    % How many integer trials in dataset 
+    Lonumotrials = floor(LolengthofsampleHOURS / triallength); % of trial
+
     clear jj;
     for jj = 1:Lonumotrials
         
@@ -153,85 +175,88 @@ figure(87); clf; hold on;
                 end
     
     end   
-
+end
 
 %% divide trials into days
 
-%high frequency fish
-clear jj;
-    for jj = length(hout):-1:1 % For each trial
-        
-        ld = hout(jj).ld;
 
-        % Divide by daylength to get the number of days in the trial
-        howmanydaysintrial = floor(triallength / (ld*2));
-        % This is the number of sample in a day
-        howmanysamplesinaday = ld * 2 * ReFs;
-
-        for k = 1:howmanydaysintrial % Each day in a trial
-
-
-            % Get the index of the start time of the trial
-            hidayidx = find(hout(jj).HiStimcont > (k-1) * (ld*2), 1) -1; % k-1 so that we start at zero
-
-            % Get the datums
-            %trial(jj).day(k).SobwAmp = out(jj).SobwAmp(dayidx:dayidx+howmanysamplesinaday-1);
-%             trial(jj).day(k).SzAmp = out(jj).SzAmp(dayidx:dayidx+howmanysamplesinaday-1);
-            hitrial(jj).day(k).HiSAmp = hout(jj).HiSAmp(hidayidx:hidayidx+howmanysamplesinaday-1);
-%             
-               hitrial(jj).ld = in.info.ld; 
-
-           
-
-        end
-            % Make a time sequence for the datums (easier than extracting from
-            % xx...)
-            hitrial(jj).tim = 1/ReFs:1/ReFs:howmanysamplesinaday/ReFs;
-
-    end
+if fishfreq < 2 %high frequency fish
+    %high frequency fish
+    clear jj;
+        for jj = length(hout):-1:1 % For each trial
+            
+            ld = hout(jj).ld;
     
-
-%low frequency fish
-clear jj;
-    for jj = length(lout):-1:1 % For each trial
-        
-        ld = lout(jj).ld;
-
-        % Divide by daylength to get the number of days in the trial
-        howmanydaysintrial = floor(triallength / (ld*2));
-        % This is the number of sample in a day
-        howmanysamplesinaday = ld * 2 * ReFs;
-
-        for k = 1:howmanydaysintrial % Each day in a trial
-
-
-            % Get the index of the start time of the trial
-            lodayidx = find(hout(jj).HiStimcont > (k-1) * (ld*2), 1) -1; % k-1 so that we start at zero
-
-            % Get the datums
-            %trial(jj).day(k).SobwAmp = out(jj).SobwAmp(dayidx:dayidx+howmanysamplesinaday-1);
-%             trial(jj).day(k).SzAmp = out(jj).SzAmp(dayidx:dayidx+howmanysamplesinaday-1);
-            lotrial(jj).day(k).LoSAmp = lout(jj).LoSAmp(lodayidx:lodayidx+howmanysamplesinaday-1);
-%             
-               lotrial(jj).ld = in.info.ld; 
-
-           
-
-        end
-            % Make a time sequence for the datums (easier than extracting from
-            % xx...)
-            lotrial(jj).tim = 1/ReFs:1/ReFs:howmanysamplesinaday/ReFs;
-
-    end
-
+            % Divide by daylength to get the number of days in the trial
+            howmanydaysintrial = floor(triallength / (ld*2));
+            % This is the number of sample in a day
+            howmanysamplesinaday = ld * 2 * ReFs;
     
+            for k = 1:howmanydaysintrial % Each day in a trial
+    
+    
+                % Get the index of the start time of the trial
+                hidayidx = find(hout(jj).HiStimcont > (k-1) * (ld*2), 1) -1; % k-1 so that we start at zero
+    
+                % Get the datums
+                %trial(jj).day(k).SobwAmp = out(jj).SobwAmp(dayidx:dayidx+howmanysamplesinaday-1);
+    %             trial(jj).day(k).SzAmp = out(jj).SzAmp(dayidx:dayidx+howmanysamplesinaday-1);
+                trial(jj).day(k).HiSAmp = hout(jj).HiSAmp(hidayidx:hidayidx+howmanysamplesinaday-1);
+    %             
+                   trial(jj).ld = in.info.ld; 
+    
+               
+    
+            end
+                % Make a time sequence for the datums (easier than extracting from
+                % xx...)
+                trial(jj).tim = 1/ReFs:1/ReFs:howmanysamplesinaday/ReFs;
+    
+        end
+        
+else    
+    %low frequency fish
+    clear jj;
+        for jj = length(lout):-1:1 % For each trial
+            
+            ld = lout(jj).ld;
+    
+            % Divide by daylength to get the number of days in the trial
+            howmanydaysintrial = floor(triallength / (ld*2));
+            % This is the number of sample in a day
+            howmanysamplesinaday = ld * 2 * ReFs;
+    
+            for k = 1:howmanydaysintrial % Each day in a trial
+    
+    
+                % Get the index of the start time of the trial
+                lodayidx = find(hout(jj).HiStimcont > (k-1) * (ld*2), 1) -1; % k-1 so that we start at zero
+    
+                % Get the datums
+                %trial(jj).day(k).SobwAmp = out(jj).SobwAmp(dayidx:dayidx+howmanysamplesinaday-1);
+    %             trial(jj).day(k).SzAmp = out(jj).SzAmp(dayidx:dayidx+howmanysamplesinaday-1);
+                trial(jj).day(k).LoSAmp = lout(jj).LoSAmp(lodayidx:lodayidx+howmanysamplesinaday-1);
+    %             
+                   trial(jj).ld = in.info.ld; 
+    
+               
+    
+            end
+                % Make a time sequence for the datums (easier than extracting from
+                % xx...)
+                trial(jj).tim = 1/ReFs:1/ReFs:howmanysamplesinaday/ReFs;
+    
+        end
+
+end    
     
  %% plot to check
 
  %trials across tims
  figure(26); clf; title('trials across time');  hold on;
+
+ if fishfreq < 2 %high frequency fish
  
- %ax(1) = subplot(211); title('high frequency fish'); 
     clear jj;
     for jj = 1:length(hout)
         
@@ -245,39 +270,40 @@ clear jj;
         plot([Hilighttimes(j), Hilighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
     end
  
-%  ax(2) = subplot(212); title('low frequency fish');
-%     clear jj;
-%     for jj = 1:length(lout)
-%         
-%         plot(lout(jj).Loentiretimcont, lout(jj).LofftAmp, '.', 'MarkerSize', 3);
-%         plot(lout(jj).LoSentiretimcont, lout(jj).LoSAmp, '-', 'LineWidth', 3);
-%         
-%     end
-%      clear j;
-%     for j = 1:length(Lolighttimes)
-%         
-%         plot([Lolighttimes(j), Lolighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
-%     end
-% 
-%  linkaxes(ax, 'x');
+ else %low frequecy fish 
+    clear jj;
+    for jj = 1:length(lout)
+        
+        plot(lout(jj).Loentiretimcont, lout(jj).LofftAmp, '.', 'MarkerSize', 3);
+        plot(lout(jj).LoSentiretimcont, lout(jj).LoSAmp, '-', 'LineWidth', 3);
+        
+    end
+     clear j;
+    for j = 1:length(Lolighttimes)
+        
+        plot([Lolighttimes(j), Lolighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
+    end
+
+ end
 
  clear himday;
- 
+
+ if fishfreq < 2 %high frequency fish
  %all days
  %average day by trial
  %high frequency fish
  figure(27); clf;title('Day average by trial High frequency fish'); hold on; 
  clear jj;
-    for jj=1:length(hitrial) 
+    for jj=1:length(trial) 
 
         %create temporary vector to calculate mean by trial
-        himday(jj,:) = zeros(1, length(hitrial(jj).tim));
+        himday(jj,:) = zeros(1, length(trial(jj).tim));
 
         clear k;
-        for k = 1:length(hitrial(jj).day)
+        for k = 1:length(trial(jj).day)
 
                 %fill temporary vector with data from each day 
-                himday(jj,:) = himday(jj,:) + hitrial(jj).day(k).HiSAmp;
+                himday(jj,:) = himday(jj,:) + trial(jj).day(k).HiSAmp;
                 subplot(211); hold on; title('Days');
                 plot(hitrial(jj).tim, hitrial(jj).day(k).HiSAmp);
                 plot([ld ld], ylim, 'k-', 'LineWidth', 1);
@@ -285,9 +311,9 @@ clear jj;
         end
 
          % To get average across days, divide by number of days
-            himday(jj,:) = himday(jj,:) / length(hitrial(jj).day);
+            himday(jj,:) = himday(jj,:) / length(trial(jj).day);
             subplot(212); hold on; title('Day average by trial hi frequency fish');
-            plot(hitrial(jj).tim, himday(jj,:), '-', 'Linewidth', 1);
+            plot(trial(jj).tim, himday(jj,:), '-', 'Linewidth', 1);
             plot([ld ld], ylim, 'k-', 'LineWidth', 1);
 
     end
@@ -296,29 +322,31 @@ clear jj;
  
     subplot(212); hold on;
      himeanofmeans = mean(himday); % Takes the mean of the means for a day from each trial 
-    plot(hitrial(jj).tim, himeanofmeans, 'k-', 'LineWidth', 3);
-    
+    plot(trial(jj).tim, himeanofmeans, 'k-', 'LineWidth', 3);
+ 
+ else
+
 %low frequency fish
  figure(28); clf; title('Day average by trial Low frequency fish'); hold on; 
  clear jj;
-    for jj=1:length(lotrial) 
+    for jj=1:length(trial) 
 
         %create temporary vector to calculate mean by trial
-        lomday(jj,:) = zeros(1, length(lotrial(jj).tim));
+        lomday(jj,:) = zeros(1, length(trial(jj).tim));
 
         clear k;
         for k = 1:length(lotrial(jj).day)
 
                 %fill temporary vector with data from each day 
-                lomday(jj,:) = lomday(jj,:) + lotrial(jj).day(k).LoSAmp;
+                lomday(jj,:) = lomday(jj,:) + trial(jj).day(k).LoSAmp;
                 subplot(211); hold on; title('Days');
-                plot(lotrial(jj).tim, lotrial(jj).day(k).LoSAmp);
+                plot(trial(jj).tim, trial(jj).day(k).LoSAmp);
                 plot([ld ld], ylim, 'k-', 'LineWidth', 1);
 
         end
 
          % To get average across days, divide by number of days
-            lomday(jj,:) = lomday(jj,:) / length(lotrial(jj).day);
+            lomday(jj,:) = lomday(jj,:) / length(trial(jj).day);
             subplot(212); hold on; title('Day average by trial hi frequency fish');
             plot(lotrial(jj).tim, lomday(jj,:), '-', 'Linewidth', 1);
             plot([ld ld], ylim, 'k-', 'LineWidth', 1);
@@ -329,15 +357,15 @@ clear jj;
  
     subplot(212); hold on;
      lomeanofmeans = mean(lomday); % Takes the mean of the means for a day from each trial 
-    plot(lotrial(jj).tim, lomeanofmeans, 'k-', 'LineWidth', 3);
-   
+    plot(trial(jj).tim, lomeanofmeans, 'k-', 'LineWidth', 3);
+ end   
     
-figure(29); clf; title('trial day means for both fish'); hold on; 
-
-    plot(hitrial(jj).tim, himeanofmeans);
-    plot(lotrial(jj).tim, lomeanofmeans);
-    plot([ld ld], ylim, 'k-', 'LineWidth', 1);
-    legend('high frequency fish', 'low frequency fish');
-     legend('boxoff')
+% figure(29); clf; title('trial day means for both fish'); hold on; 
+% 
+%     plot(hitrial(jj).tim, himeanofmeans);
+%     plot(lotrial(jj).tim, lomeanofmeans);
+%     plot([ld ld], ylim, 'k-', 'LineWidth', 1);
+%     legend('high frequency fish', 'low frequency fish');
+%      legend('boxoff')
 % 
 % 
