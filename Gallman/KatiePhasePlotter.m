b286e236ce9674e9410acc00d31383e78a55a852
Eric Fortune <eric.fortune@njit.edu>
2021-12-03 11:26:27 -0500

    / ‘Gallman/KatiePhasePlotter.m’

--- Gallman/KatiePhasePlotter.m
+++ Gallman/KatiePhasePlotter.m
@@ -30,7 +30,7 @@ ReFs = 10;
     
  
 %% plots
-
+%all together
 figure(42); clf; title('phase plots by fish'); hold on;
 
     for k = 1:length(one)
@@ -89,7 +89,65 @@ figure(42); clf; title('phase plots by fish'); hold on;
 linkaxes(ax, 'x');
 
 
+%by experiment
+%exp 1
+figure(43); clf; title('phase plots by fish'); hold on;
+clear k;
+clear ax;
+    for k = 1:4
+        
+       ax(k) = subplot(6,1,k);hold on; 
+               %plot raw data for each fish
+               plot(one(k).timcont, one(k).fft, '.');
+               %plot spline for each fish
+               plot(one(k).xx, one(k).fftyy, 'LineWidth', 2);
+               %add light transistion times as vertical lines
+               clear j;
+               for j = 1:length(one(k).lighttimes)
+                   
+                   plot([one(k).lighttimes(j), one(k).lighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
+                   
+               end
+                
+    end
+clear kk;    
+    for kk = 1:1
+
+    
+        
+        ax(k +kk) = subplot(6,1,(k +kk)); hold on;
+
+                    plot(two(kk).HiTim, two(kk).HiAmp, '.');
+                    plot(two(kk).hixx, two(kk).Hifftyy,  'LineWidth', 2);
+                    clear j;
+                    for j = 1:length(two(kk).Hilighttimes)
+
+                        plot([two(kk).Hilighttimes(j), two(kk).Hilighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
+                    end
+
+        
+    end
+    clear kkk;
+    for kkk = 1:1
+        
+        a = (k + kk + kkk);
+        
+        ax(k+ kk + kkk) = subplot(6,1,a); hold on;
+
+                        plot(two(kk).LoTim, two(kk).LoAmp, '.');
+                        plot(two(kkk).loxx, two(kkk).Lofftyy, 'LineWidth', 2);
+                        
+                        clear j;
+                        for j = 1:length(two(kkk).Lolighttimes)
+                            
+                            plot([two(kkk).Lolighttimes(j), two(kkk).Lolighttimes(j)], ylim, 'k-', 'LineWidth', 0.5);
+                            
+                        end
                     
+    end
+%     
+ 
+linkaxes(ax, 'x');
                     
                     
                     
