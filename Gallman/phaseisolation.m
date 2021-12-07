9d611aa4d4fbf9f1af9f6017194b66dd145c2afe
Eric Fortune <eric.fortune@njit.edu>
2021-12-07 12:30:42 -0500

    / ‘Gallman/phaseisolation.m’

--- Gallman/phaseisolation.m
+++ Gallman/phaseisolation.m
@@ -1,4 +1,4 @@
-
+%% single fish test case
 in = kg(64);
 channel = 1; 
 ReFs = 10;
@@ -21,3 +21,50 @@ for k = 1:length(lesslight)
     plot([lesslight(k), lesslight(k)], ylim, 'k-');
     
 end
+
+%% multiple fish
+light = 3;
+channel = 1;
+ReFs = 10;
+
+%% kg data index
+ exp1idx = [64 65 66 67];
+ 
+ %get the spline estimates for each sample
+    for k = 1:length(exp1idx)
+
+   
+    %[one(k).xx, one(k).fftyy, one(k).lighttimes] =  k_fftsubspliner(kg(onefish124idx(k)), channel, ReFs, light);
+    [one(k).xx, one(k).fftyy, one(k).lighttimes] =  k_fftsubspliner(kg(exp1idx(k)), channel, ReFs, light);
+
+    %first transistion idx
+    twidx1 = find(one(k).xx >= one(k).lighttimes(3) & one(k).xx <= (one(k).lighttimes(3) + 48));
+    twlightidx = find(one(k).lighttimes >= one(k).lighttimes(3) & one(k).lighttimes <= (one(k).lighttimes(3) + 48));
+    
+
+    one(k).lighttimes1 = one(k).lighttimes(twlightidx);
+    one(k).xx1 = one(k).xx(twidx1);
+    one(k).fftyy1 = one(k).fftyy(twidx1);
+
+    %raw data for plotting/spline check
+    one(k).timcont = [kg(exp1idx(k)).e(1).s(twidx1).timcont]/3600;
+    one(k).fft = [kg(exp1idx(k)).e(1).s(twidx1).sumfftAmp];
+
+
+    end
+
+%% plots
+
+figure(24); clf; hold on;
+    for k = 1:length(one)
+    plot(one(k).xx1, one(k).fftyy1);
+
+
+for j = 1:length(one(1).lighttimes1)
+    plot([lesslight(j), lesslight(j)], ylim, 'k-');
+    
+end
+
+
+
+   
