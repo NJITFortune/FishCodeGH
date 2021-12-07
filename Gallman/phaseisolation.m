1df2f9124e5362fd8c90a10b1b5a6ff896f1cbc6
Kathryn Gallman <keg26@njit.edu>
2021-12-06 19:54:53 -0500

    / ‘Gallman/phaseisolation.m’

--- Gallman/phaseisolation.m
+++ Gallman/phaseisolation.m
@@ -9,16 +9,14 @@ light = 3;
 
 
 
-    
-    
-        twidx1 = find(xx >= lighttimes(3) & xx <= (lighttimes(3) + 48));
-
+twidx1 = find(xx >= lighttimes(3) & xx <= (lighttimes(3) + 48));
+twlightidx = find(lighttimes >= lighttimes(3) & lighttimes <= (lighttimes(3) + 48));
     
 
 
 figure(23); clf; hold on;
 plot(xx(twidx1), fftyy(twidx1));
-lesslight = lighttimes(twidx1);
+lesslight = lighttimes(twlightidx);
 for k = 1:length(lesslight)
     plot([lesslight(k), lesslight(k)], ylim, 'k-');
     
