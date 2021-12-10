7c80e663ecc24f420bce1a711d102265834ae1c2
Kathryn Gallman <keg26@njit.edu>
2021-12-10 11:49:51 -0500

    / ‘Gallman/phaseisolation.m’

--- Gallman/phaseisolation.m
+++ Gallman/phaseisolation.m
@@ -1,35 +1,15 @@
 clearvars -except kg kg2 cc
-%% single fish test case
-in = kg(64);
-channel = 1; 
-ReFs = 10;
-light = 3;
 
-[xx, fftyy, lighttimes] =  k_fftsubspliner(in, channel, ReFs, light);
-
-
-
-
-twidx1 = find(xx >= lighttimes(3) & xx <= (lighttimes(3) + 48));
-twlightidx = find(lighttimes >= lighttimes(3) & lighttimes <= (lighttimes(3) + 48));
-    
-
-
-figure(23); clf; hold on;
-plot(xx(twidx1), fftyy(twidx1));
-lesslight = lighttimes(twlightidx);
-for k = 1:length(lesslight)
-    plot([lesslight(k), lesslight(k)], ylim, 'k-');
-    
-end
-
-%% run for real...
+%% make a function?
 light = 3;
 channel = 1;
 ReFs = 10;
 
 %% kg data index - single fish
 exp3idx = [72 74]; %RIP Paco and Cheshire
+
+
+%%
 
  clear k;
  %get the spline estimates for each sample
