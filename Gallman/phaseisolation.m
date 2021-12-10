d48447efb691eb10d00c6f0bb280a6c99f9e557b
Kathryn Gallman <keg26@njit.edu>
2021-12-10 12:08:12 -0500

    / ‘Gallman/phaseisolation.m’

--- Gallman/phaseisolation.m
+++ Gallman/phaseisolation.m
@@ -17,7 +17,7 @@ exp1idxtwL = [64 66 67]; %65 is too short for easy coding - still good data - us
 exp2idxtwD =   [68 69 70];  
     exp2idx = [68 69 70 71]; %71 is too short for easy coding
 
-    exp1xpoint = [42 198 114 294];
+    exp1xpoint = [186 30];
 
 exp3idx = [72 74]; %RIP Paco and Cheshire
 % %1st transistion
