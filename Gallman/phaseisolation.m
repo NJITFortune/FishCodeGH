5bddb1c3aeaed302ff2d0ad7ddacab081c9812f9
Kathryn Gallman <keg26@njit.edu>
2021-12-13 11:41:40 -0500

    / ‘Gallman/phaseisolation.m’

--- Gallman/phaseisolation.m
+++ Gallman/phaseisolation.m
@@ -210,7 +210,7 @@ figure(26); clf; hold on;
        
                 plot(dos(k).timcont1, dos(k).fft1, '.', 'Color', turq);
                 plot(dos(k).xx1, dos(k).fftyy1, 'LineWidth', 3, 'Color', mediumV);
-        
+        clear j;
         for j = 1:length(dos(k).lighttimes1)
                    
                    plot([dos(k).lighttimes1(j), dos(k).lighttimes1(j)], ylim, 'k-', 'LineWidth', 0.5);
