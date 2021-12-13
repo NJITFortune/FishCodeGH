9c5743eebea5b438e241c4bcbcc8908e170dd073
Kathryn Gallman <keg26@njit.edu>
2021-12-13 10:52:08 -0500

    / ‘Gallman/k_multiphaselaser.m’

--- Gallman/k_multiphaselaser.m
+++ Gallman/k_multiphaselaser.m
@@ -13,11 +13,11 @@ function multi = k_multiphaselaser(expxpoint, kk, ReFs, light, kg2)
         twlightidx1 = find(Hilighttimes >= xpoint1 & Hilighttimes <= (xpoint1 + 48));
        
         
-        multi(k).Hilighttimes1 = Hilighttimes(twlightidx1);
-        multi(k).hixx1 = hixx(twidx1);
-        multi(k).Hifftyy1 = Hifftyy(twidx1);
-        multi(k).Lofftyy1 = Lofftyy(twidx1);
-        multi(k).loxx1 = loxx(twidx1);
+        multi.Hilighttimes1 = Hilighttimes(twlightidx1);
+        multi.hixx1 = hixx(twidx1);
+        multi.Hifftyy1 = Hifftyy(twidx1);
+        multi.Lofftyy1 = Lofftyy(twidx1);
+        multi.loxx1 = loxx(twidx1);
     
          
         %raw data for plotting/spline check
@@ -25,12 +25,12 @@ function multi = k_multiphaselaser(expxpoint, kk, ReFs, light, kg2)
        hitimidx1 = find(HiTim >= xpoint1 & HiTim <= (xpoint1 + 48));
        lotimidx1 = find(LoTim >= xpoint1 & LoTim <= (xpoint1 + 48));
       
-        multi(k).HiAmp = HiAmp;
-        multi(k).HiAmp1 = HiAmp(hitimidx1);
-        multi(k).LoAmp1 = LoAmp(lotimidx1);
-        multi(k).LoAmp = LoAmp;
-        multi(k).HiTim1 = HiTim(hitimidx1);
-        multi(k).LoTim1 = LoTim(lotimidx1);
+        multi.HiAmp = HiAmp;
+        multi.HiAmp1 = HiAmp(hitimidx1);
+        multi.LoAmp1 = LoAmp(lotimidx1);
+        multi.LoAmp = LoAmp;
+        multi.HiTim1 = HiTim(hitimidx1);
+        multi.LoTim1 = LoTim(lotimidx1);
     
      %third transition - expecting 12 hours light, get 4
        xpoint2 = expxpoint(2);
@@ -39,11 +39,11 @@ function multi = k_multiphaselaser(expxpoint, kk, ReFs, light, kg2)
         twlightidx2 = find(Hilighttimes >= xpoint2 & Hilighttimes <= (xpoint2 + 48));
        
         
-        multi(k).Hilighttimes2 = Hilighttimes(twlightidx2);
-        multi(k).hixx2 = hixx(twidx2);
-        multi(k).Hifftyy2 = Hifftyy(twidx2);
-        multi(k).Lofftyy2 = Lofftyy(twidx2);
-        multi(k).loxx2 = loxx(twidx2);
+        multi.Hilighttimes2 = Hilighttimes(twlightidx2);
+        multi.hixx2 = hixx(twidx2);
+        multi.Hifftyy2 = Hifftyy(twidx2);
+        multi.Lofftyy2 = Lofftyy(twidx2);
+        multi.loxx2 = loxx(twidx2);
     
          
         %raw data for plotting/spline check
@@ -52,11 +52,11 @@ function multi = k_multiphaselaser(expxpoint, kk, ReFs, light, kg2)
        lotimidx2 = find(LoTim >= xpoint2 & LoTim <= (xpoint2 + 48));
       
       
-        multi(k).HiAmp2 = HiAmp(hitimidx2);
-        multi(k).LoAmp2 = LoAmp(lotimidx2);
+        multi.HiAmp2 = HiAmp(hitimidx2);
+        multi.LoAmp2 = LoAmp(lotimidx2);
   
-        multi(k).HiTim2 = HiTim(hitimidx2);
-        multi(k).LoTim2 = LoTim(lotimidx2);
+        multi.HiTim2 = HiTim(hitimidx2);
+        multi.LoTim2 = LoTim(lotimidx2);
     
    %four hour to twelve hour transitions 
      %second transition - expecting 4 of dark and get 12
@@ -65,11 +65,11 @@ function multi = k_multiphaselaser(expxpoint, kk, ReFs, light, kg2)
         twlightidx3 = find(Hilighttimes >= xpoint3 & Hilighttimes <= (xpoint3 + 48));
        
         
-        multi(k).Hilighttimes3 = Hilighttimes(twlightidx3);
-        multi(k).hixx3 = hixx(twidx3);
-        multi(k).Hifftyy3 = Hifftyy(twidx3);
-        multi(k).Lofftyy3 = Lofftyy(twidx3);
-        multi(k).loxx3 = loxx(twidx3);
+        multi.Hilighttimes3 = Hilighttimes(twlightidx3);
+        multi.hixx3 = hixx(twidx3);
+        multi.Hifftyy3 = Hifftyy(twidx3);
+        multi.Lofftyy3 = Lofftyy(twidx3);
+        multi.loxx3 = loxx(twidx3);
     
          
         %raw data for plotting/spline check
@@ -78,11 +78,11 @@ function multi = k_multiphaselaser(expxpoint, kk, ReFs, light, kg2)
        lotimidx3 = find(LoTim >= xpoint3 & LoTim <= (xpoint3 + 48));
       
       
-        multi(k).HiAmp3 = HiAmp(hitimidx3);
-        multi(k).LoAmp3 = LoAmp(lotimidx3);
+        multi.HiAmp3 = HiAmp(hitimidx3);
+        multi.LoAmp3 = LoAmp(lotimidx3);
   
-        multi(k).HiTim3 = HiTim(hitimidx3);
-        multi(k).LoTim3 = LoTim(lotimidx3);
+        multi.HiTim3 = HiTim(hitimidx3);
+        multi.LoTim3 = LoTim(lotimidx3);
     
       %fourth transition - 4 hours to 12 hours
         %expecting 4 hours light and get 12
@@ -91,11 +91,11 @@ function multi = k_multiphaselaser(expxpoint, kk, ReFs, light, kg2)
         twlightidx4 = find(Hilighttimes >= xpoint4 & Hilighttimes <= (xpoint4 + 48));
        
         
-        multi(k).Hilighttimes4 = Hilighttimes(twlightidx4);
-        multi(k).hixx4 = hixx(twidx4);
-        multi(k).Hifftyy4 = Hifftyy(twidx4);
-        multi(k).Lofftyy4 = Lofftyy(twidx4);
-        multi(k).loxx4 = loxx(twidx4);
+        multi.Hilighttimes4 = Hilighttimes(twlightidx4);
+        multi.hixx4 = hixx(twidx4);
+        multi.Hifftyy4 = Hifftyy(twidx4);
+        multi.Lofftyy4 = Lofftyy(twidx4);
+        multi.loxx4 = loxx(twidx4);
     
          
         %raw data for plotting/spline check
@@ -104,9 +104,9 @@ function multi = k_multiphaselaser(expxpoint, kk, ReFs, light, kg2)
        lotimidx4 = find(LoTim >= xpoint4 & LoTim <= (xpoint4 + 48));
       
       
-        multi(k).HiAmp4 = HiAmp(hitimidx4);
-        multi(k).LoAmp4 = LoAmp(lotimidx4);
+        multi.HiAmp4 = HiAmp(hitimidx4);
+        multi.LoAmp4 = LoAmp(lotimidx4);
   
-        multi(k).HiTim4 = HiTim(hitimidx4);
-        multi(k).LoTim4 = LoTim(lotimidx4);
+        multi.HiTim4 = HiTim(hitimidx4);
+        multi.LoTim4 = LoTim(lotimidx4);
      
