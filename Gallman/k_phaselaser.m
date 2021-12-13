58fecaf52baa7cc533d6cbd75c7dbd7122aa1bd2
Kathryn Gallman <keg26@njit.edu>
2021-12-13 11:03:00 -0500

    / ‘Gallman/k_phaselaser.m’

--- Gallman/k_phaselaser.m
+++ Gallman/k_phaselaser.m
@@ -23,7 +23,7 @@ clear k;
         %raw data for plotting/spline check
         one(k).timcont = [kg(expidx(k)).e(1).s.timcont]/3600;  
         timcontidx = find(one(k).timcont >= one(k).lighttimes(3) & one(k).timcont <= (one(k).lighttimes(3) + 48));
-        one(k).timcont1 = one(k).timcont(timcontidx);
+        one(k).timcont1 = one(k).timcont(timcontidx) - one(k).timcont(timcontidx(1));
         one(k).fft = [kg(expidx(k)).e(1).s.sumfftAmp];
         one(k).fft1 = one(k).fft(timcontidx);
     
@@ -35,13 +35,13 @@ clear k;
         timcontidx2 = find(one(k).timcont >= xpoint2 & one(k).timcont <= (xpoint2 + 48));
         
         %spline
-        one(k).lighttimes2 = one(k).lighttimes(twlightidx2);
-        one(k).xx2 = one(k).xx(twidx2);
+        one(k).lighttimes2 = one(k).lighttimes(twlightidx2) - one(k).lighttimes(twlightidx2(1));
+        one(k).xx2 = one(k).xx(twidx2)-one(k).xx(twidx2(1));
         one(k).fftyy2 = one(k).fftyy(twidx2);
         
     
         %raw data for plotting/spline check
-        one(k).timcont2 = one(k).timcont(timcontidx2);
+        one(k).timcont2 = one(k).timcont(timcontidx2)-one(k).timcont(timcontidx2(1));
         one(k).fft2 = one(k).fft(timcontidx2);
     
    %four hour to twelve hour transitions 
@@ -52,13 +52,13 @@ clear k;
         timcontidx3 = find(one(k).timcont >= xpoint3 & one(k).timcont <= (xpoint3 + 48));
     
         %spline
-        one(k).lighttimes3 = one(k).lighttimes(twlightidx3);
-        one(k).xx3 = one(k).xx(twidx3);
+        one(k).lighttimes3 = one(k).lighttimes(twlightidx3)-one(k).lighttimes(twlightidx3(1));
+        one(k).xx3 = one(k).xx(twidx3)-one(k).xx(twidx3(1));
         one(k).fftyy3 = one(k).fftyy(twidx3);
         
     
         %raw data for plotting/spline check
-        one(k).timcont3 = one(k).timcont(timcontidx3);
+        one(k).timcont3 = one(k).timcont(timcontidx3)-one(k).timcont(timcontidx(1));
         one(k).fft3 = one(k).fft(timcontidx3);
     
       %fourth transition - 4 hours to 12 hours
@@ -69,13 +69,13 @@ clear k;
         timcontidx4 = find(one(k).timcont >= xpoint4 & one(k).timcont <= (xpoint4 + 48));
     
         %spline
-        one(k).lighttimes4 = one(k).lighttimes(twlightidx4);
-        one(k).xx4 = one(k).xx(twidx4);
+        one(k).lighttimes4 = one(k).lighttimes(twlightidx4)-one(k).lighttimes(twlightidx4(1));
+        one(k).xx4 = one(k).xx(twidx4)-one(k).xx(twidx4(1));
         one(k).fftyy4 = one(k).fftyy(twidx4);
         
     
         %raw data for plotting/spline check
-        one(k).timcont4 = one(k).timcont(timcontidx4);
+        one(k).timcont4 = one(k).timcont(timcontidx4)-one(k).timcont(timcontidx4(1));
         one(k).fft4 = one(k).fft(timcontidx4);
     
         end
