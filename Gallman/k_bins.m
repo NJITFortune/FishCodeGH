73cb8fb12a108fa8cf6cb4e49302438287db1753
Eric Fortune <eric.fortune@njit.edu>
2022-01-13 16:11:52 -0500

    / ‘Gallman/k_bins.m’

--- Gallman/k_bins.m
+++ Gallman/k_bins.m
@@ -78,7 +78,7 @@ bintimhour = bintimmin/60;
 
 %% Average amp by bin
 
-for j = 1:(length(bintimhour))+2
+for j = 1:length(bintimhour)-1
 
     timidx = find(timcont > bintimhour(j) & timcont <= bintimhour(j+1));
     bin(j).Amp(:) = fftAmp(timidx);
