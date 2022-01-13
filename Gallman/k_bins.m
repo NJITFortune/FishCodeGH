c0b8b40ceaf91342e763a52cafb00128e378a02d
Eric Fortune <eric.fortune@njit.edu>
2022-01-13 15:34:40 -0500

    / ‘Gallman/k_bins.m’

--- Gallman/k_bins.m
+++ Gallman/k_bins.m
@@ -54,7 +54,7 @@ ld = in.info.ld;
 %plot to check
     figure(5); clf; hold on;
         plot(timcont, fftAmp);
-        plot([lighttimes' lighttimes'], ylim, 'k-', 0.5);
+        plot([lighttimes' lighttimes'], ylim, 'k-', 'LineWidth', 0.5);
 
 
 
