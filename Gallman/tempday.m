2578ce0dd06095ec24739cb24b70704ca518d6b4
Eric Fortune <eric.fortune@njit.edu>
2022-01-13 14:29:03 -0500

    / ‘Gallman/tempday.m’

--- Gallman/tempday.m
+++ Gallman/tempday.m
@@ -76,6 +76,7 @@ figure(777); clf; hold on;
             %preavg(kk, :) = preavg(kk, :) + pday(kk).obw;
 
         end
+        plot(timcont, tempraw-mean(tempraw), 'r-', 'LineWidth', 1);
         plot([temptims temptims], ylim, 'k-', 'LineWidth', 0.5);
         
 %linkaxes(xa, 'x');
