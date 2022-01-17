37d23f8ee9766b9d382a0cae9f8ad0fd486d507f
Eric Fortune <eric.fortune@njit.edu>
2022-01-17 16:23:48 -0500

    / ‘Gallman/k_bins.m’

--- Gallman/k_bins.m
+++ Gallman/k_bins.m
@@ -142,12 +142,11 @@ for jj = 2:length(darkdays)
         
 end
 
-%%
 %plot to check
 figure(7); clf; hold on;
     
     plot([bin.middletim], [bin.meanAmp], '.', 'MarkerSize', 16, 'Color','r');
     plot([darkdays' darkdays'], ylim, 'k-', 'LineWidth', 1.5);
-    text(darkdaybintims, darkdaybinAmps, num2str(darkdaybinary));
+    text(darkd.bintims, darkd.binAmps, num2str(darkd.binary));
 
 
