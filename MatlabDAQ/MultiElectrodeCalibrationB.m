c5a6ad13e08f8060139069b7bc650c2a74a00ad1
EricFortune <eric.fortune@njit.edu>
2022-01-26 15:35:49 -0500

    / ‘MatlabDAQ/MultiElectrodeCalibrationB.m’

--- MatlabDAQ/MultiElectrodeCalibrationB.m
+++ MatlabDAQ/MultiElectrodeCalibrationB.m
@@ -13,10 +13,11 @@ figure(1); clf; hold on;
         xa(1) = subplot(411); plot(data1);
         xa(2) = subplot(412); plot(data2);
    load('Eigen24LDA-01-26-2022_15-05-59.mat');
-   
-   kcalch2data = data(:,2);
-        xa(3) = subplot(413); plot(data(:,1));
-        xa(4) = subplot(414); plot(data(:,2));
+        data1 = filtfilt(h,g, data(:,1)); % Band pass filter   
+        data2 = filtfilt(h,g, data(:,2)); % Band pass filter  
+   kcalch2data = data2;
+        xa(3) = subplot(413); plot(data1);
+        xa(4) = subplot(414); plot(data2);
 
 figure(2); clf; hold on;
     ax(1) = subplot(211); 
