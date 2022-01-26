3b4668473b88b6b36141e83c910d597c1fb00538
EricFortune <eric.fortune@njit.edu>
2022-01-26 15:29:15 -0500

    + ‘MatlabDAQ/MultiElectrodeCalibrationB.asv’ / ‘MatlabDAQ/MultiElectrodeCalibrationB.m’

--- MatlabDAQ/MultiElectrodeCalibrationB.m
+++ MatlabDAQ/MultiElectrodeCalibrationB.m
@@ -14,12 +14,13 @@ figure(1); clf; hold on;
 figure(2); clf; hold on;
     ax(1) = subplot(211); 
         plot(kcalch1data);
-            [pks1, locs1] = findpeaks(kcalch1data);
-        plot(locs1, pks1);
+        findpeaks(kcalch1data,'MinPeakProminence',4,'Annotate','extents')
+           % [pks1, locs1] = findpeaks(kcalch1data);
+        %plot(locs1, pks1);
     ax(2) = subplot(212); 
         plot(kcalch2data);
-            [pks2, locs2] = findpeaks(kcalch2data);
-        plot(locs2, pks2);
+           % [pks2, locs2] = findpeaks(kcalch2data);
+        %plot(locs2, pks2);
 
     
      
