c2b49c60e36ac51c99b46270330713353c1b1459
Eric Fortune <eric.fortune@njit.edu>
2022-01-27 17:37:56 -0500

    / ‘MatlabDAQ/MultiElectrodeCalibrationB.m’

--- MatlabDAQ/MultiElectrodeCalibrationB.m
+++ MatlabDAQ/MultiElectrodeCalibrationB.m
@@ -7,7 +7,7 @@ freqs = [250 650]; %freq range of typical eigen EOD
 %plot
 figure(1); clf; hold on;
     load('/Volumes/My Book/Gallman/Calibration-Jan2022/ElectrodeCalibration-Jan-26-2022-B/Eigen24LDA-01-26-2022_14-56-51.mat');
-    load('/Volumes/My Book/Gallman/Calibration-Jan2022/ElectrodeCalibration-Jan-26-2022-B/Eigen24LDA-01-26-2022_14-56-51.mat');
+    load('/Volumes/My Book/Gallman/Calibration-Jan2022/ElectrodeCalibration-Jan-26-2022-B/Eigen24LDA-01-26-2022_14-41-12.mat');
         data1 = filtfilt(h,g, data(:,1)); % Band pass filter   
         data2 = filtfilt(h,g, data(:,2)); % Band pass filter  
    kcalch1data = data1;
