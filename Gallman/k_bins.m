3fccc90c4770bd34852798ddedc96e4dd8b358fe
Eric Fortune <eric.fortune@njit.edu>
2022-02-02 15:30:48 -0500

    / ‘Gallman/k_bins.m’

--- Gallman/k_bins.m
+++ Gallman/k_bins.m
@@ -158,7 +158,7 @@ for jj = 2:length(darkdays)
     %jj-1 so we start at 1
     darkd(jj-1).binary(:) = [bin(predidx).binary];
     darkd(jj-1).bintims(:) = [bin(predidx).tim];
-    darkd(jj-1).xtim(:) = [bin(predidx).tim]-[bin(predidx(1)).tim];
+%    darkd(jj-1).xtim(:) = [bin(predidx).tim]-[bin(predidx(1)).tim];
     darkd(jj-1).binmidtims(:) = [bin(predidx).middletim];
     darkd(jj-1).binAmps(:) = [bin(predidx).meanAmp];
     
@@ -168,7 +168,7 @@ end
 
 %plot day amp
 figure(8); clf; hold on;
-    plot(darkd.xtim, darkd.binAmps);
+    plot(darkd(1).bintims, darkd.binAmps);
         
 
 
