9eec386401f31871dfda3a62e4e648875564c14c
Eric Fortune <eric.fortune@njit.edu>
2022-02-02 13:25:10 -0500

    / ‘Gallman/k_bins.m’

--- Gallman/k_bins.m
+++ Gallman/k_bins.m
@@ -209,11 +209,11 @@ figure(27); clf; hold on;
 %% crosstab on unsummarized data (pre-percent of ones)
 for k = 2:length(darkprob)
 
-    [~,chi2((k-1),:),pval((k-1), :)] = crosstab(darkprob(k-1,:), darkprob(k),:);
+    [~,chi2((k-1),:),pval((k-1), :)] = crosstab(darkprob(k-1,:), darkprob(k,:));
     
 end
 
-
+chi2
 
 %%
 
