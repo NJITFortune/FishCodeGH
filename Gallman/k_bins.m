6a81e58132559f07ad9b23ddd95f8f2ec9a5fe03
Eric Fortune <eric.fortune@njit.edu>
2022-02-02 17:56:19 -0500

    / ‘Gallman/k_bins.m’

--- Gallman/k_bins.m
+++ Gallman/k_bins.m
@@ -251,9 +251,8 @@ figure(27); clf; hold on;
         %plot(k*ones(length(darkamp(k,:)),1), darkamp(k,:), 'k.');
         plot(pcttim(k)+((binsize/2)/60),darkamp(k,:), 'k.');
         %plot(darktims(k,:),darkamp(k,:), 'k.');
-        
+        plot([pcttim(k), pcttim(k)], ylim, 'm-');
     end
-    plot([darktims, darktims], ylim, 'm-');
     plot([transtim, transtim], ylim, 'k-');
 
 %% crosstab on unsummarized data (pre-percent of ones)
