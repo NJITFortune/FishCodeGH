656b1fc6d59f2835650459412a35c859a4a4d611
Eric Fortune <eric.fortune@njit.edu>
2022-02-03 14:12:15 -0500

    / ‘Gallman/k_bins.m’

--- Gallman/k_bins.m
+++ Gallman/k_bins.m
@@ -286,8 +286,14 @@ for k = 1:transbinnum * 2
     p0 = (n1+n2)/(N1+N2);
     %expected counts under null
     n10 = N1 * p0;
-
-
+    n20 = N2 * p0;
+   % Chi-square test, by hand
+   observed = [n1 N1-n1 n2 N2-n2];
+   expected = [n10 N1-n10 n20 N2-n20];
+   chi2stat(k,:) = sum((observed-expected).^2 ./ expected);
+   p(k,:) = 1 - chi2cdf(chi2stat,1);
+    
+   text(pcttim(k), 1, num2str(p));
 end
 %%
 
