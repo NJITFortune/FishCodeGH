6755c1c8f52c4ca3c041b8954cd42891f54ee7d9
Eric Fortune <eric.fortune@njit.edu>
2022-02-15 17:01:20 -0500

    / ‘Gallman/FishFinderCalibrationtest.m’

--- Gallman/FishFinderCalibrationtest.m
+++ Gallman/FishFinderCalibrationtest.m
@@ -56,56 +56,56 @@
             
 
 %% filter by fish frequency
-figure(1); clf;
-
-    histogram([tout.his.HIfreq1], 100); hold on;
-    
-    %Lower lim
-    fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
-    [cutofffreqL, ~]  = ginput(1);
-    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-    drawnow; 
-    
-    %Upper lim
-    fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
-    [cutofffreqH, ~]  = ginput(1);
-    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-    drawnow; 
-    
-    for j=1:length(tout.his)
-         if tout.his(j).HIfreq1 > cutofffreqL & tout.his(j).HIfreq1 < cutofffreqH
-                out.his(j).HiAmp = tout.his(j).HiAmp1;
-                out.his(j).HiTim = tout.his(j).HiTim1;
-                out.his(j).HIfreq = tout.his(j).HIfreq1;
-         end
-    end
-
-    pause(1);
-    
-    
-figure(1); clf;
-
-    histogram([tout.los.LOfreq1], 100); hold on;
-    
-    %Lower lim
-    fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
-    [cutofffreqL, ~]  = ginput(1);
-    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-    drawnow; 
-    
-    %Upper lim
-    fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
-    [cutofffreqH, ~]  = ginput(1);
-    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-    drawnow; 
-    for j=1:length(tout.los)
-         if tout.los(j).LOfreq1 > cutofffreqL & tout.los(j).LOfreq1 < cutofffreqH
-             out.los(j).LoAmp = tout.los(j).LoAmp1;
-             out.los(j).LoTim = tout.los(j).LoTim1;
-             out.los(j).LOfreq = tout.los(j).LOfreq1;
-         end
-    end
-    pause(1);
+% figure(1); clf;
+% 
+%     histogram([tout.his.HIfreq1], 100); hold on;
+%     
+%     %Lower lim
+%     fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+%     [cutofffreqL, ~]  = ginput(1);
+%     plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+%     drawnow; 
+%     
+%     %Upper lim
+%     fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+%     [cutofffreqH, ~]  = ginput(1);
+%     plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+%     drawnow; 
+%     
+%     for j=1:length(tout.his)
+%          if tout.his(j).HIfreq1 > cutofffreqL & tout.his(j).HIfreq1 < cutofffreqH
+%                 out.his(j).HiAmp = tout.his(j).HiAmp1;
+%                 out.his(j).HiTim = tout.his(j).HiTim1;
+%                 out.his(j).HIfreq = tout.his(j).HIfreq1;
+%          end
+%     end
+% 
+%     pause(1);
+%     
+%     
+% figure(1); clf;
+% 
+%     histogram([tout.los.LOfreq1], 100); hold on;
+%     
+%     %Lower lim
+%     fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+%     [cutofffreqL, ~]  = ginput(1);
+%     plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+%     drawnow; 
+%     
+%     %Upper lim
+%     fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+%     [cutofffreqH, ~]  = ginput(1);
+%     plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+%     drawnow; 
+%     for j=1:length(tout.los)
+%          if tout.los(j).LOfreq1 > cutofffreqL & tout.los(j).LOfreq1 < cutofffreqH
+%              out.los(j).LoAmp = tout.los(j).LoAmp1;
+%              out.los(j).LoTim = tout.los(j).LoTim1;
+%              out.los(j).LOfreq = tout.los(j).LOfreq1;
+%          end
+%     end
+%     pause(1);
 
 %adapted from KatieMultiFreqRemover.m
 %% Plot fish against light/temp
@@ -113,18 +113,18 @@ figure(1); clf; hold on;
 
     
     assx(1) = subplot(511); hold on; 
-        plot([tout.his.HiTim1], [tout.his.HiAmp1], 'k.');
+        %plot([tout.his.HiTim1], [tout.his.HiAmp1], 'k.');
         plot([out.his.HiTim], [out.his.HiAmp], '.');
        
 
     assx(2) = subplot(512); hold on; 
-        plot([tout.los.LoTim1], [tout.los.LoAmp1], 'k.');
+       % plot([tout.los.LoTim1], [tout.los.LoAmp1], 'k.');
         plot([out.los.LoTim], [out.los.LoAmp], '.');
         
     assx(3) = subplot(513); hold on;
-        plot([tout.his.HiTim1], [tout.his.HIfreq1], 'k.'); 
+       % plot([tout.his.HiTim1], [tout.his.HIfreq1], 'k.'); 
         plot([out.his.HiTim], [out.his.HIfreq], '.'); 
-        plot([tout.los.LoTim1], [tout.los.LOfreq1], 'k.');
+        %plot([tout.los.LoTim1], [tout.los.LOfreq1], 'k.');
         plot([out.los.LoTim], [out.los.LOfreq], '.');
         
     
