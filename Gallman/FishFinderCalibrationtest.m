16df9b21a50cb0dd2eb0416370359c4b69bc99d0
Eric Fortune <eric.fortune@njit.edu>
2022-02-15 17:12:09 -0500

    / ‘Gallman/FishFinderCalibrationtest.m’

--- Gallman/FishFinderCalibrationtest.m
+++ Gallman/FishFinderCalibrationtest.m
@@ -6,20 +6,20 @@
 %kg2(#).s = KatieSeparationAnxiety('Eigen*');
 
 %% assign amplitude data to fish by frequency
-%figure(987); clf; hold on;
+figure(987); clf; hold on;
     %Indicies when each fish was in each tube
     %threshold for ratio at 2.5
         %when each fish was in tube 2
         intube2hi = find([out.e2hiamp] ./ [out.e1hiamp] > 2.5);
-            %plot([out(intube2hi).timcont], [out(intube2hi).e2hiamp], 'b.');
+            plot([out(intube2hi).timcont], [out(intube2hi).e2hiamp], 'b.');
         intube2lo = find([out.e2loamp] ./ [out.e1loamp] > 2.5);
-            %plot([out(intube2lo).timcont], [out(intube2lo).e2loamp], 'm.');
+            plot([out(intube2lo).timcont], [out(intube2lo).e2loamp], 'm.');
 
         %when each fish was in tube 1
         intube1hi = find([out.e1hiamp] ./ [out.e2hiamp] > 2.5);
-             %plot([out(intube1hi).timcont], [out(intube1hi).e1hiamp], 'bo');
+             plot([out(intube1hi).timcont], [out(intube1hi).e1hiamp], 'bo');
         intube1lo = find([out.e1loamp] ./ [out.e2loamp] > 2.5);
-            %plot([out(intube1lo).timcont], [out(intube1lo).e1loamp], 'mo');
+            plot([out(intube1lo).timcont], [out(intube1lo).e1loamp], 'mo');
       
 
     %test alternative eric
