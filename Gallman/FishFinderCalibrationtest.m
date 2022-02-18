9052eb30530e92d73ddf53cb93ffca1d1546e9d6
Eric Fortune <eric.fortune@njit.edu>
2022-02-18 16:06:10 -0500

    / ‘Gallman/FishFinderCalibrationtest.m’

--- Gallman/FishFinderCalibrationtest.m
+++ Gallman/FishFinderCalibrationtest.m
@@ -297,7 +297,7 @@ HiFreq = [hitube1freqff, hitube2freqff];
 %hi frequency fish
 figure(453); clf; hold on;
 xa = gca;
-xa.Ylim = [0,5];
+xa.YLim = [0,5];
 
     ax(1) = subplot(311); title('tube 1 adjustments'); hold on;
             %raw amp
