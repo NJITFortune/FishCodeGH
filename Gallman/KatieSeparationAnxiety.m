1a561728c5d64c7de7c4d9eac2a099b0719d5ab1
Eric Fortune <eric.fortune@njit.edu>
2022-02-18 17:16:34 -0500

    / ‘Gallman/KatieSeparationAnxiety.m’

--- Gallman/KatieSeparationAnxiety.m
+++ Gallman/KatieSeparationAnxiety.m
@@ -159,7 +159,7 @@ for j=2:length(iFiles)
 
 % Set current frequencies
 
-currhifreq = tmphifreq2;         
+currhifreq = 450;         
 currlofreq = tmplofreq1;  
 % 
 %     if tmploamp1 > tmploamp2
