a1ef2c94428325cc726e6d26df6308db7e417eb0
Eric Fortune <eric.fortune@njit.edu>
2022-02-18 15:56:59 -0500

    / ‘Gallman/FishFinderCalibrationtest.m’

--- Gallman/FishFinderCalibrationtest.m
+++ Gallman/FishFinderCalibrationtest.m
@@ -1,4 +1,4 @@
-function fish = KatieFishFinder(out)
+%function fish = KatieFishFinder(out)
 %% usage
 %takes raw fft amp data from each tube and assigns it to high and low freq fish
 %kg2(#).fish = KatieFishFinder(kg2(#).s)
@@ -36,113 +36,113 @@ lotube1freq = [out(intube1lo).lofreq];
 lotube2freq = [out(intube2lo).lofreq];
         
 %% filter by fish frequency
-%hifreq
-% figure(1); clf;
-% 
-%     histogram(hitube1freq, 100); hold on;
-%     
-%     %Lower lim
-%     fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
-%     [cutofffreqL, ~]  = ginput(1);
-%     plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-%     drawnow; 
-%     
-%     %Upper lim
-%     fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
-%     [cutofffreqH, ~]  = ginput(1);
-%     plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-%     drawnow; 
-%     
-%     for j=1:length(hitube1amp)
-%          if hitube1freq(j) > cutofffreqL && hitube1freq(j) < cutofffreqH
-%                 hitube1ampff(j) = hitube1amp(j);
-%                 hitube1timff(j) = hitube1tim(j);
-%                 hitube1freqff(j) = hitube1freq(j);
-%          end
-%     end
-% 
-%     pause(1);
-%     
-%     
-% figure(1); clf;
-% 
-%     histogram(hitube2freq, 100); hold on;
-%     
-%     %Lower lim
-%     fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
-%     [cutofffreqL, ~]  = ginput(1);
-%     plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-%     drawnow; 
-%     
-%     %Upper lim
-%     fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
-%     [cutofffreqH, ~]  = ginput(1);
-%     plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-%     drawnow; 
-%     
-%     for j=1:length(hitube2amp)
-%          if hitube2freq(j) > cutofffreqL && hitube2freq(j) < cutofffreqH
-%                 hitube2ampff(j) = hitube2amp(j);
-%                 hitube2timff(j) = hitube2tim(j);
-%                 hitube2freqff(j) = hitube2freq(j);
-%          end
-%     end
-% 
-%     pause(1);
-% 
-% %lofreq
-% figure(1); clf;
-% 
-%     histogram(lotube1freq, 100); hold on;
-%     
-%     %Lower lim
-%     fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
-%     [cutofffreqL, ~]  = ginput(1);
-%     plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-%     drawnow; 
-%     
-%     %Upper lim
-%     fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
-%     [cutofffreqH, ~]  = ginput(1);
-%     plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-%     drawnow; 
-%     
-%     for j=1:length(lotube1amp)
-%          if lotube1freq(j) > cutofffreqL && lotube1freq(j) < cutofffreqH
-%                 lotube1ampff(j) = lotube1amp(j);
-%                 lotube1timff(j) = lotube1tim(j);
-%                 lotube1freqff(j) = lotube1freq(j);
-%          end
-%     end
-% 
-%     pause(1);
-% 
-%   
-% figure(1); clf;
-% 
-%     histogram(lotube2freq, 100); hold on;
-%     
-%     %Lower lim
-%     fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
-%     [cutofffreqL, ~]  = ginput(1);
-%     plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-%     drawnow; 
-%     
-%     %Upper lim
-%     fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
-%     [cutofffreqH, ~]  = ginput(1);
-%     plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
-%     drawnow; 
-%     
-%     for j=1:length(lotube2amp)
-%          if lotube2freq(j) > cutofffreqL && lotube2freq(j) < cutofffreqH
-%                 lotube2ampff(j) = lotube2amp(j);
-%                 lotube2timff(j) = lotube2tim(j);
-%                 lotube2freqff(j) = lotube2freq(j);
-%          end
-%     end
-% 
-%     pause(1);
+hifreq
+figure(1); clf;
+
+    histogram(hitube1freq, 100); hold on;
+    
+    %Lower lim
+    fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+    [cutofffreqL, ~]  = ginput(1);
+    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    %Upper lim
+    fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+    [cutofffreqH, ~]  = ginput(1);
+    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    for j=1:length(hitube1amp)
+         if hitube1freq(j) > cutofffreqL && hitube1freq(j) < cutofffreqH
+                hitube1ampff(j) = hitube1amp(j);
+                hitube1timff(j) = hitube1tim(j);
+                hitube1freqff(j) = hitube1freq(j);
+         end
+    end
+
+    pause(1);
+    
+    
+figure(1); clf;
+
+    histogram(hitube2freq, 100); hold on;
+    
+    %Lower lim
+    fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+    [cutofffreqL, ~]  = ginput(1);
+    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    %Upper lim
+    fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+    [cutofffreqH, ~]  = ginput(1);
+    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    for j=1:length(hitube2amp)
+         if hitube2freq(j) > cutofffreqL && hitube2freq(j) < cutofffreqH
+                hitube2ampff(j) = hitube2amp(j);
+                hitube2timff(j) = hitube2tim(j);
+                hitube2freqff(j) = hitube2freq(j);
+         end
+    end
+
+    pause(1);
+
+%lofreq
+figure(1); clf;
+
+    histogram(lotube1freq, 100); hold on;
+    
+    %Lower lim
+    fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+    [cutofffreqL, ~]  = ginput(1);
+    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    %Upper lim
+    fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+    [cutofffreqH, ~]  = ginput(1);
+    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    for j=1:length(lotube1amp)
+         if lotube1freq(j) > cutofffreqL && lotube1freq(j) < cutofffreqH
+                lotube1ampff(j) = lotube1amp(j);
+                lotube1timff(j) = lotube1tim(j);
+                lotube1freqff(j) = lotube1freq(j);
+         end
+    end
+
+    pause(1);
+
+  
+figure(1); clf;
+
+    histogram(lotube2freq, 100); hold on;
+    
+    %Lower lim
+    fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+    [cutofffreqL, ~]  = ginput(1);
+    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    %Upper lim
+    fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+    [cutofffreqH, ~]  = ginput(1);
+    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    for j=1:length(lotube2amp)
+         if lotube2freq(j) > cutofffreqL && lotube2freq(j) < cutofffreqH
+                lotube2ampff(j) = lotube2amp(j);
+                lotube2timff(j) = lotube2tim(j);
+                lotube2freqff(j) = lotube2freq(j);
+         end
+    end
+
+    pause(1);
 
 %% HIGH FREQUENCY FISH chunking for calibration
 
