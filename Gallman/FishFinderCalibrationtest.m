e6df2e1382537a8a7292ecf2fb2e6454a91925fc
Eric Fortune <eric.fortune@njit.edu>
2022-02-18 16:19:31 -0500

    / ‘Gallman/FishFinderCalibrationtest.m’

--- Gallman/FishFinderCalibrationtest.m
+++ Gallman/FishFinderCalibrationtest.m
@@ -159,7 +159,7 @@ figure(452); clf; hold on;
             plot(lotube2timff, lotube2ampff, 'mo');
             
     ax(3) = subplot(413); title('combined chunks'); hold on; ylim([300, 500]);
-         %  plot(hitube1tim, hitube1freq, 'k.');
+           plot(hitube1tim, hitube1freq, 'k.');
           plot(hitube1timff, hitube1freqff,'b.'); 
 % 
 %             plot(hitube2tim, hitube2freq, 'k.');
