c7e0cf8ccadf2bc6190dfb21ff2bfdbfbcf97e1f
Eric Fortune <eric.fortune@njit.edu>
2022-02-21 11:05:11 -0500

    / ‘Gallman/FishFinderCalibrationtest.m’

--- Gallman/FishFinderCalibrationtest.m
+++ Gallman/FishFinderCalibrationtest.m
@@ -202,110 +202,16 @@ linkaxes(ax, 'x');
             hitube1ampchunk1(j) = hitube1ampff(hifishchunk1idx(j)) * 4;
             hitube1timchunk1(j) = hitube1timff(hifishchunk1idx(j));
         end
-%%
-%     %tube2
-%     clear hifishchunk1idx;
-%     hifishchunk1idx = find(hitube2timff < 63);
-%     
-%         for j = 1:length(hifishchunk1idx)
-%             hitube2ampchunk1(j) = hitube2ampff(hifishchunk1idx(j)) * 2.5;
-%             hitube2timchunk1(j) = hitube2timff(hifishchunk1idx(j));
-%         end
 
-%chunk 2 = tim > 65 and tim < 157
-    %tube 1
-    hifishchunk2idx = find(hitube1timff > 63 & hitube1timff <= 157);
-    
-        for j = 1:length(hifishchunk2idx)
-            hitube1ampchunk2(j) = hitube1ampff(hifishchunk2idx(j)) ;
-            hitube1timchunk2(j) = hitube1timff(hifishchunk2idx(j));
-        end
-
-    clear hifishchunk2idx;
-    clear hitube2ampchunk2;
-    clear hitube2timchunk2;
-    %tube 2
-    hifishchunk2idx = find(hitube2timff > 65 & hitube2timff <= 157);
-    
-        for j = 1:length(hifishchunk2idx)
-            hitube2ampchunk2(j) = hitube2ampff(hifishchunk2idx(j)) * 1.8;
-            hitube2timchunk2(j) = hitube2timff(hifishchunk2idx(j));
-        end
-
-%chunk 3 = tim > 157 & tim < 219;
-clear hifishchunk3idx;
-clear hitube1ampchunk3;
-clear hitube1timchunk3;
-
-    %tube1
-    hifishchunk3idx = find(hitube1timff > 157 & hitube1timff <= 219);
-    
-        for j = 1:length(hifishchunk3idx)
-            hitube1ampchunk3(j) = hitube1ampff(hifishchunk3idx(j)) * 5.5;
-            hitube1timchunk3(j) = hitube1timff(hifishchunk3idx(j));
-        end
-    
     %tube2
-    clear hifishchunk3idx;
-    hifishchunk3idx = find(hitube2timff > 157 & hitube2timff <= 219);
+    clear hifishchunk1idx;
+    hifishchunk1idx = find(hitube2timff >= 40 & hitube2timff < 200);
     
-        for j = 1:length(hifishchunk3idx)
-            hitube2ampchunk3(j) = hitube2ampff(hifishchunk3idx(j)) * 2;
-            hitube2timchunk3(j) = hitube2timff(hifishchunk3idx(j));
+        for j = 1:length(hifishchunk1idx)
+            hitube2ampchunk1(j) = hitube2ampff(hifishchunk1idx(j));
+            hitube2timchunk1(j) = hitube2timff(hifishchunk1idx(j));
         end
 
-%chunk 4 = tim > 219 & tim < 229
-    %tube 1
-    hifishchunk4idx = find(hitube1timff > 219 & hitube1timff <= 229);
-    
-        for j = 1:length(hifishchunk4idx)
-            hitube1ampchunk4(j) = hitube1ampff(hifishchunk4idx(j)) * 3.5;
-            hitube1timchunk4(j) = hitube1timff(hifishchunk4idx(j));
-        end
-
-    %tube 2
-    clear hifishchunk4idx;
-    hifishchunk4idx = find(hitube2timff > 219 & hitube2timff <= 229);
-    
-        for j = 1:length(hifishchunk4idx)
-            hitube2ampchunk4(j) = hitube2ampff(hifishchunk4idx(j)) * 1.25;
-            hitube2timchunk4(j) = hitube2timff(hifishchunk4idx(j));
-        end
-
-%chunk 5 tube 1= tim > 229
-clear hifishchunk5idx;
-clear hitube1ampchunk5;
-clear hitube1timchunk5;
-clear hitube2ampchunk5;
-clear hitube2timchunk5;
-
-    %tube 1
-    hifishchunk5idx = find(hitube1timff > 229);
-    
-        for j = 1:length(hifishchunk5idx)
-            hitube1ampchunk5(j) = hitube1ampff(hifishchunk5idx(j));
-            hitube1timchunk5(j) = hitube1timff(hifishchunk5idx(j));
-        end
-
-%chunk 5 tube 2 = tim > 229 & tim < 238       
-    %tube 2
-    clear hifishchunk5idx;
-    hifishchunk5idx = find(hitube2timff > 229 & hitube2timff <= 238);
-    
-        for j = 1:length(hifishchunk5idx)
-            hitube2ampchunk5(j) = hitube2ampff(hifishchunk5idx(j)) * 1.5;
-            hitube2timchunk5(j) = hitube2timff(hifishchunk5idx(j));
-        end
-            
- 
-%chunk 6 tube 2 = tim > 238 
-    %tube 2
-    hifishchunk6idx = find(hitube2timff > 238);
-    
-        for j = 1:length(hifishchunk6idx)
-            hitube2ampchunk6(j) = hitube2ampff(hifishchunk6idx(j)) * 2;
-            hitube2timchunk6(j) = hitube2timff(hifishchunk6idx(j));
-        end
 
 
 %combine chunks
@@ -316,7 +222,7 @@ clear hitube2timchunk5;
     hitube2timcomb = [hitube2timchunk1, hitube2timchunk2, hitube2timchunk3, hitube2timchunk4, hitube2timchunk5, hitube2timchunk6];
 
 
-HiAmp = [hitube1ampcomb, hitube2ampcomb];
+HiAmp = [hitube1ampchunk1, hitube2ampchunk1];
 HiTim = [hitube1timcomb, hitube2timcomb];
 HiFreq = [hitube1freqff, hitube2freqff];
 
