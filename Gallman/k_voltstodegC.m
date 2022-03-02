4fd3f64f6ea91bc0d440437ff7001b6b2802e2c1
Eric Fortune <eric.fortune@njit.edu>
2022-03-02 13:56:29 -0500

    / ‘Gallman/k_voltstodegC.m’

--- Gallman/k_voltstodegC.m
+++ Gallman/k_voltstodegC.m
@@ -1,7 +1,9 @@
 
 in = kg(103);
+
+%define input variables
 Rig = in.info.folder(end);
-%Rig = genvarname(rigname);
+temptims = [in.info.temptims];
 
 %define variables for calculations
     %constants
@@ -9,25 +11,50 @@ Rig = in.info.folder(end);
     c2 = 2.378405444e-04; 
     c3 = 2.019202697e-07;
 
+%thermister calibration by tank
+    R1a = 9630; % adjust for each from 10000 //9500
+    R1b = 9800; %adjust for each from 10000 //9500//11500
+    R1c = 11000; % adjust for each from 10000 //9500
 
-if Rig == 'A'
-    R1 = R1a;
-end
-%     %thermister calibration by tank
-%     R1a = 9630; % adjust for each from 10000 //9500
-%     R1b = 9800; %adjust for each from 10000 //9500//11500
-%     R1c = 11000; % adjust for each from 10000 //9500
-% 
-% temptims = [in.info.temptims];
-% 
-% for j = 1:length([in.info.temptims])
-%   Va = 
-%   R2a = R1a * ((1023.0 / (float)Va) - 1.0);
-%   logR2a = log(R2a);
-%   Ta = (1.0 / (c1 + c2*logR2a + c3*logR2a*logR2a*logR2a));
-%   Ta = Ta - 273.15;
-% 
-%   Serial.print("Temperature A: "); 
-%   Serial.print(Ta);
-%   Serial.println(" C"); 
+
+    if Rig == 'A'
+        R1 = R1a;
+    end
+    
+    if Rig == 'B'
+        R1 = R1b;
+    end
+    
+    if Rig == 'C'
+        R1 = R1c;
+    end
+    
+
+
+for j = 1:length(temptims)
+  
+  R2a = R1a * ((1023.0 / temptims(j)) - 1.0);
+  logR2a = log(R2a);
+  Ta = (1.0 / (c1 + c2*logR2a + c3*logR2a*logR2a*logR2a));
+  Ta = Ta - 273.15;
+
+  Serial.print("Temperature A: "); 
+  Serial.print(Ta);
+  Serial.println(" C"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
