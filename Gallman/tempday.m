39a381df76f29aa77fd82444a4aa216e99641018
Eric Fortune <eric.fortune@njit.edu>
2022-03-02 13:30:34 -0500

    / ‘Gallman/tempday.m’

--- Gallman/tempday.m
+++ Gallman/tempday.m
@@ -138,7 +138,7 @@ ptim = zeros(1, shortesttrial);
         plot(ptim(1:shortesttrial), pmean(1:shortesttrial), 'k', 'LineWidth', 3)
 %calculate temp ld equivalent
     td = floor(temperaturetimes(2)-temperaturetimes(1));
-        plot([td, td], ylim, 'k-', 'LineWidth', 3);
+        plot([td, td], ylim, 'k-', 'LineWidth', 2);
         
  %% ERIC HELP PLEASE
  
