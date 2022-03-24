1e6521c8f539f5bff2c63da952731837e4eee7a0
Eric Fortune <eric.fortune@njit.edu>
2022-03-24 16:13:08 -0400

    / ‘Gallman/KatiefftDayTrialDessembler.m’

--- Gallman/KatiefftDayTrialDessembler.m
+++ Gallman/KatiefftDayTrialDessembler.m
@@ -1,4 +1,4 @@
-function [trial, day] = KatiefftDayTrialDessembler(in, channel,  ReFs, light)
+function [trial] = KatiefftDayTrialDessembler(in, channel,  ReFs, light)
 %% usage
 %[trial, day] = KatieDayTrialDessembler(kg(#), channel, triallength, ReFs)
 
