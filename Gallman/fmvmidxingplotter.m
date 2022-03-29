dec8c239537e81eb8dc6c553ad3b2b9bd8e4d214
Eric Fortune <eric.fortune@njit.edu>
2022-03-29 13:20:24 -0400

    + ‘Gallman/fmvmidxingplotter.m’

---
+++ Gallman/fmvmidxingplotter.m
@@ -0,0 +1,13 @@
+function fmvmidxingplotter(in)
+
+%in = fm(k)
+figure(50); clf; hold on;
+
+    plot([in.timfo.timcont], [in.sx.midxing], '.-');
+    plot([in.timfo.lighttimes' in.timfo.lighttimes'], ylim, 'k-');
+
+
+% figure(50); clf; hold on;
+%     
+%     plot([in.timfo.timcont], [in.ss.velstd], '.-');
+%     plot([in.timfo.lighttimes' in.timfo.lighttimes'], ylim, 'k-');
