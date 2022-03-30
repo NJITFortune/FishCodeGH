72d45f0baef70eed0dd68391a034d371c98fc97a
Eric Fortune <eric.fortune@njit.edu>
2022-03-30 12:18:00 -0400

    / ‘Gallman/fmvplotter.m’

--- Gallman/fmvplotter.m
+++ Gallman/fmvplotter.m
@@ -1,13 +1,13 @@
 function fmvplotter(in)
 
 %in = fm(k)
-figure(49); clf; title('average velocity'); hold on;
+figure(49);  title('average velocity'); hold on;
 
     plot([in.timfo.timcont], [in.ss.velmean], '.-');
     plot([in.timfo.lighttimes' in.timfo.lighttimes'], ylim, 'k-');
 
 
-figure(50); clf; title('velocity std'); hold on;
+figure(50);  title('velocity std'); hold on;
     
     plot([in.timfo.timcont], [in.ss.velstd], '.-');
     plot([in.timfo.lighttimes' in.timfo.lighttimes'], ylim, 'k-');
