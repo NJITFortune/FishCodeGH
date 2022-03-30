35e14fe72d36af742e2b3edffe7ffefb9e892538
Eric Fortune <eric.fortune@njit.edu>
2022-03-30 10:37:28 -0400

    / ‘Gallman/KatiefftPulseDayTrialDessemblersingleplotter.m’

--- Gallman/KatiefftPulseDayTrialDessemblersingleplotter.m
+++ Gallman/KatiefftPulseDayTrialDessemblersingleplotter.m
@@ -181,7 +181,8 @@ for k = 1:howmanydaysinsample
  
 %% plot to check
 
-darkpulse = ld
+darkpulse = ld/2;
+lightretrun = darkpulse + 1;
 
  %trials across tims
  figure(26); clf; title('trials across time');  hold on;
