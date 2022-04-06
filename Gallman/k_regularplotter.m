3d4be99e2c1c8086ae008bf9a312148de3b9e82d
Eric Fortune <eric.fortune@njit.edu>
2022-04-06 10:09:24 -0400

    / ‘Gallman/k_regularplotter.m’

--- Gallman/k_regularplotter.m
+++ Gallman/k_regularplotter.m
@@ -1,22 +1,22 @@
 function k_regularplotter(in)
 
-%outliers
-    % Prepare the data with outliers
-            ttsf{1} = 1:length([in.e(1).s.timcont]); % ttsf is indices for sumfftAmp
-            ttsf{2} = 1:length([in.e(2).s.timcont]);
-    % Prepare the data without outliers
-
-            % If we have removed outliers via KatieRemover, get the indices...    
-            if ~isempty(in.idx) 
-                ttsf{1} = in.idx(1).sumfftidx; ttsf{2} = in.idx(2).sumfftidx; % ttsf is indices for sumfftAmp
-            end
+% %outliers
+%     % Prepare the data with outliers
+%             ttsf{1} = 1:length([in.e(1).s.timcont]); % ttsf is indices for sumfftAmp
+%             ttsf{2} = 1:length([in.e(2).s.timcont]);
+%     % Prepare the data without outliers
+% 
+%             % If we have removed outliers via KatieRemover, get the indices...    
+%             if ~isempty(in.idx) 
+%                 ttsf{1} = in.idx(1).sumfftidx; ttsf{2} = in.idx(2).sumfftidx; % ttsf is indices for sumfftAmp
+%             end
 
 
 figure(543); clf; hold on;
 
 for k = 1:2
     ax(1) = subplot(411); hold on; title('sumfftAmp'); ylim([0,4]);
-        plot(in.ch(k).timcontxx(ttsf{k})/(60*60), in.ch(k).sumfftAmpyy(ttsf{k}), '.');
+       % plot(in.ch(k).timcontxx(ttsf{k})/(60*60), in.ch(k).sumfftAmpyy(ttsf{k}), '.');
         plot(in.ch(k).timcontxx/(60*60), in.ch(k).sumfftAmpyy, '.');
        
     ax(2) = subplot(412); hold on; title('frequency (black) and temperature (red)');   
