e315ccf1010ad75581547fb02614263533f3216f
Eric Fortune <eric.fortune@njit.edu>
2022-04-08 15:44:23 -0400

    / ‘Gallman/k_initialplotter.m’

--- Gallman/k_initialplotter.m
+++ Gallman/k_initialplotter.m
@@ -30,7 +30,7 @@ figure(1); clf;
     highest = max([out.e(2).s(ttsf{2}).sumfftAmp]);
 
     fudge = 1-lowest;
-    bsxfun(@plus, lowest, fudge)
+    bsxfun(@plus, [out.e(2).s(ttsf{2}).sumfftAmp], fudge)
 
 
 ax(1) = subplot(411); hold on; title('sumfftAmp'); %ylim([0,5]);
