46e10e397fc84959f1045c2bb93ebf627f7a2bc5
Eric Fortune <eric.fortune@njit.edu>
2022-04-08 15:21:00 -0400

    / ‘Gallman/k_initialplotter.m’

--- Gallman/k_initialplotter.m
+++ Gallman/k_initialplotter.m
@@ -27,8 +27,8 @@ figure(1); clf;
     set(gcf, 'Position', [200 100 2*560 2*420]);
 
 ax(1) = subplot(411); hold on; title('sumfftAmp'); %ylim([0,5]);
-    plot([out.e(2).s(ttsf{2}).timcont]/(60*60), [out.e(2).s(ttsf{2}).sumfftAmp]./ min([out.e(2).s(ttsf{2}).sumfftAmp], '.'));
-    plot([out.e(1).s(ttsf{1}).timcont]/(60*60), [out.e(1).s(ttsf{1}).sumfftAmp]  ./ min([out.e(1).s(ttsf{1}).sumfftAmp],'.'));
+    plot([out.e(2).s(ttsf{2}).timcont]/(60*60), [out.e(2).s(ttsf{2}).sumfftAmp]/ min([out.e(2).s(ttsf{2}).sumfftAmp], '.'));
+    plot([out.e(1).s(ttsf{1}).timcont]/(60*60), [out.e(1).s(ttsf{1}).sumfftAmp]/ min([out.e(1).s(ttsf{1}).sumfftAmp],'.'));
 
 ax(2) = subplot(412); hold on; title('frequency (black) and temperature (red)');   
     plot([out.e(2).s.timcont]/(60*60), [out.e(2).s.fftFreq], '.k', 'Markersize', 8);
