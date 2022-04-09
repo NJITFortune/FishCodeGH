68ed6c5e1a53d4b63dc6307887620b708256a403
Eric Fortune <eric.fortune@njit.edu>
2022-04-09 13:37:22 -0400

    / ‘Gallman/k_initialplotter.asv’ / ‘Gallman/k_initialplotter.m’

--- Gallman/k_initialplotter.m
+++ Gallman/k_initialplotter.m
@@ -36,9 +36,9 @@ figure(1); clf;
 
 
 ax(1) = subplot(411); hold on; title('sumfftAmp'); %ylim([0,5]);
-   % plot([out.e(2).s(ttsf{2}).timcont]/(60*60), onefudged/1, '.');%/max([out.e(2).s(ttsf{2}).sumfftAmp])
-    plot([out.e(1).s(ttsf{1}).timcont]/(60*60), onefudged/2,'.');%/max([out.e(1).s(ttsf{1}).sumfftAmp])
-   % plot([out.e(1).s(ttsf{1}).timcont]/(60*60), [out.e(1).s(ttsf{1}).sumfftAmp],'.');%/max([out.e(1).s(ttsf{1}).sumfftAmp])
+    plot([out.e(2).s(ttsf{2}).timcont]/(60*60), [out.e(2).s(ttsf{2}).sumfftAmp], '.');%/max([out.e(2).s(ttsf{2}).sumfftAmp])
+  %  plot([out.e(1).s(ttsf{1}).timcont]/(60*60), onefudged/2,'.');%/max([out.e(1).s(ttsf{1}).sumfftAmp])
+   plot([out.e(1).s(ttsf{1}).timcont]/(60*60), [out.e(1).s(ttsf{1}).sumfftAmp],'.');%/max([out.e(1).s(ttsf{1}).sumfftAmp])
 
 
 ax(2) = subplot(412); hold on; title('frequency (black) and temperature (red)');   
