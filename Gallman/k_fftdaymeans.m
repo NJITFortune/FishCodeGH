bb25d4ff1980ef95335940ce97e3c5bf4a8651e7
Eric Fortune <eric.fortune@njit.edu>
2022-04-11 18:13:50 -0400

    / ‘Gallman/k_fftdaymeans.m’

--- Gallman/k_fftdaymeans.m
+++ Gallman/k_fftdaymeans.m
@@ -41,7 +41,7 @@ clear mday;
          % To get average across days, divide by number of days
             
             mday(jj,:) = mday(jj,:) / length(in(j).trial(jj).day);
-            amprange(jj,:) = [min([in(j).trial(jj).trialmin]), max([in(j).trial.trialmax])];
+            amprange(jj,:) = [min([in(j).trial(jj).trialmin]), max([in(j).trial(jj).trialmax])];
     end
  
 
