1f54a1383ee59e8a72e2239c251df460027c4043
Eric Fortune <eric.fortune@njit.edu>
2022-04-14 09:46:45 -0400

    / ‘Gallman/KatieTempLabeler.m’

--- Gallman/KatieTempLabeler.m
+++ Gallman/KatieTempLabeler.m
@@ -46,50 +46,50 @@ function out = KatieTempLabeler(in)
     %output saved in vector temptims
     %plots to check
         %plot temp/time
-        figure(59); clf; hold on;
-        plot([in(1).s.timcont]/3600, [in(1).s.temp]);
-                    
-                    
-        %find idicies where the light changes (threshold of 2.5)  
-            %autoplots but does not save. Use to check output
-%             risetime([in(1).s.temp], [in(1).s.timcont]/3600);
-%             falltime([in(1).s.temp], [in(1).s.timcont]/3600);
-        
-        %r = risetime
-        %f = falltime
-        %l= lowercross
-        %u = uppercross
-        [r, lrx, ~, ~, ~] = risetime([in(1).s.temp], [in(1).s.timcont]/3600);
-        [f, ~, ufx, ~, ~] = falltime([in(1).s.temp], [in(1).s.timcont]/3600);
-        
-        
-        %save rise indicis in center of temp change
-            %lower cross plus risetime/2
-        for j = 1:length(lrx)
-            %plot([lrx(j)+(r(j)/2), lrx(j)+(r(j)/2)], ylim, 'k-');
-            rise(j,:) = lrx(j)+(r(j)/2);
-        %     plot([rise(j) rise(j)], ylim, 'b-');
-        end
-        
-        
-        %save fall indicis in center of temp change
-            %upper cross plus falltime/2
-        for k = 1:length(ufx)
-            %plot([ufx(k)+(f(k)/2), ufx(k)+(f(k)/2)],  ylim, 'r-');
-           fall(k,:) = ufx(k)+(f(k)/2);
-        end
-     
-        %concatenate rise and fall times into single vector 
-        alltemp = vertcat(rise, fall);
-        %sort vector in ascending order
-        [tempsort, ~] = sort(alltemp);
-               
-        %save vector as output
-        %plot to check
-        for j = 1:length(tempsort)
-            out.temptims(j,:) = tempsort(j);
-            plot([out.temptims(j), out.temptims(j)], ylim, 'b-');
-        end
+%         figure(59); clf; hold on;
+%         plot([in(1).s.timcont]/3600, [in(1).s.temp]);
+%                     
+%                     
+%         %find idicies where the light changes (threshold of 2.5)  
+%             %autoplots but does not save. Use to check output
+% %             risetime([in(1).s.temp], [in(1).s.timcont]/3600);
+% %             falltime([in(1).s.temp], [in(1).s.timcont]/3600);
+%         
+%         %r = risetime
+%         %f = falltime
+%         %l= lowercross
+%         %u = uppercross
+%         [r, lrx, ~, ~, ~] = risetime([in(1).s.temp], [in(1).s.timcont]/3600);
+%         [f, ~, ufx, ~, ~] = falltime([in(1).s.temp], [in(1).s.timcont]/3600);
+%         
+%         
+%         %save rise indicis in center of temp change
+%             %lower cross plus risetime/2
+%         for j = 1:length(lrx)
+%             %plot([lrx(j)+(r(j)/2), lrx(j)+(r(j)/2)], ylim, 'k-');
+%             rise(j,:) = lrx(j)+(r(j)/2);
+%         %     plot([rise(j) rise(j)], ylim, 'b-');
+%         end
+%         
+%         
+%         %save fall indicis in center of temp change
+%             %upper cross plus falltime/2
+%         for k = 1:length(ufx)
+%             %plot([ufx(k)+(f(k)/2), ufx(k)+(f(k)/2)],  ylim, 'r-');
+%            fall(k,:) = ufx(k)+(f(k)/2);
+%         end
+%      
+%         %concatenate rise and fall times into single vector 
+%         alltemp = vertcat(rise, fall);
+%         %sort vector in ascending order
+%         [tempsort, ~] = sort(alltemp);
+%                
+%         %save vector as output
+%         %plot to check
+%         for j = 1:length(tempsort)
+%             out.temptims(j,:) = tempsort(j);
+%             plot([out.temptims(j), out.temptims(j)], ylim, 'b-');
+%         end
    
 %% manual labels
  %enter into command line
