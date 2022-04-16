9ea0e7ae6b32cce40fc06163e303eba65a2de004
Eric Fortune <eric.fortune@njit.edu>
2022-04-16 14:09:04 -0400

    / ‘Gallman/k_initialplotter.m’

--- Gallman/k_initialplotter.m
+++ Gallman/k_initialplotter.m
@@ -76,7 +76,8 @@ ax(4) = subplot(414); hold on; title('light transitions');
            % offsig = (ones(1, length(offidx)));
             
             ax(4) = subplot(414); 
-            plot([out.e(2).s(onidx).timcont]/3600, onsig, 'r.', 'Markersize', 8);
+            plot([out.e(2).s.timcont]/3600, [out.e(2).s.inputsig], 'r.', 'Markersize', 8);
+            %plot([out.e(2).s(onidx).timcont]/3600, onsig, 'r.', 'Markersize', 8);
            % plot([out.e(2).s(offidx).timcont]/3600, offsig, 'r.', 'Markersize', 8);
         end
     end
