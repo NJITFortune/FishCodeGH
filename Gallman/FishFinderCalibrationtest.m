72830280cc105d3c3bd217d25ec7bc0fa6d366de
Eric Fortune <eric.fortune@njit.edu>
2022-04-16 16:01:21 -0400

    / ‘Gallman/FishFinderCalibrationtest.m’

--- Gallman/FishFinderCalibrationtest.m
+++ Gallman/FishFinderCalibrationtest.m
@@ -210,6 +210,18 @@ linkaxes(ax, 'x');
 
         end
 
+
+  clear hifishchunk2idx;
+    clear hitube2ampchunk2;
+    clear hitube2timchunk2;
+
+ hifishchunk2idx = find(hitube2timff > 50);
+%     
+        for j = 1:length(hifishchunk2idx)
+            hitube2ampchunk2(j) = hitube2ampff( hifishchunk2idx(j))*1.8;
+            hitube2timchunk2(j) = hitube2timff( hifishchunk2idx(j));
+
+        end
  %%       
     clear hifishchunk2idx;
     clear hitube1ampchunk2;
@@ -295,6 +307,7 @@ figure(453); clf; hold on;
 %             plot(hitube1timchunk6, hitube1ampchunk6, 'ko');
 % %             plot(hitube1timchunk4, hitube1ampchunk4, 'ko');
              plot(hitube2timchunk1, hitube2ampchunk1, 'ko');
+              plot(hitube2timchunk2, hitube2ampchunk2, 'ko');
 %             plot(hitube2timchunk3, hitube2ampchunk3, 'ko');
 %             plot(hitube2timchunk4, hitube2ampchunk4, 'ko');
 %             plot(hitube2timchunk5, hitube2ampchunk5, 'ko');
