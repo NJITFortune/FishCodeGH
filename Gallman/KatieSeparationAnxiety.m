63253508635143bd6c7604b9aaf7d38d753aac3a
Eric Fortune <eric.fortune@njit.edu>
2022-04-20 18:29:51 -0400

    / ‘Gallman/KatieSeparationAnxiety.m’

--- Gallman/KatieSeparationAnxiety.m
+++ Gallman/KatieSeparationAnxiety.m
@@ -156,10 +156,10 @@ for j=2:length(iFiles)
         tmploamp1 = tmp1.fftdata(tmpidx1l(lofreq1idx));
         
 
-        tmpidx2h = find(tmp2.fftfreq > midpoint & tmp2.fftfreq < midpoint);
+        tmpidx2h = find(tmp2.fftfreq > midpoint & tmp2.fftfreq < 600);
         %[out(j).e2hiamp, hifreq2idx] = max(tmp2.fftdata(tmpidx2h));
         [out(j).e2hiamp, hifreq2idx] = max(freqfinder(tmpidx2h));
-        tmpidx2l = find(tmp2.fftfreq > midpoint & tmp2.fftfreq < midpoint);
+        tmpidx2l = find(tmp2.fftfreq > midpoint & tmp2.fftfreq < 200);
         %[out(j).e2loamp, lofreq2idx] = max(tmp2.fftdata(tmpidx2l));
         [out(j).e2loamp, lofreq2idx] = max(freqfinder(tmpidx2l));
     
