94537f8b6612aafd7067eeb2120c3c7cf3487347
Eric Fortune <eric.fortune@njit.edu>
2022-04-20 16:24:58 -0400

    / ‘Gallman/KatieSeparationAnxiety.m’

--- Gallman/KatieSeparationAnxiety.m
+++ Gallman/KatieSeparationAnxiety.m
@@ -198,7 +198,7 @@ currhifreq = tmphifreq2;
     end
 
 
-    if (currhifreq - currlofreq) < 30
+    if (currhifreq - currlofreq) < 20
         currhifreq = 460;
         currlofreq = 430;
         
