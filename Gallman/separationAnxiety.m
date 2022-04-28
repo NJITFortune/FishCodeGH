98789c39aac7e059e1a7395a7956c6182970df9a
Eric Fortune <eric.fortune@njit.edu>
2022-04-28 12:57:29 -0400

    / ‘Gallman/separationAnxiety.m’

--- Gallman/separationAnxiety.m
+++ Gallman/separationAnxiety.m
@@ -4,11 +4,18 @@ Fs = 40000;
 freqs = [300 600]; %freq range of typical eigen EOD
 userfilespec = 'Eigen*';
 
+% Max frequency change
+maxchange = 20; % Maximum change in Hz between samples
+mindiff = 5; % Minimum frequency difference (Hz) between the two fish
+
+
 clickcnt = 0;
 
 % File handling
 
     numstart = 23; %1st position in file name of time stamp
+    %day count starts at 0
+    daycount = 0;
     
     %day count starts at 0
     daycount = 0;
@@ -52,37 +59,64 @@ figure(1); clf; hold on;
 
     % Get the lower freq peak
         lowfreqidx = find(f1.fftfreq > freqs(1) & f1.fftfreq < xfreq);
-            [~, maxidx] = max(summedFFT(lowfreqidx));
-            currlofreq = f1.fftfreq(lowfreqidx(maxidx));
-            plot(currlofreq, summedFFT(lowfreqidx(maxidx)), 'c.', 'MarkerSize', 16);
+            [~, lmaxidx] = max(summedFFT(lowfreqidx));
+            currlofreq = f1.fftfreq(lowfreqidx(lmaxidx));
+            plot(currlofreq, summedFFT(lowfreqidx(lmaxidx)), 'c.', 'MarkerSize', 16);
 
     % Get the higher freq peak
         hifreqidx = find(f1.fftfreq > xfreq & f1.fftfreq < freqs(2));
-            [~, maxidx] = max(summedFFT(hifreqidx));
-            currhifreq = f1.fftfreq(hifreqidx(maxidx));        
-            plot(currhifreq, summedFFT(hifreqidx(maxidx)), 'm.', 'MarkerSize', 16);
+            [~, hmaxidx] = max(summedFFT(hifreqidx));
+            currhifreq = f1.fftfreq(hifreqidx(hmaxidx));        
+            plot(currhifreq, summedFFT(hifreqidx(hmaxidx)), 'm.', 'MarkerSize', 16);
 
     % Get the midpoint and plot it for fun
             midpoint = currlofreq + ((currhifreq - currlofreq)/2);
             plot([midpoint, midpoint], [0 1], 'k');
 
     % Put the data into the output structure            
-        out(1).loamp = max([f1.fftdata(maxidx) f2.fftdata(maxidx)]);
+        out(1).loamp = max([f1.fftdata(lowfreqidx(lmaxidx)) f2.fftdata(lowfreqidx(lmaxidx))]);
         out(1).lofreq = currlofreq;
-        out(1).hiamp = max([f1.fftdata(maxidx) f2.fftdata(maxidx)]);
+        if f1.fftdata(lowfreqidx(lmaxidx)) > f2.fftdata(lowfreqidx(lmaxidx)); out(1).lotube = 1; end 
+        if f2.fftdata(lowfreqidx(lmaxidx)) > f1.fftdata(lowfreqidx(lmaxidx)); out(1).lotube = 2; end 
+
+        out(1).hiamp = max([f1.fftdata(hifreqidx(hmaxidx)) f2.fftdata(hifreqidx(hmaxidx))]);
         out(1).hifreq = currhifreq;
+        if f1.fftdata(hifreqidx(hmaxidx)) > f2.fftdata(hifreqidx(hmaxidx)); out(1).hitube = 1; end 
+        if f2.fftdata(hifreqidx(hmaxidx)) > f1.fftdata(hifreqidx(hmaxidx)); out(1).hitube = 2; end 
 
 
 oldmidpoint = midpoint;
 oldcurrhifreq = currhifreq;
 oldcurrlofreq = currlofreq;
 
+%define time for j = 1
+ % Add time stamps (in seconds) relative to computer midnight (COMES FROM THE FILENAME)
+    hour = str2double(iFiles(1).name(numstart:numstart+1)); %numstart based on time stamp text location
+    minute = str2double(iFiles(1).name(numstart+3:numstart+4));
+    second = str2double(iFiles(1).name(numstart+6:numstart+7));
+    
+% There are 86400 seconds in a day.
+    out(1).timcont = (hour*60*60) + (minute*60) + second + (daycount*86400);
+    out(1).tim24 = (hour*60*60) + (minute*60) + second;
+    
+%light and temp for j = 1
+    out(1).temp = mean(data(1,tempchan));
+    out(1).light = mean(data(1,lightchan));
             
 for j = 2:length(iFiles)
 
     load(iFiles(j).name, 'data');
     f1 = fftmachine(filtfilt(h,g,data(:,1)), Fs);
     f2 = fftmachine(filtfilt(h,g,data(:,2)), Fs);
+
+    % Add time stamps (in seconds) relative to computer midnight (COMES FROM THE FILENAME)
+    hour = str2double(iFiles(j).name(numstart:numstart+1)); %numstart based on time stamp text location
+    minute = str2double(iFiles(j).name(numstart+3:numstart+4));
+    second = str2double(iFiles(j).name(numstart+6:numstart+7));
+                
+        if j > 2 && ((hour*60*60) + (minute*60) + second) < out(j-1).tim24
+               daycount = daycount + 1;
+        end
 
     summedFFT =  f1.fftdata + f2.fftdata;
     figure(2); clf; hold on;
@@ -109,9 +143,6 @@ for j = 2:length(iFiles)
 
 % FIX ERRORS
 
-% Max frequency change
-maxchange = 15; % Maximum change in Hz between samples
-mindiff = 5; % Minimum frequency difference (Hz) between the two fish
 
 fixme = 0;
 
@@ -122,14 +153,18 @@ fixme = 0;
 
 if fixme == 1
 
+    fprintf('Last low was %3.1f and high was %3.1f \n', out(j-1).lofreq, out(j-1).hifreq);
+
     figure(1); clf; hold on;
         plot(f1.fftfreq, summedFFT);
         xlim(freqs);
 
-    [xfreq, ~] = ginput(1);
+    [xfreq, ~] = ginput;
+
+    if length(xfreq) == 1
 
     % Get the lower freq peak
-        lowfreqidx = find(f1.fftfreq > freqs(1) & f1.fftfreq < xfreq);
+        lowfreqidx = find(f1.fftfreq > 420 & f1.fftfreq < xfreq);
             [~, lmaxidx] = max(summedFFT(lowfreqidx));
             currlofreq = f1.fftfreq(lowfreqidx(lmaxidx));
             plot(currlofreq, summedFFT(lowfreqidx(lmaxidx)), 'c.', 'MarkerSize', 16);
@@ -146,23 +181,67 @@ if fixme == 1
 
             clickcnt = clickcnt + 1;
 
+    else
+
+        xfreq = sort(xfreq);
+
+        lowfreqidx = find(f1.fftfreq > freqs(1) & f1.fftfreq < freqs(2));
+        lmaxidx = find(f1.fftfreq(lowfreqidx) >= xfreq(1), 1);
+        currlofreq = f1.fftfreq(lowfreqidx(lmaxidx));
+        plot(currlofreq, summedFFT(lowfreqidx(lmaxidx)), 'c.', 'MarkerSize', 16);
+
+
+        hifreqidx = find(f1.fftfreq > freqs(1) & f1.fftfreq < freqs(2));
+        hmaxidx = find(f1.fftfreq(hifreqidx) >= xfreq(2), 1);
+        currhifreq = f1.fftfreq(hifreqidx(hmaxidx));
+        plot(currhifreq, summedFFT(hifreqidx(hmaxidx)), 'm.', 'MarkerSize', 16);
+
+            midpoint = currlofreq + ((currhifreq - currlofreq)/2);
+            plot([midpoint, midpoint], [0 1], 'k');
+
+            clickcnt = clickcnt + 2;
+
+        pause(1);
+
+    end
+
+
+
 end
-
-
 
 
 
     % Put the data into the output structure            
         out(j).loamp = max([f1.fftdata(lowfreqidx(lmaxidx)) f2.fftdata(lowfreqidx(lmaxidx))]);
         out(j).lofreq = currlofreq;
+        if f1.fftdata(lowfreqidx(lmaxidx)) > f2.fftdata(lowfreqidx(lmaxidx)); out(j).lotube = 1; end 
+        if f2.fftdata(lowfreqidx(lmaxidx)) > f1.fftdata(lowfreqidx(lmaxidx)); out(j).lotube = 2; end 
+
+
         out(j).hiamp = max([f1.fftdata(hifreqidx(hmaxidx)) f2.fftdata(hifreqidx(hmaxidx))]);
         out(j).hifreq = currhifreq;
-
+        if f1.fftdata(hifreqidx(hmaxidx)) > f2.fftdata(hifreqidx(hmaxidx)); out(j).hitube = 1; end 
+        if f2.fftdata(hifreqidx(hmaxidx)) > f1.fftdata(hifreqidx(hmaxidx)); out(j).hitube = 2; end 
 
 
 oldmidpoint = midpoint;
-oldcurrhifreq = currhifreq;
 oldcurrlofreq = currlofreq;
+oldcurrhifreq = currhifreq;
 % pause(0.3)
 end
 
+figure(3); clf; hold on; 
+
+    ax(1) = subplot(211); title('frequency'); hold on;
+        plot([out([out.lotube]==1).timcont]/3600, [out([out.lotube]==1).lofreq], 'b-o'); 
+        plot([out([out.lotube]==2).timcont]/3600, [out([out.lotube]==2).lofreq], 'c-o'); 
+        plot([out([out.hitube]==1).timcont]/3600, [out([out.hitube]==1).hifreq], 'r-o'); 
+        plot([out([out.hitube]==2).timcont]/3600, [out([out.hitube]==2).hifreq], 'm-o');
+        
+    ax(2) = subplot(212); title('amplitude'); hold on; 
+        plot([out([out.lotube]==1).timcont]/3600, [out([out.lotube]==1).loamp], 'b.'); 
+        plot([out([out.lotube]==2).timcont]/3600, [out([out.lotube]==2).loamp], 'c.'); 
+        plot([out([out.hitube]==1).timcont]/3600, [out([out.hitube]==1).hiamp], 'r.'); 
+        plot([out([out.hitube]==2).timcont]/3600, [out([out.hitube]==2).hiamp], 'm.');
+
+    linkaxes(ax, 'x');
