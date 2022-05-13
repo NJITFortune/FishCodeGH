ab6c1247f1b799bff0a1061ae89ee582ad2ad9c2
Eric Fortune <eric.fortune@njit.edu>
2022-05-13 15:38:37 -0400

    / ‘Gallman/separationAnxiety.m’

--- Gallman/separationAnxiety.m
+++ Gallman/separationAnxiety.m
@@ -123,7 +123,7 @@ for j = 2:length(iFiles)
     out(j).timcont = (hour*60*60) + (minute*60) + second + (daycount*86400);
     out(j).tim24 = (hour*60*60) + (minute*60) + second;
 
-    %light and temp for j = 1
+    %light and temp 
     out(j).temp = mean(data(1,tempchan));
     out(j).light = mean(data(1,lightchan));
 
