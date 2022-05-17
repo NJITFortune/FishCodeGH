29520cffc0cf889b939d13bc89c8c4407ec521e3
Eric Fortune <eric.fortune@njit.edu>
2022-05-17 16:42:36 -0400

    / ‘Gallman/k_multiplotter.m’

--- Gallman/k_multiplotter.m
+++ Gallman/k_multiplotter.m
@@ -35,7 +35,7 @@ figure(66); clf; title('By fish'); hold on;
         
 
     axs(2) = subplot(512); hold on; title('Low frequency fish');
-        plot([out.fish.los.LoTim], [out.fish.los.LoAmp], 'k.');
+      %  plot([out.fish.los.LoTim], [out.fish.los.LoAmp], 'k.');
         plot([out.fish.los(ttlo).LoTim], [out.fish.los(ttlo).LoAmp], '.','Color', orange);
    
 
