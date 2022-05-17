4b9b13cc84d20cec80e2297e612e9bc7b90476df
Eric Fortune <eric.fortune@njit.edu>
2022-05-17 17:51:51 -0400

    / ‘Gallman/FishFinderNewTempClicks_k15.m’

--- Gallman/FishFinderNewTempClicks_k15.m
+++ Gallman/FishFinderNewTempClicks_k15.m
@@ -349,10 +349,10 @@ linkaxes(ax, 'x');
 
    
 
-    lofishchunk1idx = find(lotube2timff < 157);
+    lofishchunk1idx = find(lotube2timff < 325);
     
         for j = 1:length(lofishchunk1idx)
-            lotube2ampchunk1(j) = lotube2ampff(lofishchunk1idx(j))/1.3;
+            lotube2ampchunk1(j) = lotube2ampff(lofishchunk1idx(j));
             lotube2timchunk1(j) = lotube2timff(lofishchunk1idx(j));
             
         end
@@ -361,7 +361,7 @@ linkaxes(ax, 'x');
     clear lofishchunk2idx;
     clear lotube2ampchunk2;
     clear lotube2timchunk2;
-    lofishchunk2idx = find(lotube2timff >= 157 & lotube2timff<220.49);
+    lofishchunk2idx = find(lotube2timff >= 325 & lotube2timff<220.49);
     
         for j = 1:length(lofishchunk2idx)
             lotube2ampchunk2(j) = lotube2ampff(lofishchunk2idx(j))*1.6;
