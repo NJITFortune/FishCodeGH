a857fe6917ea2ec5e71d5caf9c87d43be72b550a
Eric Fortune <eric.fortune@njit.edu>
2022-05-17 17:30:22 -0400

    / ‘Gallman/FishFinderNewTempClicks_k15.m’

--- Gallman/FishFinderNewTempClicks_k15.m
+++ Gallman/FishFinderNewTempClicks_k15.m
@@ -222,7 +222,7 @@ linkaxes(ax, 'x');
  hifishchunk2idx = find(hitube2timff >= 325 & hitube2timff < 399);
 %     
         for j = 1:length(hifishchunk2idx)
-            hitube2ampchunk2(j) = hitube2ampff( hifishchunk2idx(j))*1.6;
+            hitube2ampchunk2(j) = hitube2ampff( hifishchunk2idx(j))*1.2;
             hitube2timchunk2(j) = hitube2timff( hifishchunk2idx(j));
 
         end
@@ -309,7 +309,7 @@ figure(453); clf; hold on;
 %             plot(hitube1timchunk6, hitube1ampchunk6, 'ko');
 % %             plot(hitube1timchunk4, hitube1ampchunk4, 'ko');
                plot(hitube2timchunk1, hitube2ampchunk1, 'ko');
-%                 plot(hitube2timchunk2, hitube2ampchunk2, 'ko');
+                 plot(hitube2timchunk2, hitube2ampchunk2, 'ko');
 %               plot(hitube2timchunk3, hitube2ampchunk3, 'ko');
 %              plot(hitube2timchunk4, hitube2ampchunk4, 'ko');
 %             plot(hitube2timchunk5, hitube2ampchunk5, 'ko');
