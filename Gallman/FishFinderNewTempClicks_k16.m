d94546333e267cfae6dc9711af880862b6271f79
Eric Fortune <eric.fortune@njit.edu>
2022-05-17 16:28:02 -0400

    / ‘Gallman/FishFinderNewTempClicks_k16.m’

--- Gallman/FishFinderNewTempClicks_k16.m
+++ Gallman/FishFinderNewTempClicks_k16.m
@@ -279,6 +279,7 @@ linkaxes(ax, 'x');
 %%
 clear HiAmp;
 clear HiTim;
+clear HiFreq;
 
  HiAmp = [hitube1ampff, hitube2ampchunk1, hitube2ampchunk2, hitube2ampchunk3, hitube2ampchunk4];
 HiTim = [hitube1timff, hitube2timchunk1, hitube2timchunk2, hitube2timchunk3, hitube2timchunk4];
