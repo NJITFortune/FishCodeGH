8d4314a09dff9434b652a58c78292b4bc3bccdf1
Kathryn Gallman <keg26@njit.edu>
2022-07-19 17:54:34 -0400

    / ‘Gallman/KatieRegobwDayDessembler.m’

--- Gallman/KatieRegobwDayDessembler.m
+++ Gallman/KatieRegobwDayDessembler.m
@@ -194,8 +194,9 @@ figure(56); clf; hold on;
             plot(day(1).tim/3600, mmday, 'k-', 'LineWidth', 3);
             plot(day(1).tim/3600, othermday, 'b-', 'LineWidth', 3);
             plot([ld ld], ylim, 'k-', 'LineWidth', 3);
+            a = ylim;
             if light < 3
-            fill([0 0 ld ld], [ylim ylim], [0.9, 0.9, 0.9]);
+            fill([0 0 ld ld], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
             end
 
 
