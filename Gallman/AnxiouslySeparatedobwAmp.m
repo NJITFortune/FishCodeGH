e9016283052bc849db2c8ff6e565957b6b618cac
Eric Fortune <eric.fortune@njit.edu>
2022-07-25 15:37:46 -0400

    + ‘Gallman/AnxiouslySeparatedobwAmp.m’

---
+++ Gallman/AnxiouslySeparatedobwAmp.m
@@ -0,0 +1,59 @@
+in = kg2(2);
+clearvars -except kg kg2 rkg k
+%% prep
+
+Fs = 40000;
+freqs = [200 650]; %freq range of typical eigen EOD
+iFiles = dir('Eigen*');
+
+ % Band pass filter in frequency range of fish
+ [h,g] = butter(5, [freqs(1)/(Fs/2) freqs(2)/(Fs/2)]);
+
+%% obwAmp of anxiously separated data
+
+for j = 1:length(in.s)
+    
+  %load in raw data  
+    load(iFiles(j).name, 'data');
+  %filter data
+    e1 = filtfilt(h,g,data(:,1));
+    e2 = filtfilt(h,g,data(:,2));
+  %fft to get frequency ranges
+    f1 = fftmachine(filtfilt(h,g,data(:,1)), Fs);
+    f2 = fftmachine(filtfilt(h,g,data(:,2)), Fs);
+
+  %use frequencies of each fish to define new frequency ranges for obw
+    midpoint = in.s(j).lofreq + ((in.s(j).hifreq - in.s(j).lofreq)/2);
+
+  %perform obw on both fish
+    %low frequency fish
+    if in.s(j).lotube == 1
+        lowfreqidx = find(f1.fftfreq > freqs(1) & f1.fftfreq < midpoint);
+        [out(j).lobw, out(j).loflo, out(j).lofhi, out(j).loAmpobw] = obw(e1(lowfreqidx), Fs, [freqs(1) midpoint]);
+    end
+
+    if in.s(j).lotube == 2
+        lowfreqidx = find(f2.fftfreq > freqs(1) & f2.fftfreq < midpoint);
+        [out(j).lobw, out(j).loflo, out(j).lofhi, out(j).loAmpobw] = obw(e2(lowfreqidx), Fs, [freqs(1) midpoint]);
+    end
+
+    %high frequency fish
+    if in.s(j).hitube == 1
+        hifreqidx = find(f1.fftfreq > midpoint & f1.fftfreq < freqs(2));
+        [out(j).hibw, out(j).hiflo, out(j).hifhi, out(j).hiAmpobw] = obw(e1(hifreqidx), Fs, [midpoint freqs(2)]);
+    end
+
+    if in.s(j).hitube == 2
+        hifreqidx = find(f2.fftfreq > midpoint & f2.fftfreq < freqs(2));
+        [out(j).hibw, out(j).hiflo, out(j).hifhi, out(j).hiAmpobw] = obw(e2(hifreqidx), Fs, [midpoint freqs(2)]);
+    end
+
+end
+
+    figure(65); clf; hold on;
+
+        plot([in.s([in.s.lotube]==1).timcont]/3600, out([in.s.lotube]==1).loAmpobw, 'b.');
+        plot([in.s([in.s.lotube]==2).timcont]/3600, out([in.s.lotube]==2).loAmpobw, 'c.');
+        plot([in.s([in.s.hitube]==1).timcont]/3600, out([in.s.hitube]==1).loAmpobw, 'r.');
+        plot([in.s([in.s.hitube]==2).timcont]/3600, out([in.s.hitube]==2).loAmpobw, 'm.');
+    
