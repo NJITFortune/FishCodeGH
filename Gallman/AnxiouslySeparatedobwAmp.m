fca81d31782f0a08153879b81c934230abd7820e
Eric Fortune <eric.fortune@njit.edu>
2022-07-25 16:25:41 -0400

    / ‘Gallman/AnxiouslySeparatedobwAmp.m’

--- Gallman/AnxiouslySeparatedobwAmp.m
+++ Gallman/AnxiouslySeparatedobwAmp.m
@@ -63,3 +63,12 @@ pause(1); close(ff);
         plot([in.s([in.s.hitube]==1).timcont]/3600, [out([in.s.hitube]==1).hiAmpobw], 'r.');
         plot([in.s([in.s.hitube]==2).timcont]/3600, [out([in.s.hitube]==2).hiAmpobw], 'm.');
     linkaxes(ax, 'x');
+
+%%
+    figure(66); clf; hold on;
+       ax(1) = subplot(211); title('low frequency'); hold on;
+         plot([in.s([in.s.lotube]==1).timcont]/3600, [out([in.s.lotube]==1).loflo] + [out([in.s.lotube]==1).lobw]/2, 'b.');
+         plot([in.s([in.s.lotube]==2).timcont]/3600, [out([in.s.lotube]==2).loflo] + [out([in.s.lotube]==2).lobw]/2, 'c.');   
+       ax(2) = subplot(212); title('high frequency'); hold on;  
+         plot([in.s([in.s.hitube]==1).timcont]/3600, [out([in.s.hitube]==1).hiflo] + [out([in.s.hitube]==1).hibw]/2, 'r.');
+         plot([in.s([in.s.hitube]==2).timcont]/3600, [out([in.s.hitube]==2).hiflo] + [out([in.s.hitube]==2).hibw]/2, 'm.');  
