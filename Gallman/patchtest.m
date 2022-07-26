89423c60854b36e826849ec46a8541d96971ba4d
Eric Fortune <eric.fortune@njit.edu>
2022-07-26 12:40:30 -0400

    / ‘Gallman/patchtest.m’

--- Gallman/patchtest.m
+++ Gallman/patchtest.m
@@ -1,62 +1,64 @@
 
 
-% figure(22); clf; title('freq-nonphase adjusted'); hold on
+figure(22); clf; title('freq-nonphase adjusted'); hold on
+
+y1idx = find([out(1).s.pflo] > 439 & [out(1).s.pflo]<355);
+y2idx = find([out(1).s.pfhi] > 439 & [out(1).s.pfhi]<355);
+
+x = [out(1).s(y1idx).timcont]/3600;
+y1 = [out(1).s(y1idx).pflo];
+y2 = [out(1).s(y1idx).pfhi];
+
+plot(x, y1);
+plot(x, y2);
+patch([x fliplr(x)], [y1 fliplr(y2)],'r');
+
+plot([out(1).s.timcont]/3600, [out(1).s.fftFreq], 'k-', 'LineWidth',3);
+
+
+% figure(28);clf; hold on;
+%         
+%         ax(1) = subplot(311); title('Mean square amplitude'); hold on;
+%             plot([out(2).s.timcont]/3600, [out(1).s.obwAmp], 'o');
+%             a = ylim;
+%             for j = 1:length(lightlines)-1
+%                 if mod(j,2) == 0 %if j is even
+%                 fill([lightlines(j) lightlines(j) lightlines(j+1) lightlines(j+1)], [a(1) .15 .15 a(1)], [0.9, 0.9, 0.9]);
+%                 end
+%             end
 % 
-% y1idx = find([out(1).s.pflo] > 400 & [out(1).s.pflo]<430);
-% x = [out(1).s(y1idx).timcont]/3600;
-% y1 = [out(1).s(y1idx).pflo];
-% y2 = [out(1).s(y1idx).pfhi];
+%             plot([out(2).s.timcont]/3600, [out(1).s.pobwAmp], '.', 'MarkerSize', 10);
+%             plot([out(2).s.timcont]/3600, [out(1).s.obwAmp], 'o');
+%         
+%         ax(2) = subplot(312); title('Frequency-nonphase'); hold on;
 % 
-% plot(x, y1);
-% plot(x, y2);
-% patch([x fliplr(x)], [y1 fliplr(y2)],'r');
+%             plot([out(1).s.timcont]/3600, [out(1).s.flo]);
+%             plot([out(1).s.timcont]/3600, [out(1).s.fhi]);
+%             a = ylim;
+%             for j = 1:length(lightlines)-1
+%                 if mod(j,2) == 0 %if j is even
+%                 fill([lightlines(j) lightlines(j) lightlines(j+1) lightlines(j+1)], [a(1) 550 550 a(1)], [0.9, 0.9, 0.9]);
+%                 end
+%             end
 % 
-% plot([out(1).s.timcont]/3600, [out(1).s.fftFreq], 'k-', 'LineWidth',3);
-
-
-figure(28);clf; hold on;
-        
-        ax(1) = subplot(311); title('Mean square amplitude'); hold on;
-            plot([out(2).s.timcont]/3600, [out(1).s.obwAmp], 'o');
-            a = ylim;
-            for j = 1:length(lightlines)-1
-                if mod(j,2) == 0 %if j is even
-                fill([lightlines(j) lightlines(j) lightlines(j+1) lightlines(j+1)], [a(1) .15 .15 a(1)], [0.9, 0.9, 0.9]);
-                end
-            end
-
-            plot([out(2).s.timcont]/3600, [out(1).s.pobwAmp], '.', 'MarkerSize', 10);
-            plot([out(2).s.timcont]/3600, [out(1).s.obwAmp], 'o');
-        
-        ax(2) = subplot(312); title('Frequency-nonphase'); hold on;
-
-            plot([out(1).s.timcont]/3600, [out(1).s.flo]);
-            plot([out(1).s.timcont]/3600, [out(1).s.fhi]);
-            a = ylim;
-            for j = 1:length(lightlines)-1
-                if mod(j,2) == 0 %if j is even
-                fill([lightlines(j) lightlines(j) lightlines(j+1) lightlines(j+1)], [a(1) 550 550 a(1)], [0.9, 0.9, 0.9]);
-                end
-            end
-
-            patch([[out(1).s.timcont]/3600 fliplr([out(1).s.timcont]/3600)], [[out(1).s.flo] fliplr([out(1).s.fhi])],'r');
-            plot([out(1).s.timcont]/3600, [out(1).s.fftFreq], 'k-');
-            
-         ax(3) = subplot(313); title('Frequency-phase'); hold on;
-            
-            
-            plot([out(1).s.timcont]/3600, [out(1).s.pflo]);
-            plot([out(1).s.timcont]/3600, [out(1).s.pfhi]);
-
-            a = ylim;
-            for j = 1:length(lightlines)-1
-                if mod(j,2) == 0 %if j is even
-                fill([lightlines(j) lightlines(j) lightlines(j+1) lightlines(j+1)], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
-                end
-            end
-            patch([[out(1).s.timcont]/3600 fliplr([out(1).s.timcont]/3600)], [[out(1).s.pflo] fliplr([out(1).s.pfhi])],'b');
-            plot([out(1).s.timcont]/3600, [out(1).s.fftFreq], 'k-');
-            
-       
-    
-    linkaxes(ax, 'x')
+%             patch([[out(1).s.timcont]/3600 fliplr([out(1).s.timcont]/3600)], [[out(1).s.flo] fliplr([out(1).s.fhi])],'r');
+%             plot([out(1).s.timcont]/3600, [out(1).s.fftFreq], 'k-');
+%             
+%          ax(3) = subplot(313); title('Frequency-phase'); hold on;
+%             
+%             
+%             plot([out(1).s.timcont]/3600, [out(1).s.pflo]);
+%             plot([out(1).s.timcont]/3600, [out(1).s.pfhi]);
+% 
+%             a = ylim;
+%             for j = 1:length(lightlines)-1
+%                 if mod(j,2) == 0 %if j is even
+%                 fill([lightlines(j) lightlines(j) lightlines(j+1) lightlines(j+1)], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
+%                 end
+%             end
+%             patch([[out(1).s.timcont]/3600 fliplr([out(1).s.timcont]/3600)], [[out(1).s.pflo] fliplr([out(1).s.pfhi])],'b');
+%             plot([out(1).s.timcont]/3600, [out(1).s.fftFreq], 'k-');
+%             
+%        
+%     
+%     linkaxes(ax, 'x')
