a99d04603e6daf2a6e18dc76b352db1c7622e5be
Eric Fortune <eric.fortune@njit.edu>
2022-07-26 12:58:20 -0400

    / ‘Gallman/patchtest.m’

--- Gallman/patchtest.m
+++ Gallman/patchtest.m
@@ -3,7 +3,7 @@
 figure(22); clf; title('freq-nonphase adjusted'); hold on
 
 y1idx = find([out(1).s.pfhi] < 435 );
-y2idx = find([out(1).s.pflo] > 340);
+y2idx = find([out(1).s.pflo] < 340);
 
 x = [out(1).s(y1idx).timcont]/3600;
 y2 = [out(1).s(y1idx).pflo];
