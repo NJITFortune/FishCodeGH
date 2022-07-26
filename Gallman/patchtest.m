6fe4a2228850d6bb5a30343cdeb1ce655440b3a5
Eric Fortune <eric.fortune@njit.edu>
2022-07-26 11:52:43 -0400

    + ‘Gallman/patchtest.m’

---
+++ Gallman/patchtest.m
@@ -0,0 +1,11 @@
+
+
+figure(22); clf; hold on
+
+x = [out(1).s.timcont]/3600;
+y1 = [out(1).s.flo];
+y2 = [out(1).s.fhi];
+
+plot([out(1).s.timcont]/3600, [out(1).s.flo]);
+plot([out(1).s.timcont]/3600, [out(1).s.fhi]);
+patch([x fliplr(x)], [y1 fliplr(y2)]);
