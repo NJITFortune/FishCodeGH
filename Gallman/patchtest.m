afe890a340bc76e9ad3193d5776bf9bfb5e06b59
Eric Fortune <eric.fortune@njit.edu>
2022-07-26 11:59:13 -0400

    / ‘Gallman/patchtest.m’

--- Gallman/patchtest.m
+++ Gallman/patchtest.m
@@ -1,6 +1,6 @@
 
 
-figure(22); clf; hold on
+figure(22); clf; title('freq-nonphase adjusted'); hold on
 
 x = [out(1).s.timcont]/3600;
 y1 = [out(1).s.flo];
@@ -8,6 +8,6 @@ y2 = [out(1).s.fhi];
 
 plot([out(1).s.timcont]/3600, [out(1).s.flo]);
 plot([out(1).s.timcont]/3600, [out(1).s.fhi]);
-patch([x fliplr(x)], [y1 fliplr(y2)], 'EdgeColor', 'none', 'FaceColor','r');
+patch([x fliplr(x)], [y1 fliplr(y2)], 'r');
 
 plot([out(1).s.timcont]/3600, [out(1).s.fftFreq], 'k-', 'LineWidth',3);
