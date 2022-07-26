9b47c846fa9ce23fd901237c49a507e0b81a81b9
Eric Fortune <eric.fortune@njit.edu>
2022-07-26 17:57:44 -0400

    / ‘Gallman/k_fft.m’

--- Gallman/k_fft.m
+++ Gallman/k_fft.m
@@ -1,6 +1,6 @@
 function [peakfreq, peakamp, sumamp] = k_fft(in, uFs)
 
-rango = 2; % Range in Hz for summing amplitude. %OG 10hz changed to test
+rango = 5; % Range in Hz for summing amplitude. %OG 10hz changed to test
 
 tmpfft = fftmaker(in, uFs);
 
