73831e3ba0f374eec4c178c8772e452f87979d39
Eric Fortune <eric.fortune@njit.edu>
2022-07-26 13:56:22 -0400

    / ‘Gallman/obwtest.m’

--- Gallman/obwtest.m
+++ Gallman/obwtest.m
@@ -161,28 +161,28 @@ end
 
  
  %%
- figure(28);clf; hold on;
-        
-        ax(1) = subplot(511); title('Mean square amplitude'); hold on;
-            plot([out(2).s.timcont]/3600, [out(1).s.pobwAmp], 'o');
-            plot([out(2).s.timcont]/3600, [out(1).s.obwAmp], '.');
-        ax(2) = subplot(512); title('99% occupied bandwidth'); hold on;
-            plot([out(1).s.timcont]/3600, [out(1).s.bw]);
-            plot([out(1).s.timcont]/3600, [out(1).s.pbw]);
-        ax(3) = subplot(513); title('Frequency-nonphase'); hold on;
-            plot([out(1).s.timcont]/3600, [out(1).s.fftFreq]);
-            plot([out(1).s.timcont]/3600, [out(1).s.flo], 'o-');
-            plot([out(1).s.timcont]/3600, [out(1).s.fhi], 'o-');
-         ax(4) = subplot(514); title('Frequency-phase'); hold on;
-            plot([out(1).s.timcont]/3600, [out(1).s.fftFreq]);
-            plot([out(1).s.timcont]/3600, [out(1).s.pflo], 'o-');
-            plot([out(1).s.timcont]/3600, [out(1).s.pfhi], 'o-');
-        ax(5) = subplot(515); title('light'); hold on;ylim([-1, 6]);
-            plot([out(1).s.timcont]/3600, [out(1).s.light]);
-            plot([out(2).s.timcont]/3600, [out(2).s.light]);
-    
-    linkaxes(ax, 'x')
-        
+%  figure(28);clf; hold on;
+%         
+%         ax(1) = subplot(511); title('Mean square amplitude'); hold on;
+%             plot([out(2).s.timcont]/3600, [out(1).s.pobwAmp], 'o');
+%             plot([out(2).s.timcont]/3600, [out(1).s.obwAmp], '.');
+%         ax(2) = subplot(512); title('99% occupied bandwidth'); hold on;
+%             plot([out(1).s.timcont]/3600, [out(1).s.bw]);
+%             plot([out(1).s.timcont]/3600, [out(1).s.pbw]);
+%         ax(3) = subplot(513); title('Frequency-nonphase'); hold on;
+%             plot([out(1).s.timcont]/3600, [out(1).s.fftFreq]);
+%             plot([out(1).s.timcont]/3600, [out(1).s.flo], 'o-');
+%             plot([out(1).s.timcont]/3600, [out(1).s.fhi], 'o-');
+%          ax(4) = subplot(514); title('Frequency-phase'); hold on;
+%             plot([out(1).s.timcont]/3600, [out(1).s.fftFreq]);
+%             plot([out(1).s.timcont]/3600, [out(1).s.pflo], 'o-');
+%             plot([out(1).s.timcont]/3600, [out(1).s.pfhi], 'o-');
+%         ax(5) = subplot(515); title('light'); hold on;ylim([-1, 6]);
+%             plot([out(1).s.timcont]/3600, [out(1).s.light]);
+%             plot([out(2).s.timcont]/3600, [out(2).s.light]);
+%     
+%     linkaxes(ax, 'x')
+%         
 %         
 %         
 %         
