38608ba5aac2ed1f5b696782c481c0215bdb402b
Eric Fortune <eric.fortune@njit.edu>
2022-07-26 16:36:08 -0400

    / ‘Gallman/obwtest.m’

--- Gallman/obwtest.m
+++ Gallman/obwtest.m
@@ -102,15 +102,15 @@ for kk = datasubset
 
            phaseddata4analysis = data4analysis(newidx);
                     phasetim = tim(newidx)-tim(newidx(1));
-               
-figure(26); clf ;title('raw data'); hold on;
-            
-            ax(1) = subplot(211); title('nonphase'); hold on;
-                plot( nonphasetim,data4analysis);%xlim([0,250]);
-         
-            ax(2) = subplot(212); title('phase'); hold on;
-                plot(phasetim, phaseddata4analysis );%xlim([0,250]);
-           linkaxes(ax, 'xy');
+%                
+% figure(26); clf ;title('raw data'); hold on;
+%             
+%             ax(1) = subplot(211); title('nonphase'); hold on;
+%                 plot( nonphasetim,data4analysis);%xlim([0,250]);
+%          
+%             ax(2) = subplot(212); title('phase'); hold on;
+%                 plot(phasetim, phaseddata4analysis );%xlim([0,250]);
+%            linkaxes(ax, 'xy');
 
            % data4analysis = (data4analysis - mean(data4analysis));
             % ANALYSES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -120,10 +120,10 @@ figure(26); clf ;title('raw data'); hold on;
             [out(j).s(kk).pbw,out(j).s(kk).pflo,out(j).s(kk).pfhi,out(j).s(kk).pobwAmp] = obw(phaseddata4analysis, Fs, [botFreqOBW topFreqOBW]);
 %            
            
-%             figure(26); clf ;title('obw-nonphase'); hold on;
+%             figure(26); clf ;title('obw-nonphase');set(gcf,'renderer','Painters'); hold on;
 %                 obw(data4analysis, Fs, [botFreqOBW topFreqOBW]);xlim([0,1]);
 % 
-%             figure(25); clf; title('obw-phase');hold on;
+%             figure(25); clf; title('obw-phase');set(gcf,'renderer','Painters');hold on;
 %                 obw(phaseddata4analysis, Fs, [botFreqOBW topFreqOBW]); xlim([0,1]);
 %     
 
