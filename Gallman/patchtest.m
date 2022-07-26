de8e7d5b8a1e7e032989ac4f36358a6b75c40e83
Eric Fortune <eric.fortune@njit.edu>
2022-07-26 12:36:02 -0400

    / ‘Gallman/patchtest.m’

--- Gallman/patchtest.m
+++ Gallman/patchtest.m
@@ -16,7 +16,7 @@
 
 figure(28);clf; hold on;
         
-        ax(1) = subplot(411); title('Mean square amplitude'); hold on;
+        ax(1) = subplot(311); title('Mean square amplitude'); hold on;
             plot([out(2).s.timcont]/3600, [out(1).s.obwAmp], 'o');
             a = ylim;
             for j = 1:length(lightlines)-1
@@ -28,7 +28,7 @@ figure(28);clf; hold on;
             plot([out(2).s.timcont]/3600, [out(1).s.pobwAmp], '.', 'MarkerSize', 10);
             plot([out(2).s.timcont]/3600, [out(1).s.obwAmp], 'o');
         
-        ax(2) = subplot(412); title('Frequency-nonphase'); hold on;
+        ax(2) = subplot(312); title('Frequency-nonphase'); hold on;
 
             plot([out(1).s.timcont]/3600, [out(1).s.flo]);
             plot([out(1).s.timcont]/3600, [out(1).s.fhi]);
@@ -42,16 +42,21 @@ figure(28);clf; hold on;
             patch([[out(1).s.timcont]/3600 fliplr([out(1).s.timcont]/3600)], [[out(1).s.flo] fliplr([out(1).s.fhi])],'r');
             plot([out(1).s.timcont]/3600, [out(1).s.fftFreq], 'k-');
             
-         ax(3) = subplot(413); title('Frequency-phase'); hold on;
+         ax(3) = subplot(313); title('Frequency-phase'); hold on;
             
             
             plot([out(1).s.timcont]/3600, [out(1).s.pflo]);
             plot([out(1).s.timcont]/3600, [out(1).s.pfhi]);
+
+            a = ylim;
+            for j = 1:length(lightlines)-1
+                if mod(j,2) == 0 %if j is even
+                fill([lightlines(j) lightlines(j) lightlines(j+1) lightlines(j+1)], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
+                end
+            end
             patch([[out(1).s.timcont]/3600 fliplr([out(1).s.timcont]/3600)], [[out(1).s.pflo] fliplr([out(1).s.pfhi])],'b');
             plot([out(1).s.timcont]/3600, [out(1).s.fftFreq], 'k-');
             
-        ax(4) = subplot(515); title('light'); hold on;ylim([-1, 6]);
-            plot([out(1).s.timcont]/3600, [out(1).s.light]);
-            plot([out(2).s.timcont]/3600, [out(2).s.light]);
+       
     
     linkaxes(ax, 'x')
