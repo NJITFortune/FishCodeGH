25f636222ba0fb64a4b1617a4f25839eb2b8f161
Eric Fortune <eric.fortune@njit.edu>
2022-07-27 11:35:35 -0400

    / ‘Gallman/patchtest.m’

--- Gallman/patchtest.m
+++ Gallman/patchtest.m
@@ -96,7 +96,7 @@ lightlines = [21 25 29 33 37 41 45 49 53];
 
     figure(29);clf; hold on;
         
-       % ax(1) = subplot(211); title('Mean square amplitude'); xlim([27 43]);hold on;
+        ax(1) = subplot(211); title('Mean square amplitude'); xlim([27 43]);hold on;
             %plot([out(1).s.timcont]/3600, [out(1).s.psumfftAmp], '.', 'MarkerSize', 0.1,'Color', teal);
              plot([out(1).s.timcont]/3600, [out(1).s.psumfftAmp], '.', 'MarkerSize', 10, 'Color','r');
 
@@ -118,20 +118,23 @@ lightlines = [21 25 29 33 37 41 45 49 53];
             plot([out(1).s.timcont]/3600, [out(1).s.pobwAmp], '.', 'MarkerSize', 10, 'Color',Coral);
             plot([out(1).s.timcont]/3600, [out(1).s.zAmp], '.', 'MarkerSize', 10, 'Color','b');
             legend('sumfftAmp', 'peakfftAmp', 'obwAmp', 'zAmp');
-%         ax(2) = subplot(212); title('Frequency');xlim([27 43]); ylim([300 500]); hold on;
-% 
-%             plot([out(1).s.timcont]/3600, [out(1).s.fftFreq]);
-%             %plot([out(1).s.timcont]/3600, [out(1).s.pfftFreqfhi]);
-%             a = ylim;
-%             for j = 1:length(lightlines)-1
-%                 if mod(j,2) == 0 %if j is even
-%                 fill([lightlines(j) lightlines(j) lightlines(j+1) lightlines(j+1)], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
-%                 end
-%             end
-% 
-%               plot([out(1).s.timcont]/3600, [out(1).s.fftFreq]);
-%             plot([out(1).s.timcont]/3600, [out(1).s.pfftFreq]);
-%             
+
+         ax(2) = subplot(212); title('Frequency');xlim([27 43]); ylim([300 500]); hold on;
+
+            a = ylim;
+            for j = 1:length(lightlines)-1
+                if mod(j,2) == 0 %if j is even
+                fill([lightlines(j) lightlines(j) lightlines(j+1) lightlines(j+1)], [0 a(2) a(2) 0], [0.9, 0.9, 0.9]);
+                end
+            end
+            %plot([out(1).s.timcont]/3600, [out(1).s.sumfftAmp]/2, '.', 'MarkerSize', 30,'LineWidth', 1, 'Color','b');
+            plot([out(1).s.timcont]/3600, [out(1).s.psumfftAmp]/max([out(1).s.psumfftAmp]), '.', 'MarkerSize', 10, 'Color','r');
+
+           %  plot([out(1).s.timcont]/3600, [out(1).s.peakfftAmp], '.', 'MarkerSize', 30,'LineWidth', 1, 'Color',teal);
+            plot([out(1).s.timcont]/3600, [out(1).s.ppeakfftAmp]/max([out(1).s.ppeakfftAmp]), '.', 'MarkerSize', 10, 'Color',ylw);
+            plot([out(1).s.timcont]/3600, [out(1).s.pobwAmp]/max([out(1).s.pobwAmp]), '.', 'MarkerSize', 10, 'Color',Coral);
+            plot([out(1).s.timcont]/3600, [out(1).s.zAmp]/max([out(1).s.zAmp]), '.', 'MarkerSize', 10, 'Color','b');
+            legend('sumfftAmp', 'peakfftAmp', 'obwAmp', 'zAmp');
 %          
 %     
-%     linkaxes(ax, 'x')
+     linkaxes(ax, 'x')
