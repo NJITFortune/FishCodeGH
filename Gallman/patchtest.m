5a01559f3b41f2629c6fbfcafb469fd1a977c5ff
Eric Fortune <eric.fortune@njit.edu>
2022-07-27 10:35:55 -0400

    / ‘Gallman/patchtest.m’

--- Gallman/patchtest.m
+++ Gallman/patchtest.m
@@ -110,7 +110,7 @@ figure(28);clf; hold on;
            %  plot([out(1).s.timcont]/3600, [out(1).s.peakfftAmp], '.', 'MarkerSize', 30,'LineWidth', 1, 'Color',teal);
             plot([out(1).s.timcont]/3600, [out(1).s.ppeakfftAmp], '.', 'MarkerSize', 10, 'Color',ylw);
             plot([out(1).s.timcont]/3600, [out(1).s.pobwAmp], '.', 'MarkerSize', 10, 'Color',Coral);
-            plot([out(1).s.timcont]/3600, [out(1).s.zAmp], '.', 'MarkerSize', 10, 'Color',Coral);
+            plot([out(1).s.timcont]/3600, [out(1).s.zAmp], '.', 'MarkerSize', 10, 'Color','b');
         
 %         ax(2) = subplot(212); title('Frequency');xlim([27 43]); ylim([300 500]); hold on;
 % 
