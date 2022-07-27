7698ab6220abaa36a9687c5d14ff1363bd8043e6
Eric Fortune <eric.fortune@njit.edu>
2022-07-27 16:33:14 -0400

    / ‘Gallman/patchtest.m’

--- Gallman/patchtest.m
+++ Gallman/patchtest.m
@@ -28,7 +28,7 @@ ylw = [255/255 153/255 204/255];
 teal = [0/255 114/255 178/255];
 
  %numbercycles = floor([out(1).s(datasubset(2)).timcont]/(4*60*60)); %number of cycles in data
-        timz = 1:1:8;
+        timz = 1:1:9;
       
 
 lightlines = 0 + (4*(timz-1));
