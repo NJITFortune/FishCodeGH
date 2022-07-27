811fb06a469bd8fe2c68040682bb5905e724374d
Eric Fortune <eric.fortune@njit.edu>
2022-07-27 16:26:01 -0400

    / ‘Gallman/obwtest.m’

--- Gallman/obwtest.m
+++ Gallman/obwtest.m
@@ -47,7 +47,7 @@ out(1).s(length(iFiles)).name = [];
 %% CYCLE THROUGH EVERY FILE IN DIRECTORY
 
     ff = waitbar(0, 'Cycling through files.');
- datasubset = 6746:8272;
+ datasubset = 6746:8170;
 % datasubset = [6750];   
  %figure(27); clf; hold on;
  %figure(26); clf ; hold on;
