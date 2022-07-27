9d25818eb4bbf3a84550f50f10e19e2aca81929d
Eric Fortune <eric.fortune@njit.edu>
2022-07-27 16:30:12 -0400

    / ‘Gallman/patchtest.m’

--- Gallman/patchtest.m
+++ Gallman/patchtest.m
@@ -31,7 +31,7 @@ teal = [0/255 114/255 178/255];
         timz = 1:1:numbercycles;
       
 
-lightlines = 3914 + (4*(timz-1));
+lightlines = 0.3914 + (4*(timz-1));
 
 % figure(28);clf; hold on;
 %         
