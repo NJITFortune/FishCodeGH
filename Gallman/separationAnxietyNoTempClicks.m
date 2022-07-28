bf35e21b3094312eaf15444667b99850649b8a61
Kathryn Gallman <keg26@njit.edu>
2022-07-28 16:33:23 -0400

    / ‘Gallman/separationAnxietyNoTempClicks.m’

--- Gallman/separationAnxietyNoTempClicks.m
+++ Gallman/separationAnxietyNoTempClicks.m
@@ -5,7 +5,7 @@ freqs = [300 650]; %freq range of typical eigen EOD
 userfilespec = 'Eigen*';
 
 % Max frequency change
-maxchange = 5; % Maximum change in Hz between samples
+maxchange = 9; % Maximum change in Hz between samples
 mindiff = 2; % Minimum frequency difference (Hz) between the two fish
 
 
