4159b63c7d8edb38215a16334e2a1c0a52270e65
Eric Fortune <eric.fortune@njit.edu>
2022-07-28 12:47:22 -0400

    / ‘Gallman/separationAnxiety.m’

--- Gallman/separationAnxiety.m
+++ Gallman/separationAnxiety.m
@@ -89,7 +89,7 @@ figure(2); clf; hold on;
             out(1).lofreq = currlofreq;
             if f1.fftdata(lowfreqidx(lmaxidx)) > f2.fftdata(lowfreqidx(lmaxidx))
                 out(1).lotube = 1; 
-               [out(j).lobw, out(j).loflo, out(j).lofhi, out(j).loAmpobw] = obw([e1(lowfreqidx)], Fs, [freqs(1) midpoint]);
+               [out(j).lobw, out(j).loflo, out(j).lofhi, out(j).loAmpobw] = obw(e1(lowfreqidx), Fs, [freqs(1) midpoint]);
             end 
             if f2.fftdata(lowfreqidx(lmaxidx)) > f1.fftdata(lowfreqidx(lmaxidx))
                 out(1).lotube = 2; 
