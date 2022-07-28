d0e4723ee0d39572d36bd0f14502481ede64b34e
Kathryn Gallman <keg26@njit.edu>
2022-07-28 18:07:16 -0400

    / ‘Gallman/separationAnxietyNoTempClicks.m’

--- Gallman/separationAnxietyNoTempClicks.m
+++ Gallman/separationAnxietyNoTempClicks.m
@@ -175,7 +175,8 @@ for j = 2:length(iFiles)
     % Get the higher freq peak
         hifreqidx = find(f1.fftfreq > oldmidpoint & f1.fftfreq < freqs(2));
             [~, hmaxidx] = max(summedFFT(hifreqidx));
-            currhifreq = f1.fftfreq(hifreqidx(hmaxidx));        
+            currhifreq = f1.fftfreq(hifreqidx(hmaxidx));      
+        hipeakamp = max([f1.fftdata(hifreqidx(hmaxidx)) f2.fftdata(hifreqidx(hmaxidx))]);
 
     % Get the midpoint and plot it for fun          
             midpoint = currlofreq + ((currhifreq - currlofreq)/2);
@@ -213,7 +214,7 @@ mindiff = 2; % Minimum frequency difference (Hz) between the two fish
         
        %if max change in higher fish frequency
         if abs(currhifreq-oldcurrhifreq) > maxchangehi
-          if currhifreq > 419 && ccurrhifreq < 421 || lopeakamp < 0.1
+          if currhifreq > 419 && ccurrhifreq < 421 || hipeakamp < 0.1
               currhifreq = oldcurrhifreq;
           else
                 if j > 3 
