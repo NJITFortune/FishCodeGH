0b027d2033336ac51fb8e18e563168de4b69accb
Kathryn Gallman <keg26@njit.edu>
2022-07-29 11:02:10 -0400

    / ‘Gallman/separationAnxiety.m’

--- Gallman/separationAnxiety.m
+++ Gallman/separationAnxiety.m
@@ -46,6 +46,9 @@ clickcnt = 0;
 f1 = fftmachine(e1, Fs);
 f2 = fftmachine(e2, Fs);
 
+figure(3); clf; 
+    subplot(121); tmp = fftmachine(data1, Fs); plot(tmp.fftfreq, tmp.fftdata); xlim([freqs(1) freqs(2)]);
+    subplot(122); tmp = fftmachine(data2, Fs); plot(tmp.fftfreq, tmp.fftdata); xlim([freqs(1) freqs(2)]);
 
 % figure(1); clf;
 %     subplot(211); specgram(e1,1024*16, Fs, [], ceil(1024*16*0.95)); ylim([freqs(1) freqs(2)]); caxis([15 50])
