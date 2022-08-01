1fd4192facc056d947efb8457b9e11f7b0076a7f
Kathryn Gallman <keg26@njit.edu>
2022-08-01 18:00:08 -0400

    / ‘Gallman/separationAnxietyNoTempClicks.m’

--- Gallman/separationAnxietyNoTempClicks.m
+++ Gallman/separationAnxietyNoTempClicks.m
@@ -183,7 +183,7 @@ maxchangelo2 = 10;
 minloamp = 0.1;
 maxchangehi = 20;
 maxchangehi2 = 55;
-mindiff = 15; % Minimum frequency difference (Hz) between the two fish
+mindiff = 1; % Minimum frequency difference (Hz) between the two fish
 
   
         if abs(currlofreq-oldcurrlofreq) > maxchangelo1
