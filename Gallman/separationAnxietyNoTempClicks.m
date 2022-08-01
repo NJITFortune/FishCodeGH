c0846acafd9bb4c486c4b4d190481a865e28a763
Kathryn Gallman <keg26@njit.edu>
2022-08-01 11:25:19 -0400

    / ‘Gallman/separationAnxietyNoTempClicks.m’

--- Gallman/separationAnxietyNoTempClicks.m
+++ Gallman/separationAnxietyNoTempClicks.m
@@ -181,8 +181,8 @@ for j = 2:length(iFiles)
 maxchangelo1 = 5; % Maximum change in Hz between samples
 maxchangelo2 = 10;
 minloamp = 0.1;
-maxchangehi = 15;
-maxchangehi2 = 17;
+maxchangehi = 20;
+maxchangehi2 = 25;
 mindiff = 5; % Minimum frequency difference (Hz) between the two fish
 
   
