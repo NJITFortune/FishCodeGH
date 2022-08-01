82957fa48dcf69c9579033892199b62fc0235eeb
Kathryn Gallman <keg26@njit.edu>
2022-08-01 14:25:30 -0400

    / ‘Gallman/separationAnxiety.m’

--- Gallman/separationAnxiety.m
+++ Gallman/separationAnxiety.m
@@ -6,7 +6,7 @@ userfilespec = 'Eigen*';
 
 % Max frequency change
 maxchange = 20; % Maximum change in Hz between samples
-mindiff = 3; % Minimum frequency difference (Hz) between the two fish
+mindiff = 2; % Minimum frequency difference (Hz) between the two fish
 
 
 clickcnt = 0;
