9be782dccd1e9ec44b5f7214ae8f6c8891074002
Eric Fortune <eric.fortune@njit.edu>
2022-08-02 11:54:21 -0400

    / ‘Gallman/separationAnxiety.m’

--- Gallman/separationAnxiety.m
+++ Gallman/separationAnxiety.m
@@ -182,9 +182,9 @@ for j = 2:length(iFiles) %2514:8276%
 %                 if currhifreq > 539; currhifreq = out(j-2).hifreq; end
 %             end
             
-%             if j > 2000 && currhifreq > 479
-%                 currhifreq = 460;
-%             end
+            if j > 3 && currhifreq > 479
+                currhifreq = 460;
+            end
             
 %               currlofreq = oldcurrlofreq;
          % Get the lower freq peak
@@ -200,14 +200,14 @@ for j = 2:length(iFiles) %2514:8276%
 % %                        [~, lmaxidx] = max(summedFFT(lowfreqidx) < max(summedFFT(lowfreqidx)));
 % %                          currlofreq = f1.fftfreq(lowfreqidx(lmaxidx));
 % %                    end
-%                     if currlofreq < 410
-%                         lowfreqidx = find(f1.fftfreq > 425 & f1.fftfreq < currhifreq-oldmidpoint);
-%                         [~, lmaxidx] = max(summedFFT(lowfreqidx));
-%                          currlofreq = f1.fftfreq(lowfreqidx(lmaxidx));
-%                          if isempty(currlofreq)
-%                              currlofreq = 440;
-%                          end
-%                     end
+                    if currlofreq < 410
+                        lowfreqidx = find(f1.fftfreq > 425 & f1.fftfreq < currhifreq-oldmidpoint);
+                        [~, lmaxidx] = max(summedFFT(lowfreqidx));
+                         currlofreq = f1.fftfreq(lowfreqidx(lmaxidx));
+                         if isempty(currlofreq)
+                             currlofreq = 440;
+                         end
+                    end
     % Get the midpoint and plot it for fun          
             midpoint = currlofreq + abs(currhifreq - currlofreq)/2;
             plot([midpoint, midpoint], [0 1], 'k');
