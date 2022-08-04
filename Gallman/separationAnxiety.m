bdc3218f2d3bd676cfe7d15f6554d22e834830f7
Eric Fortune <eric.fortune@njit.edu>
2022-08-04 11:24:44 -0400

    / ‘Gallman/separationAnxiety.m’

--- Gallman/separationAnxiety.m
+++ Gallman/separationAnxiety.m
@@ -178,9 +178,9 @@ for j = 2:length(iFiles) %2514:8276%
         hipeakamp = max([f1.fftdata(hifreqidx(hmaxidx)) f2.fftdata(hifreqidx(hmaxidx))]);
         
             if   hipeakamp <  0.1 || currhifreq > 419 && currhifreq < 421; currhifreq = oldcurrhifreq; end %currlofreq > 419 && currlofreq < 421 || currhifreq > 539
-%             if j > 3
-%                 if currhifreq > 539; currhifreq = out(j-2).hifreq; end
-%             end
+            if j > 3
+                if currhifreq > 650 || currhifreq < 590 ; currhifreq = out(j-2).hifreq; end
+            end
             
 %             if j > 3 && currhifreq > 500
 %                 currhifreq = 460;
