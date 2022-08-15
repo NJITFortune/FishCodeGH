0948cc32fdc0f472120e10a71216096ddf4c4bcd
Kathryn Gallman <keg26@njit.edu>
2022-08-15 12:34:12 -0400

    + ‘Gallman/KatieFishCal.m’

---
+++ Gallman/KatieFishCal.m
@@ -0,0 +1,33 @@
+
+in = hi; %lo
+out = kg2(k).s;
+
+upperthresh = 2;
+lowerthresh = 0.01;
+
+figure(452); clf; hold on;
+
+
+    ax(1) = subplot(311); title('high frequency fish'); hold on; %ylim([0,2]);
+%             plot(in(1).tim, in(1).obwamp, 'bo');
+%             plot(in(2).tim, in(2).obwamp, 'co');
+           
+            plot(in(1).tim, in(1).pkamp, 'bo');
+            plot(in(2).tim, in(2).pkamp, 'mo');
+          
+            
+    ax(2) = subplot(312); title('low frequency fish'); hold on; %ylim([0,3]);
+%             plot(in(1).tim, in(1).obwamp, 'bo');
+%             plot(lo(2).tim, in(2).obwamp, 'mo');
+          
+            plot(in(1).tim, in(1).pkamp, 'bo');
+            plot(on(2).tim, in(2).pkamp, 'mo');
+            
+            yline(lowerthresh, 'k-');
+            yline(upperthresh, 'k-');
+    
+    ax(3) = subplot(313); title('light cycle'); hold on;
+            plot([out.timcont]/3600, [out.light]);
+            
+
+linkaxes(ax, 'x');
