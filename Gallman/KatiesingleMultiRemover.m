90c50a6dc6287a0b8160ae6daecfefd89d3b6fd6
Kathryn Gallman <keg26@njit.edu>
2022-08-16 11:31:21 -0400

    + ‘Gallman/KatiesingleMultiRemover.m’

---
+++ Gallman/KatiesingleMultiRemover.m
@@ -0,0 +1,54 @@
+function out = KatiesingleMultiRemover(in)
+
+% OBW        
+figure(1); clf;
+
+    histogram([in.obwAmp], 100); hold on;
+    
+    %Lower lim
+    fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+    [cutofffreqL, ~]  = ginput(1);
+    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    %Upper lim
+    fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+    [cutofffreqH, ~]  = ginput(1);
+    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    for j=1:length(in)
+         if in(j).HiAmp > cutofffreqL && in(j).HiAmp < cutofffreqH
+             out(j).obwidx = j;
+         end
+    end
+
+    pause(1);
+ 
+  %peak amp  
+    
+figure(1); clf;
+
+    histogram([in.pkAmp], 100); hold on;
+    
+    %Lower lim
+    fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+    [cutofffreqL, ~]  = ginput(1);
+    plot([cutofffreqL, cutofffreqL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    
+    %Upper lim
+    fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+    [cutofffreqH, ~]  = ginput(1);
+    plot([cutofffreqH, cutofffreqH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+    drawnow; 
+    for j=1:length(in)
+         if in(j).pkAmp > cutofffreqL && in(j).pkAmp < cutofffreqH
+             out(j).pkidx = j;
+         end
+    end
+    pause(1);
+    close(1);
+end
+    
+
