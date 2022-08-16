2fe32bbbd9fe06cfd36954cec7d1002ca83002ed
Kathryn Gallman <keg26@njit.edu>
2022-08-16 16:50:49 -0400

    / ‘Gallman/k_fishcalplotter.m’

--- Gallman/k_fishcalplotter.m
+++ Gallman/k_fishcalplotter.m
@@ -10,19 +10,19 @@ figure(455); clf; title(str); hold on;
 
 
     ax(1) = subplot(311); title(str); hold on; %ylim([0,2]);
-%             plot(in(1).tim, in(1).obwamp, 'bo');
-%             plot(in(2).tim, in(2).obwamp, 'co');
+            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(in(2).tim, in(2).obwamp, 'co');
            
-            plot(in(1).tim, in(1).pkamp, 'bo');
-            plot(in(2).tim, in(2).pkamp, 'mo');
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp, 'mo');
           
             
     ax(2) = subplot(312); title('postcal'); hold on; %ylim([0,3]);
-%             plot(in(1).tim, in(1).obwamp, 'bo');
-%             plot(lo(2).tim, in(2).obwamp, 'mo');
+            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(lo(2).tim, in(2).obwamp, 'mo');
           
-            plot(in(1).tim, in(1).pkamp, 'bo');
-            plot(in(2).tim, in(2).pkamp*1.1, 'mo');
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp*1.1, 'mo');
             
 %             yline(thresh.lower, 'k-');
 %             yline(thresh.upper, 'k-');
