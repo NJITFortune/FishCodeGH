ef4393bced0817807726e5965bf6c405c7b13e53
Kathryn Gallman <keg26@njit.edu>
2022-08-16 17:30:30 -0400

    / ‘Gallman/k_removercheckplot.m’ / ‘Gallman/kg2fish.mlx’

--- Gallman/k_removercheckplot.m
+++ Gallman/k_removercheckplot.m
@@ -29,7 +29,7 @@ figure(457); clf;  hold on;
             plot([in.timcont]/3600, [in.pkAmp], 'ko');
             plot([in(ttpk).timcont]/3600, [in(ttpk).pkAmp], 'ro');
             
-    ax(2) = subplot(412); title('frequency'); hold on; %ylim([0,3]);        
+    ax(2) = subplot(412); title('obw'); hold on; %ylim([0,3]);        
             plot([in.timcont]/3600, [in.obwAmp], 'ko');
             plot([in(tto).timcont]/3600, [in(tto).obwAmp], 'bo');
          
