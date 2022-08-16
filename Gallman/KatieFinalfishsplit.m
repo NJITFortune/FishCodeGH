3f5e089c78b38b29b3244e3e9d1d366355258639
Kathryn Gallman <keg26@njit.edu>
2022-08-16 13:20:57 -0400

    / ‘Gallman/KatieFinalfishsplit.m’

--- Gallman/KatieFinalfishsplit.m
+++ Gallman/KatieFinalfishsplit.m
@@ -8,8 +8,8 @@ Tim = [in(1).tim, in(2).tim];
 Freq = [in(1).freq, in(2).freq];
 
 for j = 1:length(Tim)
-    sout(j).pkAmp(:) = PkAmp;
-    sout(j).obwAmp(:) = ObwAmp;
-    sout(j).timcont(:) = Tim * 3600;
-    sout(j).freq(:) = Freq;
+    sout(j).pkAmp(:) = PkAmp(j);
+    sout(j).obwAmp(:) = ObwAmp(j);
+    sout(j).timcont(:) = Tim(j) * 3600;
+    sout(j).freq(:) = Freq(j);
 end
