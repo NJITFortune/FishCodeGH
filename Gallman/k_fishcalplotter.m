e034423530bf127d19db7504a315da7250ce3aac
Kathryn Gallman <keg26@njit.edu>
2022-08-16 18:47:05 -0400

    / ‘Gallman/k_fishcalplotter.m’

--- Gallman/k_fishcalplotter.m
+++ Gallman/k_fishcalplotter.m
@@ -18,7 +18,7 @@ figure(455); clf; title(str); hold on;
           
             
     ax(2) = subplot(312); title('postcal'); hold on; %ylim([0,3]);
-            plot(in(1).tim, in(1).obwamp*1.1, 'bo');
+            plot(in(1).tim, in(1).obwamp, 'bo');
             plot(in(2).tim, in(2).obwamp, 'mo');
           
 %             plot(in(1).tim, in(1).pkamp, 'bo');
