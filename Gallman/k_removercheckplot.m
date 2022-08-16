59e24bd6befc305884a571e216fdca47cb96d8fa
Kathryn Gallman <keg26@njit.edu>
2022-08-16 16:56:54 -0400

    / ‘Gallman/k_removercheckplot.m’

--- Gallman/k_removercheckplot.m
+++ Gallman/k_removercheckplot.m
@@ -26,11 +26,11 @@ figure(457); clf;  hold on;
 
     ax(1) = subplot(311); title(str); hold on; %ylim([0,2]);
            
-            plot([in.timcont]/3600, [in.pkAmp], 'ko');
-            plot([in(ttpk).timcont]/3600, [in(ttpk).pkAmp], 'o');
+            %plot([in.timcont]/3600, [in.pkAmp], 'ko');
+            plot([in(ttpk).timcont]/3600, [in(ttpk).pkAmp]/max([in(ttpk).pkAmp]), 'o');
             
-            plot([in.timcont]/3600, [in.obwAmp], 'o');
-            plot([in(tto).timcont]/3600, [in(tto).obwAmp], 'o');
+          %  plot([in.timcont]/3600, [in.obwAmp], 'o');
+            plot([in(tto).timcont]/3600, [in(tto).obwAmp]/max([in(tto).obwAmp]), 'o');
           
             
     ax(2) = subplot(312); title('frequency'); hold on; %ylim([0,3]);
