a795ee20f46e08614609f27da6ad0bfa86103b7d
Kathryn Gallman <keg26@njit.edu>
2022-08-16 11:51:31 -0400

    + ‘Gallman/k_removercheckplot.m’

---
+++ Gallman/k_removercheckplot.m
@@ -0,0 +1,42 @@
+function k_removercheckplot(inumb, out) 
+% innumb determines whether the input is for the high or low frequency fish
+% out = kg2(k);
+% str = 'high frequency fish' etc.
+
+%% prep
+ if inumb == 6
+     in = out.hifish;
+     str = 'high frequency fish';
+     tto = out.hiidx.obwidx;
+     ttpk = out.hiidx.pkidx;
+ elseif inumb == 5
+     in = out.lofish;
+     str = 'low frequency fish';
+     tto = out.loidx.obwidx;
+     ttpk = out.loidx.pkidx;
+ end
+    
+%%    
+
+figure(457); clf;  hold on;
+
+
+    ax(1) = subplot(311); title(str); hold on; %ylim([0,2]);
+           
+            plot([in.timcont]/3600, [in.pkamp], 'ko');
+            plot([in(ttpk).timcont]/3600, [in(ttpk).pkamp], 'o');
+            
+          %  plot([in(tto).timcont]/3600, [in(tto).obwamp], 'o');
+          
+            
+    ax(2) = subplot(312); title('frequency'); hold on; %ylim([0,3]);
+%
+          
+            plot([in.timcont]/3600, [in.freq], 'o');
+               
+    
+    ax(3) = subplot(313); title('light cycle'); hold on;
+            plot([out.timcont]/3600, [out.light]);
+            
+
+linkaxes(ax, 'x');
