1ffc315d931e2ecdea8766539b91d6d8cd8183b6
Kathryn Gallman <keg26@njit.edu>
2022-08-16 17:27:46 -0400

    / ‘Gallman/k_removercheckplot.m’

--- Gallman/k_removercheckplot.m
+++ Gallman/k_removercheckplot.m
@@ -24,22 +24,23 @@ function k_removercheckplot(inumb, out)
 figure(457); clf;  hold on;
 
 
-    ax(1) = subplot(311); title(str); hold on; %ylim([0,2]);
+    ax(1) = subplot(411); title(str); hold on; %ylim([0,2]);
            
             plot([in.timcont]/3600, [in.pkAmp], 'ko');
-            plot([in(ttpk).timcont]/3600, [in(ttpk).pkAmp], 'o');
+            plot([in(ttpk).timcont]/3600, [in(ttpk).pkAmp], 'ro');
             
-%             plot([in.timcont]/3600, [in.obwAmp], 'o');
-%             plot([in(tto).timcont]/3600, [in(tto).obwAmp], 'o');
-          legend('pkAmp', 'obwAmp','Location','northoutside','NumColumns',2);
+    ax(2) = subplot(412); title('frequency'); hold on; %ylim([0,3]);        
+            plot([in.timcont]/3600, [in.obwAmp], 'ko');
+            plot([in(tto).timcont]/3600, [in(tto).obwAmp], 'bo');
+         
             
-    ax(2) = subplot(312); title('frequency'); hold on; %ylim([0,3]);
+    ax(3) = subplot(413); title('frequency'); hold on; %ylim([0,3]);
 %
           
             plot([in.timcont]/3600, [in.freq], 'ko');
             plot([in(ttpk).timcont]/3600, [in(ttpk).freq], 'o');
     
-    ax(3) = subplot(313); title('light cycle'); hold on;
+    ax(4) = subplot(414); title('light cycle'); hold on;
             plot([out.s.timcont]/3600, [out.s.light]);
             
 
