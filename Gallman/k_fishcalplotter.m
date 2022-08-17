be7ea3610ce646147405749fa65ae957ab3c0fb6
Kathryn Gallman <keg26@njit.edu>
2022-08-17 19:30:24 -0400

    / ‘Gallman/k_fishcalplotter.m’

--- Gallman/k_fishcalplotter.m
+++ Gallman/k_fishcalplotter.m
@@ -1,4 +1,4 @@
-function  k_fishcalplotter(in, out, str) 
+function  k_fishcalplotter(hi, lo, out, str) 
 % in = hi; %lo
 % out = kg2(k).s;
 % str = 'high frequency fish';
@@ -7,9 +7,37 @@ function  k_fishcalplotter(in, out, str)
 % thresh.lower = 0.7;
 
 figure(455); clf; title(str); hold on;
+in = hi;
 
+    ax(1) = subplot(311); title('High frequency fish'); hold on; ylim([0,.05]);
+            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(in(2).tim, in(2).obwamp*2.5, 'mo');
+           
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp, 'mo');
+          
+            
+    ax(2) = subplot(312); title('postcal'); hold on; ylim([0,.05]);
+            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(in(2).tim, in(2).obwamp*1.6, 'mo');
+          
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp*1.1, 'mo');
+            
+%             yline(thresh.lower, 'k-');
+%             yline(thresh.upper, 'k-');
+    
+    ax(3) = subplot(313); title('light cycle'); hold on;
+            plot([out.timcont]/3600, [out.light]);
+            
 
-    ax(1) = subplot(311); title(str); hold on; ylim([0,.05]);
+linkaxes(ax, 'x');
+
+%%
+figure(456); clf; title(str); hold on;
+in = lo;
+
+    ax(1) = subplot(311); title('Low frequency fish'); hold on; ylim([0,.05]);
             plot(in(1).tim, in(1).obwamp, 'bo');
             plot(in(2).tim, in(2).obwamp*2.5, 'mo');
            
