3cda838a2fe7b73cf3c078ae4974767c37f6203d
Kathryn Gallman <keg26@njit.edu>
2022-08-17 22:40:13 -0400

    / ‘Gallman/k_fishcalplotter.m’

--- Gallman/k_fishcalplotter.m
+++ Gallman/k_fishcalplotter.m
@@ -22,7 +22,7 @@ in = hi;
 %             plot(in(1).tim, in(1).obwamp*2, 'bo');
 %             plot(in(2).tim, in(2).obwamp, 'mo');
           
-            plot(in(1).tim, in(1).pkamp*2, 'bo');
+            plot(in(1).tim, in(1).pkamp*1.5, 'bo');
             plot(in(2).tim, in(2).pkamp, 'mo');
             
 %             yline(thresh.lower, 'k-');
@@ -51,7 +51,7 @@ in = lo;
 %             plot(in(1).tim, in(1).obwamp*2, 'bo');
 %             plot(in(2).tim, in(2).obwamp, 'mo');
           
-            plot(in(1).tim, in(1).pkamp*2, 'bo');
+            plot(in(1).tim, in(1).pkamp*1.5, 'bo');
             plot(in(2).tim, in(2).pkamp, 'mo');
             
 %             yline(thresh.lower, 'k-');
