cd675ed0ef7e6624165ea27a55b32e6b4a9446a6
Kathryn Gallman <keg26@njit.edu>
2022-08-18 16:36:53 -0400

    / ‘Gallman/k_fishcalplotter.m’

--- Gallman/k_fishcalplotter.m
+++ Gallman/k_fishcalplotter.m
@@ -40,19 +40,19 @@ clear in
 in = lo;
 
     ax(1) = subplot(311); title('Low frequency fish'); hold on; %ylim([0,.05]);
-%             plot(in(1).tim, in(1).obwamp, 'bo');
-%             plot(in(2).tim, in(2).obwamp, 'mo');
+            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(in(2).tim, in(2).obwamp, 'mo');
            
-            plot(in(1).tim, in(1).pkamp, 'bo');
-            plot(in(2).tim, in(2).pkamp, 'mo');
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp, 'mo');
           
             
     ax(2) = subplot(312); title('postcal'); hold on; %ylim([0,.05]);
-%             plot(in(1).tim, in(1).obwamp*2, 'bo');
-%             plot(in(2).tim, in(2).obwamp, 'mo');
+            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(in(2).tim, in(2).obwamp, 'mo');
           
-            plot(in(1).tim, in(1).pkamp, 'bo');
-            plot(in(2).tim, in(2).pkamp, 'mo');
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp, 'mo');
             
 %             yline(thresh.lower, 'k-');
 %             yline(thresh.upper, 'k-');
