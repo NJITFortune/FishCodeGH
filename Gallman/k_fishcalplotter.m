44e6c92550064fe365b6b6e7a715e6042b97ed4d
Eric Fortune <eric.fortune@njit.edu>
2022-08-23 11:09:25 -0400

    / ‘Gallman/k_fishcalplotter.m’

--- Gallman/k_fishcalplotter.m
+++ Gallman/k_fishcalplotter.m
@@ -11,15 +11,15 @@ figure(455); clf; hold on;
 in = hi;
 
     ax(1) = subplot(311); title('High frequency fish'); hold on; %ylim([0,.205]);
-%             plot(in(1).tim, in(1).obwamp, 'bo');
-%             plot(in(2).tim, in(2).obwamp, 'mo');
+            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(in(2).tim, in(2).obwamp, 'mo');
            
-            plot(in(1).tim, in(1).pkamp*2, 'bo');
-            plot(in(2).tim, in(2).pkamp, 'mo');
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp, 'mo');
           
             
     ax(2) = subplot(312); title('postcal'); hold on;% ylim([0,.2105]);
-            plot(in(1).tim, in(1).obwamp*2, 'bo');
+            plot(in(1).tim, in(1).obwamp, 'bo');
             plot(in(2).tim, in(2).obwamp, 'mo');
           
 %             plot(in(1).tim, in(1).pkamp, 'bo');
@@ -40,15 +40,15 @@ clear in
 in = lo;
 
     ax(1) = subplot(311); title('Low frequency fish'); hold on; %ylim([0,.21]);
-%             plot(in(1).tim, in(1).obwamp, 'bo');
-%             plot(in(2).tim, in(2).obwamp, 'mo');%1.75
+            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(in(2).tim, in(2).obwamp, 'mo');%1.75
 %            
-            plot(in(1).tim, in(1).pkamp*2, 'bo');
-            plot(in(2).tim, in(2).pkamp, 'mo');
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp, 'mo');
           
             
     ax(2) = subplot(312); title('postcal'); hold on; %ylim([0,.2105]);
-            plot(in(1).tim, in(1).obwamp*2, 'bo');%*2
+            plot(in(1).tim, in(1).obwamp, 'bo');%*2
             plot(in(2).tim, in(2).obwamp, 'mo');
           
 %             plot(in(1).tim, in(1).pkamp, 'bo');
