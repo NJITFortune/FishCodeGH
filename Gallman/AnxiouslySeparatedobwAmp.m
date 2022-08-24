4d03b017fc9219bceba2bd40087c40f548316ee9
Eric Fortune <eric.fortune@njit.edu>
2022-08-24 11:55:53 -0400

    / ‘Gallman/AnxiouslySeparatedobwAmp.m’

--- Gallman/AnxiouslySeparatedobwAmp.m
+++ Gallman/AnxiouslySeparatedobwAmp.m
@@ -37,7 +37,7 @@ for j = 1:length(in.s)
     noisef2 = find(f2.fftfreq < 100);
 
   %use frequencies of each fish to define new frequency ranges for obw
-    %midpoint = in.s(j).lofreq + ((in.s(j).hifreq - in.s(j).lofreq)/2);
+    midpoint = in.s(j).lofreq + ((in.s(j).hifreq - in.s(j).lofreq)/2);
 
   %perform obw on both fish
     %low frequency fish
@@ -52,7 +52,7 @@ for j = 1:length(in.s)
 %         [n,m] = butter(5, [(in.s(j).lofreq-rango)/(Fs/2) (in.s(j).lofreq+rango)/(fs/2)]);
 %         [p,o] = bandstop()
 
-                [out(j).lobw1, out(j).loflo1, out(j).lofhi1, out(j).loAmpobw1] = obw(e1, Fs, [in.s(j).lofreq-rango in.s(j).lofreq+rango]);
+                [out(j).lobw1, out(j).loflo1, out(j).lofhi1, out(j).loAmpobw1] = obw(e1, Fs, [in.s(j).lofreq-rango midpoint]);
 %                 if mod(j,plotnum) == 0
 %                     figure(j); clf; hold on; 
 %                     obw(e1, Fs, [in.s(j).lofreq-rango in.s(j).lofreq+rango]);ylabel('low1');xlim([0,1]);
@@ -71,7 +71,7 @@ for j = 1:length(in.s)
                 out(j).bad2idx = j;
                 out(j).loAmpobw2 = -0.1;
         else
-                [out(j).lobw2, out(j).loflo2, out(j).lofhi2, out(j).loAmpobw2] = obw(e2, Fs, [in.s(j).lofreq-rango in.s(j).lofreq+rango]);
+                [out(j).lobw2, out(j).loflo2, out(j).lofhi2, out(j).loAmpobw2] = obw(e2, Fs, [in.s(j).lofreq-rango midpoint]);
 %                  if mod(j,plotnum) == 0
 %                     figure(j); clf;  hold on;
 %                     obw(e2, Fs, [in.s(j).lofreq-rango in.s(j).lofreq+rango]);ylabel('low2');xlim([0,1]); 
@@ -89,7 +89,7 @@ for j = 1:length(in.s)
                 out(j).hiAmpobw1 = -0.1;
         else
 
-            [out(j).hibw1, out(j).hiflo1, out(j).hifhi1, out(j).hiAmpobw1] = obw(e1, Fs, [in.s(j).hifreq-rango in.s(j).hifreq+rango]);
+            [out(j).hibw1, out(j).hiflo1, out(j).hifhi1, out(j).hiAmpobw1] = obw(e1, Fs, [midpoint in.s(j).hifreq+rango]);
     
 %              if mod(j,plotnum) == 0
 %                 figure(j+10); clf; hold on;
@@ -109,7 +109,7 @@ for j = 1:length(in.s)
         else
 
 
-        [out(j).hibw2, out(j).hiflo2, out(j).hifhi2, out(j).hiAmpobw2] = obw(e2, Fs, [in.s(j).hifreq-rango in.s(j).hifreq+rango]);
+        [out(j).hibw2, out(j).hiflo2, out(j).hifhi2, out(j).hiAmpobw2] = obw(e2, Fs, [midpoint in.s(j).hifreq+rango]);
 %          if mod(j,plotnum) == 0
 %             figure(j+10); clf; hold on;
 %             obw(e2, Fs, [in.s(j).hifreq-rango in.s(j).hifreq+rango]);ylabel('hi2');xlim([0,1]);
@@ -135,25 +135,27 @@ pause(1); close(ff);
     linkaxes(ax, 'x');
 
 %%
-%     figure(66); clf; hold on;
-%        ax(1) = subplot(211); title('low frequency fish'); hold on;
-%          plot([in.s([in.s.lotube]==1).timcont]/3600, [out.lofhi1], 'b.');
-%          plot([in.s([in.s.lotube]==1).timcont]/3600, [out.loflo1], 'b.');
-%          plot([in.s([in.s.lotube]==2).timcont]/3600, [out.lofhi2], 'c.'); 
-%          plot([in.s([in.s.lotube]==2).timcont]/3600, [out.loflo2], 'c.');
-%          
-%          plot([in.s([in.s.lotube]==1).timcont]/3600, [in.s([in.s.lotube]==1).lofreq], 'k.');
-%          plot([in.s([in.s.lotube]==2).timcont]/3600, [in.s([in.s.lotube]==2).lofreq], 'k.');
-% 
-%        ax(2) = subplot(212); title('high frequency fish'); hold on;  
-%          plot([in.s([in.s.hitube]==1).timcont]/3600, [out.hifhi1], 'r.');
-%          plot([in.s([in.s.hitube]==1).timcont]/3600, [out.hiflo1], 'r.');
-%          plot([in.s([in.s.hitube]==2).timcont]/3600, [out.hifhi2], 'm.'); 
-%          plot([in.s([in.s.hitube]==2).timcont]/3600, [out.hiflo2], 'm.'); 
-% 
-%          plot([in.s([in.s.hitube]==1).timcont]/3600, [in.s([in.s.hitube]==1).hifreq], 'k.'); 
-%          plot([in.s([in.s.hitube]==2).timcont]/3600, [in.s([in.s.hitube]==2).hifreq], 'k.'); 
-%      linkaxes(ax, 'x');
+    figure(66); clf; hold on;
+       ax(1) = subplot(211); title('low frequency fish'); hold on;
+       if ~isempty([out.lofhi])
+         plot([in.s([in.s.lotube]==1).timcont]/3600, [out.lofhi1], 'b.');
+         plot([in.s([in.s.lotube]==1).timcont]/3600, [out.loflo1], 'b.');
+         plot([in.s([in.s.lotube]==2).timcont]/3600, [out.lofhi2], 'c.'); 
+         plot([in.s([in.s.lotube]==2).timcont]/3600, [out.loflo2], 'c.');
+       end  
+         plot([in.s([in.s.lotube]==1).timcont]/3600, [in.s([in.s.lotube]==1).lofreq], 'k.');
+         plot([in.s([in.s.lotube]==2).timcont]/3600, [in.s([in.s.lotube]==2).lofreq], 'k.');
+
+       ax(2) = subplot(212); title('high frequency fish'); hold on;  
+       if ~isempty([out.hifhi])
+         plot([in.s([in.s.hitube]==1).timcont]/3600, [out.hifhi1], 'r.');
+         plot([in.s([in.s.hitube]==1).timcont]/3600, [out.hiflo1], 'r.');
+         plot([in.s([in.s.hitube]==2).timcont]/3600, [out.hifhi2], 'm.'); 
+         plot([in.s([in.s.hitube]==2).timcont]/3600, [out.hiflo2], 'm.'); 
+       end
+         plot([in.s([in.s.hitube]==1).timcont]/3600, [in.s([in.s.hitube]==1).hifreq], 'k.'); 
+         plot([in.s([in.s.hitube]==2).timcont]/3600, [in.s([in.s.hitube]==2).hifreq], 'k.'); 
+     linkaxes(ax, 'x');
 
 %% save into output structure
 
