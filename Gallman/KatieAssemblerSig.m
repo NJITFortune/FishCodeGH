5cfe05b86e359169e808d56dc17cbfb154b25dac
Eric Fortune <eric.fortune@njit.edu>
2022-09-09 11:52:11 -0400

    / ‘Gallman/KatieAssemblerSig.m’

--- Gallman/KatieAssemblerSig.m
+++ Gallman/KatieAssemblerSig.m
@@ -107,6 +107,24 @@ for k = 1:length(iFiles)
            data4analysis = data4analysis(newidx);
            
             % ANALYSES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            %exclude noisy data
+            fft = fftmachine(data(:,j), Fs);
+             sig = find(fft.fftfreq > 200 & fft.fftfreq < 800);
+             noise = find(fft.fftfreq < 100);
+
+         %if Noise > Signal
+            if max(fft.fftdata(sig))/max(fft.fftdata(noise)) < 1 
+                %obw 
+                out(j).s(k).bw = -0.1; out(j).s(k).flo = -0.1; out(j).s(k).fhi = -0.1;
+                out(j).s(k).obwAmp = -0.1;
+                %zAmp
+                out(j).s(k).zAmp = -0.1;
+                %fftmachine
+                out(j).s(k).fftFreq = -0.1; out(j).s(k).inputsig = -0.1;
+                out(j).s(k).peakfftAmp = -0.1; out(j).s(k).sumfftAmp = -0.1;
+                
+            else
             
             % OBW
             [~,~,~,out(j).s(k).obwAmp] = obw(data4analysis, Fs, [botFreqOBW topFreqOBW]);
@@ -114,7 +132,7 @@ for k = 1:length(iFiles)
             out(j).s(k).zAmp = k_zAmp(data4analysis);
             % FFT Machine
             [out(j).s(k).fftFreq, out(j).s(k).peakfftAmp, out(j).s(k).sumfftAmp, out(j).s(k).inputsig] = k_fftplussig(data4analysis, Fs, sigfreq); 
-        
+            end
       
             out(j).s(k).light = mean(data(:,lightchan));
             out(j).s(k).temp = mean(data(:,tempchan));
