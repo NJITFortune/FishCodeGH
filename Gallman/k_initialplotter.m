451f644fdab750651b0c630b9da7c0a5813eda44
Eric Fortune <eric.fortune@njit.edu>
2022-09-13 18:06:09 -0400

    / ‘Gallman/hkgme.mlx’

--- Gallman/k_initialplotter.m
+++ Gallman/k_initialplotter.m
@@ -33,7 +33,7 @@ ax(1) = subplot(411); hold on; title('obwAmp'); %ylim([0,5]);
 %    plot([out.e(2).s(ttsf{2}).timcont]/(60*60), [out.e(2).s(ttsf{2}).sumfftAmp], '.');
 %    plot([out.e(1).s(ttsf{1}).timcont]/(60*60), [out.e(1).s(ttsf{1}).sumfftAmp],'.');
 
-     %  plot([out.e(2).s(tto{2}).timcont]/(60*60), [out.e(2).s(tto{2}).obwAmp], '.');
+       plot([out.e(2).s(tto{2}).timcont]/(60*60), [out.e(2).s(tto{2}).obwAmp], '.');
        plot([out.e(1).s(tto{1}).timcont]/(60*60), [out.e(1).s(tto{1}).obwAmp], '.');
 
 ax(2) = subplot(412); hold on; title('fish frequency');   
