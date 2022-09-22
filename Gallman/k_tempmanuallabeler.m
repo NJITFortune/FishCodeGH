13bf093dbe4b0d611fc214b40db0b907aca05164
Eric Fortune <eric.fortune@njit.edu>
2022-09-22 10:37:10 -0400

    / ‘Gallman/k_tempmanuallabeler.m’

--- Gallman/k_tempmanuallabeler.m
+++ Gallman/k_tempmanuallabeler.m
@@ -93,6 +93,8 @@ end
     plot([temptims' temptims'], ylim, 'c-');
 
 
+xxkg(k).info.temptims = temptims
+
 % 
 %     figure(32); clf; hold on;
 %          risetime(temp, timcont);
@@ -105,42 +107,3 @@ end
 
 
 
-%  %startim = input('Enter the start time for the experiment: ');
-  startim = temptims(1)-.5;
-% 
-
-    %caclulate hours when the light changed
-        numbercycles = floor(timcont(end)/6); %number of cycles in data
-        timz = 1:1:numbercycles;
-        testtims(timz) = startim + (6*(timz-1)); %without for-loop
-
-%plot([testtims', testtims'], ylim, 'r-');
-
-%         templines(1) = startim;
-%         for j = 2:length(timz)
-%             
-%             if mod(j,2) == 0
-%                 templines(j) = templines(j-1) + hotdur;
-%             else
-%                 templines(j) = templines(j-1) + colddur;
-%             end
-% 
-%         end
-% 
-%     plot([templines', templines'], ylim, 'r-');
-
-
-% 
-%         out(timz) = startim + (in.info.ld*(timz-1)); %without for-loop
-%         
-%          %find the time indicies for the first light cycle
-%         lidx = [in.e(1).s.timcont] < out(2)*(60*60);  
-%     %take the mean of the light values in the first cycle
-%         meaninitialbright = mean([in.e(1).s(lidx).light]);
-%     
-%     %Assign negative values to luz when lights were off
-%         if meaninitialbright < 2.5 % Lights in initial period were off
-%             out(1:2:end) = -out(1:2:end); % Set initial period and every other subseqent  as off
-%         else
-%             out(2:2:end) = -out(2:2:end); % Set the second period and every other subsequent as off.
-%         end
