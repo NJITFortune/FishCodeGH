a2edc7cc589a11aee528a38114eedec1189fd5f8
Eric Fortune <eric.fortune@njit.edu>
2022-09-27 14:17:48 -0400

    / ‘Gallman/k_fishcalplotter.m’

--- Gallman/k_fishcalplotter.m
+++ Gallman/k_fishcalplotter.m
@@ -10,8 +10,8 @@ function  k_fishcalplotter(hi, lo, out)
 figure(455); clf; hold on;
 in = hi;
 
-length(in(2).tim)
-length(in(2).obwamp)
+% length(in(2).tim)
+% length(in(2).obwamp)
 
     ax(1) = subplot(311); title('High frequency fish'); hold on; %ylim([0,1]);
             plot(in(1).tim, in(1).obwamp, 'bo');
@@ -39,30 +39,30 @@ linkaxes(ax, 'x');
 
 %%
 
-% figure(543); clf; hold on;
-% clear in
-% in = lo;
-% 
-%     ax(1) = subplot(311); title('Low frequency fish'); hold on; %ylim([0,1]);
-%             plot(in(1).tim, in(1).obwamp, 'bo');
-%             plot(in(2).tim, in(2).obwamp, 'mo');%1.75
-% %            
-% %             plot(in(1).tim, in(1).pkamp, 'bo');
-% %             plot(in(2).tim, in(2).pkamp, 'mo');
-%           
-%             
-%     ax(2) = subplot(312); title('postcal'); hold on; %ylim([0,1]);
-%             plot(in(1).tim, in(1).obwamp*1.5, 'bo');%*2
-%             plot(in(2).tim, in(2).obwamp, 'mo');
-%           
-% %             plot(in(1).tim, in(1).pkamp, 'bo');
-% %             plot(in(2).tim, in(2).pkamp, 'mo');
-%             
-% %             yline(thresh.lower, 'k-');
-% %             yline(thresh.upper, 'k-');
-%     
-%     ax(3) = subplot(313); title('light cycle'); hold on;
-%             plot([out.timcont]/3600, [out.light]);
-%             
-% 
-% linkaxes(ax, 'x');
+figure(543); clf; hold on;
+clear in
+in = lo;
+
+    ax(1) = subplot(311); title('Low frequency fish'); hold on; %ylim([0,1]);
+            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(in(2).tim, in(2).obwamp, 'mo');%1.75
+%            
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp, 'mo');
+          
+            
+    ax(2) = subplot(312); title('postcal'); hold on; %ylim([0,1]);
+            plot(in(1).tim, in(1).obwamp*1.5, 'bo');%*2
+            plot(in(2).tim, in(2).obwamp, 'mo');
+          
+%             plot(in(1).tim, in(1).pkamp, 'bo');
+%             plot(in(2).tim, in(2).pkamp, 'mo');
+            
+%             yline(thresh.lower, 'k-');
+%             yline(thresh.upper, 'k-');
+    
+    ax(3) = subplot(313); title('light cycle'); hold on;
+            plot([out.timcont]/3600, [out.light]);
+            
+
+linkaxes(ax, 'x');
