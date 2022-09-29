355b3d84bd02ccd6b02e633207ebe25e7d3098c8
Eric Fortune <eric.fortune@njit.edu>
2022-09-29 12:42:38 -0400

    / ‘Gallman/splinevsmetamucil.m’

--- Gallman/splinevsmetamucil.m
+++ Gallman/splinevsmetamucil.m
@@ -102,33 +102,33 @@ obwyyminusmean = obwyy-mean(obwyy);
         %regularize data to ReFs interval
         [regtim, ~, regobwpeaks] = k_regularmetamucil(peaktim, obwpeaks, timcont, obw, oldfreq, ReFs, lighttimes);
 
-         %regobwpeaksminusmean = regobwpeaks - mean(regobwpeaks);
+         regobwpeaksminusmean = regobwpeaks - mean(regobwpeaks);
 
-
-    %filter data
-        %cut off frequency
-         highWn = 0.005/(ReFs/2); % Original but perhaps too strong for 4 and 5 hour days
-        %highWn = 0.001/(ReFs/2);
-
-        %low pass removes spikey-ness
-        lowWn = 0.025/(ReFs/2);
-        [dd,cc] = butter(5, lowWn, 'low');
-        datadata = filtfilt(dd,cc, double(regobwpeaks));
-
-        
-        %high pass removes feeding trend for high frequency experiments
-       
-        [bb,aa] = butter(5, highWn, 'high');
-      %  datadata = filtfilt(bb,aa, double(regobwpeaks)); %double vs single matrix?
-        datadata = filtfilt(bb,aa, datadata); %double vs single matrix?
-
-
+% 
+%     %filter data
+%         %cut off frequency
+%          highWn = 0.005/(ReFs/2); % Original but perhaps too strong for 4 and 5 hour days
+%         %highWn = 0.001/(ReFs/2);
+% 
 %         %low pass removes spikey-ness
-%         lowWn = 0.01/(ReFs/2);
+%         lowWn = 0.025/(ReFs/2);
 %         [dd,cc] = butter(5, lowWn, 'low');
 %         datadata = filtfilt(dd,cc, double(regobwpeaks));
 % 
-         regobwpeaksminusmean = datadata - mean(datadata);
+%         
+%         %high pass removes feeding trend for high frequency experiments
+%        
+%         [bb,aa] = butter(5, highWn, 'high');
+%       %  datadata = filtfilt(bb,aa, double(regobwpeaks)); %double vs single matrix?
+%         datadata = filtfilt(bb,aa, datadata); %double vs single matrix?
+% 
+% 
+% %         %low pass removes spikey-ness
+% %         lowWn = 0.01/(ReFs/2);
+% %         [dd,cc] = butter(5, lowWn, 'low');
+% %         datadata = filtfilt(dd,cc, double(regobwpeaks));
+% % 
+%          regobwpeaksminusmean = datadata - mean(datadata);
 
       
 
