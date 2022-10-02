ce104fac81d4f8b0d0f1065a8cf71d86888e2d4e
Kathryn Gallman <keg26@njit.edu>
2022-10-02 11:17:53 -0400

    / ‘Gallman/k_initialplotter.m’

--- Gallman/k_initialplotter.m
+++ Gallman/k_initialplotter.m
@@ -27,21 +27,21 @@ function k_initialplotter(out)
 figure(4); clf; hold on;
     %set(gcf, 'Position', [200 100 2*560 2*420]);
 
+plotnum = 5;
 
-
-ax(1) = subplot(411); hold on; title('obwAmp'); %ylim([0,5]);
+ax(1) = subplot(plotnum11); hold on; title('obwAmp'); %ylim([0,5]);
 %    plot([out.e(2).s(ttsf{2}).timcont]/(60*60), [out.e(2).s(ttsf{2}).sumfftAmp], '.');
 %    plot([out.e(1).s(ttsf{1}).timcont]/(60*60), [out.e(1).s(ttsf{1}).sumfftAmp],'.');
 
        plot([out.e(2).s(tto{2}).timcont]/(60*60), [out.e(2).s(tto{2}).obwAmp], '.');
        plot([out.e(1).s(tto{1}).timcont]/(60*60), [out.e(1).s(tto{1}).obwAmp], '.');
 
-ax(2) = subplot(412); hold on; title('fish frequency');   
+ax(2) = subplot(plotnum12); hold on; title('fish frequency');   
     
     plot([out.e(2).s(tto{2}).timcont]/(60*60), [out.e(2).s(tto{2}).fftFreq], '.k', 'Markersize', 8);
     plot([out.e(1).s(tto{1}).timcont]/(60*60), [out.e(1).s(tto{1}).fftFreq], '.k', 'Markersize', 8);
 
-ax(3) = subplot(413); hold on; title('temp');
+ax(3) = subplot(plotnum13); hold on; title('temp');
     
     plot([out.e(2).s(tto{2}).timcont]/(60*60), [out.e(2).s(tto{2}).temp], '-r', 'Markersize', 8);
     plot([out.e(1).s(tto{1}).timcont]/(60*60), [out.e(1).s(tto{1}).temp], '-r', 'Markersize', 8);
@@ -52,7 +52,7 @@ ax(3) = subplot(413); hold on; title('temp');
 %     plot([out.e(2).s.timcont]/(60*60), ch2tempC, '-r', 'Markersize', 8);
 %     plot([out.e(1).s.timcont]/(60*60), ch1tempC, '-r', 'Markersize', 8);
 %  
-ax(4) = subplot(414); hold on; title('light transitions');  
+ax(4) = subplot(plotnum14); hold on; title('light transitions');  
     plot([out.e(1).s.timcont]/(60*60), [out.e(1).s.light], '.', 'Markersize', 8);
     ylim([-1, 6]);
     xlabel('Continuous');
