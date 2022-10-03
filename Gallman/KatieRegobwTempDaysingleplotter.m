0d1e26f3e70c9e8633ac3e789eb56cc554fecdd3
Eric Fortune <eric.fortune@njit.edu>
2022-10-03 14:10:41 -0400

    / ‘Gallman/KatieRegobwTempDaysingleplotter.m’

--- Gallman/KatieRegobwTempDaysingleplotter.m
+++ Gallman/KatieRegobwTempDaysingleplotter.m
@@ -375,7 +375,7 @@ end
 
 
 
- figure(779); clf; title('frequency temp days');hold on; ylim([-.5,.5]); ylim([-50 50]);xlim([0, ttim(end)/3600]);
+ figure(779); clf; title('frequency temp days');hold on; xlim([0, ttim(end)/3600]);%ylim([-50 50])
  plot(tday(1).tim/3600, tday(1).freq);
 a = ylim;
         
