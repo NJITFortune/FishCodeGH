061cbbf25f1379918e783b31f231ef959f743360
Eric Fortune <eric.fortune@njit.edu>
2022-10-05 17:19:46 -0400

    + ‘Gallman/k_matlabcolors.m’

---
+++ Gallman/k_matlabcolors.m
@@ -0,0 +1,12 @@
+function matcol = k_matlabcolors
+
+blue = [0 0.4470 0.7410];
+orange = [0.8500 0.3250 0.0980];
+yellow = [0.9290 0.6940 0.1250];
+purple = [0.4940 0.1840 0.5560];
+green = [0.4660 0.6740 0.1880];
+lightblue = [0.3010 0.7450 0.9330];
+maroon = [0.6350 0.0780 0.1840];
+
+matcol = [blue; orange; yellow; purple; green; lightblue; maroon];
+
