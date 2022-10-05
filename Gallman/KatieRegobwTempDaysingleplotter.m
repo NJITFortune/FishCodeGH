c196a56b2fef0893980e533dd31178fa641bd861
Eric Fortune <eric.fortune@njit.edu>
2022-10-05 16:11:13 -0400

    / ‘Gallman/KatieRegobwTempDaysingleplotter.m’

--- Gallman/KatieRegobwTempDaysingleplotter.m
+++ Gallman/KatieRegobwTempDaysingleplotter.m
@@ -9,7 +9,7 @@ clearvars -except xxkg hkg k xxkg2 hkg2
 % % % 
 in = xxkg(k);
 ReFs = 20;
-
+heat = 8;
 channel = 1;
 
 % light = 4; %start with light
@@ -56,7 +56,13 @@ if isempty(poweridx) %if there are no values in poweridx []
 
     end  
 
-     
+      if heat == 7 && tiz(1) > 0 %we want start with cooling and the experiment starts with warming
+        temptims = temptims(2:end); %skip the first temptim so we start with cooling
+        tiz = tiz(2:end); hotter = hotter(2:end);
+      elseif heat == 8 && tiz(1) < 0 %we want start with warming and the experiment starts with cooling
+        temptims = temptims(2:end); %skip the first temptim so we start with cooling
+        tiz = tiz(2:end); colder = colder(2:end);
+      end
 
 else %we have poweridx values
 
@@ -79,6 +85,14 @@ else %we have poweridx values
         
             end
 
+  
+      if heat == 7 && tiz(1) > 0 %we want start with cooling and the experiment starts with warming
+        temptims = temptims(2:end); %skip the first temptim so we start with cooling
+        tiz = tiz(2:end); hotter = hotter(2:end);
+      elseif heat == 8 && tiz(1) < 0 %we want start with warming and the experiment starts with cooling
+        temptims = temptims(2:end); %skip the first temptim so we start with cooling
+        tiz = tiz(2:end);colder = colder(2:end);
+      end
 
 end
 
@@ -176,6 +190,8 @@ figure(455); clf; hold on;
         [bb,aa] = butter(5, highWn, 'high');
         datadata = filtfilt(bb,aa, datadata); %double vs single matrix?
         
+
+  
 
     %trim everything to temptims
 
