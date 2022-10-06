8bb3587f5c359763cfaa3276c903d1eaf591e1d8
Eric Fortune <eric.fortune@njit.edu>
2022-10-06 14:34:24 -0400

    / ‘Gallman/k_fishcalplotter.m’

--- Gallman/k_fishcalplotter.m
+++ Gallman/k_fishcalplotter.m
@@ -44,7 +44,7 @@ clear in
 in = lo;
 
     ax(1) = subplot(311); title('Low frequency fish'); hold on; %ylim([0,1]);
-            plot(in(1).tim, in(1).obwamp, 'bo');
+            plot(in(1).tim, in(1).obwamp*1.5, 'bo');
             plot(in(2).tim, in(2).obwamp, 'mo');%1.75
 %            
 %             plot(in(1).tim, in(1).pkamp, 'bo');
