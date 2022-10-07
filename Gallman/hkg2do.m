35a4b2aa7bb9d7a43131c429010f26b6af1ca0fc
Eric Fortune <eric.fortune@njit.edu>
2022-10-07 11:16:29 -0400

    / ‘Gallman/hkg2do.m’

--- Gallman/hkg2do.m
+++ Gallman/hkg2do.m
@@ -1,5 +1,7 @@
 function out = hkg2do(in, fish)
 %makes new data structure of individual multifish 
+%in = kg2(k)
+%fish = hifish or lofish - from within kg2 struct
 
     out.info.luz = in.info.luz;
     out.info.folder = in.info.folder;
@@ -11,8 +13,9 @@ if fish == 6 %hi frequency fish
     out.idx = in.hiidx;
     out.info.poweridx = in.info.Hipoweridx;
 
+    
     %hifish data
-    for j = 1:length(kg2(k).hifish)
+    for j = 1:length(in.hifish)
         out.s(j).obwAmp = kg2(k).hifish(j).obwAmp;
     end
     out.s.obwAmp = [in.hifish.obwAmp];
