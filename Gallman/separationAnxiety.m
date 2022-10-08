262f0e71f2a4e4e097a3063e752c7888f0b96709
Eric Fortune <eric.fortune@njit.edu>
2022-10-08 14:41:22 -0400

    / ‘Gallman/separationAnxiety.m’

--- Gallman/separationAnxiety.m
+++ Gallman/separationAnxiety.m
@@ -1,7 +1,7 @@
 % A new attempt at frequency tracking two Eigenmannia in the tank
 clearvars -except kg kg2 rkg k xxkg hkg
 Fs = 40000;
-freqs = [300 650]; %freq range of typical eigen EOD
+freqs = [200 600]; %freq range of typical eigen EOD
 userfilespec = 'Eigen*';
 
 % Max frequency change
