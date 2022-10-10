7015d6f8ce39c5e4dab9a16d0ebe491b5eda230f
Eric Fortune <eric.fortune@njit.edu>
2022-10-10 16:45:57 -0400

    / ‘Gallman/k_tempdaymeans.m’

--- Gallman/k_tempdaymeans.m
+++ Gallman/k_tempdaymeans.m
@@ -33,6 +33,7 @@ function [exp, fish, td] = k_tempdaymeans(in)
               %fill temporary vector with data from each day 
                 mtday(k,:) = in(j).tday(k).obw(1:shortest);
                 ftday(k,:) = in(j).tday(k).freq(1:shortest);%-mean(in(j).tday(k).freq(1:shortest));
+                tday(k,:) = in(j).tday(k).temp(1:shortest);
                 
                 fish(j).amprange(k,:) = in(j).tday(k).amprange;
                 amprange(k,:) = in(j).tday(k).amprange;
@@ -42,16 +43,21 @@ function [exp, fish, td] = k_tempdaymeans(in)
       %average across days
       avgrange(j,:) = mean(amprange);
  
-      %necessary so that the mean of a single day isn't one value  
-      if length(in(j).tday) > 1  
+      %necessary so that the mean of a single day isn't one value 
+       numdays = size(in(j).tday);
+      if  numdays(1) == 1  
       %average across days   
+       tdaymean(j,:) = mtday;
+       ftdaymean(j,:) = ftday-mean(ftday);
+       tempday(j,:) = tday;
+
+      else
        tdaymean(j,:) = mean(mtday);
        zeroedfmean = mean(ftday);
        ftdaymean(j,:) = zeroedfmean-mean(zeroedfmean);
        
-      else
-       tdaymean(j,:) = mtday;
-        ftdaymean(j,:) = ftday-mean(ftday);
+        tempday(j,:) = mean(tday);
+      
       end
 
 
@@ -63,13 +69,17 @@ function [exp, fish, td] = k_tempdaymeans(in)
     if numrow(1) == 1
          exp.meanoftempexperimentmeans = movmean(tdaymean, 5);
          exp.meanoftempfreqmeans = movmean(ftdaymean, 5);
+         exp.meanoftemperaturemeans = tempday;
     else
+
     %averages for each x hour set of experiments
     expmean = mean(tdaymean);
     exp.meanoftempexperimentmeans = movmean(expmean, 5);
 
     freqmean = mean(ftdaymean);
     exp.meanoftempfreqmeans = movmean(freqmean, 5);
+
+    exp.meanoftemperaturemeans = mean(tempday);
     end
     %testmean = movmean(expmean, 5);
     %average max and min
