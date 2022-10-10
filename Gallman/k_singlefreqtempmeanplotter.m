afe22a8bb7523e7ef8c550aa6159c154430ee91c
Eric Fortune <eric.fortune@njit.edu>
2022-10-10 16:02:58 -0400

    + ‘Gallman/k_singlefreqtempmeanplotter.m’

---
+++ Gallman/k_singlefreqtempmeanplotter.m
@@ -0,0 +1,46 @@
+function k_singlefreqtempmeanplotter(day)
+
+%% plot to check
+
+
+%average over single day    
+%figure(56); clf; hold on; 
+
+ 
+    subplot(121); title('temperature'); hold on;
+    %mean two ways to prove math
+  %  mday = zeros(1, length(day(1).tim));        
+     for j = 1:length(day)
+            plot(day(j).tim/3600, day(j).temp);
+            tempday(j,:) = day(j).temp;
+         %   mday = mday + day(j).Sobwyy;
+            
+     end
+        
+            ttday= mean(tempday);
+           % othermday = mday/(length(day));
+            plot(day(1).tim/3600, ttday, 'r-', 'LineWidth', 3);
+          %  plot(day(1).tim/3600, othermday, 'b-', 'LineWidth', 3);
+            plot([day(1).ld day(1).ld], ylim, 'k-', 'LineWidth', 3);
+
+
+   subplot(122); title('frequency'); hold on;
+    %mean two ways to prove math
+  %  mday = zeros(1, length(day(1).tim));        
+     for j = 1:length(day)
+            plot(day(j).tim/3600, day(j).freq);
+            freqday(j,:) = day(j).freq;
+         %   mday = mday + day(j).Sobwyy;
+            
+     end
+        
+            ffday= mean(freqday);
+           % othermday = mday/(length(day));
+            plot(day(1).tim/3600, ffday, 'b-', 'LineWidth', 3);
+          %  plot(day(1).tim/3600, othermday, 'b-', 'LineWidth', 3);
+            plot([day(1).ld day(1).ld], ylim, 'k-', 'LineWidth', 3);
+            
+            
+
+
+
