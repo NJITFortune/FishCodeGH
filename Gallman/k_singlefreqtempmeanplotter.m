e93dbb1c1002f9c16a7dc5682f0c04ed28d0590b
Eric Fortune <eric.fortune@njit.edu>
2022-10-10 16:08:22 -0400

    / ‘Gallman/k_singlefreqtempmeanplotter.m’

--- Gallman/k_singlefreqtempmeanplotter.m
+++ Gallman/k_singlefreqtempmeanplotter.m
@@ -1,13 +1,13 @@
-function k_singlefreqtempmeanplotter(day)
+function k_singlefreqtempmeanplotter(day, j)
 
 %% plot to check
 
 
 %average over single day    
-%figure(56); clf; hold on; 
+figure(j + 700); clf; hold on; 
 
  
-    subplot(121); title('temperature'); hold on;
+   % subplot(121); title('temperature'); hold on;
     %mean two ways to prove math
   %  mday = zeros(1, length(day(1).tim));        
      for j = 1:length(day)
@@ -24,21 +24,21 @@ function k_singlefreqtempmeanplotter(day)
             plot([day(1).ld day(1).ld], ylim, 'k-', 'LineWidth', 3);
 
 
-   subplot(122); title('frequency'); hold on;
-    %mean two ways to prove math
-  %  mday = zeros(1, length(day(1).tim));        
-     for j = 1:length(day)
-            plot(day(j).tim/3600, day(j).freq);
-            freqday(j,:) = day(j).freq;
-         %   mday = mday + day(j).Sobwyy;
-            
-     end
-        
-            ffday= mean(freqday);
-           % othermday = mday/(length(day));
-            plot(day(1).tim/3600, ffday, 'b-', 'LineWidth', 3);
-          %  plot(day(1).tim/3600, othermday, 'b-', 'LineWidth', 3);
-            plot([day(1).ld day(1).ld], ylim, 'k-', 'LineWidth', 3);
+%    subplot(122); title('frequency'); hold on;
+%     %mean two ways to prove math
+%   %  mday = zeros(1, length(day(1).tim));        
+%      for j = 1:length(day)
+%             plot(day(j).tim/3600, day(j).freq);
+%             freqday(j,:) = day(j).freq;
+%          %   mday = mday + day(j).Sobwyy;
+%             
+%      end
+%         
+%             ffday= mean(freqday);
+%            % othermday = mday/(length(day));
+%             plot(day(1).tim/3600, ffday, 'b-', 'LineWidth', 3);
+%           %  plot(day(1).tim/3600, othermday, 'b-', 'LineWidth', 3);
+%             plot([day(1).ld day(1).ld], ylim, 'k-', 'LineWidth', 3);
             
             
 
