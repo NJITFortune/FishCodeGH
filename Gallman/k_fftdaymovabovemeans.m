77308bacf343edc92902c516b035ac7a04b255fc
Eric Fortune <eric.fortune@njit.edu>
2022-10-11 10:43:42 -0400

    / ‘Gallman/k_fftdaymovabovemeans.m’

--- Gallman/k_fftdaymovabovemeans.m
+++ Gallman/k_fftdaymovabovemeans.m
@@ -25,7 +25,8 @@ ld = in(1).day(1).ld;
                 amprange(k,:) = in(j).day(k).amprange;
 
                 fday(k,:) = in(j).day(k).freq;
-                tday(k,:) = k_temptocelcius(in(j).day(k).temp);
+               % tday(k,:) = k_temptocelcius(in(j).day(k).temp);
+                tday(k,:) = in(j).day(k).temp;
                
         end
     
