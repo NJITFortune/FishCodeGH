bac8210edd2212569362a817b74a844fd7a3f9c1
Eric Fortune <eric.fortune@njit.edu>
2022-10-11 17:51:33 -0400

    / ‘Gallman/KatieMultiHIRegTempobwday.m’

--- Gallman/KatieMultiHIRegTempobwday.m
+++ Gallman/KatieMultiHIRegTempobwday.m
@@ -88,7 +88,7 @@ end
 %% crop data to temptims
 if isempty(poweridx) %if there are no values in poweridx []
 
-    for j = 3%:length(temptims)
+    for j = 2:length(temptims)
     
     tempidx = find(timcont/3600 >= temptims(j-1) & timcont/3600 < temptims(j));
 
