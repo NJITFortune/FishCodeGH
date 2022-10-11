110f3eef177b8313354715213b4a6bffb67d10ce
Eric Fortune <eric.fortune@njit.edu>
2022-10-11 17:24:22 -0400

    / ‘Gallman/KatieMultiHIRegTempobwday.m’

--- Gallman/KatieMultiHIRegTempobwday.m
+++ Gallman/KatieMultiHIRegTempobwday.m
@@ -100,13 +100,13 @@ if isempty(poweridx) %if there are no values in poweridx []
 
     end  
 
-%       if heat == 7 && tiz(1) > 0 %we want start with cooling and the experiment starts with warming
-%         temptims = temptims(2:end); %skip the first temptim so we start with cooling
-%         tiz = tiz(2:end); hotter = hotter(2:end);
-%       elseif heat == 8 && tiz(1) < 0 %we want start with warming and the experiment starts with cooling
-%         temptims = temptims(2:end); %skip the first temptim so we start with cooling
-%         tiz = tiz(2:end); colder = colder(2:end);
-%       end
+      if heat == 7 && tiz(1) > 0 %we want start with cooling and the experiment starts with warming
+        temptims = temptims(2:end); %skip the first temptim so we start with cooling
+        tiz = tiz(2:end); hotter = hotter(2:end);
+      elseif heat == 8 && tiz(1) < 0 %we want start with warming and the experiment starts with cooling
+        temptims = temptims(2:end); %skip the first temptim so we start with cooling
+        tiz = tiz(2:end); colder = colder(2:end);
+      end
 
 else %we have poweridx values
 
