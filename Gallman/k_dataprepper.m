bdf48b0e6ec43889fb5f6e0aded9e115a9a7e070
Eric Fortune <eric.fortune@njit.edu>
2022-10-13 14:08:34 -0400

    / ‘Gallman/k_dataprepper.m’

--- Gallman/k_dataprepper.m
+++ Gallman/k_dataprepper.m
@@ -15,13 +15,14 @@ lighttimes = k_lighttimes(in, light);
 
    %% Prepare raw data variables
 
+  if channel < 3 
     %outlier removal
      tto = [in.idx(channel).obwidx]; 
           
     %raw data
         timcont = [in.e(channel).s(tto).timcont]; %time in seconds
         obw = [in.e(channel).s(tto).obwAmp]/max([in.e(channel).s(tto).obwAmp]); %divide by max to normalize
-        oldfreq = timcont;
+        oldfreq = in.e(channel).s(tto).fftfreq];
         
         %Make a time base that starts and ends on lighttimes 
             rawidx = timcont >= lighttimes(1) & timcont <= lighttimes(end);
