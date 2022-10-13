f927e7eb98577eb7414aad6e9b2a2c82b1a629e3
Eric Fortune <eric.fortune@njit.edu>
2022-10-13 13:55:52 -0400

    + ‘Gallman/k_bodecodeplotter.m’

---
+++ Gallman/k_bodecodeplotter.m
@@ -0,0 +1,18 @@
+function k_bodecodeplotter(inSignal, outSignal, Fs)
+
+    % Estimate transfer function from data
+        [txy, f] = tfestimate(inSignal, outSignal, 1024, 1000, [], Fs);
+    % Plot Bode plot
+    figure; clf;
+        ax(1) = subplot(211); 
+            loglog(f, mag2db(abs(txy))); 
+            title('Gain')
+            ylabel('dB gain')
+        ax(2) = subplot(212); 
+            semilogx(f, phase(txy));
+            title('Phase')
+            ylabel('Degrees')
+            xlabel('Frequency')
+            
+    linkaxes(ax, 'x');
+       
