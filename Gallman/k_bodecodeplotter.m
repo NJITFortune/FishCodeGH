df25fefc93074a89bb222beddc3def5ebf59ce8f
Eric Fortune <eric.fortune@njit.edu>
2022-10-13 14:03:58 -0400

    / ‘Gallman/k_bodecodeplotter.m’

--- Gallman/k_bodecodeplotter.m
+++ Gallman/k_bodecodeplotter.m
@@ -1,9 +1,15 @@
-function k_bodecodeplotter(inSignal, outSignal, Fs)
+%function k_bodecodeplotter(in, ReFs)
+%(inSignal, outSignal, Fs)
+%in = hkg2(k).e(1) or hkg2(k)
+    %replace hkg with xxkg for temp changes
 
+%too lazy to function
+clearvars -except rkg kg2 hkg hkg2 xxkg xxkg2     
+in = hkg(k).e(1);
 
 
     % Estimate transfer function from data
-        [txy, f] = tfestimate(inSignal, outSignal, 1024, 1000, [], Fs);
+        [txy, f] = tfestimate(inSignal, outSignal, 1024, 1000, [], ReFs);
     % Plot Bode plot
     figure; clf;
         ax(1) = subplot(211); 
