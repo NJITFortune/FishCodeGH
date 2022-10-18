ed9b3ec7ec9b58b1361f18527b54ee96f35696a1
Eric Fortune <eric.fortune@njit.edu>
2022-10-18 15:59:38 -0400

    / ‘Gallman/KatieTriggers.m’

--- Gallman/KatieTriggers.m
+++ Gallman/KatieTriggers.m
@@ -21,7 +21,7 @@ ReFs = 10;
 ld = in.info.ld; % Whatever - ld is shorter than in.info.ld
 
 
-lighttimes = k_lighttimer(in);
+lighttimes = k_lighttimes(in);
 
 % lighttimes = abs(luztimes);
 % %add back the light time we subtracted 
