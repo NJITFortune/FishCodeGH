8dfed0e8ce1ed9d4aef1564338a4d631ae6176f4
Eric Fortune <eric.fortune@njit.edu>
2022-10-18 18:09:25 -0400

    / ‘Gallman/k_lightbinplotter.m’

--- Gallman/k_lightbinplotter.m
+++ Gallman/k_lightbinplotter.m
@@ -42,9 +42,12 @@ ax(plotorder) = subplot(totplot, colnum, plotorder); hold on; title('ch1 obwAmp'
                         darkon = out.info.luz(out.info.luz < 0);
                         
                         %plot
+                        if ~isempty(lighton)
                         plot([lighton' lighton']', ylim, 'y-', 'LineWidth', 2);
+                        end
+                        if ~isempty(darkon)
                         plot([abs(darkon)' abs(darkon)']', ylim, 'k-', 'LineWidth', 2);
-
+                        end
             % Add feedingtimes, if we have them... 
                if isfield(out.info, 'feedingtimes')
                 if ~isempty([out.info.feedingtimes])
