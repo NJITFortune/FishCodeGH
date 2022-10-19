3cc5450a8f4a01df2a589397d44e58ec1e2340d5
Eric Fortune <eric.fortune@njit.edu>
2022-10-19 14:40:24 -0400

    / ‘Gallman/KatieRegObwDay.m’

--- Gallman/KatieRegObwDay.m
+++ Gallman/KatieRegObwDay.m
@@ -114,7 +114,7 @@ end
 
         %low pass removes spikey-ness
        % lowWn = 0.025/(ReFs/2);%OG
-         lowWn = 0.5/(ReFs/2);
+         lowWn = 0.7/(ReFs/2);
         [dd,cc] = butter(5, lowWn, 'low');
         datadata = filtfilt(dd,cc, double(regobwpeaks));
 
