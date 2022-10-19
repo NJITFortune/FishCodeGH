a169e03404a8b49cc2f2b38d2ef8b4706ebe1521
Eric Fortune <eric.fortune@njit.edu>
2022-10-19 13:51:09 -0400

    / ‘Gallman/k_fishcalplotter.m’

--- Gallman/k_fishcalplotter.m
+++ Gallman/k_fishcalplotter.m
@@ -23,7 +23,7 @@ in = hi;
             
     ax(2) = subplot(312); title('postcal'); hold on; %ylim([0,1]);
             plot(in(1).tim, in(1).obwamp, 'bo');
-            plot(in(2).tim, in(2).obwamp*2, 'mo');
+            plot(in(2).tim, in(2).obwamp, 'mo');
           
 %             plot(in(1).tim, in(1).pkamp, 'bo');
 %             plot(in(2).tim, in(2).pkamp, 'mo');
@@ -53,7 +53,7 @@ in = lo;
             
     ax(2) = subplot(312); title('postcal'); hold on; %ylim([0,1]);
             plot(in(1).tim, in(1).obwamp, 'bo');%*1.5
-            plot(in(2).tim, in(2).obwamp*2, 'mo');
+            plot(in(2).tim, in(2).obwamp, 'mo');
           
 %             plot(in(1).tim, in(1).pkamp, 'bo');
 %             plot(in(2).tim, in(2).pkamp, 'mo');
