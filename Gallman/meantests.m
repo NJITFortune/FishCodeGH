78153279a3c0d4a6a2fe73ade4d061344ab4b853
Eric Fortune <eric.fortune@njit.edu>
2022-10-19 10:39:13 -0400

    / ‘Gallman/meantests.m’

--- Gallman/meantests.m
+++ Gallman/meantests.m
@@ -37,9 +37,10 @@ window = 5;
 %plot to check
 figure(76); clf; hold on;
     plot(timcont, obw, '.', 'MarkerSize', 8);
-    plot(timcont, tmean, 'LineWidth',1);
+    plot(timcont, tmean, 'LineWidth',1, 'DisplayName', 'Mean');
     plot([lighttimes' lighttimes'], ylim, 'k-');
     xlim([85 135]);
+    legend;
 
 
 
