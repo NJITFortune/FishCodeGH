630ece55b8d3d91eb51e086d137a074cf49fe0f2
Kathryn Gallman <keg26@njit.edu>
2022-10-24 10:43:02 -0400

    / ‘Gallman/k_derivesum.m’

--- Gallman/k_derivesum.m
+++ Gallman/k_derivesum.m
@@ -14,3 +14,18 @@ transbinnum = 8;
 [regtim, regfreq, regtemp, regobwpeaks] = k_datatrimmean(in, channel, ReFs);
 
 
+%% dark to light transitions
+
+%divide into days
+%index      
+daysz = 1:1:floor(totaltimhours/(ld*2));
+
+%dark transistions
+darkdays = lighttimes(1) + ((2*ld) * (daysz-1));
+
+%light transitions
+lightdays = lighttimes(2) + ((2*ld) * (daysz-1));
+
+%how many bins around the transistion 
+%transbinnum = 8;
+transtim = transbinnum*binsize/60;
