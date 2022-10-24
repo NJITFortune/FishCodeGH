6bdfda097f4a9cef7af3aa6d7d4d71130343b728
Kathryn Gallman <keg26@njit.edu>
2022-10-24 10:34:27 -0400

    + ‘Gallman/k_derivesum.m’

---
+++ Gallman/k_derivesum.m
@@ -0,0 +1,15 @@
+function out = k_derivesum(in, channel, ReFs)
+%% prep 
+clearvars -except kg kg2 hkg hkg2 xxkg xxkg2 k
+
+in = hkg(2);
+channel = 1;
+%kg(12) starts with light
+
+%binsize in minutes
+binsize = 20;
+transbinnum = 8;
+
+
+
+[regtim, regfreq, regtemp, regobwpeaks] = k_datatrimmean(in, channel, ReFs)
