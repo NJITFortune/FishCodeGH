d25cc4fa0f17226403751240c73c71994896f262
Eric Fortune <eric.fortune@njit.edu>
2022-10-31 16:27:34 -0400

    + ‘Gallman/bintest.m’

---
+++ Gallman/bintest.m
@@ -0,0 +1,29 @@
+clearvars -except xxkg
+out = xxkg(24);
+
+luz = [out.info.luz];
+
+ax(plotorder) = subplot(totplot, colnum, plotorder); hold on; title('tube triggers'); ylim([0,50]);
+       
+       
+       figure(2); clf; hold on;
+            for k = 2:length(luz)
+            
+                if luz(k-1) < 0
+                  
+                  d = histogram([out.e(1).s(tto{1}).timcont]/(60*60), 'BinWidth', binwidth,'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
+                  [N(k-1,:), edges(k-1,:), bin(k-1,:)] = histcounts([out.e(1).s(tto{1}).timcont]/(60*60), 'BinWidth', binwidth,'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
+                  d.Normalization = 'countdensity';
+                  d.FaceColor = [0.9 0.9 0.9];
+                else
+                    
+                   l = histogram([out.e(1).s(tto{1}).timcont]/(60*60),'BinWidth', binwidth, 'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
+                  [N(k-1,:), edges(k-1,:), bin(k-1,:)] = histcounts([out.e(1).s(tto{1}).timcont]/(60*60), 'BinWidth', binwidth,'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
+                   l.Normalization = 'countdensity';
+                   l.FaceColor = 'y';
+                end
+            end
+
+      figure(3);
+
+        plot(edges, N);
