5c09c5076d876d21beab77232197d0bbad54b6f1
Eric Fortune <eric.fortune@njit.edu>
2022-10-31 18:10:45 -0400

    / ‘Gallman/bintest.m’

--- Gallman/bintest.m
+++ Gallman/bintest.m
@@ -25,26 +25,51 @@ binwidth = 1;
                   
                    l.Normalization = 'countdensity';
                    l.FaceColor = [255/255 232/255 124/255];
-%                     l.FaceColor = [0.9290 0.6940 0.1250];
-                %   l.FaceColor = 'y';
+                    l.FaceColor = [0.9290 0.6940 0.1250];
+                  l.FaceColor = 'y';
                 end
             end
 
      % figure(3);clf;hold on;
 
-%     [N, edges] = histcounts([out.e(1).s.timcont]/(60*60), 'BinWidth', binwidth);
-%         edges = edges(2:end) - (edges(2)-edges(1))/2;
+    [N, edges] = histcounts([out.e(1).s.timcont]/(60*60), 'BinWidth', binwidth);
+        edges = edges(2:end) - (edges(2)-edges(1))/2;
+
+        plot(edges, N,'k-', 'LineWidth',2);
 % 
-%         plot(edges, N,'k-', 'LineWidth',2);
-% % 
-% % 
-% %         highWn = 0.005/(binwidth/2); % Original but perhaps too strong for 4 and 5 hour days
-% %          [bb,aa] = butter(5, highWn, 'high');
-% % 
-%         lowWn = 0.075/(binwidth/2);
-%         [dd,cc] = butter(5, lowWn, 'low');
-%         filtN = filtfilt(dd,cc, double(N));
-% %        % filtN = filtfilt(bb,aa, filtN); %high pass
-% %       
-%         plot(edges, filtN, 'LineWidth',2)  
+% 
+%         highWn = 0.005/(binwidth/2); % Original but perhaps too strong for 4 and 5 hour days
+%          [bb,aa] = butter(5, highWn, 'high');
+% 
+        lowWn = 0.075/(binwidth/2);
+        [dd,cc] = butter(5, lowWn, 'low');
+        filtN = filtfilt(dd,cc, double(N));
+%        % filtN = filtfilt(bb,aa, filtN); %high pass
+%       
+        plot(edges, filtN, 'LineWidth',2)  
 %          findpeaks(filtN, edges); xlim([13 116]); ylim([0 60]);
+
+
+
+
+
+ %raw data
+                timcont = [out.e(1).s(tto{1}).timcont]; %time in seconds
+                obw =  [out.e(1).s(tto{1}).obwAmp]/max([out.e(1).s(tto{1}).obwAmp]); %divide by max to normalize
+                oldfreq = [out.e(1).s(tto{1}).fftFreq];
+                oldtemp = [out.e(1).s(tto{1}).temp];
+            
+             %trimmed mean
+             window = 5;
+              fcn = @(x) trimmean(x,33);
+              obwtrim = matlab.tall.movingWindow(fcn, window, obw');
+              freqtrim = matlab.tall.movingWindow(fcn, window, oldfreq');
+              temptrim = matlab.tall.movingWindow(fcn, window, oldtemp');
+
+        lighttimes = k_lighttimes(out, 3);
+            %Regularize
+                %regularize data to ReFs interval
+                [regtim, ~, ~, regobwpeaks] = k_regularmetamucil(timcont, obwtrim', timcont, obw, freqtrim', temptrim', 20, lighttimes);
+
+
+      plot(regtim/3600, regobwpeaks, 'LineWidth',1);
