91266f580f2c447ddb9aec15982a15eec63a20be
Eric Fortune <eric.fortune@njit.edu>
2022-10-31 16:41:54 -0400

    / ‘Gallman/bintest.m’

--- Gallman/bintest.m
+++ Gallman/bintest.m
@@ -29,7 +29,7 @@ binwidth = 1;
 
       figure(3);
 
-[N, edges] = histcounts([out.e(1).s.timcont]/(60*60), 'BinWidth', binwidth);
+    [N, edges] = histcounts([out.e(1).s.timcont]/(60*60), 'BinWidth', binwidth);
         edges = edges(2:end) - (edges(2)-edges(1))/2;
 
         plot(edges, N);
