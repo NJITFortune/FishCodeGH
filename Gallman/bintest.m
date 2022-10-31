c7563477ce8f365a838d8392012649e32ada4628
Eric Fortune <eric.fortune@njit.edu>
2022-10-31 16:39:58 -0400

    / ‘Gallman/bintest.m’

--- Gallman/bintest.m
+++ Gallman/bintest.m
@@ -12,7 +12,7 @@ binwidth = 1;
                   
                   d = histogram([out.e(1).s.timcont]/(60*60), 'BinWidth', binwidth,'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
                   for j = abs(luz(k-1)):binwidth:abs(luz(k))-1
-                  [N(j,:), edges(j,:), bin(j,:)] = histcounts([out.e(1).s.timcont]/(60*60), 'BinWidth', binwidth,'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
+                  [N(j,:), edges(j,:)] = histcounts([out.e(1).s.timcont]/(60*60), 'BinWidth', binwidth,'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
                   end
                   d.Normalization = 'countdensity';
                   d.FaceColor = [0.9 0.9 0.9];
@@ -20,7 +20,7 @@ binwidth = 1;
                     
                    l = histogram([out.e(1).s.timcont]/(60*60),'BinWidth', binwidth, 'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
                   for j = abs(luz(k-1)):binwidth:abs(luz(k))
-                  [N(j,:), edges(j,:), bin(j,:)] = histcounts([out.e(1).s.timcont]/(60*60), 'BinWidth', binwidth,'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
+                  [N(j,:), edges(j,:)] = histcounts([out.e(1).s.timcont]/(60*60), 'BinWidth', binwidth,'BinLimits',[abs(luz(k-1)),abs(luz(k))]);
                   end
                    l.Normalization = 'countdensity';
                    l.FaceColor = 'y';
@@ -30,6 +30,6 @@ binwidth = 1;
       figure(3);
 
 
-edges = edges(2:end) - (edges(2)-edges(1))/2;
+        edges = edges(2:end) - (edges(2)-edges(1))/2;
 
         plot(edges, N);
