d20ad90a670b707162cd6cb12d28eebc920d912b
Eric Fortune <eric.fortune@njit.edu>
2022-11-01 19:18:23 -0400

    / ‘Gallman/KatieRawObwDay.m’

--- Gallman/KatieRawObwDay.m
+++ Gallman/KatieRawObwDay.m
@@ -66,7 +66,7 @@ if light == 3
 
     %divide raw data into days that start with dark
     for jj = 2:length(darkdays)
-   
+   jj
         darkidx = find(timcont >= darkdays(jj-1) & timcont < darkdays(jj));
             dday(jj-1).tim(:) = timcont(darkidx)-timcont(darkidx(1));
             dday(jj-1).amp(:) = obw(darkidx);
