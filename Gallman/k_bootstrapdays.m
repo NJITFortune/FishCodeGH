6b94c56cd1be820f2f2ce58b9cf554d466108981
Eric Fortune <eric.fortune@njit.edu>
2022-11-01 13:12:00 -0400

    / ‘Gallman/k_bootstrapdays.m’

--- Gallman/k_bootstrapdays.m
+++ Gallman/k_bootstrapdays.m
@@ -33,23 +33,14 @@ ld = in1(1).day(1).ld;
                  
                  for i = 1:length(in1(j).day)
                   
-                 singlealldays(ii+i,:) = in1(j).day(i).Sobwyy;
+                 multialldays(ii+i,:) = in1(j).day(i).Sobwyy;
     
     
                  end
             
                  ii = i;
              end
-%     %multifish
-%         multialldays(1) = in2(1).day;
-%         
-%         
-%          for jj = 2:length(in2) % experiments of x hour length
-%           
-%             multialldays(jj,:) = [multialldays; in2(jj).day];
-%         
-%          end
-% 
-%       
-% 
 
+
+  if length(multialldays) < length(singlealldays)
+      
