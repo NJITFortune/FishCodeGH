66d1fbc05b5def62298bfe61647341e400020c39
Eric Fortune <eric.fortune@njit.edu>
2022-11-02 14:13:22 -0400

    / ‘Gallman/KatieRawObwDay.m’

--- Gallman/KatieRawObwDay.m
+++ Gallman/KatieRawObwDay.m
@@ -1,13 +1,13 @@
-%function [darkhalfamp, darkhalftim, lighthalfamp, lighthalftim] = KatieRawObwDay(in, channel, light)
+function [darkhalfamp, darkhalftim, lighthalfamp, lighthalftim] = KatieRawObwDay(in, channel, light)
 % %% prep 
 
 % % 
- clearvars -except kg kg2 hkg hkg2 xxkg xxkg2 k
-% 
-in = hkg(k);
-channel = 3;
-light = 3;
-ReFs = 20;
+%  clearvars -except kg kg2 hkg hkg2 xxkg xxkg2 k
+% % 
+% in = hkg(k);
+% channel = 1;
+% light = 3;
+% ReFs = 20;
 
 % %kg(12) starts with light
 % 
