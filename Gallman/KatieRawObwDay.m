91aab58bf41022c6173e8284b14999fc3831aad7
Eric Fortune <eric.fortune@njit.edu>
2022-11-02 09:58:59 -0400

    / ‘Gallman/KatieRawObwDay.m’

--- Gallman/KatieRawObwDay.m
+++ Gallman/KatieRawObwDay.m
@@ -1,12 +1,12 @@
-%function [darkhalfamp, darkhalftim, lighthalfamp, lighthalftim] = KatieRawObwDay(in, channel, light)
+function [darkhalfamp, darkhalftim, lighthalfamp, lighthalftim] = KatieRawObwDay(in, channel, light)
 % %% prep 
 
 
- clearvars -except kg kg2 hkg hkg2 xxkg xxkg2 k
-% 
-in = hkg(k);
-channel = 1;
-light = 3;
+%  clearvars -except kg kg2 hkg hkg2 xxkg xxkg2 k
+% % 
+% in = hkg(k);
+% channel = 1;
+% light = 3;
 
 % %kg(12) starts with light
 % 
@@ -130,30 +130,30 @@ if light == 4
     lighthalftim = ldaytim(lighthalfidx);
 
 end
-%% plot to check
- darkhalfidx = find(ddaytim<ld);
-    darkhalfamp = ddayamp(darkhalfidx);
-    darkhalftim = ddaytim(darkhalfidx);
-    
-    lighthalfidx = find(ddaytim >= ld);
-    lighthalfamp = ddayamp(lighthalfidx);
-    lighthalftim = ddaytim(lighthalfidx);
-
-
-figure(45); clf; hold on;
-
-    plot(darkhalftim, darkhalfamp, '.', 'Color', [0.7, 0.7, 0.7]);
-    plot(lighthalftim, lighthalfamp, 'm.');
-    plot([ld ld], ylim, 'k-', 'LineWidth', 2);
-
-
-figure(68);clf; hold on;
-
-    for j = 1:length(dday)
-        plot(dday(j).entiretimcont, dday(j).amp, '.');
-    end
-    
-    plot([darkdays' darkdays'], ylim, 'k-');
+% %% plot to check
+%  darkhalfidx = find(ddaytim<ld);
+%     darkhalfamp = ddayamp(darkhalfidx);
+%     darkhalftim = ddaytim(darkhalfidx);
+%     
+%     lighthalfidx = find(ddaytim >= ld);
+%     lighthalfamp = ddayamp(lighthalfidx);
+%     lighthalftim = ddaytim(lighthalfidx);
+% 
+% 
+% figure(45); clf; hold on;
+% 
+%     plot(darkhalftim, darkhalfamp, '.', 'Color', [0.7, 0.7, 0.7]);
+%     plot(lighthalftim, lighthalfamp, 'm.');
+%     plot([ld ld], ylim, 'k-', 'LineWidth', 2);
+% 
+% 
+% figure(68);clf; hold on;
+% 
+%     for j = 1:length(dday)
+%         plot(dday(j).entiretimcont, dday(j).amp, '.');
+%     end
+%     
+%     plot([darkdays' darkdays'], ylim, 'k-');
 
 
 
