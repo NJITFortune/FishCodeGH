56de810158fcfd74c6d4e38e11f25452a9a42fc8
Eric Fortune <eric.fortune@njit.edu>
2022-11-03 16:30:35 -0400

    / ‘Gallman/rawestplot.m’

--- Gallman/rawestplot.m
+++ Gallman/rawestplot.m
@@ -2,7 +2,7 @@ figure(342); clf; hold on;
 set(gcf, 'renderer', 'painters');
     ax(1) = subplot(211); 
         plot(tim,data(:,1)); 
-        ylable('Amplitude');
+        ylabel('Amplitude');
     ax(2) = subplot(212); 
     plot(tim,data(:,2)); 
     xlabel('Time (seconds)');
