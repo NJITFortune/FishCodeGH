d8243532a1ec3f68139cdb04f4e1f70d63fb4d94
Eric Fortune <eric.fortune@njit.edu>
2022-11-04 18:32:46 -0400

    / ‘Gallman/KatieRegObwDay.m’

--- Gallman/KatieRegObwDay.m
+++ Gallman/KatieRegObwDay.m
@@ -128,7 +128,7 @@ end
         %high pass removes feeding trend for high frequency experiments
         %cut off frequency
          highWn = 0.005/(ReFs/2); % Original but perhaps too strong for 4 and 5 hour days
-         [bb,aa] = butter(5, highWn, 'high');
+%          [bb,aa] = butter(5, highWn, 'high');
 
          %less strong low pass filter - otherwise fake prediction 
                lowWn = 0.9/(ReFs/2);
