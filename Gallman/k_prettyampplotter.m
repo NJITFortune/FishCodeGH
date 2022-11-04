c238658ad3ac906506c7a9573a878684a7c7ad4c
Eric Fortune <eric.fortune@njit.edu>
2022-11-04 19:22:41 -0400

    / ‘Gallman/k_prettyampplotter.m’

--- Gallman/k_prettyampplotter.m
+++ Gallman/k_prettyampplotter.m
@@ -64,7 +64,7 @@ figure(31); clf; hold on;
         ylabel('Mean square amplitude');
        % xlabel('Time (hours)');
 
-    ax(2) = subplot(312); hold on; xlim([0, timcont(end)-timcont(1)]); %ylim([400 600]);
+    ax(2) = subplot(312); hold on; xlim([0, timcont(end)-timcont(1)]); ylim([400 600]);
               plot(timcont-timcont(1), freq, '.', 'Color', [252/255, 108/255, 133/255]);
                plot(regtim/3600 - timcont(1), regfreq, 'k-', 'LineWidth', 2);
               ylabel('Frequency (Hz)');
