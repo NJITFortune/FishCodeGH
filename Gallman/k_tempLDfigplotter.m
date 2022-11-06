5db75ca578bc8a8756fd7d4ac466bd9a17d28064
Eric Fortune <eric.fortune@njit.edu>
2022-11-06 11:06:58 -0500

    / ‘Gallman/k_tempLDfigplotter.m’ / ‘Gallman/xxkgme.mlx’

--- Gallman/k_tempLDfigplotter.m
+++ Gallman/k_tempLDfigplotter.m
@@ -5,7 +5,7 @@
 %no function
 clearvars -except k hkg hkg2 xxkg xxkg2 cold hot coldmulti hot multi dark dark multi light lightmulti
 in = xxkg(k);
-channel = 1;
+channel = 2;
 lightstart = 3;
 
 %% data prep
