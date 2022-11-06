dd1f2b5cadf3aefac0a5178e7fa1e15cbca32a26
Eric Fortune <eric.fortune@njit.edu>
2022-11-06 12:21:55 -0500

    / ‘Gallman/k_tempLDfigplotter.m’

--- Gallman/k_tempLDfigplotter.m
+++ Gallman/k_tempLDfigplotter.m
@@ -572,5 +572,32 @@ figure(4566); clf ; hold on;
     plot(coldday(1).tim/3600, coldmean, 'k-','LineWidth', 2 );
    
 
+%%
 
+figure(57); clf; hold on; 
+
+ 
+        for j = 1:length(day)
+                    plot(day(j).tim/3600, day(j).Sobwyy);
+                    meanday(j,:) = day(j).Sobwyy;
+        end
+        a = ylim;
+    %create fill box 
+    if light < 4 %we start with dark
+        fill([0 0 ld ld], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
+    else %we start with light
+        fill([ld ld ld*2 ld*2], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
+    end
     
+        
+     for j = 1:length(day)
+            plot(day(j).tim/3600, day(j).Sobwyy);
+            meanday(j,:) = day(j).Sobwyy;
+     end
+        
+            mmday= mean(meanday);
+            %othermday = mday/(length(day));
+            plot(day(1).tim/3600, mmday, 'k-', 'LineWidth', 3);
+           
+            %plot(day(1).tim/3600, othermday, 'b-', 'LineWidth', 3);
+            plot([ld ld], ylim, 'k-', 'LineWidth', 3);
