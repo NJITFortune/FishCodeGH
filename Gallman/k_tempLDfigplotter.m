48192a01c81ae476d75e4f089cebd0edf1e1f3cb
Eric Fortune <eric.fortune@njit.edu>
2022-11-06 12:28:19 -0500

    / ‘Gallman/k_tempLDfigplotter.asv’

--- Gallman/k_tempLDfigplotter.m
+++ Gallman/k_tempLDfigplotter.m
@@ -486,7 +486,7 @@ figure(55); clf; hold on;
 
     xa(3) = subplot(413); hold on;
 
-        histogram(tempxx/3600, 'BinWidth', 0.5);
+        histogram(tempxx/3600, 'BinWidth', 0.5, 'BinLimits', );
             
      xa(4) = subplot(414); hold on;
 
