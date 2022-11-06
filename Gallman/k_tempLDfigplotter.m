e8abf73cda85d22b017bb8a38f53aa95aacbd2bd
Eric Fortune <eric.fortune@njit.edu>
2022-11-06 11:06:13 -0500

    / ‘Gallman/k_tempLDfigplotter.m’

--- Gallman/k_tempLDfigplotter.m
+++ Gallman/k_tempLDfigplotter.m
@@ -220,9 +220,36 @@ linkaxes(xa, 'x');
 
 
 
+%%
 
 
+figure(57); clf; hold on; 
 
+ 
+    %create fill box 
+    if light < 4 %we start with dark
+        fill([0 0 ld ld], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
+    else %we start with light
+        fill([ld ld ld*2 ld*2], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
+    end
+    
+    %mean two ways to prove math
+    mday = zeros(1, length(day(1).tim));        
+     for j = 1:length(day)
+            plot(day(j).tim/3600, day(j).Sobwyy);
+            meanday(j,:) = day(j).Sobwyy;
+            mday = mday + day(j).Sobwyy;
+            
+     end
+        
+            mmday= mean(meanday);
+            %othermday = mday/(length(day));
+            plot(day(1).tim/3600, mmday, 'k-', 'LineWidth', 3);
+           
+            %plot(day(1).tim/3600, othermday, 'b-', 'LineWidth', 3);
+            plot([ld ld], ylim, 'k-', 'LineWidth', 3);
+            
+            
 
 
 
