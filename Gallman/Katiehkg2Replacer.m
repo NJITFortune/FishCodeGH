ae1882a4723fd073e24684f224235e85dba61ae8
Eric Fortune <eric.fortune@njit.edu>
2022-11-10 16:36:39 -0500

    + ‘Gallman/Katiehkg2Replacer.m’

---
+++ Gallman/Katiehkg2Replacer.m
@@ -0,0 +1,54 @@
+function out = Katiehkg2Replacer(in)
+
+% OBW        
+figure(1); clf;
+
+    luz = [in.info.luz];
+      
+       
+            for k = 2:length(luz)-1
+            
+     
+
+                  kidx =  find([in.s.timcont]/3600 >= abs(luz(k-1))& [in.s.timcont]/3600 <abs(luz(k+1)));
+                  
+                  histogram([in.s(kidx).obwAmp],100);
+
+                  %Lower lim
+                    fprintf('Click cutoff for eliminating erroneously low amplitude measurements.\n');
+                    [cutoffampL, ~]  = ginput(1);
+                    plot([cutoffampL, cutoffampL], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+                    drawnow; 
+                    
+                  %Upper lim
+                    fprintf('Click cutoff for eliminating erroneously high amplitude measurements.\n');
+                    [cutoffampH, ~]  = ginput(1);
+                    plot([cutoffampH, cutoffampH], [0 10], 'r-', 'LineWidth', 2, 'MarkerSize', 12);
+                    drawnow; 
+                    
+                    obwidx(k-1) = find([in.obwAmp] > cutoffampL & [in.obwAmp] < cutoffampH);
+
+                     pause(1);
+                 
+                
+            end
+
+            
+        close(1);
+
+
+            for j = 1:length(obwidx)
+
+                out.s.newobw = in.s(obwidx(k)).obwAmp;
+
+            end
+
+
+
+
+
+
+
+
+  
+
