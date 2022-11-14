1fce5ba18caf560fb10d51dcd229af14a390b658
Eric Fortune <eric.fortune@njit.edu>
2022-11-14 11:41:47 -0500

    / ‘Gallman/k_prettypeakplotter.m’

--- Gallman/k_prettypeakplotter.m
+++ Gallman/k_prettypeakplotter.m
@@ -77,8 +77,9 @@ figure(31); clf; hold on;
 
         %plot boxes
     
+        luz = [in.info.luz];
         for j = 1:length(lighttimes)-1
-            if mod(j,2) == 1 %if j is odd
+            if luz(j) < 0 %if j is odd
             fill([lighttimes(j)-timcont(1) lighttimes(j)-timcont(1) lighttimes(j+1)-timcont(1) lighttimes(j+1)-timcont(1)], [0 a(2) a(2) 0], [0.9, 0.9, 0.9]);
             end
         end
@@ -111,7 +112,7 @@ figure(31); clf; hold on;
 %         plot(regtim/3600 - timcont(1), darkdy,'k-', 'LineWidth', 2);
          a = ylim;
          for j = 1:length(lighttimes)-1
-            if mod(j,2) == 1 %if j is odd
+            if luz(j) < 0  %if j is odd
             fill([lighttimes(j)-timcont(1) lighttimes(j)-timcont(1) lighttimes(j+1)-timcont(1) lighttimes(j+1)-timcont(1)], [a(1) a(2) a(2) a(1)], [0.9, 0.9, 0.9]);
             end
          end
@@ -150,7 +151,7 @@ figure(31); clf; hold on;
 
          a = ylim;
         for j = 1:length(lighttimes)-1
-            if mod(j,2) == 1 %if j is odd
+            if luz(j) < 0  %if j is odd
             fill([lighttimes(j)-timcont(1) lighttimes(j)-timcont(1) lighttimes(j+1)-timcont(1) lighttimes(j+1)-timcont(1)], [0 a(2) a(2) 0], [0.9, 0.9, 0.9]);
             end
         end
