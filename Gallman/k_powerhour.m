b7b298db43ed7a92c5a8bc2729d614e243fe605b
Eric Fortune <eric.fortune@njit.edu>
2022-11-14 14:48:59 -0500

    / ‘Gallman/k_powerhour.m’

--- Gallman/k_powerhour.m
+++ Gallman/k_powerhour.m
@@ -23,15 +23,6 @@ lighttimes = k_lighttimes(in, 3);
     freq = regfreq(timidx);
     temp = regtemp(timidx);
 
-    N = length(obw);
-
-%     %periodogram(obw, rectwin(N),N, Fs)
-% 
-%     [pxx,f] = pwelch(obw,hamming(N),[],N,Fs);
-% 
-%    
-%  figure(6);clf; hold on;
-% 
-%     plot(f,pxx);
+  
 
    
