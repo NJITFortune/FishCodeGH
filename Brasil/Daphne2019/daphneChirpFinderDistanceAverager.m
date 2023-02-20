7140f5507a94c7d27a78951eddbe9dab12038db5
Eric Fortune <eric.fortune@njit.edu>
2023-02-20 12:11:41 -0500

    / ‘Brasil/Daphne2019/daphneChirpFinderDistanceAverager.asv’ / ‘Brasil/Daphne2019/daphneChirpFinderDistanceAverager.m’ + ‘Brasil/Daphne2019/daphneCollisionFinderEODaverager.asv’ / ‘Brasil/Daphne2019/daphneCollisionFinderEODaverager.m’

--- Brasil/Daphne2019/daphneChirpFinderDistanceAverager.m
+++ Brasil/Daphne2019/daphneChirpFinderDistanceAverager.m
@@ -119,3 +119,33 @@ end
 %     axx(3) = subplot(313); plot(avgDistance(:,usethese));
 %     subplot(312); hold on; plot(mean(avgDF(:,usethese)'), 'k', 'LineWidth', 6);
 %     subplot(313); hold on; plot(mean(avgDistance(:,usethese)'), 'k', 'LineWidth', 6);
+
+figure(2); clf;
+meanDist = avgDistance(:,1);
+meanDf = avgDF(:,1);
+ll = 0;
+
+for z = [4,5,6,7,8,10,11,12,13,14]
+for k=1:length(datum(z).pair)
+    if ~isempty(datum(z).pair(k).epoch)
+    for j=1:length(datum(z).pair(k).epoch)
+        if ~isempty(datum(z).pair(k).epoch(j).avgDistance)
+        meanDist = meanDist + datum(z).pair(k).epoch(j).avgDistance; 
+        meanDf = meanDf + datum(z).pair(k).epoch(j).avgDF; 
+
+%        figure(2); subplot(211); hold on; plot(datum(z).pair(k).epoch(j).avgDF - mean(datum(z).pair(k).epoch(j).avgDF))
+        figure(2); subplot(211); hold on; plot(datum(z).pair(k).epoch(j).avgDF)
+        figure(2); subplot(212); hold on; plot(datum(z).pair(k).epoch(j).avgDistance)
+        ll = ll+1;
+        end
+    end
+    end
+end
+
+end
+
+figure(3); xline(200.5, 'g', 'LineWidth', 4)
+    plot(meanDf/ll, 'k', 'LineWidth',6);
+    
+figure(4); xline(200.5, 'g', 'LineWidth', 4)
+    plot(meanDist/ll, 'k', 'LineWidth',6);
