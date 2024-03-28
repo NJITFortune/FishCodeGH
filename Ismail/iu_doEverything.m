f11e8ab157d44eabbb2846d70f0b51c8fc0813b6
Eric Fortune <40925700+NJITFortune@users.noreply.github.com>
2024-03-28 16:05:51 -0400

    There is a problem with getALLstas

--- Ismail/iu_doEverything.m
+++ Ismail/iu_doEverything.m
@@ -1,13 +1,72 @@
 %% This is a script to process everything
-load ~/Downloads/finalIsmaildata2024.mat
-
+%load('~/Downloads/finalIsmaildata2024.mat', 'curfish');
+load('~/Documents/uyanik_neurophys/finalIsmaildata2024.mat', 'curfish');
 
 %% Plot everything for the fish as a start
-fishno = 3;
+fishno = 1;
 
-out = iu_sta(curfish(fishno).spikes.times, [], curfish(fishno).error_vel, curfish(fishno).fs, 2);
+[evOUT, faOUT] = iu_getAllstas(curfish, fishno); 
 
 %% Pick neuron
 
-neuronNum = 2;
+neuronNum = 1;
+spiketimes = curfish(fishno).spikes.times(curfish(fishno).spikes.codes == neuronNum);
+spikeCategory = [];
+for j=length(spiketimes):-1:1
+    curidx = find(curfish(fishno).time < spiketimes(j), 1, "last"); 
+    spikeCategory(j) = curfish(fishno).tracking(curidx); 
+end
+
+asidx = find(spikeCategory == 0 | spikeCategory == 2);
+spidx = find(spikeCategory == 1 | spikeCategory == 3);
+
+evSP = iu_sta(spiketimes(spidx), [], curfish(fishno).error_acc, curfish(fishno).fs, 2);
+faSP = iu_sta(spiketimes(spidx), [], curfish(fishno).fish_acc, curfish(fishno).fs, 2);
+evAS = iu_sta(spiketimes(asidx), [], curfish(fishno).error_acc, curfish(fishno).fs, 2);
+faAS = iu_sta(spiketimes(asidx), [], curfish(fishno).fish_acc, curfish(fishno).fs, 2);
+
+figure(3); clf;
+ax(1)=subplot(121); hold on;
+    yyaxis left; plot(evSP.time, evSP.MEAN);
+    yyaxis right; plot(faSP.time, faSP.MEAN);
+    xline(0);
+    title('Smooth Pursuit')
+ax(2)=subplot(122); hold on;
+    yyaxis left; plot(evAS.time, evAS.MEAN);
+    yyaxis right; plot(faAS.time, faAS.MEAN);
+    xline(0);
+    title('Active Sensing')
+linkaxes(ax,'x')
+
+%% 
+sensoryOffset = -0.200;
+motorOffset = 0.200;
+
+[evAmp, evCat] = i_tim2stim(spiketimes, curfish(fishno).error_vel, curfish(fishno).time, curfish(fishno).tracking, sensoryOffset);
+[eaAmp, eaCat] = i_tim2stim(spiketimes, curfish(fishno).error_vel, curfish(fishno).time, curfish(fishno).tracking, sensoryOffset);
+
+
+% STA
+
+% Divide by type of tracking (2 categories)
+
+
+% Divide by velocity (3 categories)
+
+
+
+
+
+
+
+% [evAmp, evCat] = i_tim2stim(spiketimes, curfish.error_vel, curfish.time, curfish.tracking, -0.1);
+% [eaAmp, eaCat] = i_tim2stim(spiketimes, curfish.error_acc, curfish.time, curfish.tracking, -0.1);
+
+[evAmp, evCat] = i_tim2stim(spiketimes, curfish(fishno).error_vel, curfish(fishno).time, curfish(fishno).tracking, sensoryOffset);
+[eaAmp, eaCat] = i_tim2stim(spiketimes, curfish(fishno).error_vel, curfish(fishno).time, curfish(fishno).tracking, sensoryOffset);
+
+vsout = i_vsOnly(spikesig1, spikesig2, sig1, sig2);
+
+
+
 
--- Ismail/iu_getAllstas.m
+++ Ismail/iu_getAllstas.m
@@ -13,17 +13,27 @@ codes = unique(curfish(ff).spikes.codes);
         if length(find(curfish(ff).spikes.codes == codes(c))) > fewestNumberofSpikes
 
             spiketimes = curfish(ff).spikes.times(curfish(ff).spikes.codes == codes(c));
+            
             tmpEV = iu_sta(spiketimes, [], curfish(ff).error_vel, curfish(ff).fs, windowwidth);
+            tmpEA = iu_sta(spiketimes, [], curfish(ff).error_acc, curfish(ff).fs, windowwidth);
             tmpFA = iu_sta(spiketimes, [], curfish(ff).fish_acc, curfish(ff).fs, windowwidth);
 
             if ~exist('evOUT', 'var')
                 evOUT = tmpEV;
-                faOUT = tmpFA;
             else
                 evOUT(end+1) = tmpEV;
+            end
+            if ~exist('eaOUT', 'var')
+                eaOUT = tmpEA;
+            else
+                eaOUT(end+1) = tmpEA;
+            end
+            if ~exist('faOUT', 'var')
+                faOUT = tmpFA;
+            else
                 faOUT(end+1) = tmpFA;
             end
-        end
+         end
     end
 end
 
@@ -32,17 +42,22 @@ end
 figure(1); clf; 
 
 for nn = 1:length(evOUT)
-    ax(1)=subplot(221); hold on; 
+    ax(1)=subplot(231); hold on; 
         plot(evOUT(nn).time,evOUT(nn).MEAN);
-        xline(0);
+        xline(0); title('Error Vel');
         % plot(out(nn).time,out(nn).randMEAN); 
-    ax(2)=subplot(222); hold on; 
+    ax(2)=subplot(232); hold on; 
+        plot(eaOUT(nn).time,eaOUT(nn).MEAN);
+        xline(0); title('Error Acc');
+    ax(3)=subplot(233); hold on; 
         plot(faOUT(nn).time,faOUT(nn).MEAN);
-        xline(0);
+        xline(0); title('Fish Acc');
 
-    ax(3)=subplot(223); hold on;
+    ax(4)=subplot(234); hold on;
         plot(evOUT(nn).time, evOUT(nn).Pval); 
-    ax(4)=subplot(224); hold on;
+    ax(5)=subplot(235); hold on;
+        plot(eaOUT(nn).time, eaOUT(nn).Pval); 
+    ax(6)=subplot(236); hold on;
         plot(faOUT(nn).time, faOUT(nn).Pval); 
 end
 
