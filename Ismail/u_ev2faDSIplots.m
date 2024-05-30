3b5d3c9167733200a14ea202380469c857c57dde
Eric Fortune <eric.fortune@njit.edu>
2024-05-30 07:44:32 -0400

    ev2faplots

--- Ismail/u_ev2faDSIplots.m
+++ Ismail/u_ev2faDSIplots.m
@@ -116,3 +116,63 @@ plot([f7n1FA.dels], [f7n1FA.dsi.spikes], 'm', 'LineWidth', 3);
 xline(0); yline(0); ylim([-maxY, maxY]);
 title('Fish 7, neuron 1')
 
+
+
+
+
+
+%% Fish 3, neuron 4
+f = 3; n = 4;
+spiketimes = curfish(f).spikes.times(curfish(f).spikes.codes == n);
+tim = curfish(f).time;    
+    sig = curfish(f).error_vel; 
+    f3n4EV = u_DSItimeplot(spiketimes, sig, tim);
+    sig = curfish(f).error_acc; 
+    f3n4EA = u_DSItimeplot(spiketimes, sig, tim);
+    sig = curfish(f).fish_acc; 
+    f3n4FA = u_DSItimeplot(spiketimes, sig, tim);
+
+figure(3); subplot(2,2,1); hold on;
+plot([f3n4EV.dels], [f3n4EV.dsi.spikes], 'b', 'LineWidth', 3);
+plot([f3n4EA.dels], [f3n4EA.dsi.spikes], 'c', 'LineWidth', 2);
+plot([f3n4FA.dels], [f3n4FA.dsi.spikes], 'm', 'LineWidth', 3);
+xline(0); yline(0); ylim([-maxY, maxY]);
+title('Fish 3, neuron 4')
+
+
+%% Fish 3, neuron 5
+f = 3; n = 5;
+spiketimes = curfish(f).spikes.times(curfish(f).spikes.codes == n);
+tim = curfish(f).time;    
+    sig = curfish(f).error_vel; 
+    f3n5EV = u_DSItimeplot(spiketimes, sig, tim);
+    sig = curfish(f).error_acc; 
+    f3n5EA = u_DSItimeplot(spiketimes, sig, tim);
+    sig = curfish(f).fish_acc; 
+    f3n5FA = u_DSItimeplot(spiketimes, sig, tim);
+
+figure(3); subplot(2,2,2); hold on;
+plot([f3n5EV.dels], [f3n5EV.dsi.spikes], 'b', 'LineWidth', 3);
+plot([f3n5EA.dels], [f3n5EA.dsi.spikes], 'c', 'LineWidth', 2);
+plot([f3n5FA.dels], [f3n5FA.dsi.spikes], 'm', 'LineWidth', 3);
+xline(0); yline(0); ylim([-maxY, maxY]);
+title('Fish 3, neuron 5')
+
+%% Fish 3, neuron 6
+f = 3; n = 6;
+spiketimes = curfish(f).spikes.times(curfish(f).spikes.codes == n);
+tim = curfish(f).time;    
+    sig = curfish(f).error_vel; 
+    f3n6EV = u_DSItimeplot(spiketimes, sig, tim);
+    sig = curfish(f).error_acc; 
+    f3n6EA = u_DSItimeplot(spiketimes, sig, tim);
+    sig = curfish(f).fish_acc; 
+    f3n6FA = u_DSItimeplot(spiketimes, sig, tim);
+
+figure(3); subplot(2,2,3); hold on;
+plot([f3n6EV.dels], [f3n6EV.dsi.spikes], 'b', 'LineWidth', 3);
+plot([f3n6EA.dels], [f3n6EA.dsi.spikes], 'c', 'LineWidth', 2);
+plot([f3n6FA.dels], [f3n6FA.dsi.spikes], 'm', 'LineWidth', 3);
+xline(0); yline(0); ylim([-maxY, maxY]);
+title('Fish 3, neuron 6')
+
