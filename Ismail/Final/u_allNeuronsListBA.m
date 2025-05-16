ad8662dc29b379b0aa1e5f2928c4415b9fea0b6d
Eric Fortune <eric.fortune@njit.edu>
2025-05-16 14:33:29 +0300

    Fixed Indexing BIG mistake.

--- Ismail/Final/u_allNeuronsListBA.m
+++ Ismail/Final/u_allNeuronsListBA.m
@@ -70,63 +70,63 @@ neuronsAll(30,:) = [5, 4, 299, 1909, 14, 70]; %
 neuronsAll(31,:) = [5, 5, 299, 807, 14, 70]; % TOO FEW SPIKES
 
 % brownie_p1_2019_01_26, 539 (125 px EV 330 FA)
-neuronsAll(31,:) = [6, 1, 539, 2777, 125, 330]; %%%%  NegSens   Sensory NegHi
-neuronsAll(32,:) = [6, 2, 539, 3501, 125, 330]; %%%%  NR
-neuronsAll(33,:) = [6, 3, 539, 2433, 125, 330]; %%%%  NR   Weakest Sensory NegHi
-neuronsAll(34,:) = [6, 4, 539, 4106, 125, 330]; %%%%  NR
-neuronsAll(35,:) = [6, 5, 539, 2937, 125, 330]; %%%%  NegSens WEAK 
-neuronsAll(36,:) = [6, 6, 539, 1334, 125, 330]; % 
-neuronsAll(37,:) = [6, 7, 539, 2597, 125, 330]; %%%%  NR   Weak Sensory NegHi
-neuronsAll(38,:) = [6, 8, 539, 516, 125, 330]; % TOO FEW SPIKES
+neuronsAll(32,:) = [6, 1, 539, 2777, 125, 330]; %%%%  NegSens   Sensory NegHi
+neuronsAll(33,:) = [6, 2, 539, 3501, 125, 330]; %%%%  NR
+neuronsAll(34,:) = [6, 3, 539, 2433, 125, 330]; %%%%  NR   Weakest Sensory NegHi
+neuronsAll(35,:) = [6, 4, 539, 4106, 125, 330]; %%%%  NR
+neuronsAll(36,:) = [6, 5, 539, 2937, 125, 330]; %%%%  NegSens WEAK 
+neuronsAll(37,:) = [6, 6, 539, 1334, 125, 330]; % 
+neuronsAll(38,:) = [6, 7, 539, 2597, 125, 330]; %%%%  NR   Weak Sensory NegHi
+neuronsAll(39,:) = [6, 8, 539, 516, 125, 330]; % TOO FEW SPIKES
 
 % brownie_p2_2019_01_26, 389 (100 px EV 360 FA)
-neuronsAll(39,:) = [7, 1, 389, 3847, 100, 360]; %%%% PosCent   Something is going on here
-neuronsAll(40,:) = [7, 2, 389, 1055, 100, 360]; % Messy
-neuronsAll(41,:) = [7, 3, 389, 277, 100, 360]; % TOO FEW SPIKES
-neuronsAll(42,:) = [7, 4, 389, 1072, 100, 360]; % NR
+neuronsAll(40,:) = [7, 1, 389, 3847, 100, 360]; %%%% PosCent   Something is going on here
+neuronsAll(41,:) = [7, 2, 389, 1055, 100, 360]; % Messy
+neuronsAll(42,:) = [7, 3, 389, 277, 100, 360]; % TOO FEW SPIKES
+neuronsAll(43,:) = [7, 4, 389, 1072, 100, 360]; % NR
 
 % bumpy_2019_04_03, 1229 (55 px EV 165 FA)
-neuronsAll(43,:) = [8, 1, 1229, 24303, 55, 165]; %%% Too late, NR Reanalyze %% Limits %%%
-neuronsAll(44,:) = [8, 2, 1229, 10599, 55, 165]; % Messy Reanalyze
-neuronsAll(45,:) = [8, 3, 1229, 8508, 55, 165]; %%%%   NegIcon FAST ONLY   Something good, probably
-neuronsAll(46,:) = [8, 4, 1229, 7867, 55, 165]; %%%%   NegIcon FAST ONLY Weak  % Motor 0 delay Reanalyze
-neuronsAll(47,:) = [8, 5, 1229, 12816, 55, 165]; %%%%  PosAccel Iconic Weak  % Pos fast Bias Reanalyze
-neuronsAll(48,:) = [8, 6, 1229, 18279, 55, 165]; %%%%  PosAccel Iconic Weak  % Pos fast Bias Reanalyze
-neuronsAll(49,:) = [8, 7, 1229, 7954, 55, 165]; %%%%%  PosCent Sens   Pos Fast Motor Reanalyze
+neuronsAll(44,:) = [8, 1, 1229, 24303, 55, 165]; %%% Too late, NR Reanalyze %% Limits %%%
+neuronsAll(45,:) = [8, 2, 1229, 10599, 55, 165]; % Messy Reanalyze
+neuronsAll(46,:) = [8, 3, 1229, 8508, 55, 165]; %%%%   NegIcon FAST ONLY   Something good, probably
+neuronsAll(47,:) = [8, 4, 1229, 7867, 55, 165]; %%%%   NegIcon FAST ONLY Weak  % Motor 0 delay Reanalyze
+neuronsAll(48,:) = [8, 5, 1229, 12816, 55, 165]; %%%%  PosAccel Iconic Weak  % Pos fast Bias Reanalyze
+neuronsAll(49,:) = [8, 6, 1229, 18279, 55, 165]; %%%%  PosAccel Iconic Weak  % Pos fast Bias Reanalyze
+neuronsAll(50,:) = [8, 7, 1229, 7954, 55, 165]; %%%%%  PosCent Sens   Pos Fast Motor Reanalyze
 
 % fin_2019_04_14, 1019 (85 px EV 345 FA)
-neuronsAll(50,:) = [9, 2, 1019, 12079, 85, 345]; % NR
-neuronsAll(51,:) = [9, 3, 1019, 5172, 85, 345]; %%%%%%%PosIcon PosHi/PosHi
-neuronsAll(52,:) = [9, 4, 1019, 31508, 85, 345]; %%%%%%PosIcon PosHi/PosHi
+neuronsAll(51,:) = [9, 2, 1019, 12079, 85, 345]; % NR
+neuronsAll(52,:) = [9, 3, 1019, 5172, 85, 345]; %%%%%%%PosIcon PosHi/PosHi
+neuronsAll(53,:) = [9, 4, 1019, 31508, 85, 345]; %%%%%%PosIcon PosHi/PosHi
 
 % goldy_2019_03_28, 599 (35 px EV 160 FA)
-neuronsAll(53,:) = [10, 1, 599, 1412, 35, 160]; % 
-neuronsAll(54,:) = [10, 2, 599, 7218, 35, 160]; % NR
-neuronsAll(55,:) = [10, 3, 599, 562, 35, 160]; % TOO FEW SPIKES
-neuronsAll(56,:) = [10, 4, 599, 2442, 35, 160]; % Slow
-neuronsAll(57,:) = [10, 5, 599, 2357, 35, 160]; % NR
-neuronsAll(58,:) = [10, 6, 599, 958, 35, 160]; % TOO FEW SPIKES
-neuronsAll(59,:) = [10, 7, 599, 1536, 35, 160]; % 
-neuronsAll(60,:) = [10, 8, 599, 5013, 35, 160]; %%%% PosSens   
-neuronsAll(61,:) = [10, 9, 599, 1249, 35, 160]; % 
+neuronsAll(54,:) = [10, 1, 599, 1412, 35, 160]; % 
+neuronsAll(55,:) = [10, 2, 599, 7218, 35, 160]; % NR
+neuronsAll(56,:) = [10, 3, 599, 562, 35, 160]; % TOO FEW SPIKES
+neuronsAll(57,:) = [10, 4, 599, 2442, 35, 160]; % Slow
+neuronsAll(58,:) = [10, 5, 599, 2357, 35, 160]; % NR
+neuronsAll(59,:) = [10, 6, 599, 958, 35, 160]; % TOO FEW SPIKES
+neuronsAll(60,:) = [10, 7, 599, 1536, 35, 160]; % 
+neuronsAll(61,:) = [10, 8, 599, 5013, 35, 160]; %%%% PosSens   
+neuronsAll(62,:) = [10, 9, 599, 1249, 35, 160]; % 
 
 % hobo_2019_04_01, 1019 (45 px EV 150 FA)
-neuronsAll(62,:) = [11, 1, 1019, 1432, 45, 150]; % 
-neuronsAll(63,:) = [11, 2, 1019, 1617, 45, 150]; %%%%  NegSense
-neuronsAll(64,:) = [11, 3, 1019, 503, 45, 150]; % TOO FEW SPIKES
-neuronsAll(65,:) = [11, 5, 1019, 235, 45, 150]; % TOO FEW SPIKES
+neuronsAll(63,:) = [11, 1, 1019, 1432, 45, 150]; % 
+neuronsAll(64,:) = [11, 2, 1019, 1617, 45, 150]; %%%%  NegSense
+neuronsAll(65,:) = [11, 3, 1019, 503, 45, 150]; % TOO FEW SPIKES
+neuronsAll(66,:) = [11, 5, 1019, 235, 45, 150]; % TOO FEW SPIKES
 
 % penn_2019_04_13, 269 (65 px EV 165 FA)
-neuronsAll(66,:) = [12, 1, 269, 4055, 65, 165]; % PosSlow/Pos/Slow
-neuronsAll(67,:) = [12, 2, 269, 366, 65, 165]; % TOO FEW SPIKES
-neuronsAll(68,:) = [12, 3, 269, 524, 65, 165]; % TOO FEW SPIKES
+neuronsAll(67,:) = [12, 1, 269, 4055, 65, 165]; % PosSlow/Pos/Slow
+neuronsAll(68,:) = [12, 2, 269, 366, 65, 165]; % TOO FEW SPIKES
+neuronsAll(69,:) = [12, 3, 269, 524, 65, 165]; % TOO FEW SPIKES
 
 % tolstoy_2019_01_29, 569 (90 px EV 185 FA)
-neuronsAll(69,:) = [13, 1, 569, 3666, 90, 185]; % NR*
-neuronsAll(70,:) = [13, 2, 569, 28640, 90, 185]; % NR
-neuronsAll(71,:) = [13, 3, 569, 13401, 90, 185]; % NR
-neuronsAll(72,:) = [13, 4, 569, 11166, 90, 185]; % NR
-neuronsAll(73,:) = [13, 5, 569, 13055, 90, 185]; % NR
+neuronsAll(70,:) = [13, 1, 569, 3666, 90, 185]; % NR*
+neuronsAll(71,:) = [13, 2, 569, 28640, 90, 185]; % NR
+neuronsAll(72,:) = [13, 3, 569, 13401, 90, 185]; % NR
+neuronsAll(73,:) = [13, 4, 569, 11166, 90, 185]; % NR
+neuronsAll(74,:) = [13, 5, 569, 13055, 90, 185]; % NR
 
 
 %% Filter
